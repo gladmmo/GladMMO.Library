4c928d3c0f0d424178195a80ac255798d3845b62
HelloKitty <andrew.blakely@ymail.com>
2019-06-03 05:53:10 -0500

    Refactored webbuilder extensions

--- src/Server/GladMMO.Service.Common/Extensions/IWebBuilderExtensions.cs
+++ src/Server/GladMMO.Service.Common/Extensions/IWebBuilderExtensions.cs
@@ -17,7 +17,7 @@ namespace GladMMO
 		/// </summary>
 		/// <param name="builder">The builder.</param>
 		/// <returns>The builder.</returns>
-		public static IWebHostBuilder UseKestrelGuardiansConfig(this IWebHostBuilder builder, string[] args)
+		public static IWebHostBuilder UseKestrelGuardiansConfig(this IWebHostBuilder builder, string[] args, bool shouldUseDefaultUrlIfNoneProvided = true)
 		{
 			//If we're local then we don't want to set this IP
 			//because it won't work. Since we won't be on our own EC2 instance with IIS
@@ -26,7 +26,7 @@ namespace GladMMO
 			.UseKestrel()
 			.UseUrls(@"http://0.0.0.0:7777");
 #else
-			builder.ConfigureKestrelHostWithCommandlinArgs(args);
+			builder.ConfigureKestrelHostWithCommandlinArgs(args, shouldUseDefaultUrlIfNoneProvided);
 #endif
 
 			return builder;
@@ -39,104 +39,79 @@ namespace GladMMO
 		/// <param name="builder">The web host builder.</param>
 		/// <param name="args">The commandline args.</param>
 		/// <returns>The provided <see cref="IWebHostBuilder"/>for fluent chaining.</returns>
-		public static IWebHostBuilder ConfigureKestrelHostWithCommandlinArgs(this IWebHostBuilder builder, string[] args)
+		public static IWebHostBuilder ConfigureKestrelHostWithCommandlinArgs(this IWebHostBuilder builder, string[] args, bool shouldUseDefaultUrlIfNoneProvided = true)
 		{
 			Parser.Default.ParseArguments<DefaultWebHostingArgumentsModel>(args)
 				.WithParsed(model =>
 				{
-#if NETCOREAPP2_0
 					//If https is enabled then a certifcate should be available for loading.
 					builder.UseKestrel(options =>
 					{
-
-						int port = 5000;
 						//Get the port
 						if(model.isCustomUrlDefined)
 						{
+							int port = 5000;
 							int.TryParse(model.Url.Split(':').Last(), out port);
-						}
 
-						//Remov http
-						string ip = model.Url.Replace("http://", "");
-						ip = ip.Replace("https://", "");
-						if(ip.Contains(':'))
-							ip = ip.Split(':').First();
+							//Idea here is that if they specified the port, then we can actually use it as the HTTPS port setting.
+							if (model.isHttpsEnabled)
+								builder.UseSetting("https_port", port.ToString());
 
-						options.Listen(new IPEndPoint(IPAddress.Parse(ip), port), listenOptions =>
-						{
-							if(model.isHttpsEnabled)
-							{
-								var httpsConnectionAdapterOptions = new HttpsConnectionAdapterOptions()
-								{
-									//TODO: Do we need this in ASP Core 2.0?
-									//ClientCertificateMode = ClientCertificateMode.AllowCertificate,
+							//Remov http
+							string ip = model.Url.Replace("http://", "");
+							ip = ip.Replace("https://", "");
+							if (ip.Contains(':'))
+								ip = ip.Split(':').First();
 
-									//TODO: Mono doesn't support Tls1 or Tls2 and we have no way to config this. 
-									//Ssl3 is mostly safe and supported by Mono which means it will work in Unity3D now.
-									SslProtocols = System.Security.Authentication.SslProtocols.Tls
-										| System.Security.Authentication.SslProtocols.Tls11
-										| System.Security.Authentication.SslProtocols.Tls12,
-
-									ServerCertificate = X509Certificate2Loader.Create(model.HttpsCertificateName).Load()
-								};
-
-								listenOptions.UseHttps(httpsConnectionAdapterOptions);
-							}
-						});
-
-						//We still need this for the purposes for Features addresses
-						//It won't conflict with the above
-						if(model.isCustomUrlDefined)
-						{
-							builder.UseUrls(model.isHttpsEnabled
+							var modelUrl = model.isHttpsEnabled
 								? model.Url
 									.ToLower()
 									.Replace(@"http://", @"https://")
-								: model.Url);
+								: model.Url;
+
+							//TODO: This won't actually work, it's not an IP.
+							if (model.isHttpsEnabled)
+								builder.UseSetting("https_endpoint", modelUrl);
+
+							builder.UseUrls(modelUrl);
 						}
-						else
+						else if(shouldUseDefaultUrlIfNoneProvided)
 						{
 							string prefix = model.isHttpsEnabled ? @"https://" : @"http://";
 							builder.UseUrls($@"{prefix}localhost:5000");
 						}
-					});
-#else
-					if(model.isHttpsEnabled)
-					{
-						builder.UseKestrel(options =>
+
+						//.UseSetting("https_port", "443")
+						if (model.isHttpsEnabled)
 						{
-							options.UseHttps(new HttpsConnectionFilterOptions()
+							options.Listen(new IPEndPoint(IPAddress.Parse(builder.GetSetting("https_endpoint")), Int32.Parse(builder.GetSetting("https_port"))), listenOptions =>
 							{
-								//TODO: Mono doesn't support Tls1 or Tls2 and we have no way to config this. 
-								//Ssl3 is mostly safe and supported by Mono which means it will work in Unity3D now.
-								SslProtocols = System.Security.Authentication.SslProtocols.Tls
-									| System.Security.Authentication.SslProtocols.Tls11
-									| System.Security.Authentication.SslProtocols.Tls12,
+								if (model.isHttpsEnabled)
+								{
+									var httpsConnectionAdapterOptions = new HttpsConnectionAdapterOptions()
+									{
+										//TODO: Do we need this in ASP Core 2.0?
+										//ClientCertificateMode = ClientCertificateMode.AllowCertificate,
 
-								//Load the cert with the cert loader
-								ServerCertificate = X509Certificate2Loader.Create(model.HttpsCertificateName).Load()
+										//TODO: Mono doesn't support Tls1 or Tls2 and we have no way to config this. 
+										//Ssl3 is mostly safe and supported by Mono which means it will work in Unity3D now.
+										SslProtocols = System.Security.Authentication.SslProtocols.Tls
+										               | System.Security.Authentication.SslProtocols.Tls11
+										               | System.Security.Authentication.SslProtocols.Tls12,
+
+										ServerCertificate = X509Certificate2Loader.Create(model.HttpsCertificateName).Load()
+									};
+
+									listenOptions.UseHttps(httpsConnectionAdapterOptions);
+								}
 							});
-						});
-					}
-					else
-						builder.UseKestrel();
+						}
 
-					//TODO: Should we allow both https and non-https traffic?
-					if(model.isCustomUrlDefined)
-					{
-						builder.UseUrls(model.isHttpsEnabled
-							? model.Url
-								.ToLower()
-								.Replace(@"http://", @"https://")
-							: model.Url);
-					}
-					else
-					{
-						string prefix = model.isHttpsEnabled ? @"https://" : @"http://";
-						builder.UseUrls($@"{prefix}localhost:5000");
-					}
-				
-#endif
+						//Check if we have an http setting set
+						string potentialHttpValue = builder.GetSetting("http_endpoint");
+						if (!String.IsNullOrWhiteSpace(potentialHttpValue))
+							options.Listen(new IPEndPoint(IPAddress.Parse(builder.GetSetting("http_endpoint")), Int32.Parse(builder.GetSetting("http_port"))));
+					});
 				});
 
 			return builder;
