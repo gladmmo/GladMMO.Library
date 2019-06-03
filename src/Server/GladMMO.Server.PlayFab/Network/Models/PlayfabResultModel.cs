2bbf8d9fb98a787cc74676598831d6b47aee0cdc
HelloKitty <andrew.blakely@ymail.com>
2019-06-03 06:40:59 -0500

    Added PlayfabResultModel

---
+++ src/Server/GladMMO.Server.PlayFab/Network/Models/PlayfabResultModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class PlayFabResultModel<TResponseObjectType>
+	{
+		[JsonProperty(PropertyName = "code")]
+		public HttpStatusCode ResultCode { get; private set; }
+
+		[JsonProperty(PropertyName = "status")]
+		public string ResultStatus { get; private set; }
+
+		[JsonProperty(PropertyName = "data")]
+		public TResponseObjectType Data { get; private set; }
+
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		public PlayFabResultModel()
+		{
+			
+		}
+	}
+}
