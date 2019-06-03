df0a4d19919cbdb9b7a2a712dcb77a6f824c3806
HelloKitty <andrew.blakely@ymail.com>
2019-06-03 06:41:15 -0500

    Added IPlayfabAuthenticationClient

---
+++ src/Server/GladMMO.Server.PlayFab/Network/Clients/IPlayfabAuthenticationClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlayFab.ClientModels;
+using Refit;
+
+namespace GladMMO
+{
+	[Headers("User-Agent: GuardiansServer")]
+	public interface IPlayfabAuthenticationClient
+	{
+		//Full path: POST https://titleId.playfabapi.com/Client/LoginWithOpenIdConnect
+		//See: https://docs.microsoft.com/en-us/rest/api/playfab/client/authentication/loginwithopenidconnect?view=playfab-rest
+		/// <summary>
+		/// Attempts to login/authenticate with a provide OpenId value.
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns></returns>
+		[Post("/Client/LoginWithOpenIdConnect")]
+		Task<PlayFabResultModel<GladMMOPlayFabLoginResult>> LoginWithOpenId(LoginWithOpenIdConnectRequest request);
+	}
+}
