e438196ac2cd6f9df0bf1b72f40c1f7abdd82947
HelloKitty <andrew.blakely@ymail.com>
2019-06-04 03:10:23 -0500

    Added PlayFabServerAuthenticationHeaderAttribute

---
+++ src/Server/GladMMO.Server.PlayFab/Attributes/PlayFabServerAuthenticationHeaderAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Attribute that indicates server authentication is required.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+	public sealed class PlayFabServerAuthenticationHeaderAttribute : HeadersAttribute
+	{
+		public PlayFabServerAuthenticationHeaderAttribute()
+			: base("X-SecretKey: temp") //t for temp, will be replaced.
+		{
+		}
+	}
+}
