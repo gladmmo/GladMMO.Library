94d62fe8c546ce08b7fde8bd1a596cd1c13ea4f6
HelloKitty <andrew.blakely@ymail.com>
2019-06-03 20:13:34 -0500

    Simplified Authentication Service Registeration module

--- src/Client/GladMMO.Client.Game/IoC/Modules/AuthenticationDependencyAutofacModule.cs
+++ src/Client/GladMMO.Client.Game/IoC/Modules/AuthenticationDependencyAutofacModule.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace GladMMO
 {
-	public sealed class AuthenticationDependencyAutofacModule : Module
+	public sealed class AuthenticationDependencyAutofacModule : NetworkServiceDiscoveryableAutofaceModule
 	{
 		public AuthenticationDependencyAutofacModule()
 		{
@@ -25,21 +25,6 @@ namespace GladMMO
 
 				return new AsyncEndpointAuthenticationService(QueryForRemoteServiceEndpoint(serviceDiscovery, "Authentication"), new RefitSettings() { HttpMessageHandlerFactory = () => new FiddlerEnabledWebProxyHandler() });
 			});
-		}
-
-		//TODO: Put this in a base class or something
-		private async Task<string> QueryForRemoteServiceEndpoint(IServiceDiscoveryService serviceDiscovery, string serviceType)
-		{
-			ResolveServiceEndpointResponse endpointResponse = await serviceDiscovery.DiscoverService(new ResolveServiceEndpointRequest(ClientRegionLocale.US, serviceType));
-
-			if(!endpointResponse.isSuccessful)
-				throw new InvalidOperationException($"Failed to query for Service: {serviceType} Result: {endpointResponse.ResultCode}");
-
-			//TODO: Logging
-			//Debug.Log($"Recieved service discovery response: {endpointResponse.Endpoint.EndpointAddress}:{endpointResponse.Endpoint.EndpointPort} for Type: {serviceType}");
-
-			//TODO: Do we need extra slash?
-			return $"{endpointResponse.Endpoint.EndpointAddress}:{endpointResponse.Endpoint.EndpointPort}/";
 		}
 	}
 }
