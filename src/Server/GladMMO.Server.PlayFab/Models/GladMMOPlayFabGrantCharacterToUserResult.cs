153cd050c799a85358a3e0235d9a2c58ad01031d
HelloKitty <andrew.blakely@ymail.com>
2019-06-04 03:10:39 -0500

    Added GladMMOPlayFabGrantCharacterToUserResult

---
+++ src/Server/GladMMO.Server.PlayFab/Models/GladMMOPlayFabGrantCharacterToUserResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public class GladMMOPlayFabGrantCharacterToUserResult
+	{
+		/// <summary>
+		/// Unique identifier tagged to this character.
+		/// </summary>
+		[JsonProperty]
+		public string CharacterId { get; private set; }
+
+		public GladMMOPlayFabGrantCharacterToUserResult(string characterId)
+		{
+			if (string.IsNullOrWhiteSpace(characterId))
+				throw new ArgumentException("Value cannot be null or whitespace.", nameof(characterId));
+
+			CharacterId = characterId;
+		}
+
+		[JsonConstructor]
+		private GladMMOPlayFabGrantCharacterToUserResult()
+		{
+			
+		}
+	}
+}
