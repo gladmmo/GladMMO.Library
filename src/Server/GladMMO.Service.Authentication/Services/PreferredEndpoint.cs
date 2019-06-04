740e65ef2a7414b7f45baac52c4b0e2054fbd878
HelloKitty <andrew.blakely@ymail.com>
2019-06-04 00:21:26 -0500

    Added PreferredEndpoint and registered in start
    Only way to get the actual host See:
    https://stackoverflow.com/a/51450471

---
+++ src/Server/GladMMO.Service.Authentication/Services/PreferredEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class PreferredEndpoint
+	{
+		public string Endpoint { get; }
+
+		public short Port { get; }
+
+		public PreferredEndpoint([JetBrains.Annotations.NotNull] string endpoint, short port)
+		{
+			if (string.IsNullOrEmpty(endpoint))
+				throw new ArgumentException("Value cannot be null or empty.", nameof(endpoint));
+
+			Endpoint = endpoint;
+			Port = port;
+		}
+	}
+}
