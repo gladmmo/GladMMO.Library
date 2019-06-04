528f28d7530530fb44061da52a32e3600d01cd6f
HelloKitty <andrew.blakely@ymail.com>
2019-06-04 03:10:30 -0500

    Added GladMMOPlayFabGrantCharacterToUserRequest

---
+++ src/Server/GladMMO.Server.PlayFab/Models/GladMMOPlayFabGrantCharacterToUserRequest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Grants a character to the user of the type and name specified in the request.
+	/// </summary>
+	[JsonObject]
+	public sealed class GladMMOPlayFabGrantCharacterToUserRequest
+	{
+		/// <summary>
+		/// Non-unique display name of the character being granted (1-20 characters in length).
+		/// </summary>
+		[JsonProperty]
+		public string CharacterName { get; private set; }
+
+		/// <summary>
+		/// Type of the character being granted; statistics can be sliced based on this value.
+		/// </summary>
+		[JsonProperty]
+		public string CharacterType { get; private set; }
+
+		/// <summary>
+		/// Unique PlayFab assigned ID of the user on whom the operation will be performed.
+		/// </summary>
+		[JsonProperty]
+		public string PlayFabId { get; private set; }
+
+		public GladMMOPlayFabGrantCharacterToUserRequest(string characterName, string characterType, string playFabId)
+		{
+			if (string.IsNullOrWhiteSpace(characterName))
+				throw new ArgumentException("Value cannot be null or whitespace.", nameof(characterName));
+			if (string.IsNullOrWhiteSpace(characterType))
+				throw new ArgumentException("Value cannot be null or whitespace.", nameof(characterType));
+			if (string.IsNullOrWhiteSpace(playFabId))
+				throw new ArgumentException("Value cannot be null or whitespace.", nameof(playFabId));
+
+			CharacterName = characterName;
+			CharacterType = characterType;
+			PlayFabId = playFabId;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		private GladMMOPlayFabGrantCharacterToUserRequest()
+		{
+			
+		}
+	}
+}
