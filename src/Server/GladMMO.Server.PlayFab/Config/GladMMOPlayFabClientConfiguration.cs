e6a1ae88618d213a7aeff3d2e92884dd28c25d50
HelloKitty <andrew.blakely@ymail.com>
2019-06-04 03:39:18 -0500

    Added GladMMOPlayFabClientConfiguration

---
+++ src/Server/GladMMO.Server.PlayFab/Config/GladMMOPlayFabClientConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class GladMMOPlayFabClientConfiguration
+	{
+		public string PlayFabSecretKey { get; }
+
+		public string PlayFabId { get; }
+
+		public GladMMOPlayFabClientConfiguration(string playFabSecretKey, string playFabId)
+		{
+			//They can be null.
+			PlayFabSecretKey = playFabSecretKey;
+			PlayFabId = playFabId;
+		}
+
+		public void AssertContainsSecretKey()
+		{
+			if(String.IsNullOrWhiteSpace(PlayFabSecretKey))
+				throw new InvalidOperationException($"Expected {PlayFabSecretKey} to be valid in {nameof(GladMMOPlayFabClientConfiguration)}.");
+		}
+	}
+}
