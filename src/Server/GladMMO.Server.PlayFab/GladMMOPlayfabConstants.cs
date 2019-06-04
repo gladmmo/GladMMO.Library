3b9f1e63a6d26252804c8457e0ae7236ef5c8138
HelloKitty <andrew.blakely@ymail.com>
2019-06-04 00:11:15 -0500

    Added GladMMOPlayfabConstants with playfab id claim type constant

---
+++ src/Server/GladMMO.Server.PlayFab/GladMMOPlayfabConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public static class GladMMOPlayfabConstants
+	{
+		public const string PLAYFAB_JWT_CLAIM_TYPE = "pfid";
+	}
+}
