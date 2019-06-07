7c65d27c7e6faad5d0b56c91e5a5dde7a9420f82
HelloKitty <andrew.blakely@ymail.com>
2019-06-06 19:14:01 -0500

    Created LoadingScreenDependencyAutofacModule

---
+++ src/Client/GladMMO.Client.Game/IoC/Modules/LoadingScreenDependencyAutofacModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class LoadingScreenDependencyAutofacModule : NetworkServiceDiscoveryableAutofaceModule
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterModule<CharacterServiceDependencyAutofacModule>();
+		}
+	}
+}
