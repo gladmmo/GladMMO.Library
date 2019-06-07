44d2d59f9ccf75ba64e47e783d44579ad9bf4e38
HelloKitty <andrew.blakely@ymail.com>
2019-06-07 04:39:00 -0500

    Partially implemented PreBurstQueryDownloadableSceneInitializable

--- src/Client/GladMMO.Client.Game/Engine/PreBurstScreen/Initializables/PreBurstQueryDownloadableSceneInitializable.cs
+++ src/Client/GladMMO.Client.Game/Engine/PreBurstScreen/Initializables/PreBurstQueryDownloadableSceneInitializable.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Logging;
 using Glader.Essentials;
+using Nito.AsyncEx;
 
 namespace GladMMO
 {
@@ -13,28 +15,49 @@ namespace GladMMO
 	/// </summary>
 	public sealed class PreBurstQueryDownloadableSceneInitializable : ThreadUnSafeBaseSingleEventListenerInitializable<ICharacterSessionDataChangedEventSubscribable, CharacterSessionDataChangedEventArgs>
 	{
-		//private IZoneServerService ZoneDataService { get; }
+		private IZoneServerService ZoneDataService { get; }
 
-		//private IContentServerServiceClient ContentService { get; }
+		private IContentServerServiceClient ContentService { get; }
 
-		public PreBurstQueryDownloadableSceneInitializable(ICharacterSessionDataChangedEventSubscribable subscriptionService) 
+		private ILog Logger { get; }
+
+		public PreBurstQueryDownloadableSceneInitializable(ICharacterSessionDataChangedEventSubscribable subscriptionService,
+			[NotNull] ILog logger) 
 			: base(subscriptionService)
 		{
-
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
 		protected override void OnThreadUnSafeEventFired(object source, CharacterSessionDataChangedEventArgs args)
 		{
-			/*//TODO: Handle failure
-			ProjectVersionStage.AssertAlpha();
-			//TODO: Handle throwing/error
-			//We need to know the world the zone is it, so we can request a download URL for it.
-			long worldId = await ZoneDataService.GetZoneWorld(characterSessionData.ZoneId)
-				.ConfigureAwait(false);
+			UnityAsyncHelper.UnityMainThreadContext.PostAsync(async () =>
+			{
+				long worldId = 0;
+				try
+				{
+					//TODO: Handle failure
+					ProjectVersionStage.AssertAlpha();
+					//TODO: Handle throwing/error
+					//We need to know the world the zone is it, so we can request a download URL for it.
+					worldId = await ZoneDataService.GetZoneWorld(args.ZoneIdentifier)
+						.ConfigureAwait(false);
 
-			//With the worldid we can get the download URL.
-			ContentDownloadURLResponse urlDownloadResponse = await ContentService.RequestWorldDownloadUrl(worldId, AuthTokenRepo.RetrieveWithType())
-				.ConfigureAwait(false);*/
+					//With the worldid we can get the download URL.
+					ContentDownloadURLResponse urlDownloadResponse = await ContentService.RequestWorldDownloadUrl(worldId)
+						.ConfigureAwait(false);
+
+					if(Logger.IsInfoEnabled)
+						Logger.Info($"World Download Url: {urlDownloadResponse.DownloadURL}");
+				}
+				catch (Exception e)
+				{
+					if(Logger.IsErrorEnabled)
+						Logger.Error($"Failed to query for Download URL for ZoneId: {args.ZoneIdentifier} WorldId: {worldId} (0 if never succeeded request).");
+
+					Console.WriteLine(e);
+					throw;
+				}
+			});
 		}
 	}
 }
