1f43d4aa75b15fb34832b8e4d23e0ef4ee906938
HelloKitty <andrew.blakely@ymail.com>
2019-06-07 00:15:35 -0500

    Added IContentResourceExistenceVerifier

---
+++ src/Server/GladMMO.Service.ContentServer/Services/IContentResourceExistenceVerifier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public interface IContentResourceExistenceVerifier
+	{
+		Task<bool> VerifyResourceExists(UserContentType contentType, Guid contentGuid);
+	}
+}
