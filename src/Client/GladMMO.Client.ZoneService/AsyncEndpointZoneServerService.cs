0372e19765b9a7a66005d77fbd92658898135d29
HelloKitty <andrew.blakely@ymail.com>
2019-06-06 21:08:56 -0500

    Added AsyncEndpointZoneServerService

---
+++ src/Client/GladMMO.Client.ZoneService/AsyncEndpointZoneServerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GladMMO;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncEndpointZoneServerService : BaseAsyncEndpointService<IZoneServerService>, IZoneServerService
+	{
+		/// <inheritdoc />
+		public AsyncEndpointZoneServerService(Task<string> futureEndpoint)
+			: base(futureEndpoint)
+		{
+		}
+
+		/// <inheritdoc />
+		public AsyncEndpointZoneServerService(Task<string> futureEndpoint, RefitSettings settings)
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		public async Task<long> GetZoneWorld(int zoneId)
+		{
+			return await(await GetService().ConfigureAwait(false)).GetZoneWorld(zoneId).ConfigureAwait(false);
+		}
+
+		public async Task<ResolveServiceEndpointResponse> GetServerEndpoint(int zoneId)
+		{
+			return await(await GetService().ConfigureAwait(false)).GetServerEndpoint(zoneId).ConfigureAwait(false);
+		}
+	}
+}
