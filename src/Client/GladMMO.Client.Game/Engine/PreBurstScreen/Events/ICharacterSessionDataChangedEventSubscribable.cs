5c190e4180c80682e4594299a7f6792c3259e69b
HelloKitty <andrew.blakely@ymail.com>
2019-06-06 19:01:07 -0500

    Added ICharacterSessionDataChangedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/PreBurstScreen/Events/ICharacterSessionDataChangedEventSubscribable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ICharacterSessionDataChangedEventSubscribable
+	{
+		event EventHandler<ServerRequestedSceneChangeEventArgs> OnCharacterSessionDataChanged;
+	}
+
+	public sealed class CharacterSessionDataChangedEventArgs : EventArgs
+	{
+		public int ZoneIdentifier { get; }
+
+		public CharacterSessionDataChangedEventArgs(int zoneIdentifier)
+		{
+			if (zoneIdentifier <= 0) throw new ArgumentOutOfRangeException(nameof(zoneIdentifier));
+
+			ZoneIdentifier = zoneIdentifier;
+		}
+	}
+}
