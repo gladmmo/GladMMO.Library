2e1e547244a2cdae6e5b8e34ed4336d62ca703dc
HelloKitty <andrew.blakely@ymail.com>
2019-06-07 00:16:21 -0500

    Added AzureBlobStorageResourceExistenceVerifier

---
+++ src/Server/GladMMO.Service.ContentServer/Services/AzureBlobStorageResourceExistenceVerifier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace GladMMO
+{
+	public class AzureBlobStorageResourceExistenceVerifier : IContentResourceExistenceVerifier
+	{
+		private CloudBlobClient BlobClient { get; }
+
+		public AzureBlobStorageResourceExistenceVerifier(CloudBlobClient blobClient)
+		{
+			BlobClient = blobClient;
+		}
+
+		public async Task<bool> VerifyResourceExists(UserContentType contentType, Guid contentGuid)
+		{
+			//Container name format is {contentType}s.
+			CloudBlobContainer container = BlobClient.GetContainerReference($"{contentType.ToString()}s");
+
+			SharedAccessBlobPolicy sasConstraints = new SharedAccessBlobPolicy();
+			sasConstraints.SharedAccessExpiryTime = DateTime.UtcNow.AddMinutes(30);
+			sasConstraints.Permissions = SharedAccessBlobPermissions.Read; //download or retrievial should be READ ONLY.
+
+			return await container.GetBlockBlobReference($"{contentGuid.ToString()}.bin")
+				.ExistsAsync();
+		}
+	}
+}
