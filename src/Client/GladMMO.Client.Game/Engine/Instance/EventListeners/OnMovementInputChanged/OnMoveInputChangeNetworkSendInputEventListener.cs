dec0fe24fd0700dda26d77d93b693b5220aa6fab
HelloKitty <andrew.blakely@ymail.com>
2019-06-08 04:00:04 -0500

    Implemented move input sending

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnMovementInputChanged/OnMoveInputChangeNetworkSendInputEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using GladNet;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class OnMoveInputChangeNetworkSendInputEventListener : BaseSingleEventListenerInitializable<IMovementInputChangedEventSubscribable, MovementInputChangedEventArgs>
+	{
+		private IPeerPayloadSendService<GameClientPacketPayload> SendService { get; }
+
+		public OnMoveInputChangeNetworkSendInputEventListener(IMovementInputChangedEventSubscribable subscriptionService,
+			[NotNull] IPeerPayloadSendService<GameClientPacketPayload> sendService) 
+			: base(subscriptionService)
+		{
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+		}
+
+		protected override void OnEventFired(object source, MovementInputChangedEventArgs args)
+		{
+			SendService.SendMessage(new ClientMovementDataUpdateRequest(new Vector2(args.NewHorizontalInput, args.NewVerticalInput)));
+		}
+	}
+}
