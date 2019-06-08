ebb39803d3fadea3435545cb60cf6f319726c2d5
HelloKitty <andrew.blakely@ymail.com>
2019-06-08 03:59:26 -0500

    Added IdleMovementGenerator

---
+++ src/Server/GladMMO.Server.ZoneServer/MovementGenerators/IdleMovementGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// The do nothing no good movement generator.
+	/// </summary>
+	public sealed class IdleMovementGenerator : MoveGenerator
+	{
+		protected override void Start(GameObject entity, long currentTime)
+		{
+			//Let's do nothing!
+		}
+
+		protected override void InternalUpdate(GameObject entity, long currentTime)
+		{
+			//Let's do nothing, forever!
+		}
+	}
+}
