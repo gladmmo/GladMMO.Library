1d061cf059db73f2eeffed5ba15582510a26f0ca
HelloKitty <andrew.blakely@ymail.com>
2019-06-08 04:00:22 -0500

    Hackily re-enabled serverside movement

--- src/Server/GladMMO.Server.ZoneServer/Factory/AdditionalServerRegisterationPlayerEntityFactoryDecorator.cs
+++ src/Server/GladMMO.Server.ZoneServer/Factory/AdditionalServerRegisterationPlayerEntityFactoryDecorator.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using GladNet;
 using JetBrains.Annotations;
+using Nito.AsyncEx;
 using UnityEngine;
 
 namespace GladMMO
@@ -21,17 +22,29 @@ namespace GladMMO
 
 		private IDictionary<int, NetworkEntityGuid> ConnectionIdToControllingEntityMap { get; }
 
+		private IEntityGuidMappable<AsyncReaderWriterLock> AsyncLockMappable { get; }
+
+		private IEntityGuidMappable<CharacterController> CharacterControllerMappable { get; }
+
+		private IEntityGuidMappable<IMovementGenerator<GameObject>> MovementGeneratorMappable { get; }
+
 		/// <inheritdoc />
 		public AdditionalServerRegisterationPlayerEntityFactoryDecorator(
 			IFactoryCreatable<GameObject, PlayerEntityCreationContext> decoratedFactory, 
 			IEntityGuidMappable<IPeerPayloadSendService<GameServerPacketPayload>> guidToSessionMappable, 
 			IEntityGuidMappable<InterestCollection> guidToInterestCollectionMappable, 
-			IDictionary<int, NetworkEntityGuid> connectionIdToControllingEntityMap)
+			IDictionary<int, NetworkEntityGuid> connectionIdToControllingEntityMap, 
+			IEntityGuidMappable<AsyncReaderWriterLock> asyncLockMappable, 
+			IEntityGuidMappable<CharacterController> characterControllerMappable,
+			[NotNull] IEntityGuidMappable<IMovementGenerator<GameObject>> movementGeneratorMappable)
 		{
 			DecoratedFactory = decoratedFactory;
 			GuidToSessionMappable = guidToSessionMappable;
 			GuidToInterestCollectionMappable = guidToInterestCollectionMappable;
 			ConnectionIdToControllingEntityMap = connectionIdToControllingEntityMap;
+			AsyncLockMappable = asyncLockMappable;
+			CharacterControllerMappable = characterControllerMappable;
+			MovementGeneratorMappable = movementGeneratorMappable ?? throw new ArgumentNullException(nameof(movementGeneratorMappable));
 		}
 
 		/// <inheritdoc />
@@ -49,6 +62,11 @@ namespace GladMMO
 
 			//We just create our own manaul interest collection here.
 			GuidToInterestCollectionMappable.AddObject(context.EntityGuid, playerInterestCollection);
+			AsyncLockMappable.AddObject(context.EntityGuid, new AsyncReaderWriterLock());
+			CharacterControllerMappable.AddObject(context.EntityGuid, gameObject.GetComponent<CharacterController>());
+
+			//TODO: We cannot do this in the future if we want to have flight paths, I guess.
+			MovementGeneratorMappable.AddObject(context.EntityGuid, new IdleMovementGenerator()); //idle, since we are spawning.
 
 			//We don't need to touch the gameobject, we can just return it.
 			return gameObject;
