21d04a74ebf869249dfda4066a08fa4d6a589d92
HelloKitty <andrew.blakely@ymail.com>
2019-06-07 21:35:19 -0500

    Added IZoneDataRepository

---
+++ src/GladMMO.Client.Common/Repository/IZoneDataRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IZoneDataRepository : IReadonlyZoneDataRepository
+	{
+		//The reason we don't use a property setter is due to MS conventions
+		//as it would hide a potentially LARGE, complex and far reaching state change (or at least I expect it to).
+		/// <summary>
+		/// Sets the zone id to the provided <see cref="zoneId"/>
+		/// value.
+		/// Resets and sets dirty all other data in this repository
+		/// if the current <see cref="ZoneId"/> does not match.
+		/// </summary>
+		/// <param name="zoneId"></param>
+		void UpdateZoneId(int zoneId);
+	}
+
+	public interface IReadonlyZoneDataRepository
+	{
+		/// <summary>
+		/// The zone id.
+		/// </summary>
+		int ZoneId { get; }
+	}
+}
---
+++ src/GladMMO.Client.Common/Repository/LocalZoneDataRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class LocalZoneDataRepository : IZoneDataRepository
+	{
+		private readonly object SyncObj = new object();
+
+		//We need this static because it should never change. The local ID should always be this ID
+		//and any changes should reflect in any instance of this repository.
+		private static int Id { get; set; }
+
+		/// <inheritdoc />
+		public int ZoneId
+		{
+			get
+			{
+				lock(SyncObj)
+					return Id;
+			}
+		}
+
+		/// <inheritdoc />
+		public void UpdateZoneId(int zoneId)
+		{
+			lock(SyncObj)
+				//TODO: Check if it has changed, unload cache or something.
+				Id = zoneId;
+		}
+	}
+}
