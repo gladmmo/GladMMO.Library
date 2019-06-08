4cd1ae24312a25998c01b14e9869434ca30b57a3
HelloKitty <andrew.blakely@ymail.com>
2019-06-08 02:50:15 -0500

    Created IEntityWorldRepresentationCreatedEventSubscribable event

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/IEntityWorldRepresentationCreatedEventSubscribable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// This event will fire after the <see cref="GameObject"/> representation
+	/// of an entity has been created. The Entity data is not PROMISED to be fully
+	/// initialized at this point. It may not have all <see cref="IEntityGuidMappable{TValue}"/>
+	/// containers initialized. It will have the <see cref="GameObject"/> created though.
+	/// </summary>
+	public interface IEntityWorldRepresentationCreatedEventSubscribable
+	{
+		event EventHandler<EntityWorldRepresentationCreatedEventArgs> OnEntityWorldRepresentationCreated;
+	}
+
+	public sealed class EntityWorldRepresentationCreatedEventArgs : EventArgs
+	{
+		/// <summary>
+		/// The entity guid of the creating entity.
+		/// </summary>
+		public NetworkEntityGuid EntityGuid { get; }
+
+		/// <summary>
+		/// The world/gameobject representation of the entity.
+		/// </summary>
+		public GameObject EntityWorldRepresentation { get; }
+
+		/// <inheritdoc />
+		public EntityWorldRepresentationCreatedEventArgs([NotNull] NetworkEntityGuid entityGuid, GameObject entityWorldRepresentation)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+			EntityWorldRepresentation = entityWorldRepresentation; //don't check null, due to threading problems.
+		}
+	}
+}
