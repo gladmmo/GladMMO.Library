db0ca4d5c5bd7d9227fcb660b16c5fc9e4d8507f
HelloKitty <andrew.blakely@ymail.com>
2019-06-07 21:40:24 -0500

    Added OnCharacterSessionDataChangedInitializeZoneDataEventListener
    Just listening for character session changes and then inits the zone
    data.

---
+++ src/Client/GladMMO.Client.Game/Engine/LoadingScreen/Initializables/OnCharacterSessionDataChangedInitializeZoneDataEventListener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.PreZoneBurstingScreen)]
+	public sealed class OnCharacterSessionDataChangedInitializeZoneDataEventListener : BaseSingleEventListenerInitializable<ICharacterSessionDataChangedEventSubscribable, CharacterSessionDataChangedEventArgs>
+	{
+		private IZoneDataRepository ZoneDataRepository { get; }
+
+		public OnCharacterSessionDataChangedInitializeZoneDataEventListener(ICharacterSessionDataChangedEventSubscribable subscriptionService,
+			[NotNull] IZoneDataRepository zoneDataRepository) 
+			: base(subscriptionService)
+		{
+			ZoneDataRepository = zoneDataRepository ?? throw new ArgumentNullException(nameof(zoneDataRepository));
+		}
+
+		protected override void OnEventFired(object source, CharacterSessionDataChangedEventArgs args)
+		{
+			//We just need to update the identifier.
+			ZoneDataRepository.UpdateZoneId(args.ZoneIdentifier);
+		}
+	}
+}
