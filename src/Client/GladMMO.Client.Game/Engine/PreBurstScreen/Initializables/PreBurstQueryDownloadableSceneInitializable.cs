805dbdf8dfb4bf527f6ac3d8e61b3c184d4ababd
HelloKitty <andrew.blakely@ymail.com>
2019-06-07 20:55:02 -0500

    Refactored loading downloaded scenes

--- src/Client/GladMMO.Client.Game/Engine/PreBurstScreen/Initializables/PreBurstQueryDownloadableSceneInitializable.cs
+++ src/Client/GladMMO.Client.Game/Engine/PreBurstScreen/Initializables/PreBurstQueryDownloadableSceneInitializable.cs
@@ -7,6 +7,7 @@ using Common.Logging;
 using Glader.Essentials;
 using Nito.AsyncEx;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
@@ -77,18 +78,9 @@ namespace GladMMO
 						foreach(string p in paths)
 							Debug.Log($"Found Scene in Bundle: {p}");
 
-						AsyncOperation sceneAsync = SceneManager.LoadSceneAsync(System.IO.Path.GetFileNameWithoutExtension(paths.First()));
+						string scenePath = paths.First();
 
-						sceneAsync.completed += operation1 =>
-						{
-							//When the scene is finished loading we should cleanup the asset bundle
-							//Don't clean up the WHOLE BUNDLE, just the compressed downloaded data
-							bundle.Unload(false);
-
-							//TODO: We need a way/system to reference the bundle later so it can be cleaned up inbetween scene loads.
-						};
-
-						sceneAsync.allowSceneActivation = true;
+						LoadDownloadedScene(scenePath, bundle);
 					};
 				}
 				catch (Exception e)
@@ -100,6 +92,45 @@ namespace GladMMO
 					throw;
 				}
 			});
+		}
+
+		private static AsyncOperation LoadDownloadedScene(string scenePath, AssetBundle bundle)
+		{
+			//The idea here is that AFTER the scene has been loaded, we need to then load the ACTUAL
+			//networking-based gameplay scene. Containing sceneject and all that other stuff.
+			//It's basically a layered scene-based approach. Layer 1 is the downloaded scene, layer 2 is the actual nuts and bolts
+			//that drive the game.
+			SceneManager.sceneLoaded += OnDownloadedSceneFinishedLoading;
+
+			//After this upcoming scene is completed
+			AsyncOperation sceneAsync = SceneManager.LoadSceneAsync(System.IO.Path.GetFileNameWithoutExtension(scenePath));
+
+			sceneAsync.completed += operation1 =>
+			{
+				//When the scene is finished loading we should cleanup the asset bundle
+				//Don't clean up the WHOLE BUNDLE, just the compressed downloaded data
+				bundle.Unload(false);
+
+				//TODO: We need a way/system to reference the bundle later so it can be cleaned up inbetween scene loads.
+			};
+
+			sceneAsync.allowSceneActivation = true;
+
+			return sceneAsync;
+		}
+
+		private static void OnDownloadedSceneFinishedLoading(Scene sceneLoaded, LoadSceneMode mode)
+		{
+			//Cheapest thing to check first
+			if (mode == LoadSceneMode.Single)
+			{
+				AsyncOperation loadSceneAsync = SceneManager.LoadSceneAsync(GladMMOClientConstants.INSTANCE_SERVER_SCENE_NAME, LoadSceneMode.Additive);
+				loadSceneAsync.allowSceneActivation = true;
+				loadSceneAsync.completed += o => Debug.Log($"Loaded {GladMMOClientConstants.INSTANCE_SERVER_SCENE_NAME}");
+			}
+
+			//Unregister this event.
+			SceneManager.sceneLoaded -= OnDownloadedSceneFinishedLoading;
 		}
 	}
 }
