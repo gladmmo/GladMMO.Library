747720388da3717fc01aac94d2b996d9c2b9ed1c
HelloKitty <andrew.blakely@ymail.com>
2019-06-08 02:31:42 -0500

    Creared IEntityCreationStartingEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/IEntityCreationStartingEventSubscribable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Event that should be subscribed to BEFORE the "spawning"
+	/// of the entity. Subscribers can deal with initialization
+	/// logic and such.
+	/// </summary>
+	public interface IEntityCreationStartingEventSubscribable
+	{
+		event EventHandler<EntityCreationEventArgs> OnEntityCreationStarting;
+	}
+
+	public sealed class EntityCreationEventArgs : EventArgs
+	{
+		/// <summary>
+		/// The entity guid of the creating entity.
+		/// </summary>
+		public NetworkEntityGuid EntityGuid { get; }
+
+		/// <inheritdoc />
+		public EntityCreationEventArgs([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
