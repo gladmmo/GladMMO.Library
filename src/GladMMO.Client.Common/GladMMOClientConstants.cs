5d7fe7a24bff6c0e258713dcceb24e5147902340
HelloKitty <andrew.blakely@ymail.com>
2019-06-07 20:54:48 -0500

    Added GladMMOClientConstants

---
+++ src/GladMMO.Client.Common/GladMMOClientConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public static class GladMMOClientConstants
+	{
+		public const string INSTANCE_SERVER_SCENE_NAME = "InstanceServerScene";
+	}
+}
