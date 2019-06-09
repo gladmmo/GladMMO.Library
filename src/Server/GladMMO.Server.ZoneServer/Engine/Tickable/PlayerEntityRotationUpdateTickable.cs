259197bb63d6d82b2c15b50ab91e318ed2c00225
HelloKitty <andrew.blakely@ymail.com>
2019-06-09 05:04:48 -0500

    Added server-side rotation main thread tickable

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/Tickable/PlayerEntityRotationUpdateTickable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Common.Logging;
+using Glader.Essentials;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[GameInitializableOrdering(1)]
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class PlayerEntityRotationUpdateTickable : EventQueueBasedTickable<IPlayerRotationChangeEventSubscribable, PlayerRotiationChangeEventArgs>
+	{
+		private IReadonlyEntityGuidMappable<GameObject> GameObjectMappable { get; }
+
+		protected override void HandleEvent(PlayerRotiationChangeEventArgs args)
+		{
+			GameObjectMappable.RetrieveEntity(args.EntityGuid).transform.Rotate(Vector3.up, args.Rotation);
+		}
+
+		public PlayerEntityRotationUpdateTickable(IPlayerRotationChangeEventSubscribable subscriptionService, 
+			ILog logger,
+			[NotNull] IReadonlyEntityGuidMappable<GameObject> gameObjectMappable) 
+			: base(subscriptionService, true, logger)
+		{
+			GameObjectMappable = gameObjectMappable ?? throw new ArgumentNullException(nameof(gameObjectMappable));
+		}
+	}
+}
