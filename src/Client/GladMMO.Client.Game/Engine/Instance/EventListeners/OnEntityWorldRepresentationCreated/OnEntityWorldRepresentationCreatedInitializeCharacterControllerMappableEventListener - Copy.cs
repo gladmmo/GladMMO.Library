b3819a3d240c601983b3508ae25d372584fef656
HelloKitty <andrew.blakely@ymail.com>
2019-06-09 13:33:42 -0500

    Added GameObjectDirectory initialization for entitie spawning

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnEntityWorldRepresentationCreated/OnEntityWorldRepresentationCreatedInitializeCharacterControllerMappableEventListener - Copy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class OnEntityWorldRepresentationCreatedInitializeGameObjectDirectoryEventListener : BaseSingleEventListenerInitializable<IEntityWorldRepresentationCreatedEventSubscribable, EntityWorldRepresentationCreatedEventArgs>
+	{
+		private IEntityGuidMappable<EntityGameObjectDirectory> GameObjectDirectoryMappable { get; }
+
+		public OnEntityWorldRepresentationCreatedInitializeGameObjectDirectoryEventListener(IEntityWorldRepresentationCreatedEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<EntityGameObjectDirectory> gameObjectDirectoryMappable)
+			: base(subscriptionService)
+		{
+			GameObjectDirectoryMappable = gameObjectDirectoryMappable ?? throw new ArgumentNullException(nameof(gameObjectDirectoryMappable));
+		}
+
+		protected override void OnEventFired(object source, EntityWorldRepresentationCreatedEventArgs args)
+		{
+			EntityGameObjectDirectory characterController = args.EntityWorldRepresentation.GetComponent<EntityGameObjectDirectory>();
+
+			//TODO: Assert it being there
+			GameObjectDirectoryMappable.AddObject(args.EntityGuid, characterController);
+		}
+	}
+}
