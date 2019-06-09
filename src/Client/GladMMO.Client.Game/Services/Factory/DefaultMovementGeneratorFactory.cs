4f6bf1f46c4d75cf0b97aff0ec939cfac7e729a5
HelloKitty <andrew.blakely@ymail.com>
2019-06-08 19:58:28 -0500

    Added WIP DefaultMovementGeneratorFactory

---
+++ src/Client/GladMMO.Client.Game/Services/Factory/DefaultMovementGeneratorFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	//TODO: This is a WIP. It does not support movement generator creation will. It does not really support anything but players.
+	public sealed class DefaultMovementGeneratorFactory : IFactoryCreatable<IMovementGenerator<GameObject>, EntityAssociatedData<IMovementData>>
+	{
+		private IReadonlyEntityGuidMappable<CharacterController> ControllerMappable { get; }
+
+		public DefaultMovementGeneratorFactory([NotNull] IReadonlyEntityGuidMappable<CharacterController> controllerMappable)
+		{
+			ControllerMappable = controllerMappable ?? throw new ArgumentNullException(nameof(controllerMappable));
+		}
+
+		public IMovementGenerator<GameObject> Create(EntityAssociatedData<IMovementData> context)
+		{
+			//TODO: redo all this of this garbage
+			if (context.Data is PositionChangeMovementData pcmd)
+			{
+				return new ClientSideInputMovementGenerator(pcmd, ControllerMappable.RetrieveEntity(context.EntityGuid));
+			}
+
+			throw new NotSupportedException($"TODO: Encountered unsupported movement data: {context.Data.GetType().Name}");
+		}
+	}
+}
