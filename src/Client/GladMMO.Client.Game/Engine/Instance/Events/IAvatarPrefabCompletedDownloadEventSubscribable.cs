7aedec035811e730be3520cf307e3e94f2062126
HelloKitty <andrew.blakely@ymail.com>
2019-06-09 11:31:54 -0500

    Added IAvatarPrefabCompletedDownloadEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/IAvatarPrefabCompletedDownloadEventSubscribable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public interface IAvatarPrefabCompletedDownloadEventSubscribable
+	{
+		event EventHandler<AvatarPrefabCompletedDownloadEventArgs> OnAvatarPrefabCompletedDownloading;
+	}
+
+	public sealed class AvatarPrefabCompletedDownloadEventArgs : EventArgs
+	{
+		public IPrefabContentResourceHandle PrefabHandle { get; }
+
+		public GameObject DownloadedPrefabObject { get; }
+
+		public NetworkEntityGuid EntityGuid { get; }
+
+		public AvatarPrefabCompletedDownloadEventArgs([NotNull] IPrefabContentResourceHandle prefabHandle, [NotNull] GameObject downloadedPrefabObject, [NotNull] NetworkEntityGuid entityGuid)
+		{
+			PrefabHandle = prefabHandle ?? throw new ArgumentNullException(nameof(prefabHandle));
+			DownloadedPrefabObject = downloadedPrefabObject ?? throw new ArgumentNullException(nameof(downloadedPrefabObject));
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
