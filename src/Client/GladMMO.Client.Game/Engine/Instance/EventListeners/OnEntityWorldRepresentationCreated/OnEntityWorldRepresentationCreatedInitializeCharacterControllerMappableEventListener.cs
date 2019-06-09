24643395de20ae00d637750a576b0b85b2e7df0b
HelloKitty <andrew.blakely@ymail.com>
2019-06-08 21:14:29 -0500

    Created character controller entity mappable initialization

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnEntityWorldRepresentationCreated/OnEntityWorldRepresentationCreatedInitializeCharacterControllerMappableEventListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class OnEntityWorldRepresentationCreatedInitializeCharacterControllerMappableEventListener : BaseSingleEventListenerInitializable<IEntityWorldRepresentationCreatedEventSubscribable, EntityWorldRepresentationCreatedEventArgs>
+	{
+		private IEntityGuidMappable<CharacterController> CharacterControllerMappable { get; }
+
+		public OnEntityWorldRepresentationCreatedInitializeCharacterControllerMappableEventListener(IEntityWorldRepresentationCreatedEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<CharacterController> characterControllerMappable) 
+			: base(subscriptionService)
+		{
+			CharacterControllerMappable = characterControllerMappable ?? throw new ArgumentNullException(nameof(characterControllerMappable));
+		}
+
+		protected override void OnEventFired(object source, EntityWorldRepresentationCreatedEventArgs args)
+		{
+			CharacterController characterController = args.EntityWorldRepresentation.GetComponent<CharacterController>();
+
+			//TODO: Assert it being there
+			CharacterControllerMappable.AddObject(args.EntityGuid, characterController);
+		}
+	}
+}
