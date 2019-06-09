4bcd83fa211a47ba76e908320d48304477dbbb5e
HelloKitty <andrew.blakely@ymail.com>
2019-06-09 13:21:54 -0500

    Added EntityGameObjectDirectory for tracking non-root GameObjects

---
+++ src/Client/GladMMO.Client.Game/Component/EntityGameObjectDirectory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Basically the directory component for addressing non-root <see cref="GameObject"/>
+	/// associated with the avatar/entity/player.
+	/// </summary>
+	public sealed class EntityGameObjectDirectory : MonoBehaviour
+	{
+		[SerializeField]
+		private GameObject RootGameObject;
+
+		[SerializeField]
+		private GameObject IKRoot;
+
+		public enum Type
+		{
+			Root = 0,
+			IKRoot = 1,
+		}
+
+		public GameObject GetGameObject(EntityGameObjectDirectory.Type gameObjectType)
+		{
+			if(!Enum.IsDefined(typeof(Type), gameObjectType)) throw new InvalidEnumArgumentException(nameof(gameObjectType), (int)gameObjectType, typeof(Type));
+
+			switch (gameObjectType)
+			{
+				case Type.Root:
+					return RootGameObject;
+				case Type.IKRoot:
+					return IKRoot;
+				default:
+					throw new ArgumentOutOfRangeException(nameof(gameObjectType), gameObjectType, null);
+			}
+		}
+	}
+}
