2401e7a136e0b1a277cdb6328928d99c75500a36
HelloKitty <andrew.blakely@ymail.com>
2019-06-09 14:04:43 -0500

    Fixed new avatar spawning

--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnAvatarPrefabCompletedDownload/OnAvatarDownloadedSpawnNewAvatarGameObjectEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnAvatarPrefabCompletedDownload/OnAvatarDownloadedSpawnNewAvatarGameObjectEventListener.cs
@@ -15,27 +15,42 @@ namespace GladMMO
 		private ILog Logger { get; }
 
 		public OnAvatarDownloadedSpawnNewAvatarGameObjectEventListener(IAvatarPrefabCompletedDownloadEventSubscribable subscriptionService,
-			[NotNull] ILog logger) 
+			[NotNull] ILog logger,
+			[NotNull] IReadonlyEntityGuidMappable<EntityGameObjectDirectory> gameObjectDirectoryMappable) 
 			: base(subscriptionService)
 		{
 			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			GameObjectDirectoryMappable = gameObjectDirectoryMappable ?? throw new ArgumentNullException(nameof(gameObjectDirectoryMappable));
 		}
 
 		protected override void OnEventFired(object source, AvatarPrefabCompletedDownloadEventArgs args)
 		{
-			//Now we've assigned the handle, we need to actually handle the spawning/loading of the avatar.
-			GameObject ikRootGameObject = GameObjectDirectoryMappable.RetrieveEntity(args.EntityGuid).GetGameObject(EntityGameObjectDirectory.Type.IKRoot);
-			GameObject currentAvatarRootGameObject = ikRootGameObject.transform.GetChild(0).gameObject;
-			GameObject newlySpawnedAvatar = InstantiateNewFromPrefab(args.DownloadedPrefabObject, currentAvatarRootGameObject);
-
-			GameObject.DestroyImmediate(currentAvatarRootGameObject, false);
-
 			if(Logger.IsInfoEnabled)
-				Logger.Info($"Instantiating new Avatar for Entity: {args.EntityGuid}");
+				Logger.Info($"About to create new Avatar for Entity: {args.EntityGuid}");
+
+			try
+			{
+				//Now we've assigned the handle, we need to actually handle the spawning/loading of the avatar.
+				GameObject ikRootGameObject = GameObjectDirectoryMappable.RetrieveEntity(args.EntityGuid).GetGameObject(EntityGameObjectDirectory.Type.IKRoot);
+				GameObject currentAvatarRootGameObject = ikRootGameObject.transform.GetChild(0).gameObject;
+				GameObject newlySpawnedAvatar = InstantiateNewFromPrefab(args.DownloadedPrefabObject, currentAvatarRootGameObject);
+
+				GameObject.DestroyImmediate(currentAvatarRootGameObject, false);
+			}
+			catch (Exception e)
+			{
+				if(Logger.IsErrorEnabled)
+					Logger.Error($"Failed to create Avatar for Entity: {args.EntityGuid}. Error: {e.Message}\n\nStack: {e.StackTrace}");
+
+				throw;
+			}
 		}
 
-		private GameObject InstantiateNewFromPrefab(GameObject prefab, GameObject oldAvatar)
+		private GameObject InstantiateNewFromPrefab([NotNull] GameObject prefab, [NotNull] GameObject oldAvatar)
 		{
+			if (prefab == null) throw new ArgumentNullException(nameof(prefab));
+			if (oldAvatar == null) throw new ArgumentNullException(nameof(oldAvatar));
+
 			//This should be the avatar prefab
 			//and we should be on the main thread here so we can now do the spawning and such
 			//Replace the current avatar root gameobject with the new one.
