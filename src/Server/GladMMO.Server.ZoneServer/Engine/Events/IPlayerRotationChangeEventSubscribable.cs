2fff64837b6da0c341d68bffbbce829f721e27ce
HelloKitty <andrew.blakely@ymail.com>
2019-06-09 02:47:30 -0500

    Added IPlayerRotationChangeEventSubscribable

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/Events/IPlayerRotationChangeEventSubscribable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IPlayerRotationChangeEventSubscribable
+	{
+		event EventHandler<PlayerRotiationChangeEventArgs> OnPlayerRotationChanged;
+	}
+
+	public sealed class PlayerRotiationChangeEventArgs : EventArgs
+	{
+		public NetworkEntityGuid EntityGuid { get; }
+
+		public float Rotation { get; }
+
+		public PlayerRotiationChangeEventArgs([NotNull] NetworkEntityGuid entityGuid, float rotation)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+			Rotation = rotation;
+		}
+	}
+}
