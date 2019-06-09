6e409d36ae9f5ff9b335a95e200e7c54b097f46c
HelloKitty <andrew.blakely@ymail.com>
2019-06-08 19:05:30 -0500

    Readded TimeSyncronizationResponseHandler from Guardians

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/TimeSyncronizationResponseHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+using GladMMO;
+using GladNet;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class TimeSyncronizationResponseHandler : BaseGameClientGameMessageHandler<ServerTimeSyncronizationResponsePayload>
+	{
+		private INetworkTimeService TimeService { get; }
+
+		/// <inheritdoc />
+		public TimeSyncronizationResponseHandler(ILog logger, INetworkTimeService timeService)
+			: base(logger)
+		{
+			TimeService = timeService ?? throw new ArgumentNullException(nameof(timeService));
+		}
+
+		//TODO: This is a work in progress, we need a time service.
+		/// <inheritdoc />
+		public override Task HandleMessage(IPeerMessageContext<GameClientPacketPayload> context, ServerTimeSyncronizationResponsePayload payload)
+		{
+			TimeService.SetTimeSyncronization(payload.SentLocalTime, payload.ServerTime);
+
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"ApproxLatency: {TimeService.CurrentLatency / TimeSpan.TicksPerMillisecond} ms TimeDiff: {TimeService.CurrentTimeOffset / TimeSpan.TicksPerMillisecond} ms");
+
+			return Task.CompletedTask;
+		}
+	}
+}
