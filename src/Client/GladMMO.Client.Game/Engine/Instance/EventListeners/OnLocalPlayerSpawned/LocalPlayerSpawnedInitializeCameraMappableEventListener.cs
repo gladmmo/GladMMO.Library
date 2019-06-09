db9ba8d13cd080d2928a395724e2e898aa9df1fe
HelloKitty <andrew.blakely@ymail.com>
2019-06-08 23:49:16 -0500

    Added local camera initialization on local player spawn

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnLocalPlayerSpawned/LocalPlayerSpawnedInitializeCameraMappableEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class LocalPlayerSpawnedInitializeCameraMappableEventListener : OnLocalPlayerSpawnedEventListener
+	{
+		private IEntityGuidMappable<Camera> CameraMappable { get; }
+
+		public LocalPlayerSpawnedInitializeCameraMappableEventListener(ILocalPlayerSpawnedEventSubscribable subscriptionService, 
+			[NotNull] IEntityGuidMappable<Camera> cameraMappable) 
+			: base(subscriptionService)
+		{
+			CameraMappable = cameraMappable ?? throw new ArgumentNullException(nameof(cameraMappable));
+		}
+
+		protected override void OnLocalPlayerSpawned(LocalPlayerSpawnedEventArgs args)
+		{
+			//Just add the main camera.
+			CameraMappable.AddObject(args.EntityGuid, Camera.main);
+		}
+	}
+}
