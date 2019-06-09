f70fa46b1c75652f2509f7f871fb24cdb52aed9a
HelloKitty <andrew.blakely@ymail.com>
2019-06-09 12:21:42 -0500

    Added SingleReleaseablePrefabContentResourceHandleDecorator

---
+++ src/Client/GladMMO.Client.Game/ContentManagement/SingleReleaseablePrefabContentResourceHandleDecorator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Decorates the <see cref="IPrefabContentResourceHandle"/> with semantics
+	/// that make it so that it can only ever be 
+	/// </summary>
+	public sealed class SingleReleaseablePrefabContentResourceHandleDecorator : IPrefabContentResourceHandle
+	{
+		private IPrefabContentResourceHandle DecoratedHandle { get; }
+
+		private bool isReleased { get; set; } = false;
+
+		private readonly object SyncObj = new object();
+
+		public SingleReleaseablePrefabContentResourceHandleDecorator([NotNull] IPrefabContentResourceHandle decoratedHandle)
+		{
+			DecoratedHandle = decoratedHandle ?? throw new ArgumentNullException(nameof(decoratedHandle));
+		}
+
+		public int CurrentUseCount => DecoratedHandle.CurrentUseCount;
+
+		public void Release()
+		{
+			if (isReleased)
+				return;
+
+			lock (SyncObj)
+			{
+				//Double check locking.
+				if (isReleased)
+					return;
+
+				DecoratedHandle.Release();
+				isReleased = true;
+			}
+		}
+
+		public GameObject LoadPrefab()
+		{
+			return DecoratedHandle.LoadPrefab();
+		}
+
+		public Task<GameObject> LoadPrefabAsync()
+		{
+			return DecoratedHandle.LoadPrefabAsync();
+		}
+	}
+}
