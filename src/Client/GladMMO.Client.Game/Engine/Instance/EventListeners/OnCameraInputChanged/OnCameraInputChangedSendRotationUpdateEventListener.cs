73f95fbb11dce87d23ba6e79f1ba66c3436db8e0
HelloKitty <andrew.blakely@ymail.com>
2019-06-09 01:21:03 -0500

    Added OnCameraInputChangedSendRotationUpdateEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnCameraInputChanged/OnCameraInputChangedSendRotationUpdateEventListener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using GladNet;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class OnCameraInputChangedSendRotationUpdateEventListener : BaseSingleEventListenerInitializable<ICameraInputChangedEventSubscribable, CameraInputChangedEventArgs>
+	{
+		private IPeerPayloadSendService<GameClientPacketPayload> SendService { get; }
+
+		public OnCameraInputChangedSendRotationUpdateEventListener(ICameraInputChangedEventSubscribable subscriptionService,
+			[NotNull] IPeerPayloadSendService<GameClientPacketPayload> sendService) 
+			: base(subscriptionService)
+		{
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+		}
+
+		protected override void OnEventFired(object source, CameraInputChangedEventArgs args)
+		{
+			SendService.SendMessage(new ClientRotationDataUpdateRequest(args.Rotation));
+		}
+	}
+}
