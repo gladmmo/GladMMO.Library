1971b341d448dacb4517219c3ce6301c74ea3e24
HelloKitty <andrew.blakely@ymail.com>
2019-06-11 06:21:35 -0500

    Rewrote IVoiceNetworkInitializedEventSubscribable

--- src/Client/GladMMO.Client.Game/Engine/Instance/Events/IVoiceNetworkInitializedEventSubscribable.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/IVoiceNetworkInitializedEventSubscribable.cs
@@ -11,16 +11,6 @@ namespace GladMMO
 	/// </summary>
 	public interface IVoiceNetworkInitializedEventSubscribable
 	{
-		event EventHandler<VoiceNetworkInitializedEventArgs> OnVoiceNetworkInitialized;
-	}
-
-	public sealed class VoiceNetworkInitializedEventArgs : EventArgs
-	{
-		public VivoxUnity.ILoginSession Session { get; }
-
-		public VoiceNetworkInitializedEventArgs([NotNull] ILoginSession session)
-		{
-			Session = session ?? throw new ArgumentNullException(nameof(session));
-		}
+		event EventHandler OnVoiceNetworkInitialized;
 	}
 }
