ddf01cd39725807c9dbc26d5edc589ed29308803
HelloKitty <andrew.blakely@ymail.com>
2019-06-11 05:45:36 -0500

    Added VivoxChannelSessionExtensions.ConnectionAsync

--- src/Client/GladMMO.Client.Game/Extensions/VivoxExtensions.cs
+++ src/Client/GladMMO.Client.Game/Extensions/VivoxExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,7 +21,18 @@ namespace GladMMO
 			if (server == null) throw new ArgumentNullException(nameof(server));
 			if (accessToken == null) throw new ArgumentNullException(nameof(accessToken));
 
-			return Task.Factory.FromAsync(session.BeginLogin(server, accessToken, null), session.EndLogin);
+			return Task.Factory.FromAsync(session.BeginLogin(server, accessToken, new AsyncCallback(ar => {})), session.EndLogin);
+		}
+	}
+
+	public static class VivoxChannelSessionExtensions
+	{
+		public static Task ConnectionAsync([NotNull] this VivoxUnity.IChannelSession channelSession, bool connectAudio, bool connectText, VivoxUnity.TransmitPolicy transmit, string accessToken)
+		{
+			if (channelSession == null) throw new ArgumentNullException(nameof(channelSession));
+			if (!Enum.IsDefined(typeof(VivoxUnity.TransmitPolicy), transmit)) throw new InvalidEnumArgumentException(nameof(transmit), (int) transmit, typeof(VivoxUnity.TransmitPolicy));
+
+			return Task.Factory.FromAsync(channelSession.BeginConnect(connectAudio, connectText, transmit, accessToken, new AsyncCallback(ar => { })), channelSession.EndConnect);
 		}
 	}
 }
