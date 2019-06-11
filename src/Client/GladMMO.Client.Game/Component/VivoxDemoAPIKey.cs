1bbf49a7ced3d8ab4e8078d50f8936d2e767de08
HelloKitty <andrew.blakely@ymail.com>
2019-06-11 05:52:25 -0500

    Added Vivox demo/test API key component

---
+++ src/Client/GladMMO.Client.Game/Component/VivoxDemoAPIKey.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class VivoxDemoAPIKey : MonoBehaviour
+	{
+		[SerializeField]
+		private string _AccessKey;
+
+		public static string AccessKey { get; private set; }
+
+		void Awake()
+		{
+			AccessKey = _AccessKey;
+		}
+	}
+}
