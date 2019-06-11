237dbb94a0d972c55ece36b9a5cf72a6725abdec
HelloKitty <andrew.blakely@ymail.com>
2019-06-11 06:21:24 -0500

    Added IVoiceSessionAuthenticatedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/IVoiceSessionAuthenticatedEventSubscribable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IVoiceSessionAuthenticatedEventSubscribable
+	{
+		event EventHandler OnVoiceSessionAuthenticated;
+	}
+
+	public sealed class VoiceSessionAuthenticatedEventArgs : EventArgs
+	{
+		public VivoxUnity.ILoginSession Session { get; }
+
+		public VoiceSessionAuthenticatedEventArgs([NotNull] VivoxUnity.ILoginSession session)
+		{
+			Session = session ?? throw new ArgumentNullException(nameof(session));
+		}
+	}
+}
