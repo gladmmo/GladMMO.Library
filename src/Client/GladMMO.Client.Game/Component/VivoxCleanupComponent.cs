adbb7414f4a6734a793bc9e76ea21ba039a3e181
HelloKitty <andrew.blakely@ymail.com>
2019-06-11 04:30:18 -0500

    Added VivoxCleanupComponent

---
+++ src/Client/GladMMO.Client.Game/Component/VivoxCleanupComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[Injectee]
+	public sealed class VivoxCleanupComponent : MonoBehaviour
+	{
+		[Inject]
+		public VivoxUnity.Client VoiceClient;
+
+		void OnApplicationQuit()
+		{
+			VoiceClient.Uninitialize();
+		}
+	}
+}
