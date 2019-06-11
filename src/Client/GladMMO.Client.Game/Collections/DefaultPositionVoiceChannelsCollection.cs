6881b1794dd63dff83c3e23835275e24a0cde374
HelloKitty <andrew.blakely@ymail.com>
2019-06-11 07:21:44 -0500

    Added IPositionalVoiceChannelCollection

---
+++ src/Client/GladMMO.Client.Game/Collections/DefaultPositionVoiceChannelsCollection.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using VivoxUnity;
+
+namespace GladMMO
+{
+	public sealed class DefaultPositionVoiceChannelsCollection : IPositionalVoiceChannelCollection
+	{
+		private List<IChannelSession> InternalChannels { get; } = new List<IChannelSession>(2);
+
+		public IEnumerator<IChannelSession> GetEnumerator()
+		{
+			return InternalChannels.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return ((IEnumerable) InternalChannels).GetEnumerator();
+		}
+
+		public void Add(IChannelSession item)
+		{
+			InternalChannels.Add(item);
+		}
+
+		public void Clear()
+		{
+			InternalChannels.Clear();
+		}
+
+		public bool Contains(IChannelSession item)
+		{
+			return InternalChannels.Contains(item);
+		}
+
+		public void CopyTo(IChannelSession[] array, int arrayIndex)
+		{
+			InternalChannels.CopyTo(array, arrayIndex);
+		}
+
+		public bool Remove(IChannelSession item)
+		{
+			return InternalChannels.Remove(item);
+		}
+
+		public int Count => InternalChannels.Count;
+
+		public bool IsReadOnly => false;
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/Collections/IPositionalVoiceChannelCollection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VivoxUnity;
+
+namespace GladMMO
+{
+	public interface IReadonlyPositionalVoiceChannels : IEnumerable<IChannelSession>
+	{
+
+	}
+
+	public interface IPositionalVoiceChannels : IReadonlyPositionalVoiceChannels, ICollection<IChannelSession>
+	{
+
+	}
+}
