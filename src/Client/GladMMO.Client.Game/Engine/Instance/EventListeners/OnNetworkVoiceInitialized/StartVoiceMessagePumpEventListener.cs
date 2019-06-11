34e2aeeca4b0a462dd213baa18a12dcfaa2a5c95
HelloKitty <andrew.blakely@ymail.com>
2019-06-11 06:21:51 -0500

    Added StartVoiceMessagePumpEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnNetworkVoiceInitialized/StartVoiceMessagePumpEventListener.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class StartVoiceMessagePumpEventListener : BaseSingleEventListenerInitializable<IVoiceNetworkInitializedEventSubscribable>, IGameTickable
+	{
+		private bool isInitialized { get; set; } = false;
+
+		private ILog Logger { get; }
+
+		public StartVoiceMessagePumpEventListener(IVoiceNetworkInitializedEventSubscribable subscriptionService,
+			[NotNull] ILog logger) 
+			: base(subscriptionService)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		protected override void OnEventFired(object source, EventArgs args)
+		{
+			isInitialized = true;
+		}
+
+		public void Tick()
+		{
+			try
+			{
+				//TODO: Maybe hide this behind an extension
+				if(isInitialized)
+					VivoxUnity.Client.RunOnce();
+			}
+			catch(Exception e)
+			{
+				if(Logger.IsErrorEnabled)
+					Logger.Error($"Vivox Error: {e.Message}\n\nStack: {e.StackTrace}");
+			}
+		}
+	}
+}
