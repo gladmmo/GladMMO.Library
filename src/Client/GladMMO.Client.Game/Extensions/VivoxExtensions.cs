c5bb3b2bf9141927b11d543610aaefc7375c0eb9
HelloKitty <andrew.blakely@ymail.com>
2019-06-11 02:56:22 -0500

    Added VivoxLoginSessionExtensions.LoginAsync

---
+++ src/Client/GladMMO.Client.Game/Extensions/VivoxExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public static class VivoxLoginSessionExtensions
+	{
+		/// <summary>
+		/// The async version of Vivox BeginLogin.
+		/// </summary>
+		/// <param name="session"></param>
+		/// <param name="server"></param>
+		/// <param name="accessToken"></param>
+		/// <returns></returns>
+		public static Task LoginAsync([NotNull] this VivoxUnity.ILoginSession session, [NotNull] Uri server, [NotNull] string accessToken)
+		{
+			if (session == null) throw new ArgumentNullException(nameof(session));
+			if (server == null) throw new ArgumentNullException(nameof(server));
+			if (accessToken == null) throw new ArgumentNullException(nameof(accessToken));
+
+			return Task.Factory.FromAsync(session.BeginLogin(server, accessToken, null), session.EndLogin);
+		}
+	}
+}
