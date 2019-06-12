8550594eaec954d265e114017b0c50b44c3b18f5
HelloKitty <andrew.blakely@ymail.com>
2019-06-11 23:45:11 -0500

    Added NetworkMovementTrackerTypeFlags

---
+++ src/GladMMO.ZoneServer.Common/Enums/NetworkMovementTrackerTypeFlags.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Bitmask for network movement tracker type.
+	/// </summary>
+	[Flags]
+	public enum NetworkMovementTrackerTypeFlags : int
+	{
+		None = 0,
+
+		Head = 1 << 0,
+		RightHand = 1 << 1,
+		LeftHand = 1 << 2
+	}
+}
