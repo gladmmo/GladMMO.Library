2bee82bc81597699546d6eb7e16a3d597d78f800
HelloKitty <andrew.blakely@ymail.com>
2019-06-12 02:37:13 -0500

    Added server paylod for PlayerNetworkTrackerChangeUpdateEvent

---
+++ src/GladMMO.Payloads.ZoneServer/Payloads/PlayerNetworkTrackerChangeUpdateEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[ProtoContract]
+	[GamePayload(GamePayloadOperationCode.PlayerTrackerDataChange)]
+	public sealed class PlayerNetworkTrackerChangeUpdateEvent : GameServerPacketPayload
+	{
+		[ProtoMember(1)]
+		public EntityAssociatedData<PlayerNetworkTrackerChangeUpdateRequest> PlayerTrackerUpdate { get; }
+
+		public PlayerNetworkTrackerChangeUpdateEvent([NotNull] EntityAssociatedData<PlayerNetworkTrackerChangeUpdateRequest> playerTrackerUpdate)
+		{
+			PlayerTrackerUpdate = playerTrackerUpdate ?? throw new ArgumentNullException(nameof(playerTrackerUpdate));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private PlayerNetworkTrackerChangeUpdateEvent()
+		{
+			
+		}
+	}
+}
