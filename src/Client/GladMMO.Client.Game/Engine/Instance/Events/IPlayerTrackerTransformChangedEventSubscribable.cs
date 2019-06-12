44471b7d8ffda2270cb89f4f26afaa0ba0d2bafc
HelloKitty <andrew.blakely@ymail.com>
2019-06-12 02:56:08 -0500

    Added IPlayerTrackerTransformChangedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/IPlayerTrackerTransformChangedEventSubscribable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IPlayerTrackerTransformChangedEventSubscribable
+	{
+		event EventHandler<PlayerTrackerTransformChangedEventArgs> OnTrackerTransformChanged;
+	}
+
+	public sealed class PlayerTrackerTransformChangedEventArgs : EventArgs
+	{
+		public EntityAssociatedData<PlayerNetworkTrackerChangeUpdateRequest> ChangeInformation { get; }
+
+		public PlayerTrackerTransformChangedEventArgs([NotNull] EntityAssociatedData<PlayerNetworkTrackerChangeUpdateRequest> changeInformation)
+		{
+			ChangeInformation = changeInformation ?? throw new ArgumentNullException(nameof(changeInformation));
+		}
+	}
+}
