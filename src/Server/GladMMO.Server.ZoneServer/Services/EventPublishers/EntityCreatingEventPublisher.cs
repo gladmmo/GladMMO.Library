df9a5bc1868bc6a259a52165c9d0bb3be0609098
HelloKitty <andrew.blakely@ymail.com>
2019-06-13 13:16:32 -0500

    Added EntityCreatingEventPublisher

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/EventPublishers/EntityCreatingEventPublisher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Handles the subscrption of event <see cref="IEntityCreatingEventSubscribable"/>
+	/// and the publishing through <see cref="IEventPublisher{TEventPublisherInterface,TEventArgsType}"/>.
+	/// </summary>
+	public sealed class EntityCreatingEventPublisher : IEventPublisher<IEntityCreatingEventSubscribable, EntityCreatingEventArgs>, IEntityCreatingEventSubscribable
+	{
+		public event EventHandler<EntityCreatingEventArgs> OnEntityCreating;
+
+		public void PublishEvent([NotNull] object sender, [NotNull] EntityCreatingEventArgs eventArgs)
+		{
+			if (sender == null) throw new ArgumentNullException(nameof(sender));
+			if (eventArgs == null) throw new ArgumentNullException(nameof(eventArgs));
+
+			OnEntityCreating?.Invoke(sender, eventArgs);
+		}
+	}
+}
