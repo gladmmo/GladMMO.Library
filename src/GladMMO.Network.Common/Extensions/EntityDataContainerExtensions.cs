fa9eb71bed96008df879d9d61faf4a8ebee4e6a4
HelloKitty <andrew.blakely@ymail.com>
2019-06-13 13:48:23 -0500

    Added EntityDataContainerExtensions

---
+++ src/GladMMO.Network.Common/Extensions/EntityDataContainerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Generic.Math;
+
+namespace GladMMO
+{
+	public static class EntityDataContainerExtensions
+	{
+		/// <summary>
+		/// Helper extension for setting entity data in <see cref="IEntityDataFieldContainer"/>
+		/// based on the int value of a specified Enum value <see cref="index"/>.
+		/// </summary>
+		/// <typeparam name="TEnumType"></typeparam>
+		/// <typeparam name="TValueType"></typeparam>
+		/// <param name="container"></param>
+		/// <param name="index"></param>
+		/// <param name="value"></param>
+		public static void SetFieldValue<TEnumType, TValueType>(this IEntityDataFieldContainer container, TEnumType index, TValueType value)
+			where TValueType : struct
+			where TEnumType : Enum
+		{
+			if (container == null) throw new ArgumentNullException(nameof(container));
+
+			container.SetFieldValue(GenericMath.Convert<TEnumType, int>(index), value);
+		}
+	}
+}
