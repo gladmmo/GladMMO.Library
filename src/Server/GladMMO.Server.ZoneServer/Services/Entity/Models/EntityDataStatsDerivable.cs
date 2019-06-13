bf9728575881dcf448bc8a0fa58fa129ae1095ed
HelloKitty <andrew.blakely@ymail.com>
2019-06-13 12:53:04 -0500

    Added EntityDataStatsDerivable

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Entity/Models/EntityDataStatsDerivable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Type that describes relevant information
+	/// about an entity that can be used
+	/// to derive stats information/data from.
+	/// </summary>
+	public sealed class EntityDataStatsDerivable
+	{
+		/// <summary>
+		/// The type of the entity.
+		/// </summary>
+		public EntityType EntityType { get; }
+
+		//TODO: Add class here
+
+		/// <summary>
+		/// The level of the entity.
+		/// </summary>
+		public int Level { get; }
+
+		public EntityDataStatsDerivable(EntityType entityType, int level)
+		{
+			if(!Enum.IsDefined(typeof(EntityType), entityType)) throw new InvalidEnumArgumentException(nameof(entityType), (int)entityType, typeof(EntityType));
+
+			EntityType = entityType;
+			Level = level;
+		}
+	}
+}
