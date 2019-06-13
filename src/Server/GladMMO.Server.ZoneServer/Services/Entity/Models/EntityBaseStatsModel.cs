ee14b096c2fccd355ee8aaac316edcc02fa3834e
HelloKitty <andrew.blakely@ymail.com>
2019-06-13 12:52:51 -0500

    Added EntityBaseStatsModel

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Entity/Models/EntityBaseStatsModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class EntityBaseStatsModel
+	{
+		/// <summary>
+		/// The base health value of the entity.
+		/// </summary>
+		public int BaseHealth { get; }
+
+		/// <summary>
+		/// The base mana of the entity.
+		/// Could be 0 if the user has no mana potential.
+		/// </summary>
+		public int BaseMana { get; }
+
+		public EntityBaseStatsModel(int baseHealth, int baseMana)
+		{
+			if (baseHealth < 0) throw new ArgumentOutOfRangeException(nameof(baseHealth));
+			if (baseMana < 0) throw new ArgumentOutOfRangeException(nameof(baseMana));
+
+			BaseHealth = baseHealth;
+			BaseMana = baseMana;
+		}
+	}
+}
