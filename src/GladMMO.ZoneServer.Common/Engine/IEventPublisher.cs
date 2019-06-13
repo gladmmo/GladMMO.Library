d44878f326d694e7da36a658763a6a8e74b6a941
HelloKitty <andrew.blakely@ymail.com>
2019-06-13 13:13:07 -0500

    Added generic IEventPublisher<TEventPublisherInterface, TEventArgsType>

---
+++ src/GladMMO.ZoneServer.Common/Engine/IEventPublisher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for a type that publishes a particular event interface type.
+	/// </summary>
+	/// <typeparam name="TEventArgsType"></typeparam>
+	public interface IEventPublisher<TEventPublisherInterface, in TEventArgsType> //One day I dream of a generic type arg that can be constrained to a type in the implementing types hierarchy.
+		where TEventArgsType : EventArgs
+	{
+		void PublishEvent(object sender, TEventArgsType argsType);
+	}
+}
