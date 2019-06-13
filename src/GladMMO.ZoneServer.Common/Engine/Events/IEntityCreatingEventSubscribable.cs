975c722e7e122946eaf1024ea8c2a17579be9cc1
HelloKitty <andrew.blakely@ymail.com>
2019-06-13 13:03:29 -0500

    Added IEntityCreatingEventSubscribable event

---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/IEntityCreatingEventSubscribable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for event publisher that
+	/// publishes an event when an entity has started creation.
+	/// </summary>
+	public interface IEntityCreatingEventSubscribable
+	{
+		event EventHandler<EntityCreatingEventArgs> OnEntityCreating;
+	}
+
+	public sealed class EntityCreatingEventArgs : EventArgs
+	{
+		/// <summary>
+		/// The entity guid of the entity starting creating.
+		/// </summary>
+		public NetworkEntityGuid EntityGuid { get; }
+
+		public EntityCreatingEventArgs([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
