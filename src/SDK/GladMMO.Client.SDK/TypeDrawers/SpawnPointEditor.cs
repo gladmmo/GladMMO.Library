6528810db07ce337c80e8bbcb63c84e259e0697e
HelloKitty <andrew.blakely@ymail.com>
2019-06-14 02:25:42 -0500

    Added skeleton StaticSpawnPointDefinitionEditor

---
+++ src/SDK/GladMMO.Client.SDK/TypeDrawers/SpawnPointEditor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GladMMO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Booma.Proxy
+{
+	//based on: https://unity3d.college/2016/09/12/unity-oninspectorgui/
+	[CustomEditor(typeof(StaticSpawnPointDefinition))]
+	public class StaticSpawnPointDefinitionEditor : ExtendedMonoBehaviourTypeDrawer
+	{
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+		}
+	}
+}
