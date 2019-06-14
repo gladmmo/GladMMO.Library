e0079b08b75ecaa2e6230458e95808bd2c73e951
HelloKitty <andrew.blakely@ymail.com>
2019-06-14 02:15:02 -0500

    Added GladMMOSDKMonoBehaviour

---
+++ src/SDK/GladMMO.Common.SDK/Component/Base/GladMMOSDKMonoBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// The base abstract Type for all GladMMO SDK <see cref="MonoBehaviour"/>s.
+	/// </summary>
+	public abstract class GladMMOSDKMonoBehaviour : MonoBehaviour
+	{
+
+	}
+}
