76211f2558e2589389c3c8fa30e8774a929f6432
HelloKitty <andrew.blakely@ymail.com>
2019-06-14 04:12:30 -0500

    Added PlayerSpawnStrategyQueue

---
+++ src/Server/GladMMO.Server.ZoneServer/Collections/PlayerSpawnStrategyQueue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public class PlayerSpawnStrategyQueue : ConcurrentQueue<ISpawnPointStrategy>
+	{
+
+	}
+}
