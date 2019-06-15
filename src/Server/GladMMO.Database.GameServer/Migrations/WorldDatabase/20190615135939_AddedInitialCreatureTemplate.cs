5ff485123d281f05d4f4224706f5efff7da94d13
HelloKitty <andrew.blakely@ymail.com>
2019-06-15 09:00:40 -0500

    Added migration for initial Creature Template

---
+++ src/Server/GladMMO.Database.GameServer/Migrations/WorldDatabase/20190615135939_AddedInitialCreatureTemplate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GladMMO.Database.GameServer.Migrations.WorldDatabase
+{
+    public partial class AddedInitialCreatureTemplate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "creature_template",
+                columns: table => new
+                {
+                    CreatureTemplateId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AccountId = table.Column<int>(nullable: false),
+                    ModelId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_creature_template", x => x.CreatureTemplateId);
+                    table.ForeignKey(
+                        name: "FK_creature_template_creature_model_entry_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "creature_model_entry",
+                        principalColumn: "CreatureId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_creature_template_ModelId",
+                table: "creature_template",
+                column: "ModelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "creature_template");
+        }
+    }
+}
