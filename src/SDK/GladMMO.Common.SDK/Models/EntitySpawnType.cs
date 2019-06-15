bf27d43169cdd2d8990a5be2e6f4436579a6cc6c
HelloKitty <andrew.blakely@ymail.com>
2019-06-15 10:54:41 -0500

    Added EntitySpawnType

---
+++ src/SDK/GladMMO.Common.SDK/Models/EntitySpawnType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public enum EntitySpawnType
+	{
+		Unknown = 0,
+
+		Player = 1,
+
+		Creature = 2,
+	}
+}
