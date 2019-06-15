29c93cb51635da31b851e5ee6bc3422873b6547b
HelloKitty <andrew.blakely@ymail.com>
2019-06-15 06:01:01 -0500

    Refactored Avatar and World controllers to use BaseCustomControllerController

--- src/Server/GladMMO.Service.ContentServer/Controllers/AvatarController.cs
+++ src/Server/GladMMO.Service.ContentServer/Controllers/AvatarController.cs
@@ -14,69 +14,13 @@ namespace GladMMO
 {
 	//TODO: This is mostly a copy-paste from Worlds, we need to significantly extend and refactor this for public avatars and private avatars support.
 	[Route("api/[controller]")]
-	public class AvatarController : AuthorizationReadyController
+	public class AvatarController : BaseCustomControllerController<AvatarEntryModel>
 	{
-		// GET api/values
-		/// <inheritdoc />
-		public AvatarController(IClaimsPrincipalReader claimsReader, ILogger<AvatarController> logger)
-			: base(claimsReader, logger)
+		public AvatarController(IClaimsPrincipalReader claimsReader, 
+			ILogger<AuthorizationReadyController> logger, 
+			UserContentType contentType)
+			: base(claimsReader, logger, contentType)
 		{
-
-		}
-
-		/// <summary>
-		/// POST request that requests an a download URL for a world.
-		/// The user must be authorized.
-		/// </summary>
-		/// <returns>A <see cref="ContentDownloadURLResponse"/> that either contains error information or the upload URL if it was successful.</returns>
-		[HttpPost("{id}/downloadurl")]
-		[AuthorizeJwt]
-		[NoResponseCache]
-		public async Task<IActionResult> RequestAvatarDownloadUrl(
-			[FromRoute(Name = "id")] long avatarId,
-			[FromServices] IAvatarEntryRepository avatarEntryRepository,
-			[FromServices] IStorageUrlBuilder urlBuilder,
-			[FromServices] IContentDownloadAuthroizationValidator downloadAuthorizer)
-		{
-			if(avatarEntryRepository == null) throw new ArgumentNullException(nameof(avatarEntryRepository));
-
-			//TODO: We want to rate limit access to this API
-			//TODO: We should use both app logging but also another logging service that always gets hit
-
-			//TODO: Consolidate this shared logic between controllers
-			if(Logger.IsEnabled(LogLevel.Information))
-				Logger.LogInformation($"Recieved {nameof(RequestAvatarDownloadUrl)} request from {ClaimsReader.GetUserName(User)}:{ClaimsReader.GetUserId(User)}.");
-
-			//TODO: We should probably check the flags of world to see if it's private (IE hidden from user). Or if it's unlisted or removed.
-			//It's possible a user is requesting a world that doesn't exist
-			//Could be malicious or it could have been deleted for whatever reason
-			if(!await avatarEntryRepository.ContainsAsync(avatarId).ConfigureAwait(false))
-				return Json(new ContentDownloadURLResponse(ContentDownloadURLResponseCode.NoContentId));
-
-			//TODO: Refactor this into a validation dependency
-			//Now we need to do some validation to determine if they should even be downloading this world
-			//we do not want people downloading a world they have no business of going to
-			int userId = ClaimsReader.GetUserIdInt(User);
-
-			if(!await downloadAuthorizer.CanUserAccessWorldContet(userId, avatarId))
-				return Json(new ContentDownloadURLResponse(ContentDownloadURLResponseCode.AuthorizationFailed));
-
-			//We can get the URL from the urlbuilder if we provide the world storage GUID
-			string downloadUrl = await urlBuilder.BuildRetrivalUrl(UserContentType.Avatar, (await avatarEntryRepository.RetrieveAsync(avatarId)).StorageGuid);
-
-			//TODO: Should we be validating S3 availability?
-			if(String.IsNullOrEmpty(downloadUrl))
-			{
-				if(Logger.IsEnabled(LogLevel.Error))
-					Logger.LogError($"Failed to create avatar upload URL for {ClaimsReader.GetUserName(User)}:{ClaimsReader.GetUserId(User)} with ID: {avatarId}.");
-
-				return Json(new ContentDownloadURLResponse(ContentDownloadURLResponseCode.ContentDownloadServiceUnavailable));
-			}
-
-			if(Logger.IsEnabled(LogLevel.Information))
-				Logger.LogInformation($"Success. Sending {ClaimsReader.GetUserName(User)} URL: {downloadUrl}");
-
-			return Json(new ContentDownloadURLResponse(downloadUrl));
 		}
 
 		[HttpPost("{id}/uploaded")]
@@ -128,51 +72,11 @@ namespace GladMMO
 			return Ok();
 		}
 
-		/// <summary>
-		/// POST request that requests an upload URL for a world.
-		/// The user must be authorized.
-		/// </summary>
-		/// <returns>A <see cref="RequestedUrlResponseModel"/> that either contains error information or the upload URL if it was successful.</returns>
-		[HttpPost("create")]
-		[AuthorizeJwt]
-		public async Task<IActionResult> RequestAvatarUploadUrl([FromServices] IAvatarEntryRepository avatarEntryRepository, [FromServices] IStorageUrlBuilder urlBuilder)
+		protected override AvatarEntryModel GenerateNewModel()
 		{
-			if(avatarEntryRepository == null) throw new ArgumentNullException(nameof(avatarEntryRepository));
-
-			//TODO: We want to rate limit access to this API
-			//TODO: We should use both app logging but also another logging service that always gets hit
-
-			if(Logger.IsEnabled(LogLevel.Information))
-				Logger.LogInformation($"Recieved {nameof(RequestAvatarUploadUrl)} request from {ClaimsReader.GetUserName(User)}:{ClaimsReader.GetUserId(User)}.");
-
 			int userId = ClaimsReader.GetUserIdInt(User);
 
-			//TODO: We should send this if we can't get a user id
-			//return new JsonResult(RequestedUrlResponseModel.CreateFailure("Failed to authorize action.", RequestedUrlResponseCode.AuthorizationFailed));
-			//TODO: Abstract this behind an issuer
-			Guid avatarGuid = Guid.NewGuid();
-
-			//TODO: Check if the result is valid? We should maybe return bool from this API (we do return bool from this API now)
-			//The idea is to create an entry which will contain a GUID. From that GUID we can then generate the upload URL
-			AvatarEntryModel world = new AvatarEntryModel(userId, this.HttpContext.Connection.RemoteIpAddress.ToString(), avatarGuid);
-			bool result = await avatarEntryRepository.TryCreateAsync(world); //TODO: Ok to just provide a guid right?
-
-			//TODO: Check world's worldid has been set
-
-			string uploadUrl = await urlBuilder.BuildUploadUrl(UserContentType.Avatar, avatarGuid);
-
-			if(String.IsNullOrEmpty(uploadUrl))
-			{
-				if(Logger.IsEnabled(LogLevel.Error))
-					Logger.LogError($"Failed to create world upload URL for {ClaimsReader.GetUserName(User)}:{ClaimsReader.GetUserId(User)} with GUID: {avatarGuid}.");
-
-				return new JsonResult(RequestedUrlResponseModel.CreateFailure("Upload service unavailable.", RequestedUrlResponseCode.ServiceUnavailable));
-			}
-
-			if(Logger.IsEnabled(LogLevel.Information))
-				Logger.LogInformation($"Success. Sending {ClaimsReader.GetUserName(User)} URL: {uploadUrl}");
-
-			return new JsonResult(RequestedUrlResponseModel.CreateSuccess(uploadUrl, world.AvatarId));
+			return new AvatarEntryModel(userId, HttpContext.Connection.RemoteIpAddress.ToString(), Guid.NewGuid());
 		}
 	}
 }
--- src/Server/GladMMO.Service.ContentServer/Controllers/WorldController.cs
+++ src/Server/GladMMO.Service.ContentServer/Controllers/WorldController.cs
@@ -14,69 +14,13 @@ namespace GladMMO
 {
 	//From ProjectVindictive: https://github.com/HelloKitty/ProjectVindictive.Library/blob/master/src/ProjectVindictive.Service.UserContentManagement/Controllers/WorldController.cs
 	[Route("api/[controller]")]
-	public class WorldController : AuthorizationReadyController
+	public class WorldController : BaseCustomControllerController<WorldEntryModel>
 	{
-		// GET api/values
-		/// <inheritdoc />
-		public WorldController(IClaimsPrincipalReader claimsReader, ILogger<WorldController> logger)
-			: base(claimsReader, logger)
+		public WorldController(IClaimsPrincipalReader claimsReader, 
+			ILogger<AuthorizationReadyController> logger, 
+			UserContentType contentType) 
+			: base(claimsReader, logger, contentType)
 		{
-
-		}
-
-		/// <summary>
-		/// POST request that requests an a download URL for a world.
-		/// The user must be authorized.
-		/// </summary>
-		/// <returns>A <see cref="ContentDownloadURLResponse"/> that either contains error information or the upload URL if it was successful.</returns>
-		[HttpPost("{id}/downloadurl")]
-		[AuthorizeJwt]
-		[NoResponseCache]
-		public async Task<IActionResult> RequestWorldDownloadUrl(
-			[FromRoute(Name = "id")] long worldId,
-			[FromServices] IWorldEntryRepository worldEntryRepository,
-			[FromServices] IStorageUrlBuilder urlBuilder,
-			[FromServices] IContentDownloadAuthroizationValidator downloadAuthorizer)
-		{
-			if(worldEntryRepository == null) throw new ArgumentNullException(nameof(worldEntryRepository));
-
-			//TODO: We want to rate limit access to this API
-			//TODO: We should use both app logging but also another logging service that always gets hit
-
-			//TODO: Consolidate this shared logic between controllers
-			if(Logger.IsEnabled(LogLevel.Information))
-				Logger.LogInformation($"Recieved {nameof(RequestWorldDownloadUrl)} request from {ClaimsReader.GetUserName(User)}:{ClaimsReader.GetUserId(User)}.");
-
-			//TODO: We should probably check the flags of world to see if it's private (IE hidden from user). Or if it's unlisted or removed.
-			//It's possible a user is requesting a world that doesn't exist
-			//Could be malicious or it could have been deleted for whatever reason
-			if(!await worldEntryRepository.ContainsAsync(worldId).ConfigureAwait(false))
-				return Json(new ContentDownloadURLResponse(ContentDownloadURLResponseCode.NoContentId));
-
-			//TODO: Refactor this into a validation dependency
-			//Now we need to do some validation to determine if they should even be downloading this world
-			//we do not want people downloading a world they have no business of going to
-			int userId = ClaimsReader.GetUserIdInt(User);
-
-			if(!await downloadAuthorizer.CanUserAccessWorldContet(userId, worldId))
-				return Json(new ContentDownloadURLResponse(ContentDownloadURLResponseCode.AuthorizationFailed));
-
-			//We can get the URL from the urlbuilder if we provide the world storage GUID
-			string downloadUrl = await urlBuilder.BuildRetrivalUrl(UserContentType.World, (await worldEntryRepository.RetrieveAsync(worldId)).StorageGuid);
-
-			//TODO: Should we be validating S3 availability?
-			if(String.IsNullOrEmpty(downloadUrl))
-			{
-				if(Logger.IsEnabled(LogLevel.Error))
-					Logger.LogError($"Failed to create world upload URL for {ClaimsReader.GetUserName(User)}:{ClaimsReader.GetUserId(User)} with ID: {worldId}.");
-
-				return Json(new ContentDownloadURLResponse(ContentDownloadURLResponseCode.ContentDownloadServiceUnavailable));
-			}
-
-			if(Logger.IsEnabled(LogLevel.Information))
-				Logger.LogInformation($"Success. Sending {ClaimsReader.GetUserName(User)} URL: {downloadUrl}");
-
-			return Json(new ContentDownloadURLResponse(downloadUrl));
 		}
 
 		[HttpPost("{id}/uploaded")]
@@ -129,51 +73,11 @@ namespace GladMMO
 			return Ok();
 		}
 
-		/// <summary>
-		/// POST request that requests an upload URL for a world.
-		/// The user must be authorized.
-		/// </summary>
-		/// <returns>A <see cref="RequestedUrlResponseModel"/> that either contains error information or the upload URL if it was successful.</returns>
-		[HttpPost("create")]
-		[AuthorizeJwt]
-		public async Task<IActionResult> RequestWorldUploadUrl([FromServices] IWorldEntryRepository worldEntryRepository, [FromServices] IStorageUrlBuilder urlBuilder)
+		protected override WorldEntryModel GenerateNewModel()
 		{
-			if(worldEntryRepository == null) throw new ArgumentNullException(nameof(worldEntryRepository));
-
-			//TODO: We want to rate limit access to this API
-			//TODO: We should use both app logging but also another logging service that always gets hit
-
-			if(Logger.IsEnabled(LogLevel.Information))
-				Logger.LogInformation($"Recieved {nameof(RequestWorldUploadUrl)} request from {ClaimsReader.GetUserName(User)}:{ClaimsReader.GetUserId(User)}.");
-
 			int userId = ClaimsReader.GetUserIdInt(User);
 
-			//TODO: We should send this if we can't get a user id
-			//return new JsonResult(RequestedUrlResponseModel.CreateFailure("Failed to authorize action.", RequestedUrlResponseCode.AuthorizationFailed));
-			//TODO: Abstract this behind an issuer
-			Guid worldGuid = Guid.NewGuid();
-
-			//TODO: Check if the result is valid? We should maybe return bool from this API (we do return bool from this API now)
-			//The idea is to create an entry which will contain a GUID. From that GUID we can then generate the upload URL
-			WorldEntryModel world = new WorldEntryModel(userId, this.HttpContext.Connection.RemoteIpAddress.ToString(), worldGuid);
-			bool result = await worldEntryRepository.TryCreateAsync(world); //TODO: Ok to just provide a guid right?
-
-			//TODO: Check world's worldid has been set
-
-			string uploadUrl = await urlBuilder.BuildUploadUrl(UserContentType.World, worldGuid);
-
-			if(String.IsNullOrEmpty(uploadUrl))
-			{
-				if(Logger.IsEnabled(LogLevel.Error))
-					Logger.LogError($"Failed to create world upload URL for {ClaimsReader.GetUserName(User)}:{ClaimsReader.GetUserId(User)} with GUID: {worldGuid}.");
-
-				return new JsonResult(RequestedUrlResponseModel.CreateFailure("Upload service unavailable.", RequestedUrlResponseCode.ServiceUnavailable));
-			}
-
-			if(Logger.IsEnabled(LogLevel.Information))
-				Logger.LogInformation($"Success. Sending {ClaimsReader.GetUserName(User)} URL: {uploadUrl}");
-
-			return new JsonResult(RequestedUrlResponseModel.CreateSuccess(uploadUrl, world.WorldId));
+			return new WorldEntryModel(userId, HttpContext.Connection.RemoteIpAddress.ToString(), Guid.NewGuid());
 		}
 	}
 }
