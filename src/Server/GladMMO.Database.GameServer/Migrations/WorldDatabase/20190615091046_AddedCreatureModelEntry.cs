cdba4f0fb6322b944096f60826cec43a74f35874
HelloKitty <andrew.blakely@ymail.com>
2019-06-15 04:23:40 -0500

    Added migration for CreatureModelEntry

---
+++ src/Server/GladMMO.Database.GameServer/Migrations/WorldDatabase/20190615091046_AddedCreatureModelEntry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GladMMO.Database.GameServer.Migrations.WorldDatabase
+{
+    public partial class AddedCreatureModelEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "creature_model_entry",
+                columns: table => new
+                {
+                    CreatureId = table.Column<long>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AccountId = table.Column<int>(nullable: false),
+                    CreationTime = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn),
+                    CreationIp = table.Column<string>(maxLength: 15, nullable: false),
+                    StorageGuid = table.Column<Guid>(nullable: false),
+                    IsValidated = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_creature_model_entry", x => x.CreatureId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "creature_model_entry");
+        }
+    }
+}
