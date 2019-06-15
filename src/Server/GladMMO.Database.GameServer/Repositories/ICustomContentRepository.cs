ca63c0924668fdb121ba6b582a21165f97b9fa01
HelloKitty <andrew.blakely@ymail.com>
2019-06-15 04:47:06 -0500

    Added ICustomContentRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/ICustomContentRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for repository that provide generic methods for <see cref="IClientContentPersistable"/> model types.
+	/// </summary>
+	/// <typeparam name="TCustomModelType">The <see cref="IClientContentPersistable"/> type.</typeparam>
+	public interface ICustomContentRepository<TCustomModelType> : IGenericRepositoryCrudable<long, TCustomModelType>
+		where TCustomModelType : IClientContentPersistable
+	{
+
+	}
+}
