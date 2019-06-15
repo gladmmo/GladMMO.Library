d966c05393db387ea489681abbf9bc39017119b8
HelloKitty <andrew.blakely@ymail.com>
2019-06-15 07:29:41 -0500

    Added initial CreatureController implementation

---
+++ src/Server/GladMMO.Service.ContentServer/Controllers/CreatureController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	[Route("api/[controller]")]
+	public sealed class CreatureController : BaseCustomControllerController<CreatureModelEntryModel>
+	{
+		public CreatureController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger, UserContentType.Creature)
+		{
+
+		}
+
+		protected override CreatureModelEntryModel GenerateNewModel()
+		{
+			int userId = ClaimsReader.GetUserIdInt(User);
+			return new CreatureModelEntryModel(userId, HttpContext.Connection.RemoteIpAddress.ToString(), Guid.NewGuid());
+		}
+	}
+}
