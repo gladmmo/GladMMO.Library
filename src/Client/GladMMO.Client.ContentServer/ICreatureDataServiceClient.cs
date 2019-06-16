8c8a05052c77430c32dbfdb7963806b561d90949
HelloKitty <andrew.blakely@ymail.com>
2019-06-16 10:58:05 -0500

    Added ICreatureDataServiceClient

---
+++ src/Client/GladMMO.Client.ContentServer/ICreatureDataServiceClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	//TODO: Automate user-agent SDK version headers
+	[Headers("User-Agent: SDK 0.0.1")]
+	public interface ICreatureDataServiceClient
+	{
+		[Get("/api/CreatureData/template/{id}")]
+		Task<CreatureTemplateQueryResponseModel> GetCreatureTemplate([AliasAs("id")] int creatureId);
+	}
+}
