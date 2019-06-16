05fa82fc1f151acf867af304100b3206118e2e59
HelloKitty <andrew.blakely@ymail.com>
2019-06-16 09:13:11 -0500

    Added initial CreatureStaticSpawnPointDefinition

---
+++ src/SDK/GladMMO.Common.SDK/Component/Spawn/CreatureStaticSpawnPointDefinition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class CreatureStaticSpawnPointDefinition : StaticSpawnPointDefinition
+	{
+		public override EntitySpawnType EntityType => EntitySpawnType.Creature;
+
+		[Tooltip("This should be the ID of the creature template that holds information about what creature is suppose to spawn.")]
+		[SerializeField]
+		private int _CreatureTemplateId = -1; //default to -1 so it's not known.
+
+		/// <summary>
+		/// The ID of the creature template to use for spawning.
+		/// </summary>
+		public int CreatureTemplateId => _CreatureTemplateId;
+	}
+}
