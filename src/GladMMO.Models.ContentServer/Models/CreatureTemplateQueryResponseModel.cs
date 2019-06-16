3be4c0bc9cbad6f89363ef85ca47567fb6e1ddae
HelloKitty <andrew.blakely@ymail.com>
2019-06-16 10:29:41 -0500

    Added CreatureTemplateQueryResponseModel

---
+++ src/GladMMO.Models.ContentServer/Models/CreatureTemplateQueryResponseModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class CreatureTemplateQueryResponseModel : IResponseModel<CreatureEntryCollectionResponseCode>, ISucceedable
+	{
+		[JsonProperty]
+		public CreatureEntryCollectionResponseCode ResultCode { get; private set; }
+
+		[JsonIgnore]
+		public bool isSuccessful => ResultCode == CreatureEntryCollectionResponseCode.Success;
+
+		public CreatureTemplateModel CreatureTemplate { get; }
+
+		public CreatureTemplateQueryResponseModel(CreatureEntryCollectionResponseCode resultCode)
+		{
+			//TODO: Check it's not success, this is failure CTOR.
+			if (!Enum.IsDefined(typeof(CreatureEntryCollectionResponseCode), resultCode)) throw new InvalidEnumArgumentException(nameof(resultCode), (int) resultCode, typeof(CreatureEntryCollectionResponseCode));
+			ResultCode = resultCode;
+		}
+
+		public CreatureTemplateQueryResponseModel([NotNull] CreatureTemplateModel creatureTemplate)
+		{
+			CreatureTemplate = creatureTemplate ?? throw new ArgumentNullException(nameof(creatureTemplate));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private CreatureTemplateQueryResponseModel()
+		{
+			
+		}
+	}
+}
---
+++ src/GladMMO.Models.ContentServer/ResultCodes/CreatureTemplateQueryResponseCode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Response code enumeration for the <see cref="CreatureTemplateQueryResponseModel"/>.
+	/// </summary>
+	public enum CreatureTemplateQueryResponseCode
+	{
+		Success = 1,
+
+		/// <summary>
+		/// Indicates that no templates with the specified id were found.
+		/// </summary>
+		NoneFound = 2,
+	}
+}
