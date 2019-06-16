7363804306f1785278553b039c69298b83857b9c
HelloKitty <andrew.blakely@ymail.com>
2019-06-16 10:42:40 -0500

    Added CreatureTemplateTableToNetworkTypeConverter

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/CreatureTemplateTableToNetworkTypeConverter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class CreatureTemplateTableToNetworkTypeConverter : ITypeConverterProvider<CreatureTemplateEntryModel, CreatureTemplateModel>
+	{
+		public CreatureTemplateModel Convert(CreatureTemplateEntryModel fromObject)
+		{
+			return new CreatureTemplateModel(fromObject.ModelId, fromObject.CreatureName, fromObject.MinimumLevel, fromObject.MaximumLevel);
+		}
+	}
+}
