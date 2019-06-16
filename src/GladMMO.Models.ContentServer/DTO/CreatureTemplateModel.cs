54624cd289e2fb3efe260c37287cdd016293fd3a
HelloKitty <andrew.blakely@ymail.com>
2019-06-16 10:50:03 -0500

    Added missing CreatureTemplateModel

---
+++ src/GladMMO.Models.ContentServer/DTO/CreatureTemplateModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class CreatureTemplateModel
+	{
+		/// <summary>
+		/// The ID of the creature's model.
+		/// </summary>
+		[JsonProperty]
+		public long ModelId { get; private set; }
+
+		/// <summary>
+		/// The name of the Creature. Will be the one the client sees, the one the NameQuery will return.
+		/// </summary>
+		[JsonProperty]
+		public string CreatureName { get; private set; }
+
+		//Min/Max level based on TrinityCore.
+		/// <summary>
+		/// The minimum level of the creature if the creature has a level range.
+		/// </summary>
+		[JsonProperty]
+		public int MinimumLevel { get; private set; }
+
+		/// <summary>
+		/// The maximum level of the creature if the creature has a level range. When added to world, a level in chosen in the specified level range.
+		/// </summary>
+		[JsonProperty]
+		public int MaximumLevel { get; private set; }
+
+		public CreatureTemplateModel(long modelId, [NotNull] string creatureName, int minimumLevel, int maximumLevel)
+		{
+			if (modelId <= 0) throw new ArgumentOutOfRangeException(nameof(modelId));
+			if (string.IsNullOrWhiteSpace(creatureName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(creatureName));
+			if (minimumLevel <= 0) throw new ArgumentOutOfRangeException(nameof(minimumLevel));
+			if (maximumLevel <= 0) throw new ArgumentOutOfRangeException(nameof(maximumLevel));
+
+			ModelId = modelId;
+			CreatureName = creatureName;
+			MinimumLevel = minimumLevel;
+			MaximumLevel = maximumLevel;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private CreatureTemplateModel()
+		{
+			
+		}
+	}
+}
