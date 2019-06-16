8d5a7e24de1aa3fa45d28b1861ac3a8e4e16400a
HelloKitty <andrew.blakely@ymail.com>
2019-06-16 10:40:32 -0500

    Extended CreatureTemplate schema

---
+++ src/Server/GladMMO.Database.GameServer/Migrations/WorldDatabase/20190616153636_AddedLevelNameToCreatureTemplate.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GladMMO.Database.GameServer.Migrations.WorldDatabase
+{
+    public partial class AddedLevelNameToCreatureTemplate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatureName",
+                table: "creature_template",
+                nullable: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaximumLevel",
+                table: "creature_template",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MinimumLevel",
+                table: "creature_template",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatureName",
+                table: "creature_template");
+
+            migrationBuilder.DropColumn(
+                name: "MaximumLevel",
+                table: "creature_template");
+
+            migrationBuilder.DropColumn(
+                name: "MinimumLevel",
+                table: "creature_template");
+        }
+    }
+}
--- src/Server/GladMMO.Database.GameServer/Models/Content/Creature/CreatureTemplateEntryModel.cs
+++ src/Server/GladMMO.Database.GameServer/Models/Content/Creature/CreatureTemplateEntryModel.cs
@@ -13,6 +13,7 @@ namespace GladMMO
 		/// <summary>
 		/// The id of the creature template.
 		/// </summary>
+		[Column(Order = 1)]
 		[Key]
 		[Range(0, long.MaxValue)]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -22,6 +23,7 @@ namespace GladMMO
 		/// <summary>
 		/// Key for the account associated with the creation/registeration of this creature template.
 		/// </summary>
+		[Column(Order = 2)]
 		[Required]
 		[Range(0, int.MaxValue)]
 		public int AccountId { get; private set; }
@@ -30,6 +32,7 @@ namespace GladMMO
 		/// The primary unique 64bit integer key used for the
 		/// creature model's unique ID.
 		/// </summary>
+		[Column(Order = 3)]
 		[ForeignKey(nameof(CreatureModelEntry))]
 		[Range(0, long.MaxValue)]
 		public long ModelId { get; private set; }
@@ -37,18 +40,48 @@ namespace GladMMO
 		//Navigation property
 		public virtual CreatureModelEntryModel CreatureModelEntry { get; private set; }
 
-		public CreatureTemplateEntryModel(int accountId, long modelId)
+		/// <summary>
+		/// The name of the Creature. Will be the one the client sees, the one the NameQuery will return.
+		/// </summary>
+		[Column(Order = 4)]
+		[Required]
+		public string CreatureName { get; private set; }
+
+		//Min/Max level based on TrinityCore.
+		/// <summary>
+		/// The minimum level of the creature if the creature has a level range.
+		/// </summary>
+		[Column(Order = 5)]
+		[Required]
+		[Range(1, 255)]
+		public int MinimumLevel { get; private set; }
+
+		/// <summary>
+		/// The maximum level of the creature if the creature has a level range. When added to world, a level in chosen in the specified level range.
+		/// </summary>
+		[Column(Order = 6)]
+		[Required]
+		[Range(1, 255)]
+		public int MaximumLevel { get; private set; }
+
+		public CreatureTemplateEntryModel(int accountId, long modelId, [NotNull] string creatureName, int minimumLevel, int maximumLevel)
 		{
 			if (accountId <= 0) throw new ArgumentOutOfRangeException(nameof(accountId));
 			if (modelId <= 0) throw new ArgumentOutOfRangeException(nameof(modelId));
+			if (string.IsNullOrWhiteSpace(creatureName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(creatureName));
+			if (minimumLevel <= 0) throw new ArgumentOutOfRangeException(nameof(minimumLevel));
+			if (maximumLevel <= 0) throw new ArgumentOutOfRangeException(nameof(maximumLevel));
 
 			AccountId = accountId;
 			ModelId = modelId;
+			CreatureName = creatureName;
+			MinimumLevel = minimumLevel;
+			MaximumLevel = maximumLevel;
 		}
 
-		protected CreatureTemplateEntryModel(long modelId)
+		protected CreatureTemplateEntryModel()
 		{
-			ModelId = modelId;
+
 		}
 	}
 }
