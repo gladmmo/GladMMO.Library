a8a76365ce5ba51fc7ddc9e09a469cfb93e279a9
HelloKitty <andrew.blakely@ymail.com>
2019-06-16 10:04:50 -0500

    Removed NameQuery from CreatureDataController

--- src/Server/GladMMO.Service.ContentServer/Controllers/CreatureDataController.cs
+++ src/Server/GladMMO.Service.ContentServer/Controllers/CreatureDataController.cs
@@ -50,34 +50,5 @@ namespace GladMMO
 			//TODO: Create a Vector3 converter
 			return new CreatureInstanceModel(guid, creature.CreatureTemplateId, new Vector3(creature.SpawnPosition.X, creature.SpawnPosition.Y, creature.SpawnPosition.Z));
 		}
-
-		//TODO: Conslidate/centralize name query stuff via entity GUID.
-		[AllowAnonymous]
-		[ProducesJson]
-		[ResponseCache(Duration = int.MaxValue)] //NPC names can be cached forever.
-		[HttpGet("name/{id}")]
-		public async Task<IActionResult> NameQuery([FromRoute(Name = "id")] int npcId, [FromServices] ICreatureTemplateRepository templateRepository)
-		{
-			if(templateRepository == null) throw new ArgumentNullException(nameof(templateRepository));
-
-			if(npcId < 0)
-				return BuildNotFoundUnknownIdResponse();
-
-			bool knownId = await templateRepository.ContainsAsync(npcId);
-
-			//TODO: JSON Response
-			if(!knownId)
-				return BuildNotFoundUnknownIdResponse();
-
-			//Else if it is a known id we should grab the name of the character
-			string name = await templateRepository.RetrieveNameAsync(npcId);
-
-			return Ok(new NameQueryResponse(name));
-		}
-
-		private IActionResult BuildNotFoundUnknownIdResponse()
-		{
-			return NotFound(new NameQueryResponse(NameQueryResponseCode.UnknownIdError));
-		}
 	}
 }
