6b29ddbb789ae1606f6909ecc84d490ef403a12d
HelloKitty <andrew.blakely@ymail.com>
2019-06-16 12:49:52 -0500

    Added CreatureInstanceTableToNetworkTypeConverter

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/CreatureInstanceTableToNetworkTypeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class CreatureInstanceTableToNetworkTypeConverter : ITypeConverterProvider<CreatureEntryModel, CreatureInstanceModel>
+	{
+		public CreatureInstanceModel Convert([NotNull] CreatureEntryModel fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			NetworkEntityGuid guid = new NetworkEntityGuidBuilder()
+				.WithId(fromObject.CreatureEntryId)
+				.WithType(EntityType.Npc)
+				.WithTemplate(fromObject.CreatureTemplateId)
+				.Build();
+
+			//TODO: better handle position crap
+			return new CreatureInstanceModel(guid, fromObject.CreatureTemplateId, new Vector3(fromObject.SpawnPosition.X, fromObject.SpawnPosition.Y, fromObject.SpawnPosition.Z), fromObject.InitialOrientation);
+		}
+	}
+}
