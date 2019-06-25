eeef10cda3aabc78d278cd23c7ac0d6824aa2162
HelloKitty <andrew.blakely@ymail.com>
2019-06-25 17:56:09 -0500

    Added debug testing component for button attribute

---
+++ src/SDK/GladMMO.Common.SDK/Component/TestSDKComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class TestSDKComponent : GladMMOSDKMonoBehaviour
+	{
+		[Button]
+		public void TestLogging()
+		{
+			Debug.Log($"Button pressed.");
+		}
+	}
+}
