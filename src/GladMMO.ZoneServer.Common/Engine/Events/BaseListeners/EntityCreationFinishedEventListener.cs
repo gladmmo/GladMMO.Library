8b1f7c1c192d1343997b7813db84349d4273cee0
HelloKitty <andrew.blakely@ymail.com>
2019-06-26 17:32:23 -0500

    Added simplified EntityCreationFinishedEventListener

---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/BaseListeners/EntityCreationFinishedEventListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class EntityCreationFinishedEventListener : BaseSingleEventListenerInitializable<IEntityCreationFinishedEventSubscribable, EntityCreationFinishedEventArgs>
+	{
+		protected EntityCreationFinishedEventListener(IEntityCreationFinishedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
