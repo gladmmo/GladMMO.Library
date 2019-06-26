e8972fd09e725d3047c4d2d4098cc8d11d5749d5
HelloKitty <andrew.blakely@ymail.com>
2019-06-25 19:35:35 -0500

    Added IEntityCreationFinishedEventSubscribable

---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/IEntityCreationFinishedEventSubscribable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IEntityCreationFinishedEventSubscribable
+	{
+		event EventHandler<EntityCreationFinishedEventArgs> OnEntityCreationFinished;
+	}
+
+	public sealed class EntityCreationFinishedEventArgs : EventArgs, IEntityGuidContainer
+	{
+		/// <summary>
+		/// The entity guid of the creating entity.
+		/// </summary>
+		public NetworkEntityGuid EntityGuid { get; }
+
+		/// <inheritdoc />
+		public EntityCreationFinishedEventArgs([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
