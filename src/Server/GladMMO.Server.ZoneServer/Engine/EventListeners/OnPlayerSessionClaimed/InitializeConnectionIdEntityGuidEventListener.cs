37b79ad190a5938066ef4078c94065d506b28ffe
HelloKitty <andrew.blakely@ymail.com>
2019-06-26 22:54:30 -0500

    Added InitializeConnectionIdEntityGuidEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnPlayerSessionClaimed/InitializeConnectionIdEntityGuidEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class InitializeConnectionIdEntityGuidEventListener : BaseSingleEventListenerInitializable<IPlayerSessionClaimedEventSubscribable, PlayerSessionClaimedEventArgs>
+	{
+		private IConnectionEntityCollection ConnectionIdToControllingEntityMap { get; }
+
+		public InitializeConnectionIdEntityGuidEventListener(IPlayerSessionClaimedEventSubscribable subscriptionService,
+			[NotNull] IConnectionEntityCollection connectionIdToControllingEntityMap) 
+			: base(subscriptionService)
+		{
+			ConnectionIdToControllingEntityMap = connectionIdToControllingEntityMap ?? throw new ArgumentNullException(nameof(connectionIdToControllingEntityMap));
+		}
+
+		protected override void OnEventFired(object source, PlayerSessionClaimedEventArgs args)
+		{
+			ConnectionIdToControllingEntityMap.Add(args.SessionContext.ConnectionId, args.EntityGuid);
+		}
+	}
+}
