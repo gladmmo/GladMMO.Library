aa155d1c6697a5de955e5a844c98e72310750454
HelloKitty <andrew.blakely@ymail.com>
2019-06-26 21:42:53 -0500

    Added InitializeCharacterControllerMappableEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnEntityWorldRepresentationCreated/ServerInitializeCharacterControllerMappableEventListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public class ServerInitializeCharacterControllerMappableEventListener : SharedInitializeCharacterControllerMappableEventListener
+	{
+		public ServerInitializeCharacterControllerMappableEventListener(IEntityWorldRepresentationCreatedEventSubscribable subscriptionService, IEntityGuidMappable<CharacterController> characterControllerMappable) 
+			: base(subscriptionService, characterControllerMappable)
+		{
+
+		}
+	}
+}
