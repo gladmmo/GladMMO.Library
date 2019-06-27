fbfea43f9238df1bc36e1d849c488c24750a87c2
HelloKitty <andrew.blakely@ymail.com>
2019-06-26 23:07:16 -0500

    Added ServerCreatingInitializeDefaultMovementGeneratorEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnEntityCreationStarting/ServerCreatingInitializeDefaultMovementGeneratorEventListener.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class ServerCreatingInitializeDefaultMovementGeneratorEventListener : SharedCreatingInitializeDefaultMovementGeneratorEventListener
+	{
+		public ServerCreatingInitializeDefaultMovementGeneratorEventListener(IEntityCreationStartingEventSubscribable subscriptionService, IEntityGuidMappable<IMovementGenerator<GameObject>> movementGeneratorMappable, IFactoryCreatable<IMovementGenerator<GameObject>, EntityAssociatedData<IMovementData>> movementGeneratorFactory, IReadonlyEntityGuidMappable<IMovementData> movementDataMappable) : base(subscriptionService, movementGeneratorMappable, movementGeneratorFactory, movementDataMappable)
+		{
+
+		}
+	}
+}
