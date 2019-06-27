ab9acce7efc05362341e4945d4818713c2538793
HelloKitty <andrew.blakely@ymail.com>
2019-06-26 22:46:18 -0500

    Added InitializeInterestCollectionEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnEntityCreationStarting/InitializeInterestCollectionEventListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class InitializeInterestCollectionEventListener : BaseSingleEventListenerInitializable<IEntityCreationStartingEventSubscribable, EntityCreationStartingEventArgs>
+	{
+		private IEntityGuidMappable<InterestCollection> GuidToInterestCollectionMappable { get; }
+
+		public InitializeInterestCollectionEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<InterestCollection> guidToInterestCollectionMappable) 
+			: base(subscriptionService)
+		{
+			GuidToInterestCollectionMappable = guidToInterestCollectionMappable ?? throw new ArgumentNullException(nameof(guidToInterestCollectionMappable));
+		}
+
+		protected override void OnEventFired(object source, EntityCreationStartingEventArgs args)
+		{
+			InterestCollection playerInterestCollection = new InterestCollection();
+
+			//directly add ourselves so we don't become interest in ourselves after spawning
+			playerInterestCollection.Add(args.EntityGuid);
+
+			//We just create our own manaul interest collection here.
+			GuidToInterestCollectionMappable.AddObject(args.EntityGuid, playerInterestCollection);
+		}
+	}
+}
