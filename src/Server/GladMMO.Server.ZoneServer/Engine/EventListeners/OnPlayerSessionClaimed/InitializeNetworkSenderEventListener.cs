90278a8e5a1817e6b1cbe0a3c158dec2e897bee1
HelloKitty <andrew.blakely@ymail.com>
2019-06-26 23:04:47 -0500

    Added InitializeNetworkSenderEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnPlayerSessionClaimed/InitializeNetworkSenderEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using GladNet;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class InitializeNetworkSenderEventListener : BaseSingleEventListenerInitializable<IPlayerSessionClaimedEventSubscribable, PlayerSessionClaimedEventArgs>
+	{
+		private IEntityGuidMappable<IPeerPayloadSendService<GameServerPacketPayload>> GuidToSessionMappable { get; }
+
+		public InitializeNetworkSenderEventListener(IPlayerSessionClaimedEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<IPeerPayloadSendService<GameServerPacketPayload>> guidToSessionMappable) : base(subscriptionService)
+		{
+			GuidToSessionMappable = guidToSessionMappable ?? throw new ArgumentNullException(nameof(guidToSessionMappable));
+		}
+
+		protected override void OnEventFired(object source, PlayerSessionClaimedEventArgs args)
+		{
+			GuidToSessionMappable.AddObject(args.EntityGuid, args.SessionContext.ZoneSession);
+		}
+	}
+}
