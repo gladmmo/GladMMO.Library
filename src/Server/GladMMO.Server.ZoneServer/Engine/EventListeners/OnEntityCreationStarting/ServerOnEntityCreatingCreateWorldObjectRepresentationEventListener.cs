bf40ad16256544cb95649aaf0fd11f24b169ee3d
HelloKitty <andrew.blakely@ymail.com>
2019-06-26 20:39:27 -0500

    Added a serverside CreateWorldObjectRepresentationEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnEntityCreationStarting/ServerOnEntityCreatingCreateWorldObjectRepresentationEventListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public class ServerOnEntityCreatingCreateWorldObjectRepresentationEventListener : SharedOnEntityCreatingCreateWorldObjectRepresentationEventListener
+	{
+		public ServerOnEntityCreatingCreateWorldObjectRepresentationEventListener(IEntityCreationStartingEventSubscribable subscriptionService, 
+			IFactoryCreatable<GameObject, EntityPrefab> prefabFactory, 
+			IReadonlyEntityGuidMappable<IMovementData> movementDataMappable) 
+			: base(subscriptionService, prefabFactory, movementDataMappable)
+		{
+		}
+
+		protected override EntityPrefab ComputePrefabType([NotNull] NetworkEntityGuid entityGuid)
+		{
+			if (entityGuid == null) throw new ArgumentNullException(nameof(entityGuid));
+
+			switch (entityGuid.EntityType)
+			{
+				case EntityType.None:
+					break;
+				case EntityType.Player:
+					return EntityPrefab.RemotePlayer;
+				case EntityType.GameObject:
+					break;
+				case EntityType.Npc:
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+
+			throw new InvalidOperationException($"Failed to compute {nameof(EntityPrefab)} from Guid: {entityGuid}");
+		}
+	}
+}
