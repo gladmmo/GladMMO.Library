7b7271f177bf7e4bf1d83fdbc65a1b560e0d3a33
HelloKitty <andrew.blakely@ymail.com>
2019-06-27 03:18:33 -0500

    Created a sharable EntityCreationRequestEventPublisher

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventPublishers/EntityCreationRequestEventPublisher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(IEventPublisher<IEntityCreationRequestedEventSubscribable, EntityCreationRequestedEventArgs>))]
+	[AdditionalRegisterationAs(typeof(IEntityCreationRequestedEventSubscribable))]
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class EntityCreationRequestEventPublisher : IEventPublisher<IEntityCreationRequestedEventSubscribable, EntityCreationRequestedEventArgs>, IEntityCreationRequestedEventSubscribable, IGameInitializable
+	{
+		public event EventHandler<EntityCreationRequestedEventArgs> OnEntityCreationRequested;
+
+		public void PublishEvent([NotNull] object sender, [NotNull] EntityCreationRequestedEventArgs eventArgs)
+		{
+			if (sender == null) throw new ArgumentNullException(nameof(sender));
+			if (eventArgs == null) throw new ArgumentNullException(nameof(eventArgs));
+
+			OnEntityCreationRequested?.Invoke(sender, eventArgs);
+		}
+
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
