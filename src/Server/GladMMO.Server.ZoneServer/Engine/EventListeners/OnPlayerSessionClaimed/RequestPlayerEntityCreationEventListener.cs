25295b3bfc12b5472a0458c52c6c49ae383cca0f
HelloKitty <andrew.blakely@ymail.com>
2019-06-27 03:45:09 -0500

    Added EntityCreationRequest publishing on PlayerSessionClaim

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnPlayerSessionClaimed/RequestPlayerEntityCreationEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class RequestPlayerEntityCreationEventListener : BaseSingleEventListenerInitializable<IPlayerSessionClaimedEventSubscribable, PlayerSessionClaimedEventArgs>
+	{
+		private IEventPublisher<IEntityCreationRequestedEventSubscribable, EntityCreationRequestedEventArgs> EntityCreationRequestPublisher { get; }
+
+		public RequestPlayerEntityCreationEventListener(IPlayerSessionClaimedEventSubscribable subscriptionService,
+			[NotNull] IEventPublisher<IEntityCreationRequestedEventSubscribable, EntityCreationRequestedEventArgs> entityCreationRequestPublisher) 
+			: base(subscriptionService)
+		{
+			EntityCreationRequestPublisher = entityCreationRequestPublisher ?? throw new ArgumentNullException(nameof(entityCreationRequestPublisher));
+		}
+
+		protected override void OnEventFired(object source, PlayerSessionClaimedEventArgs args)
+		{
+			//When we encounter a claimed player session
+			//we should publish a request to create an entity with the requested guid.
+			EntityCreationRequestPublisher.PublishEvent(source, new EntityCreationRequestedEventArgs(args.EntityGuid));
+		}
+	}
+}
