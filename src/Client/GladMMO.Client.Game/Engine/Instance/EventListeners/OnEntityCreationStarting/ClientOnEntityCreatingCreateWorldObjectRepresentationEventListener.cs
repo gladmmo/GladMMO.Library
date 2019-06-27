1e281be42721fa386265ffd47950d18593f5fc29
HelloKitty <andrew.blakely@ymail.com>
2019-06-26 20:03:40 -0500

    Refactored CreateWorldObjectRepresentationEventListener to common library

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnEntityCreationStarting/ClientOnEntityCreatingCreateWorldObjectRepresentationEventListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fasterflect;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	//Conceptually this is like a partial factory
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ClientOnEntityCreatingCreateWorldObjectRepresentationEventListener : SharedOnEntityCreatingCreateWorldObjectRepresentationEventListener, IEntityWorldRepresentationCreatedEventSubscribable
+	{
+		private ICharacterDataRepository CharacterDataRepository { get; }
+
+		public ClientOnEntityCreatingCreateWorldObjectRepresentationEventListener(IEntityCreationStartingEventSubscribable subscriptionService, 
+			IFactoryCreatable<GameObject, EntityPrefab> prefabFactory, 
+			IReadonlyEntityGuidMappable<IMovementData> movementDataMappable, 
+			ICharacterDataRepository characterDataRepository) 
+			: base(subscriptionService, prefabFactory, movementDataMappable)
+		{
+			CharacterDataRepository = characterDataRepository;
+		}
+
+		protected override EntityPrefab ComputePrefabType(NetworkEntityGuid entityGuid)
+		{
+			if (entityGuid.EntityType == EntityType.Player)
+			{
+				//It could be remote player or local player.
+				if (entityGuid.EntityId == CharacterDataRepository.CharacterId)
+					return EntityPrefab.LocalPlayer;
+				else
+					return EntityPrefab.RemotePlayer;
+			}
+
+			//TODO: Handle other cases.
+			throw new NotSupportedException($"TODO: Implement support for: {entityGuid} creation.");
+		}
+	}
+}
--- src/GladMMO.ZoneServer.Common/Engine/Instance/OnEntityCreationStarting/OnEntityCreatingCreateWorldObjectRepresentationEventListener.cs
+++ src/GladMMO.ZoneServer.Common/Engine/Instance/OnEntityCreationStarting/OnEntityCreatingCreateWorldObjectRepresentationEventListener.cs
@@ -9,25 +9,20 @@ namespace GladMMO
 {
 	//Conceptually this is like a partial factory
 	[AdditionalRegisterationAs(typeof(IEntityWorldRepresentationCreatedEventSubscribable))]
-	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
-	public sealed class OnEntityCreatingCreateWorldObjectRepresentationEventListener : BaseSingleEventListenerInitializable<IEntityCreationStartingEventSubscribable, EntityCreationStartingEventArgs>, IEntityWorldRepresentationCreatedEventSubscribable
+	public abstract class SharedOnEntityCreatingCreateWorldObjectRepresentationEventListener : BaseSingleEventListenerInitializable<IEntityCreationStartingEventSubscribable, EntityCreationStartingEventArgs>, IEntityWorldRepresentationCreatedEventSubscribable
 	{
 		public event EventHandler<EntityWorldRepresentationCreatedEventArgs> OnEntityWorldRepresentationCreated;
 
 		private IFactoryCreatable<GameObject, EntityPrefab> PrefabFactory { get; }
 
-		private ICharacterDataRepository CharacterDataRepository { get; }
-
 		private IReadonlyEntityGuidMappable<IMovementData> MovementDataMappable { get; }
 
-		public OnEntityCreatingCreateWorldObjectRepresentationEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
+		protected SharedOnEntityCreatingCreateWorldObjectRepresentationEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
 			[NotNull] IFactoryCreatable<GameObject, EntityPrefab> prefabFactory,
-			[NotNull] ICharacterDataRepository characterDataRepository,
 			[NotNull] IReadonlyEntityGuidMappable<IMovementData> movementDataMappable)
 			: base(subscriptionService)
 		{
 			PrefabFactory = prefabFactory ?? throw new ArgumentNullException(nameof(prefabFactory));
-			CharacterDataRepository = characterDataRepository ?? throw new ArgumentNullException(nameof(characterDataRepository));
 			MovementDataMappable = movementDataMappable ?? throw new ArgumentNullException(nameof(movementDataMappable));
 		}
 
@@ -43,19 +38,6 @@ namespace GladMMO
 			OnEntityWorldRepresentationCreated?.Invoke(this, new EntityWorldRepresentationCreatedEventArgs(args.EntityGuid, entityGameObject));
 		}
 
-		private EntityPrefab ComputePrefabType(NetworkEntityGuid entityGuid)
-		{
-			if (entityGuid.EntityType == EntityType.Player)
-			{
-				//It could be remote player or local player.
-				if (entityGuid.EntityId == CharacterDataRepository.CharacterId)
-					return EntityPrefab.LocalPlayer;
-				else
-					return EntityPrefab.RemotePlayer;
-			}
-
-			//TODO: Handle other cases.
-			throw new NotSupportedException($"TODO: Implement support for: {entityGuid} creation.");
-		}
+		protected abstract EntityPrefab ComputePrefabType(NetworkEntityGuid entityGuid);
 	}
 }
