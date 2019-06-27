8ebd58cd4d9c6e2f330d212ddde08ecdc9566d86
HelloKitty <andrew.blakely@ymail.com>
2019-06-26 22:17:53 -0500

    Added InitializeEntityAsyncLockEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnEntityCreationStarting/InitializeEntityAsyncLockEventListener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using Nito.AsyncEx;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class InitializeEntityAsyncLockEventListener : BaseSingleEventListenerInitializable<IEntityCreationStartingEventSubscribable, EntityCreationStartingEventArgs>
+	{
+		private IEntityGuidMappable<AsyncReaderWriterLock> AsyncLockMappable { get; }
+
+		public InitializeEntityAsyncLockEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<AsyncReaderWriterLock> asyncLockMappable) 
+			: base(subscriptionService)
+		{
+			AsyncLockMappable = asyncLockMappable ?? throw new ArgumentNullException(nameof(asyncLockMappable));
+		}
+
+		protected override void OnEventFired(object source, EntityCreationStartingEventArgs args)
+		{
+			AsyncLockMappable.AddObject(args.EntityGuid, new AsyncReaderWriterLock());
+		}
+	}
+}
