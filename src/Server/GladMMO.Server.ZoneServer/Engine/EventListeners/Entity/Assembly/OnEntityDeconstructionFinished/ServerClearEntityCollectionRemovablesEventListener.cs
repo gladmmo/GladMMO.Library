40119d60588261785914dd70c9fb10b8981c24d1
HelloKitty <andrew.blakely@ymail.com>
2019-06-28 07:08:13 -0500

    Added ServerClearEntityCollectionRemovablesEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/Assembly/OnEntityDeconstructionFinished/ServerClearEntityCollectionRemovablesEventListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class ServerClearEntityCollectionRemovablesEventListener : SharedClearEntityCollectionRemovablesEventListener
+	{
+		public ServerClearEntityCollectionRemovablesEventListener(IEntityWorldRepresentationDeconstructionFinishedEventSubscribable subscriptionService, 
+			IReadOnlyCollection<IEntityCollectionRemovable> removableCollections) 
+			: base(subscriptionService, removableCollections)
+		{
+
+		}
+	}
+}
