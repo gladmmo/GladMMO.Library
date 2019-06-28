2e52f6ebba92b11805c471a7bc9f58827d3bfde3
HelloKitty <andrew.blakely@ymail.com>
2019-06-27 23:30:30 -0500

    Added EntityDeconstruction event interfaces

---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/Entity/Deconstruction/IEntityDeconstructionFinishedEventSubscribable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IEntityDeconstructionFinishedEventSubscribable
+	{
+		event EventHandler<EntityCreationFinishedEventArgs> OnEntityDeconstructionFinished;
+	}
+
+	public sealed class EntityDeconstructionFinishedEventArgs : EventArgs, IEntityGuidContainer
+	{
+		/// <summary>
+		/// The entity guid of the deconstructing entity.
+		/// </summary>
+		public NetworkEntityGuid EntityGuid { get; }
+
+		/// <inheritdoc />
+		public EntityDeconstructionFinishedEventArgs([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/Entity/Deconstruction/IEntityDeconstructionStartingEventSubscribable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Event that should be subscribed to BEFORE the "destruction"
+	/// of the entity. Subscribers can deal with disassembly
+	/// logic and such.
+	/// </summary>
+	public interface IEntityDeconstructionStartingEventSubscribable
+	{
+		event EventHandler<EntityCreationStartingEventArgs> OnEntityDeconstructionStarting;
+	}
+
+	public sealed class EntityDeconstructionStartingEventArgs : EventArgs, IEntityGuidContainer
+	{
+		/// <summary>
+		/// The entity guid of the deconstructing entity.
+		/// </summary>
+		public NetworkEntityGuid EntityGuid { get; }
+
+		/// <inheritdoc />
+		public EntityDeconstructionStartingEventArgs([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
