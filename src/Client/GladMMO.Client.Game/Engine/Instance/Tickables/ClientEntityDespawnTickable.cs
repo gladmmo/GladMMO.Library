4a51769661d128fe85cbdda2e0303395265004bd
HelloKitty <andrew.blakely@ymail.com>
2019-06-28 04:37:13 -0500

    Replaced client EntityDespawnTickable with ClientEntityDespawnTickable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Tickables/ClientEntityDespawnTickable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ClientEntityDespawnTickable : SharedEntityDespawnTickable<INetworkEntityVisibilityLostEventSubscribable, NetworkEntityVisibilityLostEventArgs>
+	{
+		public ClientEntityDespawnTickable(INetworkEntityVisibilityLostEventSubscribable subscriptionService, 
+			ILog logger, 
+			IKnownEntitySet knownEntities) 
+			: base(subscriptionService, logger, knownEntities)
+		{
+		}
+	}
+}
