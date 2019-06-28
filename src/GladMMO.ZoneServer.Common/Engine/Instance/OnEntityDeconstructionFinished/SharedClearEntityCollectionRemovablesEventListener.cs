695b2c282ab10651e160b2c444a1a5ec2d625181
HelloKitty <andrew.blakely@ymail.com>
2019-06-28 04:58:32 -0500

    Added SharedClearEntityCollectionRemovablesEventListener

---
+++ src/GladMMO.ZoneServer.Common/Engine/Instance/OnEntityDeconstructionFinished/SharedClearEntityCollectionRemovablesEventListener.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Base type for entity collection removable cleanup listener.
+	/// </summary>
+	public sealed class SharedClearEntityCollectionRemovablesEventListener : BaseSingleEventListenerInitializable<IEntityWorldRepresentationDeconstructionFinishedEventSubscribable, EntityWorldRepresentationDeconstructionFinishedEventArgs>
+	{
+		private IReadOnlyCollection<IEntityCollectionRemovable> RemovableCollections { get; }
+
+		public SharedClearEntityCollectionRemovablesEventListener(IEntityWorldRepresentationDeconstructionFinishedEventSubscribable subscriptionService,
+			[NotNull] IReadOnlyCollection<IEntityCollectionRemovable> removableCollections) 
+			: base(subscriptionService)
+		{
+			RemovableCollections = removableCollections ?? throw new ArgumentNullException(nameof(removableCollections));
+		}
+
+		protected override void OnEventFired(object source, EntityWorldRepresentationDeconstructionFinishedEventArgs args)
+		{
+			//When the entity deconstruction is finally finished, we're free to cleanup/freeup the entity
+			//data within the collections.
+			foreach(var removable in RemovableCollections)
+				removable.RemoveEntityEntry(args.EntityGuid);
+		}
+	}
+}
