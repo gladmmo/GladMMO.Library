a7038737ae66658932eb698627735506d2d9f342
HelloKitty <andrew.blakely@ymail.com>
2019-06-28 01:06:23 -0500

    Added IEntityWorldRepresentationDeconstructionFinishedEventSubscribable

---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/Entity/Deconstruction/IEntityWorldRepresentationDeconstructionFinishedEventSubscribable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for event that is published AFTER the world representation of an Entity
+	/// is deconstructed. Meaning, it no longer exists after this event is called.
+	/// Ex. Unity3D GameObject has been destroyed.
+	/// </summary>
+	public interface IEntityWorldRepresentationDeconstructionFinishedEventSubscribable
+	{
+		event EventHandler<EntityWorldRepresentationDeconstructionStartingEventArgs> OnEntityWorldRepresentationDeconstructionFinished;
+	}
+
+	public sealed class EntityWorldRepresentationDeconstructionFinishedEventArgs : EventArgs, IEntityGuidContainer
+	{
+		/// <summary>
+		/// The entity guid of the deconstructing entity.
+		/// </summary>
+		public NetworkEntityGuid EntityGuid { get; }
+
+		/// <inheritdoc />
+		public EntityWorldRepresentationDeconstructionFinishedEventArgs([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
