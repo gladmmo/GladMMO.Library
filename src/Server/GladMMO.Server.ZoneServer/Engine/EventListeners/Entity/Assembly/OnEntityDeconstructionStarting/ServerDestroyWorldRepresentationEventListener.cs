4e41e990df1728e4a288d62a0adb5322edf1ed65
HelloKitty <andrew.blakely@ymail.com>
2019-06-28 07:07:57 -0500

    Added ServerDestroyWorldRepresentationEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/Assembly/OnEntityDeconstructionStarting/ServerDestroyWorldRepresentationEventListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class ServerDestroyWorldRepresentationEventListener : SharedDestroyWorldRepresentationEventListener
+	{
+		public ServerDestroyWorldRepresentationEventListener(IEntityDeconstructionStartingEventSubscribable subscriptionService, IReadonlyEntityGuidMappable<GameObject> guidToGameObjectMappable, IGameObjectToEntityMappable gameObjectToEntityMap) 
+			: base(subscriptionService, guidToGameObjectMappable, gameObjectToEntityMap)
+		{
+
+		}
+	}
+}
