d1c3e2a87b125fc4c620b733b6d031d2f357ddb0
HelloKitty <andrew.blakely@ymail.com>
2019-06-28 07:08:36 -0500

    Added ServerEntityDespawnTickable

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnEntityDeconstructionRequested/ServerEntityDespawnTickable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class ServerEntityDespawnTickable : SharedEntityDespawnTickable<IEntityDeconstructionRequestedEventSubscribable, EntityDeconstructionRequestedEventArgs>
+	{
+		public ServerEntityDespawnTickable(IEntityDeconstructionRequestedEventSubscribable subscriptionService, 
+			ILog logger, 
+			IKnownEntitySet knownEntities) 
+			: base(subscriptionService, logger, knownEntities)
+		{
+		}
+	}
+}
