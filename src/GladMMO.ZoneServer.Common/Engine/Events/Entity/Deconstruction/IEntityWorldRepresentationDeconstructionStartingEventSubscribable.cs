2353976c844f6282b05ee648ddcef7558ce4cd38
HelloKitty <andrew.blakely@ymail.com>
2019-06-28 01:06:15 -0500

    Added IEntityWorldRepresentationDeconstructionStartingEventSubscribable

---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/Entity/Deconstruction/IEntityWorldRepresentationDeconstructionStartingEventSubscribable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for event that is published BEFORE the world representation of an Entity
+	/// is deconstructed. Meaning, it still exists at the time of the calling the event.
+	/// </summary>
+	public interface IEntityWorldRepresentationDeconstructionStartingEventSubscribable
+	{
+		event EventHandler<EntityWorldRepresentationDeconstructionStartingEventArgs> OnEntityWorldRepresentationDeconstructionStarting;
+	}
+
+	public sealed class EntityWorldRepresentationDeconstructionStartingEventArgs : EventArgs, IEntityGuidContainer
+	{
+		/// <summary>
+		/// The entity guid of the deconstructing entity.
+		/// </summary>
+		public NetworkEntityGuid EntityGuid { get; }
+
+		/// <inheritdoc />
+		public EntityWorldRepresentationDeconstructionStartingEventArgs([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
