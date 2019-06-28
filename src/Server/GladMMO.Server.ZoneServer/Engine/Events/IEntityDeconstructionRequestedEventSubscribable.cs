ffb8a79193207935bae896f889016e4769b49db3
HelloKitty <andrew.blakely@ymail.com>
2019-06-28 07:07:13 -0500

    Added server IEntityDeconstructionRequestedEventSubscribable

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/Events/IEntityDeconstructionRequestedEventSubscribable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IEntityDeconstructionRequestedEventSubscribable
+	{
+		event EventHandler<EntityDeconstructionRequestedEventArgs> OnEntityDeconstructionRequested;
+	}
+
+	public sealed class EntityDeconstructionRequestedEventArgs : EventArgs, IEntityGuidContainer
+	{
+		public NetworkEntityGuid EntityGuid { get; }
+
+		public EntityDeconstructionRequestedEventArgs([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
