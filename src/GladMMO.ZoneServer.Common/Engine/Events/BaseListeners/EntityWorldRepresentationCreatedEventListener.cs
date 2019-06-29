6f2aad19527f45ac7ae7acd4f6fe690575772c9a
HelloKitty <andrew.blakely@ymail.com>
2019-06-29 16:54:16 -0500

    Added simple EntityWorldRepresentationCreatedEventListener event listener Type

---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/BaseListeners/EntityWorldRepresentationCreatedEventListener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Simplified base-type for single event listeners that listen to: <see cref="IEntityWorldRepresentationCreatedEventSubscribable"/>.
+	/// </summary>
+	public abstract class EntityWorldRepresentationCreatedEventListener : BaseSingleEventListenerInitializable<IEntityWorldRepresentationCreatedEventSubscribable, EntityWorldRepresentationCreatedEventArgs>
+	{
+		protected EntityWorldRepresentationCreatedEventListener(IEntityWorldRepresentationCreatedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+
+		protected sealed override void OnEventFired(object source, EntityWorldRepresentationCreatedEventArgs args)
+		{
+			OnEntityWorldRepresentationCreated(args);
+		}
+
+		protected abstract void OnEntityWorldRepresentationCreated(EntityWorldRepresentationCreatedEventArgs args);
+	}
+}
