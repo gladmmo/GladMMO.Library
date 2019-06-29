30e09ca9366c0deefedd486a1bca3f8a7a38bbf6
HelloKitty <andrew.blakely@ymail.com>
2019-06-28 19:16:55 -0500

    Change InterestCleanup to DeconstructionFinished

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/Assembly/OnEntityDeconstructionFinished/CleanupInterestCollectionsEventListener.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class CleanupInterestCollectionsEventListener : BaseSingleEventListenerInitializable<IEntityDeconstructionFinishedEventSubscribable, EntityDeconstructionFinishedEventArgs>
+	{
+		private IReadonlyEntityGuidMappable<InterestCollection> InterestCollections { get; }
+
+		private IEntityInterestChangeEventSpoofable InterestEventSpoofer { get; }
+
+		private IReadonlyKnownEntitySet KnownEntities { get; }
+
+		public CleanupInterestCollectionsEventListener(IEntityDeconstructionFinishedEventSubscribable subscriptionService, 
+			IReadonlyEntityGuidMappable<InterestCollection> interestCollections, 
+			IEntityInterestChangeEventSpoofable interestEventSpoofer,
+			[NotNull] IReadonlyKnownEntitySet knownEntities) 
+			: base(subscriptionService)
+		{
+			InterestCollections = interestCollections;
+			InterestEventSpoofer = interestEventSpoofer;
+			KnownEntities = knownEntities ?? throw new ArgumentNullException(nameof(knownEntities));
+		}
+
+		protected override void OnEventFired(object source, EntityDeconstructionFinishedEventArgs args)
+		{
+			//At this point, the entity deconstruction is FINISHED so we can actually tell all known entites to forget it.
+			using (KnownEntities.LockObject.ReaderLock())
+			{
+				foreach(NetworkEntityGuid entity in KnownEntities)
+				{
+					InterestCollection ic = InterestCollections.RetrieveEntity(entity);
+
+					if(ic.Contains(args.EntityGuid))
+					{
+						//We just spoof an exit to every interested collection who knows of the entity being cleaned up.
+						InterestEventSpoofer.SpoofExitInterest(new EntityInterestChangeEventArgs(entity, args.EntityGuid, EntityInterestChangeEventArgs.ChangeType.Exit));
+					}
+				}
+			}
+		}
+	}
+}
