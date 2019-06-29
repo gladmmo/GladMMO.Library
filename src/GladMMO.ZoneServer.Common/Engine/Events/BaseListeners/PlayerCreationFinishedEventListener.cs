24a134d1dc0eed0f100fcb321d2de04d53ce97e7
HelloKitty <andrew.blakely@ymail.com>
2019-06-29 16:45:02 -0500

    Added PlayerCreationFinishedEventListener

---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/BaseListeners/PlayerCreationFinishedEventListener.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Base event listener type for <see cref="EntityCreationFinishedEventListener"/> who are
+	/// only listening for Player entity creation finishing.
+	/// </summary>
+	public abstract class PlayerCreationFinishedEventListener : EntityCreationFinishedEventListener
+	{
+		protected PlayerCreationFinishedEventListener(IEntityCreationFinishedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+
+		protected sealed override void OnEventFired(object source, EntityCreationFinishedEventArgs args)
+		{
+			if (args.EntityGuid.EntityType != EntityType.Player)
+				return;
+
+			OnPlayerEntityCreationFinished(args);
+		}
+
+		protected abstract void OnPlayerEntityCreationFinished(EntityCreationFinishedEventArgs args);
+	}
+}
