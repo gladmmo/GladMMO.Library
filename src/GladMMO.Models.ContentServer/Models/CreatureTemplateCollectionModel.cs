1c3b6a172ee83ff854a74b894b79b4340e6af564
HelloKitty <andrew.blakely@ymail.com>
2019-06-30 17:32:23 -0500

    Added CreatureTemplateCollectionModel

---
+++ src/GladMMO.Models.ContentServer/Models/CreatureTemplateCollectionModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class CreatureTemplateCollectionModel
+	{
+		[JsonProperty(PropertyName = "Templates")]
+		private CreatureTemplateModel[] _Templates { get; set; }
+
+		[JsonIgnore]
+		public IReadOnlyCollection<CreatureTemplateModel> Templates => _Templates;
+
+		/// <summary>
+		/// Creatures a Successful <see cref="CreatureTemplateCollectionModel"/>
+		/// </summary>
+		/// <param name="entries">The entries to send.</param>
+		public CreatureTemplateCollectionModel([NotNull] CreatureTemplateModel[] templates)
+		{
+			if(templates == null) throw new ArgumentNullException(nameof(templates));
+			if(templates.Length == 0) throw new ArgumentException("Value cannot be an empty collection.", nameof(templates));
+
+			_Templates = templates;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		protected CreatureTemplateCollectionModel()
+		{
+			
+		}
+	}
+}
