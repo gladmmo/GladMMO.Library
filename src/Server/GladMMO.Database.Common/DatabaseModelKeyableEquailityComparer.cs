4e161d5812a7b00b52c8d214d2f647d7e35a0f11
HelloKitty <andrew.blakely@ymail.com>
2019-06-30 19:10:03 -0500

    Maded DatabaseModelKeyableEquailityComparer generic

--- src/Server/GladMMO.Database.Common/DatabaseModelKeyableEquailityComparer.cs
+++ src/Server/GladMMO.Database.Common/DatabaseModelKeyableEquailityComparer.cs
@@ -4,9 +4,12 @@ using System.Text;
 
 namespace GladMMO
 {
-	public sealed class DatabaseModelKeyableEquailityComparer : EqualityComparer<IDatabaseModelKeyable>
+	public sealed class DatabaseModelKeyableEquailityComparer<TModelType> : EqualityComparer<TModelType>
+		where TModelType : IDatabaseModelKeyable
 	{
-		public override bool Equals(IDatabaseModelKeyable x, IDatabaseModelKeyable y)
+		public static DatabaseModelKeyableEquailityComparer<TModelType> Instance { get; } = new DatabaseModelKeyableEquailityComparer<TModelType>();
+
+		public override bool Equals(TModelType x, TModelType y)
 		{
 			if (x == null)
 				return y == null;
@@ -16,7 +19,7 @@ namespace GladMMO
 				return x.EntryKey == y.EntryKey;
 		}
 
-		public override int GetHashCode(IDatabaseModelKeyable obj)
+		public override int GetHashCode(TModelType obj)
 		{
 			if (obj == null)
 				return 0;
