e8f7c505e7c24c801871fa6a7b093ea79d96642f
HelloKitty <andrew.blakely@ymail.com>
2019-06-30 19:08:15 -0500

    Added DatabaseModelKeyableEquailityComparer

---
+++ src/Server/GladMMO.Database.Common/DatabaseModelKeyableEquailityComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DatabaseModelKeyableEquailityComparer : EqualityComparer<IDatabaseModelKeyable>
+	{
+		public override bool Equals(IDatabaseModelKeyable x, IDatabaseModelKeyable y)
+		{
+			if (x == null)
+				return y == null;
+			else if (y == null)
+				return false;
+			else
+				return x.EntryKey == y.EntryKey;
+		}
+
+		public override int GetHashCode(IDatabaseModelKeyable obj)
+		{
+			if (obj == null)
+				return 0;
+
+			return obj.EntryKey.GetHashCode();
+		}
+	}
+}
