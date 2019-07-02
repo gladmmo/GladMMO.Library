5df7ae2ec0fcd96cc2f4c1d4ad2614c48c39a1cf
HelloKitty <andrew.blakely@ymail.com>
2019-07-02 06:30:09 -0500

    Added initial positon to IdleMovementGenerator

--- src/GladMMO.ZoneServer.Common/Movement/MovementGenerators/IdleMovementGenerator.cs
+++ src/GladMMO.ZoneServer.Common/Movement/MovementGenerators/IdleMovementGenerator.cs
@@ -11,16 +11,23 @@ namespace GladMMO
 	/// </summary>
 	public sealed class IdleMovementGenerator : MoveGenerator
 	{
+		private Vector3 InitialPosition { get; }
+
+		public IdleMovementGenerator(Vector3 initialPosition)
+		{
+			InitialPosition = initialPosition;
+		}
+
 		protected override Vector3 Start(GameObject entity, long currentTime)
 		{
 			//Let's do nothing!
-			return entity.transform.position;
+			return InitialPosition;
 		}
 
 		protected override Vector3 InternalUpdate(GameObject entity, long currentTime)
 		{
 			//Let's do nothing, forever!
-			return entity.transform.position;
+			return InitialPosition;
 		}
 	}
 }
