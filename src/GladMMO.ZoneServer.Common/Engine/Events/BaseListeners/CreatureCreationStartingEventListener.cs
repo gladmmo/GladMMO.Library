3841a03120762afa25b751c853bd0390f673c6b5
HelloKitty <andrew.blakely@ymail.com>
2019-07-02 06:07:45 -0500

    Added CreatureCreationStartingEventListener

---
+++ src/GladMMO.ZoneServer.Common/Engine/Events/BaseListeners/CreatureCreationStartingEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class CreatureCreationStartingEventListener : BaseSingleEventListenerInitializable<IEntityCreationStartingEventSubscribable, EntityCreationStartingEventArgs>
+	{
+		protected CreatureCreationStartingEventListener(IEntityCreationStartingEventSubscribable subscriptionService) : base(subscriptionService)
+		{
+
+		}
+
+		protected sealed override void OnEventFired(object source, EntityCreationStartingEventArgs args)
+		{
+			if(args.EntityGuid.EntityType != EntityType.Creature)
+				return;
+
+			OnCreatureEntityCreationStarting(args);
+		}
+
+		protected abstract void OnCreatureEntityCreationStarting(EntityCreationStartingEventArgs args);
+	}
+}
