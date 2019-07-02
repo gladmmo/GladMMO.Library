60f6c45a9de3087ecbef01cb64b6e9ce03dea09c
HelloKitty <andrew.blakely@ymail.com>
2019-07-02 06:31:50 -0500

    Added CreatureInitializeDefaultMovementGeneratorEventListener
    May not actually use this forever. But, it works for now.

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/Assembly/OnEntityCreationStarting/CreatureInitializeDefaultMovementGeneratorEventListener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class CreatureInitializeDefaultMovementGeneratorEventListener : CreatureCreationStartingEventListener
+	{
+		private IEntityGuidMappable<IMovementData> MovementDataMappable { get; }
+
+		private IReadonlyEntityGuidMappable<CreatureInstanceModel> CreatureInstanceModel { get; }
+
+		public CreatureInitializeDefaultMovementGeneratorEventListener(IEntityCreationStartingEventSubscribable subscriptionService, 
+			[NotNull] IEntityGuidMappable<IMovementData> movementDataMappable,
+			[NotNull] IReadonlyEntityGuidMappable<CreatureInstanceModel> creatureInstanceModel) 
+			: base(subscriptionService)
+		{
+			MovementDataMappable = movementDataMappable ?? throw new ArgumentNullException(nameof(movementDataMappable));
+			CreatureInstanceModel = creatureInstanceModel ?? throw new ArgumentNullException(nameof(creatureInstanceModel));
+		}
+
+		protected override void OnCreatureEntityCreationStarting(EntityCreationStartingEventArgs args)
+		{
+			//We do nothing if something has already initialized the movement.
+			if (MovementDataMappable.ContainsKey(args.EntityGuid))
+				return;
+
+			CreatureInstanceModel instanceModel = CreatureInstanceModel.RetrieveEntity(args.EntityGuid);
+
+			MovementDataMappable.AddObject(args.EntityGuid, new PositionChangeMovementData(0, instanceModel.InitialPosition, Vector2.zero, instanceModel.YAxisRotation));
+		}
+	}
+}
