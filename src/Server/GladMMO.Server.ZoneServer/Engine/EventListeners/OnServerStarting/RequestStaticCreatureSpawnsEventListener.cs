979bf3f9196cd58dc6fb491e1096f09ef1a34d27
HelloKitty <andrew.blakely@ymail.com>
2019-07-02 06:02:41 -0500

    Added RequestStaticCreatureSpawnsEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/OnServerStarting/RequestStaticCreatureSpawnsEventListener.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using Glader.Essentials;
+using Nito.AsyncEx;
+using Unitysync.Async;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class RequestStaticCreatureSpawnsEventListener : BaseSingleEventListenerInitializable<IServerStartingEventSubscribable>
+	{
+		private ICreatureDataServiceClient CreatureContentDataClient { get; }
+
+		private IEventPublisher<IEntityCreationRequestedEventSubscribable, EntityCreationRequestedEventArgs> EntityCreationRequester { get; }
+
+		private IFactoryCreatable<NetworkEntityGuid, CreatureInstanceModel> CreatureGuidFactory { get; }
+
+		private ILog Logger { get; }
+
+		private IEntityGuidMappable<CreatureTemplateModel> CreatureTemplateMappable { get; }
+
+		private IEntityGuidMappable<CreatureInstanceModel> CreatureInstanceMappable { get; }
+
+		public RequestStaticCreatureSpawnsEventListener(IServerStartingEventSubscribable subscriptionService,
+			[NotNull] ICreatureDataServiceClient creatureContentDataClient,
+			[NotNull] IEventPublisher<IEntityCreationRequestedEventSubscribable, EntityCreationRequestedEventArgs> entityCreationRequester,
+			[NotNull] IFactoryCreatable<NetworkEntityGuid, CreatureInstanceModel> creatureGuidFactory,
+			[NotNull] ILog logger, [NotNull] IEntityGuidMappable<CreatureTemplateModel> creatureTemplateMappable,
+			[NotNull] IEntityGuidMappable<CreatureInstanceModel> creatureInstanceMappable)
+			: base(subscriptionService)
+		{
+			CreatureContentDataClient = creatureContentDataClient ?? throw new ArgumentNullException(nameof(creatureContentDataClient));
+			EntityCreationRequester = entityCreationRequester ?? throw new ArgumentNullException(nameof(entityCreationRequester));
+			CreatureGuidFactory = creatureGuidFactory ?? throw new ArgumentNullException(nameof(creatureGuidFactory));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			CreatureTemplateMappable = creatureTemplateMappable ?? throw new ArgumentNullException(nameof(creatureTemplateMappable));
+			CreatureInstanceMappable = creatureInstanceMappable ?? throw new ArgumentNullException(nameof(creatureInstanceMappable));
+		}
+
+		protected override void OnEventFired(object source, EventArgs args)
+		{
+			//TODO: We should simplify the ability to have async handlers.
+			//TODO: Don't hardcode the test world id.
+			UnityAsyncHelper.UnityMainThreadContext.PostAsync(async () =>
+			{
+				ResponseModel<CreatureEntryCollectionModel, CreatureCollectionResponseCode> entriesByWorld = await CreatureContentDataClient.GetCreatureEntriesByWorld(30);
+				ResponseModel<CreatureTemplateCollectionModel, CreatureCollectionResponseCode> templatesByWorld = await CreatureContentDataClient.GetCreatureTemplatesByWorld(30);
+
+				if (entriesByWorld.isSuccessful && templatesByWorld.isSuccessful)
+				{
+					AddCreatureTemplates(templatesByWorld.Result.Templates);
+					AddCreatureInstances(entriesByWorld.Result.Entries);
+
+					ProcessCreatureEntries(entriesByWorld.Result.Entries);
+				}
+				else
+				{
+					if(Logger.IsWarnEnabled)
+						Logger.Warn($"Didn't properly query Creature Data. This may not actually be an error. Instance Reason: {entriesByWorld.ResultCode} Template Reason: {templatesByWorld.ResultCode}");
+				}
+			});
+		}
+
+		private void AddCreatureInstances([NotNull] IReadOnlyCollection<CreatureInstanceModel> resultEntries)
+		{
+			if (resultEntries == null) throw new ArgumentNullException(nameof(resultEntries));
+
+			foreach (var instance in resultEntries)
+			{
+				if(Logger.IsInfoEnabled)
+					Logger.Info($"Processing Creature Instance: {instance.Guid}");
+
+				CreatureInstanceMappable.Add(instance.Guid, instance);
+			}
+		}
+
+		private void AddCreatureTemplates([NotNull] IReadOnlyCollection<CreatureTemplateModel> resultTemplates)
+		{
+			if (resultTemplates == null) throw new ArgumentNullException(nameof(resultTemplates));
+
+			foreach (var template in resultTemplates)
+			{
+				if(Logger.IsInfoEnabled)
+					Logger.Info($"Processing Creature Template: {template.CreatureTemplateId} Name: {template.CreatureName}");
+
+				CreatureTemplateMappable.Add(NetworkEntityGuid.Empty, template);
+			}
+		}
+
+		private void ProcessCreatureEntries([NotNull] IReadOnlyCollection<CreatureInstanceModel> instanceEntries)
+		{
+			if (instanceEntries == null) throw new ArgumentNullException(nameof(instanceEntries));
+
+			foreach (var entry in instanceEntries)
+			{
+				NetworkEntityGuid guid = CreatureGuidFactory.Create(entry);
+				EntityCreationRequester.PublishEvent(this, new EntityCreationRequestedEventArgs(guid));
+			}
+		}
+	}
+}
