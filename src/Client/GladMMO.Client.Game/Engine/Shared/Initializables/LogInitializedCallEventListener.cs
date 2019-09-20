7a1add47baced191fe5fdda917a8ff0dffcd0fbc
HelloKitty <andrew.blakely@ymail.com>
2019-09-20 15:16:10 -0500

    Added scene start logging for game framework

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/Initializables/LogInitializedCallEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.TitleScreen)]
+	public sealed class LogInitializedCallEventListener : IGameStartable
+	{
+		private ILog Logger { get; }
+
+		public LogInitializedCallEventListener([NotNull] ILog logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public Task Start()
+		{
+			Logger.Error($"Scene Initialized called.");
+
+			return Task.CompletedTask;
+		}
+	}
+}
