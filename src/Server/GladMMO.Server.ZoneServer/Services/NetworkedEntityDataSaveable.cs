c3e38e2c649d5abdbe86175105009c96b4cf8bea
HelloKitty <andrew.blakely@ymail.com>
2019-09-20 12:58:25 -0500

    Fixed NetworkedEntityDataSaveable for entity saving

--- src/Server/GladMMO.Server.ZoneServer/Services/NetworkedEntityDataSaveable.cs
+++ src/Server/GladMMO.Server.ZoneServer/Services/NetworkedEntityDataSaveable.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
+using UnityEngine;
 
 namespace GladMMO
 {
 	public sealed class NetworkedEntityDataSaveable : IEntityDataSaveable
 	{
-		private IReadonlyEntityGuidMappable<IMovementData> MovementDataMap { get; }
+		private IReadonlyEntityGuidMappable<IMovementGenerator<GameObject>> MovementDataMap { get; }
 
 		private IZoneServerToGameServerClient ZoneToSeverClient { get; }
 
 		/// <inheritdoc />
-		public NetworkedEntityDataSaveable([NotNull] IReadonlyEntityGuidMappable<IMovementData> movementDataMap, [NotNull] IZoneServerToGameServerClient zoneToSeverClient)
+		public NetworkedEntityDataSaveable([NotNull] IReadonlyEntityGuidMappable<IMovementGenerator<GameObject>> movementDataMap, [NotNull] IZoneServerToGameServerClient zoneToSeverClient)
 		{
 			MovementDataMap = movementDataMap ?? throw new ArgumentNullException(nameof(movementDataMap));
 			ZoneToSeverClient = zoneToSeverClient ?? throw new ArgumentNullException(nameof(zoneToSeverClient));
@@ -28,15 +29,18 @@ namespace GladMMO
 		/// <inheritdoc />
 		public async Task SaveAsync(NetworkEntityGuid guid)
 		{
-			//TODO: Check that the entity actually exists
-			IMovementData movementData = MovementDataMap[guid];
-
 			//We can only handle players at the moment, not sure how NPC data would be saved.
 			if(guid.EntityType != EntityType.Player)
 				return;
 
+			//TODO: Check that the entity actually exists
+			IMovementGenerator<GameObject> movementData = MovementDataMap.RetrieveEntity(guid);
+
 			//TODO: Handle map ID.
-			await ZoneToSeverClient.SaveCharacterLocation(new ZoneServerCharacterLocationSaveRequest(guid.EntityId, movementData.InitialPosition, 1))
+			await ZoneToSeverClient.SaveCharacterLocation(new ZoneServerCharacterLocationSaveRequest(guid.EntityId, movementData.CurrentPosition, 1))
+				.ConfigureAwait(false);
+
+			await ZoneToSeverClient.ReleaseActiveSession(guid.EntityId)
 				.ConfigureAwait(false);
 		}
 	}
