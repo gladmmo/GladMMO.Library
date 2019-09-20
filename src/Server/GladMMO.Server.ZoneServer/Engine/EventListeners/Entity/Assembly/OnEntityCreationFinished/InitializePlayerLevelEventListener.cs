419568243437c1534da5a9a16bc03aa074f922ac
HelloKitty <andrew.blakely@ymail.com>
2019-09-20 12:57:47 -0500

    Added InitializePlayerLevelEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/Assembly/OnEntityCreationFinished/InitializePlayerLevelEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	//Basically a stub for where/how to implement player level initialization.
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class InitializePlayerLevelEventListener : PlayerCreationFinishedEventListener
+	{
+		private IReadonlyEntityGuidMappable<IEntityDataFieldContainer> EntityDataMappable { get; }
+
+		public InitializePlayerLevelEventListener(IEntityCreationFinishedEventSubscribable subscriptionService,
+			[NotNull] IReadonlyEntityGuidMappable<IEntityDataFieldContainer> entityDataMappable) 
+			: base(subscriptionService)
+		{
+			EntityDataMappable = entityDataMappable ?? throw new ArgumentNullException(nameof(entityDataMappable));
+		}
+
+		protected override void OnPlayerEntityCreationFinished(EntityCreationFinishedEventArgs args)
+		{
+			//TODO: Demo code.
+			IEntityDataFieldContainer entityDataFieldContainer = EntityDataMappable.RetrieveEntity(args.EntityGuid);
+
+			entityDataFieldContainer.SetFieldValue((int)EUnitFields.UNIT_FIELD_LEVEL, 1);
+		}
+	}
+}
