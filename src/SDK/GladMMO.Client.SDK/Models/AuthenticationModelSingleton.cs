723fd1d3c672d7f4b46747779ed0df614abbe599
HelloKitty <andrew.blakely@ymail.com>
2019-09-22 11:01:04 -0500

    Added SDK editor auth model singleton

---
+++ src/SDK/GladMMO.Client.SDK/Models/AuthenticationModelSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class AuthenticationModelSingleton
+	{
+		public static AuthenticationModelSingleton Instance { get; }
+
+		public bool isAuthenticated { get; private set; } = false;
+
+		public string AuthenticationToken { get; private set; } = null;
+
+		static AuthenticationModelSingleton()
+		{
+			Instance = new AuthenticationModelSingleton();
+		}
+
+		//Hide ctor
+		private AuthenticationModelSingleton()
+		{
+			
+		}
+
+		public void SetAuthenticationState(bool state)
+		{
+			isAuthenticated = state;
+
+			//Clear token data when authentication state changes.
+			if (!isAuthenticated)
+				AuthenticationToken = null;
+		}
+
+		public void SetAuthenticationToken([NotNull] string token)
+		{
+			if(string.IsNullOrWhiteSpace(token)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(token));
+
+			AuthenticationToken = token;
+		}
+	}
+}
