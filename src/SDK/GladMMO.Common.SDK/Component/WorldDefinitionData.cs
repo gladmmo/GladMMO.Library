f5de04fe824479e4760f3719bfec97e792eb817a
HelloKitty <andrew.blakely@ymail.com>
2019-09-22 11:27:49 -0500

    Added WorldDefinitionData scene component Type

---
+++ src/SDK/GladMMO.Common.SDK/Component/WorldDefinitionData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Data component for the World/Scene data link.
+	/// </summary>
+	public sealed class WorldDefinitionData : GladMMOSDKMonoBehaviour
+	{
+		[Tooltip("The unique world id.")]
+		[SerializeField]
+		private long _WorldId;
+
+		[Tooltip("The unique content identifier for the content.")]
+		[SerializeField]
+		private string _WorldGuid;
+
+		/// <summary>
+		/// The GUID for the world content.
+		/// </summary>
+		public string WorldGuid
+		{
+			get => _WorldGuid;
+			set => _WorldGuid = value;
+		}
+
+		/// <summary>
+		/// The identifier for the world.
+		/// </summary>
+		public long WorldId
+		{
+			get => _WorldId;
+			set => _WorldId = value;
+		}
+
+		[Button]
+		public void UnlinkContent()
+		{
+			_WorldId = 0;
+			_WorldGuid = null;
+		}
+	}
+}
