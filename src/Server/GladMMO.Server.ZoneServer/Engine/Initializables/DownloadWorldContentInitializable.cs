34fb4bdab3ca3eaea368fd22e44f1fe16d3ec437
HelloKitty <andrew.blakely@ymail.com>
2019-09-22 21:59:44 -0500

    Added world downloading on the zoneserver

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/Initializables/DownloadWorldContentInitializable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Initialization component that downloads the world scene.
+	/// </summary>
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class DownloadWorldContentInitializable : IGameInitializable
+	{
+		private IDownloadableContentServerServiceClient ContentService { get; }
+
+		private WorldConfiguration WorldConfig { get; }
+
+		private ILog Logger { get; }
+
+		public DownloadWorldContentInitializable([NotNull] IDownloadableContentServerServiceClient contentService,
+			[NotNull] WorldConfiguration worldConfig,
+			[NotNull] ILog logger)
+		{
+			ContentService = contentService ?? throw new ArgumentNullException(nameof(contentService));
+			WorldConfig = worldConfig ?? throw new ArgumentNullException(nameof(worldConfig));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public async Task OnGameInitialized()
+		{
+			//TODO: Handle throwing/error
+			ContentDownloadURLResponse downloadUrlResponse = await ContentService.RequestWorldDownloadUrl(WorldConfig.WorldId)
+				.ConfigureAwait(false);
+
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"World Download Url: {downloadUrlResponse.DownloadURL}");
+
+			//Can't do web request not on the main thread, sadly.
+			await new UnityYieldAwaitable();
+
+			//TODO: Handle failure.
+			WorldDownloader downloader = new WorldDownloader(Logger);
+
+			//At this point after this finishes the world shoud be downloaded.
+			downloader.DownloadAsync(downloadUrlResponse.DownloadURL);
+		}
+	}
+}
