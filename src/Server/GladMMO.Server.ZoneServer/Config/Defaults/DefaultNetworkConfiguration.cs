742ae1f3121ccf9e2a7cadd121e0b1cd8c1a768b
HelloKitty <andrew.blakely@ymail.com>
2019-09-22 21:38:02 -0500

    Added default zoneserver configuration Types

---
+++ src/Server/GladMMO.Server.ZoneServer/Config/Defaults/DefaultNetworkConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DefaultNetworkConfiguration : NetworkConfiguration
+	{
+		public DefaultNetworkConfiguration()
+			: base(5006, "192.168.0.12")
+		{
+			
+		}
+	}
+}
---
+++ src/Server/GladMMO.Server.ZoneServer/Config/Defaults/DefaultWorldConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DefaultWorldConfiguration : WorldConfiguration
+	{
+		public DefaultWorldConfiguration()
+			: base(8) //default world 8.
+		{
+			
+		}
+	}
+}
