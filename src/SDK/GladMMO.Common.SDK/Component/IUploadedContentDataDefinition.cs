fbf338ac74f5633ce605f4fbee32b2659390dc35
HelloKitty <andrew.blakely@ymail.com>
2019-09-24 16:08:42 -0500

    Added IUploadedContentDataDefinition

---
+++ src/SDK/GladMMO.Common.SDK/Component/IUploadedContentDataDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO.SDK
+{
+	public interface IUploadedContentDataDefinition
+	{
+		long ContentId { get; }
+
+		Guid ContentGuid { get; }
+	}
+}
