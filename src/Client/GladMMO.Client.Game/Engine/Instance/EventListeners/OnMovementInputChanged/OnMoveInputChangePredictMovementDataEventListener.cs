cb29d3f17f99ba0b0933b133992656de722c3678
HelloKitty <andrew.blakely@ymail.com>
2019-09-23 21:15:44 -0500

    Implement clientside movement prediction on input changed

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnMovementInputChanged/OnMoveInputChangePredictMovementDataEventListener.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using GladNet;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class OnMoveInputChangePredictMovementDataEventListener : BaseSingleEventListenerInitializable<IMovementInputChangedEventSubscribable, MovementInputChangedEventArgs>
+	{
+		private MovementUpdateMessageHandler MovementUpdateHandler { get; }
+
+		private ILocalPlayerDetails PlayerDetails { get; }
+
+		private INetworkTimeService TimeService { get; }
+
+		private IReadonlyEntityGuidMappable<WorldTransform> TransformMap { get; }
+
+		private IEntityGuidMappable<IMovementData> MovementDataMappable { get; }
+
+		public OnMoveInputChangePredictMovementDataEventListener(IMovementInputChangedEventSubscribable subscriptionService,
+			[NotNull] ILocalPlayerDetails playerDetails,
+			[NotNull] INetworkTimeService timeService,
+			[NotNull] IReadonlyEntityGuidMappable<WorldTransform> transformMap,
+			[NotNull] MovementUpdateMessageHandler movementUpdateHandler,
+			[NotNull] IEntityGuidMappable<IMovementData> movementDataMappable)
+			: base(subscriptionService)
+		{
+			PlayerDetails = playerDetails ?? throw new ArgumentNullException(nameof(playerDetails));
+			TimeService = timeService ?? throw new ArgumentNullException(nameof(timeService));
+			TransformMap = transformMap ?? throw new ArgumentNullException(nameof(transformMap));
+			MovementUpdateHandler = movementUpdateHandler ?? throw new ArgumentNullException(nameof(movementUpdateHandler));
+			MovementDataMappable = movementDataMappable ?? throw new ArgumentNullException(nameof(movementDataMappable));
+		}
+
+		protected override void OnEventFired(object source, MovementInputChangedEventArgs args)
+		{
+			Vector2 direction = new Vector2(args.NewHorizontalInput, args.NewVerticalInput);
+			WorldTransform worldTransform = TransformMap.RetrieveEntity(PlayerDetails.LocalPlayerGuid);
+			long predictedTime = TimeService.CurrentRemoteTime + TimeService.CurrentLatency;
+			IMovementData data = new PositionChangeMovementData(predictedTime, worldTransform.Position, direction, worldTransform.YAxisRotation);
+
+			MovementDataMappable.ReplaceObject(PlayerDetails.LocalPlayerGuid, data);
+			MovementUpdateHandler.HandleMovementUpdate(new EntityAssociatedData<IMovementData>(PlayerDetails.LocalPlayerGuid, data));
+		}
+	}
+}
