38a06b83be98819c492e272ff234a0d236dc2831
HelloKitty <andrew.blakely@ymail.com>
2019-09-23 19:08:19 -0500

    Added spawn event listener that initializes WorldTransform collection

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/Assembly/OnEntityCreationStarting/CreateInitialEntityWorldTransformEventListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class CreateInitialEntityWorldTransformEventListener : BaseSingleEventListenerInitializable<IEntityCreationStartingEventSubscribable, EntityCreationStartingEventArgs>
+	{
+		private IEntityGuidMappable<WorldTransform> TransformMap { get; }
+
+		private IReadonlyEntityGuidMappable<IMovementData> MovementDataMappable { get; }
+
+		public CreateInitialEntityWorldTransformEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<WorldTransform> transformMap,
+			[NotNull] IReadonlyEntityGuidMappable<IMovementData> movementDataMappable) : base(subscriptionService)
+		{
+			TransformMap = transformMap ?? throw new ArgumentNullException(nameof(transformMap));
+			MovementDataMappable = movementDataMappable ?? throw new ArgumentNullException(nameof(movementDataMappable));
+		}
+
+		protected override void OnEventFired(object source, EntityCreationStartingEventArgs args)
+		{
+			IMovementData movementData = MovementDataMappable.RetrieveEntity(args.EntityGuid);
+
+			//We just need to make the world transform match
+			//the initial movement data
+			TransformMap.AddObject(args.EntityGuid, new WorldTransform(movementData.InitialPosition, movementData.Rotation));
+		}
+	}
+}
