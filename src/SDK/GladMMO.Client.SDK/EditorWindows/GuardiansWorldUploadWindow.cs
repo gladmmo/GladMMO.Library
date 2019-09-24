438150badd9874a2ca5050d21d925307d2a7b2c6
HelloKitty <andrew.blakely@ymail.com>
2019-09-24 16:48:16 -0500

    Converted WorldUpload window to use base generalized content uploading window

--- src/SDK/GladMMO.Client.SDK/EditorWindows/GuardiansWorldUploadWindow.cs
+++ src/SDK/GladMMO.Client.SDK/EditorWindows/GuardiansWorldUploadWindow.cs
@@ -18,10 +18,16 @@ using UnityEngine.SceneManagement;
 
 namespace GladMMO.SDK
 {
-	public sealed class GuardiansWorldUploadWindow : AuthenticatableEditorWindow
+	public sealed class GuardiansWorldUploadWindow : BaseCustomContentUploadEditorWindow
 	{
 		[SerializeField]
 		private UnityEngine.Object SceneObject;
+
+		public GuardiansWorldUploadWindow()
+			: base(UserContentType.World)
+		{
+
+		}
 
 		[MenuItem("VRGuardians/Content/WorldUpload")]
 		public static void ShowWindow()
@@ -40,106 +46,26 @@ namespace GladMMO.SDK
 			//TODO: We should do more than just render it.
 			WorldDefinitionData definitionData = FindObjectOfType<WorldDefinitionData>();
 
-			if (definitionData != null)
+			//Let's create a definition if one doesn't already exist.
+			if (definitionData == null)
+				definitionData = CreateWorldDefinitionData();
+
+			base.OnRenderUploadGUI(definitionData, SceneObject, token =>
 			{
-				EditorGUILayout.LabelField($"World Id: {definitionData.ContentId}");
-				EditorGUILayout.LabelField($"World GUID: {definitionData.ContentGuid.ToString()}");
-
-				//TODO: Conssolidate
-				if (GUILayout.Button("Update World"))
-				{
-					string assetBundlePath = GenerateWorldBundle();
-
-					var contentServerServiceClient = new DownloadableContentServiceClientFactory().Create(EmptyFactoryContext.Instance);
-					UploadWorldAssetBundle(assetBundlePath, false, new UpdatedContentUploadTokenFactory(contentServerServiceClient, definitionData));
-				}
-			}
-			else
-			{
-				//TODO: Consolidate
-				//If there is no world data definition then we should allow users
-				//to upload a new world.
-				if(GUILayout.Button("Upload World"))
-				{
-					string assetBundlePath = GenerateWorldBundle();
-
-					var contentServerServiceClient = new DownloadableContentServiceClientFactory().Create(EmptyFactoryContext.Instance);
-					UploadWorldAssetBundle(assetBundlePath, true, new NewContentUploadTokenFactory(contentServerServiceClient));
-				}
-			}
+				definitionData.ContentGuid = token.ContentGuid;
+				definitionData.ContentId = token.ContentId;
+			});
 		}
 
-		private static void UploadWorldAssetBundle(string assetBundlePath, bool shouldCreateSceneWorldData, IContentUploadTokenFactory tokenFactory)
+		private static WorldDefinitionData CreateWorldDefinitionData()
 		{
-			//Done out here, must be called on the main thread
-			string projectPath = Application.dataPath.ToLower().TrimEnd(@"assets".ToCharArray());
-
-			UnityAsyncHelper.InitializeSyncContext();
-
-			UnityAsyncHelper.UnityMainThreadContext.Post(async o =>
-			{
-				EditorUtility.DisplayProgressBar("Uploading Content", "Please wait until complete.", 0.5f);
-				try
-				{
-					ResponseModel<ContentUploadToken, ContentUploadResponseCode> contentUploadToken = await tokenFactory.Create(new ContentUploadTokenFactoryContext(UserContentType.World))
-						.ConfigureAwait(true);
-
-					//TODO: Handle failure.
-					await UploadWorldContent(assetBundlePath, contentUploadToken.Result.UploadUrl, projectPath);
-
-					if(shouldCreateSceneWorldData)
-						CreateWorldDefinition(contentUploadToken);
-				}
-				catch (Exception e)
-				{
-					Debug.LogError($"Failed to upload. Reason: {e.Message}");
-					throw;
-				}
-				finally
-				{
-					//Important to ALWAYS end the progress bar, even if failed.
-					EditorUtility.ClearProgressBar();
-				}
-			}, null);
-		}
-
-		private static void CreateWorldDefinition(ResponseModel<ContentUploadToken, ContentUploadResponseCode> contentUploadToken)
-		{
-			//If it's sucessfully uploaded we should create a world definition
-			//object in the scene.
-			WorldDefinitionData definitionData = new GameObject("World Definition").AddComponent<WorldDefinitionData>();
-
-			definitionData.ContentGuid = contentUploadToken.Result.ContentGuid;
-			definitionData.ContentId = contentUploadToken.Result.ContentId;
+			WorldDefinitionData definitionData;
+			definitionData = new GameObject("World Definition").AddComponent<WorldDefinitionData>();
 
 			EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
 			EditorUtility.SetDirty(definitionData);
-		}
 
-		private static async Task UploadWorldContent(string assetBundlePath, string uploadUrl, string projectPath)
-		{
-			Debug.Log($"Uploading to: {uploadUrl}.");
-
-			var cloudBlockBlob = new CloudBlockBlob(new Uri(uploadUrl));
-			await cloudBlockBlob.UploadFromFileAsync(Path.Combine(projectPath, "AssetBundles", "temp", assetBundlePath))
-				.ConfigureAwait(true);
-
-			Debug.Log("Successfully uploaded.");
-		}
-
-		private string GenerateWorldBundle()
-		{
-			//Once authenticated we need to try to build the bundle.
-			ProjectVindictiveAssetbundleBuilder builder = new ProjectVindictiveAssetbundleBuilder(SceneObject);
-
-			//TODO: Handle uploading build
-			AssetBundleManifest manifest = builder.BuildBundle();
-			string bundlePath = manifest.GetAllAssetBundles().First();
-
-			Debug.Log($"Generated AssetBundle with Path: {bundlePath}");
-
-			//TODO: Refactor all this crap
-			return bundlePath;
+			return definitionData;
 		}
 
 		protected override void UnAuthenticatedOnGUI()
