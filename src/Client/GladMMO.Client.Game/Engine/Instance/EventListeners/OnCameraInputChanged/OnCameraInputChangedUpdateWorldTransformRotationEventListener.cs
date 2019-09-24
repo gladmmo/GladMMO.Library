529f3b39ba9f6abe91d66bec22fda093cdb84051
HelloKitty <andrew.blakely@ymail.com>
2019-09-23 21:05:14 -0500

    Added camera input listener that updates WorldTransform

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnCameraInputChanged/OnCameraInputChangedUpdateWorldTransformRotationEventListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using GladNet;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class OnCameraInputChangedUpdateWorldTransformRotationEventListener : BaseSingleEventListenerInitializable<ICameraInputChangedEventSubscribable, CameraInputChangedEventArgs>
+	{
+		private ILocalPlayerDetails PlayerDetails { get; }
+
+		private IEntityGuidMappable<WorldTransform> TransformMappable { get; }
+
+		public OnCameraInputChangedUpdateWorldTransformRotationEventListener(ICameraInputChangedEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<WorldTransform> transformMappable,
+			[NotNull] ILocalPlayerDetails playerDetails)
+			: base(subscriptionService)
+		{
+			TransformMappable = transformMappable ?? throw new ArgumentNullException(nameof(transformMappable));
+			PlayerDetails = playerDetails ?? throw new ArgumentNullException(nameof(playerDetails));
+		}
+
+		protected override void OnEventFired(object source, CameraInputChangedEventArgs args)
+		{
+			WorldTransform worldTransform = TransformMappable.RetrieveEntity(PlayerDetails.LocalPlayerGuid);
+			TransformMappable.ReplaceObject(PlayerDetails.LocalPlayerGuid, new WorldTransform(worldTransform.Position, args.Rotation));
+		}
+	}
+}
