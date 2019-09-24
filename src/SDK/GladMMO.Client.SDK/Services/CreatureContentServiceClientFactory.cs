3ac27443582030d8898deee43deed10e6bb835e0
HelloKitty <andrew.blakely@ymail.com>
2019-09-24 18:26:33 -0500

    Added CreatureContentServiceClientFactory to SDK

---
+++ src/SDK/GladMMO.Client.SDK/Services/CreatureContentServiceClientFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+
+namespace GladMMO.SDK
+{
+	public sealed class CreatureContentServiceClientFactory : IFactoryCreatable<ICreatureDataServiceClient, EmptyFactoryContext>
+	{
+		public ICreatureDataServiceClient Create(EmptyFactoryContext context)
+		{
+			return Refit.RestService.For<ICreatureDataServiceClient>("http://72.190.177.214:5005/", new RefitSettings() { HttpMessageHandlerFactory = () => new AuthenticatedHttpClientHandler(new ContentSDKAuthenticationTokenRepository()) });
+		}
+	}
+}
