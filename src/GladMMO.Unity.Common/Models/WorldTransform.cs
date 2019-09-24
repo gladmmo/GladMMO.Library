1fea9eedab7ff8342e8bf243c17e19ae3de534bb
HelloKitty <andrew.blakely@ymail.com>
2019-09-23 19:00:44 -0500

    Added WorldTransform model

---
+++ src/GladMMO.Unity.Common/Models/WorldTransform.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class WorldTransform
+	{
+		public Vector3 Position { get; }
+
+		public float YAxisRotation { get; }
+
+		public WorldTransform(Vector3 position, float yAxisRotation)
+		{
+			Position = position;
+			YAxisRotation = yAxisRotation;
+		}
+	}
+}
