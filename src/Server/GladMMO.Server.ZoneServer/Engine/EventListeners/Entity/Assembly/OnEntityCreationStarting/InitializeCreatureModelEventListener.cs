97025dccf320820378e17f4897deaef231a6fdff
HelloKitty <andrew.blakely@ymail.com>
2019-09-25 15:51:30 -0500

    Added InitializeCreatureModelEventListener on EntityStartingCreation

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/Assembly/OnEntityCreationStarting/InitializeCreatureModelEventListener.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class InitializeCreatureModelEventListener : BaseSingleEventListenerInitializable<IEntityCreationStartingEventSubscribable, EntityCreationStartingEventArgs>
+	{
+		private IReadonlyEntityGuidMappable<IEntityDataFieldContainer> EntityDataMappable { get; }
+
+		private IEntityGuidMappable<CreatureTemplateModel> CreatureTemplateMappable { get; }
+
+		public InitializeCreatureModelEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
+			[NotNull] IReadonlyEntityGuidMappable<IEntityDataFieldContainer> entityDataMappable,
+			[NotNull] IEntityGuidMappable<CreatureTemplateModel> creatureTemplateMappable) 
+			: base(subscriptionService)
+		{
+			EntityDataMappable = entityDataMappable ?? throw new ArgumentNullException(nameof(entityDataMappable));
+			CreatureTemplateMappable = creatureTemplateMappable ?? throw new ArgumentNullException(nameof(creatureTemplateMappable));
+		}
+
+		protected override void OnEventFired(object source, EntityCreationStartingEventArgs args)
+		{
+			//Only do creatures.
+			if (args.EntityGuid.EntityType != EntityType.Creature)
+				return;
+
+			IEntityDataFieldContainer dataContainer = EntityDataMappable.RetrieveEntity(args.EntityGuid);
+			CreatureTemplateModel template = CreatureTemplateMappable.RetrieveEntity(args.EntityGuid);
+
+			//Initialize the creature display model ID.
+			dataContainer.SetFieldValue(EUnitFields.UNIT_FIELD_DISPLAYID, template.ModelId);
+		}
+	}
+}
