72a9e3985215896dbafa6a0b1b833fa898cec16a
HelloKitty <andrew.blakely@ymail.com>
2019-09-26 17:36:59 -0500

    Created unified Prefab Content Upload Window

--- src/SDK/GladMMO.Client.SDK/EditorWindows/GuardiansCreatureModelUploadWindow.cs
+++ src/SDK/GladMMO.Client.SDK/EditorWindows/GuardiansCreatureModelUploadWindow.cs
@@ -18,11 +18,8 @@ using UnityEngine.SceneManagement;
 namespace GladMMO.SDK
 {
 	//TODO: Refactor
-	public sealed class GuardiansCreatureModelUploadWindow : BaseCustomContentUploadEditorWindow
+	public sealed class GuardiansCreatureModelUploadWindow : BasePrefabedCustomContentUploadEditorWindow
 	{
-		[SerializeField]
-		private UnityEngine.GameObject CreaturePrefab;
-
 		public GuardiansCreatureModelUploadWindow()
 			: base(UserContentType.Creature)
 		{
@@ -33,46 +30,6 @@ namespace GladMMO.SDK
 		public static void ShowWindow()
 		{
 			EditorWindow.GetWindow(typeof(GuardiansCreatureModelUploadWindow));
-		}
-
-		protected override void AuthenticatedOnGUI()
-		{
-			CreaturePrefab = EditorGUILayout.ObjectField("Creature Prefab", CreaturePrefab, typeof(GameObject), false) as GameObject;
-
-			if(CreaturePrefab != null)
-			{
-				if(PrefabUtility.GetPrefabAssetType(CreaturePrefab) == PrefabAssetType.NotAPrefab)
-				{
-					CreaturePrefab = null;
-					Debug.LogError($"Provided creature prefab MUST be a prefab.");
-					return;
-				}
-			}
-			else
-				return; //TODO: Try to discover the avatar's prefab in the scene.
-
-			//We need the avatar data definition now.
-			CreatureDefinitionData definitionData = CreaturePrefab.GetComponent<CreatureDefinitionData>();
-
-			if(definitionData == null)
-				Debug.LogError($"Provided creature must contain Component: {nameof(CreatureDefinitionData)} on root {nameof(GameObject)}");
-
-			base.OnRenderUploadGUI(definitionData, CreaturePrefab, token =>
-			{
-				//DO NOT REFERNECE THE ABOVE DEFINITION. IT NO LONGER EXISTS
-				//THIS IS DUE TO SCENE RELOAD
-				definitionData = CreaturePrefab.GetComponent<CreatureDefinitionData>();
-
-				definitionData.ContentGuid = token.ContentGuid;
-				definitionData.ContentId = token.ContentId;
-
-				PrefabUtility.SavePrefabAsset(CreaturePrefab);
-			});
-		}
-
-		protected override void UnAuthenticatedOnGUI()
-		{
-			//TODO: Redirect to login.
 		}
 	}
 }
