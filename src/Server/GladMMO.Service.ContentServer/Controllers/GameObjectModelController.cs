f74dfc8a18c770b81524c06c02718969650b5272
HelloKitty <andrew.blakely@ymail.com>
2019-09-26 16:39:12 -0500

    Added GameObjectModel controller

---
+++ src/Server/GladMMO.Service.ContentServer/Controllers/GameObjectModelController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	[Route("api/[controller]")]
+	public sealed class GameObjectModelController : BaseCustomContentController<GameObjectModelEntryModel>
+	{
+		public GameObjectModelController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger, UserContentType.Creature)
+		{
+
+		}
+
+		protected override GameObjectModelEntryModel GenerateNewModel()
+		{
+			int userId = ClaimsReader.GetUserIdInt(User);
+			return new GameObjectModelEntryModel(userId, HttpContext.Connection.RemoteIpAddress.ToString(), Guid.NewGuid());
+		}
+	}
+}
