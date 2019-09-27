7d78d57f5ea27c6059ad3dc1c12ee5e60427e134
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 13:55:20 -0500

    Created base object template controller

---
+++ src/Server/GladMMO.Service.ContentServer/Controllers/Base/TemplateObjectDataController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	public abstract class TemplateObjectDataController<TTemplateEntryType, TTemplateTransferType, TInstanceRepositoryType> : AuthorizationReadyController
+		where TInstanceRepositoryType : IGenericRepositoryCrudable<int, TTemplateEntryType>, ITemplateableWorldObjectRepository<TTemplateEntryType>
+		where TTemplateTransferType : class
+		where TTemplateEntryType : IDatabaseModelKeyable
+	{
+		protected TemplateObjectDataController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger)
+			: base(claimsReader, logger)
+		{
+
+		}
+
+		[ProducesJson]
+		[HttpGet("template/{id}")]
+		public async Task<IActionResult> GetObjectTemplate([FromRoute(Name = "id")] int objectTemplateId,
+			[FromServices] [NotNull] TInstanceRepositoryType objectTemplateRepository,
+			[FromServices] [NotNull] ITypeConverterProvider<TTemplateEntryType, TTemplateTransferType> tableToNetworkModelConverter)
+		{
+			if (objectTemplateRepository == null) throw new ArgumentNullException(nameof(objectTemplateRepository));
+			if (tableToNetworkModelConverter == null) throw new ArgumentNullException(nameof(tableToNetworkModelConverter));
+
+			//If unknown templateId, then just indicate such.
+			if(!await objectTemplateRepository.ContainsAsync(objectTemplateId))
+				return BuildFailedResponseModel(SceneContentQueryResponseCode.TemplateUnknown);
+
+			//Load the model, convert and send back.
+			TTemplateEntryType entryModel = await objectTemplateRepository.RetrieveAsync(objectTemplateId);
+			TTemplateTransferType templateModel = tableToNetworkModelConverter.Convert(entryModel);
+
+			return BuildSuccessfulResponseModel(templateModel);
+		}
+
+		[ProducesJson]
+		[HttpGet("{world}/template")]
+		public async Task<IActionResult> GetObjectTemplatesForWorld([FromRoute(Name = "world")] int worldId,
+			[FromServices] TInstanceRepositoryType objectTemplateRepository,
+			[FromServices] ITypeConverterProvider<TTemplateEntryType, TTemplateTransferType> tableToNetworkModelConverter)
+		{
+			//We can actually get all the templates FROM the creature instances.
+			IReadOnlyCollection<TTemplateEntryType> models = await objectTemplateRepository.RetrieveTemplatesByWorldIdAsync((int)worldId);
+
+			if(models.Count == 0)
+				return BuildFailedResponseModel(CreatureCollectionResponseCode.NoneFound);
+
+			TTemplateTransferType[] templateModels = models
+				.Distinct(DatabaseModelKeyableEquailityComparer<TTemplateEntryType>.Instance)
+				.Select(tableToNetworkModelConverter.Convert)
+				.ToArray();
+
+			return BuildSuccessfulResponseModel(new InstanceObjectEntryCollectionModel<TTemplateTransferType>(templateModels));
+		}
+	}
+}
--- src/Server/GladMMO.Service.ContentServer/Controllers/CreatureTemplateDataController.cs
+++ src/Server/GladMMO.Service.ContentServer/Controllers/CreatureTemplateDataController.cs
@@ -11,50 +11,13 @@ using UnityEngine;
 namespace GladMMO
 {
 	[Route("api/[controller]")]
-	public class CreatureTemplateDataController : AuthorizationReadyController
+	public class CreatureTemplateDataController : TemplateObjectDataController<CreatureTemplateEntryModel, CreatureTemplateModel, ICreatureTemplateRepository>
 	{
 		/// <inheritdoc />
 		public CreatureTemplateDataController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
 			: base(claimsReader, logger)
 		{
 
-		}
-
-		[ProducesJson]
-		[HttpGet("template/{id}")]
-		public async Task<IActionResult> GetCreatureTemplate([FromRoute(Name = "id")] int creatureTemplateId, 
-			[FromServices] ICreatureTemplateRepository creatureTemplateRepository,
-			[FromServices] ITypeConverterProvider<CreatureTemplateEntryModel, CreatureTemplateModel> tableToNetworkModelConverter)
-		{
-			//If unknown templateId, then just indicate such.
-			if (!await creatureTemplateRepository.ContainsAsync(creatureTemplateId))
-				return BuildFailedResponseModel(SceneContentQueryResponseCode.TemplateUnknown);
-
-			//Load the model, convert and send back.
-			CreatureTemplateEntryModel entryModel = await creatureTemplateRepository.RetrieveAsync(creatureTemplateId);
-			CreatureTemplateModel templateModel = tableToNetworkModelConverter.Convert(entryModel);
-
-			return BuildSuccessfulResponseModel(templateModel);
-		}
-
-		[ProducesJson]
-		[HttpGet("{world}/template")]
-		public async Task<IActionResult> GetCreatureTemplates([FromRoute(Name = "world")] int worldId,
-			[FromServices] ICreatureTemplateRepository creatureEntryRepository,
-			[FromServices] ITypeConverterProvider<CreatureTemplateEntryModel, CreatureTemplateModel> tableToNetworkModelConverter)
-		{
-			//We can actually get all the templates FROM the creature instances.
-			IReadOnlyCollection<CreatureTemplateEntryModel> models = await creatureEntryRepository.RetrieveTemplatesByWorldIdAsync((int)worldId);
-
-			if(models.Count == 0)
-				return BuildFailedResponseModel(CreatureCollectionResponseCode.NoneFound);
-
-			CreatureTemplateModel[] templateModels = models
-				.Distinct(DatabaseModelKeyableEquailityComparer<CreatureTemplateEntryModel>.Instance)
-				.Select(tableToNetworkModelConverter.Convert)
-				.ToArray();
-
-			return BuildSuccessfulResponseModel(new InstanceObjectEntryCollectionModel<CreatureTemplateModel>(templateModels));
 		}
 	}
 }
