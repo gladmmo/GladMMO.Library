ca53040cef147e870f325e95d08209644d575113
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 13:40:09 -0500

    Added ITemplateableWorldObjectRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Common/ITemplateableWorldObjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public interface ITemplateableWorldObjectRepository<TTemplateModelType>
+	{
+		//TODO: Doc
+		Task<IReadOnlyCollection<TTemplateModelType>> RetrieveTemplatesByWorldIdAsync(int worldId);
+	}
+}
