86835dc8eebc7256dd95f51cc09a842799f1e9ed
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 16:45:08 -0500

    Added GameObjectTemplateModel

---
+++ src/GladMMO.Models.ContentServer/Models/GameObjectTemplateModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class GameObjectTemplateModel
+	{
+		/// <summary>
+		/// The GameObject's template id.
+		/// </summary>
+		[JsonProperty]
+		public int GameObjectTemplateId { get; private set; }
+
+		/// <summary>
+		/// The ID of the GameObject's model.
+		/// </summary>
+		[JsonProperty]
+		public long ModelId { get; private set; }
+
+		/// <summary>
+		/// The name of the GameObject. Will be the one the client sees, the one the NameQuery will return.
+		/// </summary>
+		[JsonProperty]
+		public string GameObjectName { get; private set; }
+
+		public GameObjectTemplateModel(int creatureTemplateId, long modelId, [NotNull] string gameObjectName)
+		{
+			if (modelId <= 0) throw new ArgumentOutOfRangeException(nameof(modelId));
+			if (string.IsNullOrWhiteSpace(gameObjectName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(gameObjectName));
+			if (creatureTemplateId <= 0) throw new ArgumentOutOfRangeException(nameof(creatureTemplateId));
+
+			GameObjectTemplateId = creatureTemplateId;
+			ModelId = modelId;
+			GameObjectName = gameObjectName;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private GameObjectTemplateModel()
+		{
+			
+		}
+	}
+}
