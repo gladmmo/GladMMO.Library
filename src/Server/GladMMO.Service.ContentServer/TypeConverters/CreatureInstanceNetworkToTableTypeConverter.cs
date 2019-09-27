05b8694955f8e757259e47ac167a002782e80d5b
HelloKitty <andrew.blakely@ymail.com>
2019-09-26 21:10:24 -0500

    Added CreatureInstanceNetworkToTableTypeConverter
    Doesn't fully work, there is no worldid data.

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/CreatureInstanceNetworkToTableTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladMMO.Database;
+using UnityEngine;
+
+namespace GladMMO
+{
+	
+	public sealed class CreatureInstanceNetworkToTableTypeConverter : ITypeConverterProvider<CreatureInstanceModel, CreatureEntryModel>
+	{
+		public CreatureEntryModel Convert([NotNull] CreatureInstanceModel fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			//TODO: Kinda hacky, we don't have a valid world id.
+			//TODO: better handle position crap
+			return new CreatureEntryModel(fromObject.TemplateId, new Vector3<float>(fromObject.InitialPosition.x, fromObject.InitialPosition.y, fromObject.InitialPosition.z), fromObject.YAxisRotation, int.MaxValue);
+		}
+	}
+}
