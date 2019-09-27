a1d3c98a29d3e575ef429d8bbd1b46495044aefd
HelloKitty <andrew.blakely@ymail.com>
2019-09-26 20:48:59 -0500

    Added WorldInstanceableEntryModelCreationContext

---
+++ src/Server/GladMMO.Service.ContentServer/Services/Factory/WorldInstanceableEntryModelCreationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class WorldInstanceableEntryModelCreationContext
+	{
+		public long WorldId { get; }
+
+		public WorldInstanceableEntryModelCreationContext(long worldId)
+		{
+			if (worldId < 0) throw new ArgumentOutOfRangeException(nameof(worldId));
+
+			WorldId = worldId;
+		}
+	}
+}
