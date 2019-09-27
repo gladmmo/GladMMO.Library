3d1df76a056f44abf2ca16d21eef65185b7e21f7
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 16:27:54 -0500

    Implemented database backed IGameObjectTemplateRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/GameObject/DatabaseBackedGameObjectTemplateEntryRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	public sealed class DatabaseBackedGameObjectTemplateEntryRepository : IGameObjectTemplateRepository
+	{
+		private ContentDatabaseContext Context { get; }
+
+		private IGenericRepositoryCrudable<int, GameObjectTemplateEntryModel> GenericRepository { get; }
+
+		public DatabaseBackedGameObjectTemplateEntryRepository([NotNull] ContentDatabaseContext context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+			Context = context;
+
+			GenericRepository = new GeneralGenericCrudRepositoryProvider<int, GameObjectTemplateEntryModel>(context.Set<GameObjectTemplateEntryModel>(), context);
+		}
+
+		public Task<bool> ContainsAsync(int key)
+		{
+			return GenericRepository.ContainsAsync(key);
+		}
+
+		public Task<bool> TryCreateAsync(GameObjectTemplateEntryModel model)
+		{
+			return GenericRepository.TryCreateAsync(model);
+		}
+
+		public Task<GameObjectTemplateEntryModel> RetrieveAsync(int key)
+		{
+			return GenericRepository.RetrieveAsync(key);
+		}
+
+		public Task<bool> TryDeleteAsync(int key)
+		{
+			return GenericRepository.TryDeleteAsync(key);
+		}
+
+		public Task UpdateAsync(int key, GameObjectTemplateEntryModel model)
+		{
+			return GenericRepository.UpdateAsync(key, model);
+		}
+
+		public async Task<string> RetrieveNameAsync(int key)
+		{
+			GameObjectTemplateEntryModel findAsync = await Context.GameObjectTemplates
+				.FindAsync(key);
+
+			return findAsync.GameObjectName;
+		}
+
+		public async Task<IReadOnlyCollection<GameObjectTemplateEntryModel>> RetrieveTemplatesByWorldIdAsync(int worldId)
+		{
+			if(worldId <= 0) throw new ArgumentOutOfRangeException(nameof(worldId));
+
+			throw new NotImplementedException($"TODO: Implement this when GameObject instances exist.");
+
+			/*return await Context.Creatures
+				.Where(c => c.WorldId == worldId)
+				.Include(c => c.CreatureTemplate)
+				.Select(c => c.CreatureTemplate)
+				.ToArrayAsync();*/
+		}
+	}
+}
