f774a08d1f468305b372c7cda1188407591ea244
HelloKitty <andrew.blakely@ymail.com>
2019-09-26 20:51:41 -0500

    Added IModelEntryUpdateable

---
+++ src/Server/GladMMO.Database.GameServer/Models/IModelEntryUpdateable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for types that a data models that can be updated
+	/// with <typeparamref name="TModelType"/>.
+	/// </summary>
+	/// <typeparam name="TModelType">The model type used to update the model.</typeparam>
+	public interface IModelEntryUpdateable<in TModelType>
+	{
+		/// <summary>
+		/// Updates the model with data from <see cref="model"/>.
+		/// </summary>
+		/// <param name="model">The model to be used to update from.</param>
+		void Update(TModelType model);
+	}
+}
