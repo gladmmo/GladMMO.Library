f11e9663df5d5138658a0e75a0632cb3455666f0
HelloKitty <andrew.blakely@ymail.com>
2019-09-26 19:26:40 -0500

    Added IInstanceableWorldObjectRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Common/IInstanceableWorldObjectRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public interface IInstanceableWorldObjectRepository<TInstanceModelType>
+	{
+		/// <summary>
+		/// Retrieves all <see cref="TInstanceModelType"/> with the specified <see cref="worldId"/>.
+		/// This can be used to get the aggregate of Instanceable Objects in a particular zone/map.
+		/// (Ex. You want all the Objects of this type in a city instance, so you query by the instance's mapId so that the zone server
+		/// can load each of the Objects into the city at start up).
+		/// </summary>
+		/// <param name="worldId">The id of the map to load the Object instance data for.</param>
+		/// <returns>A collection of all Object entries with the specified <see cref="worldId"/>.</returns>
+		Task<IReadOnlyCollection<TInstanceModelType>> RetrieveAllWorldIdAsync(int worldId);
+	}
+}
--- src/Server/GladMMO.Database.GameServer/Repositories/Creature/ICreatureEntryRepository.cs
+++ src/Server/GladMMO.Database.GameServer/Repositories/Creature/ICreatureEntryRepository.cs
@@ -5,18 +5,8 @@ using System.Threading.Tasks;
 
 namespace GladMMO
 {
-	public interface ICreatureEntryRepository : IGenericRepositoryCrudable<int, CreatureEntryModel>
+	public interface ICreatureEntryRepository : IGenericRepositoryCrudable<int, CreatureEntryModel>, IInstanceableWorldObjectRepository<CreatureEntryModel>
 	{
-		/// <summary>
-		/// Retrieves all <see cref="CreatureEntryModel"/> with the specified <see cref="worldId"/>.
-		/// This can be used to get the aggregate of Creatures in a particular zone/map.
-		/// (Ex. You want all the Creatures in a city instance, so you query by the instance's mapId so that the zone server
-		/// can load each of the Creatures into the city at start up).
-		/// </summary>
-		/// <param name="worldId">The id of the map to load the Creature instance data for.</param>
-		/// <returns>A collection of all Creature entries with the specified <see cref="worldId"/>.</returns>
-		Task<IReadOnlyCollection<CreatureEntryModel>> RetrieveAllWorldIdAsync(int worldId);
-
 		//TODO: Doc
 		Task<IReadOnlyCollection<CreatureTemplateEntryModel>> RetrieveTemplatesByWorldIdAsync(int worldId);
 	}
