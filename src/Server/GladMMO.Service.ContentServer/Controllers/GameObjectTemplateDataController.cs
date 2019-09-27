e3b0aade149d84ff029feae991cb8a5b97120b14
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 16:59:39 -0500

    Added GameObjectTemplateDataController

---
+++ src/Server/GladMMO.Service.ContentServer/Controllers/GameObjectTemplateDataController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladMMO.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[Route("api/[controller]")]
+	public class GameObjectTemplateDataController : TemplateObjectDataController<GameObjectTemplateEntryModel, GameObjectTemplateModel, IGameObjectTemplateRepository>
+	{
+		/// <inheritdoc />
+		public GameObjectTemplateDataController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger)
+		{
+
+		}
+	}
+}
