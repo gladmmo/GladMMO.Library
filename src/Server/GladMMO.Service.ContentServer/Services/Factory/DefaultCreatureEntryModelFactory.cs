67038bfd51faf6608031579281397a14b71b74fe
HelloKitty <andrew.blakely@ymail.com>
2019-09-26 21:25:55 -0500

    Added DefaultCreatureEntryModelFactory

---
+++ src/Server/GladMMO.Service.ContentServer/Services/Factory/DefaultCreatureEntryModelFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladMMO.Database;
+
+namespace GladMMO
+{
+	public sealed class DefaultCreatureEntryModelFactory : IFactoryCreatable<CreatureEntryModel, WorldInstanceableEntryModelCreationContext>
+	{
+		public CreatureEntryModel Create([NotNull] WorldInstanceableEntryModelCreationContext context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+
+			return new CreatureEntryModel(1, new Vector3<float>(0, 0, 0), 0, context.WorldId);
+		}
+	}
+}
