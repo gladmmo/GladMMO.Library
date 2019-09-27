0e93761b463721667708be170b4a4dc37a252662
HelloKitty <andrew.blakely@ymail.com>
2019-09-26 22:33:25 -0500

    Make Creature Instance Data controller actions Generic
    This will make it a lot easier to add other instanceable content like
    GameObjects and etc.

---
+++ src/Server/GladMMO.Service.ContentServer/Controllers/Base/InstanceObjectDataController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladMMO.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	public abstract class InstanceObjectDataController<TInstanceEntryModelType, TInstanceTransportModelType, TInstanceRepositoryType> : AuthorizationReadyController
+		where TInstanceRepositoryType : IInstanceableWorldObjectRepository<TInstanceEntryModelType>, IGenericRepositoryCrudable<int, TInstanceEntryModelType>
+		where TInstanceEntryModelType : IInstanceableWorldObjectModel, IModelEntryUpdateable<TInstanceEntryModelType>
+		where TInstanceTransportModelType : class
+	{
+		protected InstanceObjectDataController(IClaimsPrincipalReader claimsReader,
+			ILogger<AuthorizationReadyController> logger)
+			: base(claimsReader, logger)
+		{
+
+		}
+
+		[ProducesJson]
+		[HttpGet("{world}/instance")]
+		public async Task<IActionResult> GetObjectEntries([FromRoute(Name = "world")] long worldId,
+			[FromServices] TInstanceRepositoryType instanceObjectRepository,
+			[FromServices] ITypeConverterProvider<TInstanceEntryModelType, TInstanceTransportModelType> tableToNetworkModelConverter)
+		{
+			IReadOnlyCollection<TInstanceEntryModelType> models = await instanceObjectRepository.RetrieveAllWorldIdAsync((int) worldId);
+
+			if (models.Count == 0)
+				return BuildFailedResponseModel(CreatureCollectionResponseCode.NoneFound);
+
+			TInstanceTransportModelType[] instanceModels = models
+				.Select(tableToNetworkModelConverter.Convert)
+				.ToArray();
+
+			return BuildSuccessfulResponseModel(new InstanceObjectEntryCollectionModel<TInstanceTransportModelType>(instanceModels));
+		}
+
+		//TODO: Eventually we need to require authorization, because they need to own the world.
+		//[AuthorizeJwt]
+		[ProducesJson]
+		[HttpPost("{world}/instance")]
+		public async Task<IActionResult> CreateObjectInstance([FromRoute(Name = "world")] long worldId,
+			[FromServices] [NotNull] TInstanceRepositoryType instanceObjectRepository,
+			[FromServices] [NotNull] IFactoryCreatable<TInstanceEntryModelType, WorldInstanceableEntryModelCreationContext> defaultInstanceEntryModelFactory,
+			[FromServices] [NotNull] ITypeConverterProvider<TInstanceEntryModelType, TInstanceTransportModelType> tableToNetworkModelConverter)
+		{
+			if (instanceObjectRepository == null) throw new ArgumentNullException(nameof(instanceObjectRepository));
+			if (defaultInstanceEntryModelFactory == null) throw new ArgumentNullException(nameof(defaultInstanceEntryModelFactory));
+			if (tableToNetworkModelConverter == null) throw new ArgumentNullException(nameof(tableToNetworkModelConverter));
+
+			TInstanceEntryModelType entry = defaultInstanceEntryModelFactory.Create(new WorldInstanceableEntryModelCreationContext(worldId));
+
+			bool result = await instanceObjectRepository.TryCreateAsync(entry);
+
+			//No known reason that this should fail.
+			if (!result)
+				return BuildFailedResponseModel(SceneContentQueryResponseCode.GeneralServerError);
+
+			//Otherwise, it has been created so let's get it
+			return await GetObjectInstance(entry.ObjectInstanceId, instanceObjectRepository, tableToNetworkModelConverter);
+		}
+
+		[ProducesJson]
+		[HttpGet("instance/{id}")]
+		public async Task<IActionResult> GetObjectInstance([FromRoute(Name = "id")] int objectId,
+			[FromServices] TInstanceRepositoryType instanceObjectRepository,
+			[FromServices] ITypeConverterProvider<TInstanceEntryModelType, TInstanceTransportModelType> tableToNetworkModelConverter)
+		{
+			//If unknown templateId, then just indicate such.
+			if (!await instanceObjectRepository.ContainsAsync(objectId))
+				return BuildFailedResponseModel(SceneContentQueryResponseCode.TemplateUnknown);
+
+			//Load the model, convert and send back.
+			TInstanceEntryModelType entryModel = await instanceObjectRepository.RetrieveAsync(objectId);
+			TInstanceTransportModelType instanceModel = tableToNetworkModelConverter.Convert(entryModel);
+
+			return BuildSuccessfulResponseModel(instanceModel);
+		}
+
+		[HttpPut("instance/{id}")]
+		public async Task<IActionResult> UpdateObjectInstance(
+			[FromBody] [NotNull] TInstanceTransportModelType model,
+			[FromRoute(Name = "id")] int objectId,
+			[FromServices] [NotNull] TInstanceRepositoryType instanceObjectRepository,
+			[FromServices] [NotNull] ITypeConverterProvider<TInstanceTransportModelType, TInstanceEntryModelType> networkToTableConverter)
+		{
+			if (model == null) throw new ArgumentNullException(nameof(model));
+			if (instanceObjectRepository == null) throw new ArgumentNullException(nameof(instanceObjectRepository));
+			if(networkToTableConverter == null) throw new ArgumentNullException(nameof(networkToTableConverter));
+
+			//It's TRUE that they should be sending a valid GUID.
+			//But it doesn't matter, it's not this action's responsibility to deal with it.
+			//if(model.Guid.EntryId != creatureId)
+			//	return BadRequest();
+
+			TInstanceEntryModelType entryModel = await instanceObjectRepository.RetrieveAsync(objectId);
+
+			//The idea here is we CONVERT the incoming data model to the table model type
+			//and then we tell the table model type it needs to update.
+			entryModel.Update(networkToTableConverter.Convert(model));
+
+			//Then we save it.
+			await instanceObjectRepository.UpdateAsync(objectId, entryModel);
+
+			return Ok();
+		}
+	}
+}
--- src/Server/GladMMO.Service.ContentServer/Controllers/CreatureDataController.cs
+++ src/Server/GladMMO.Service.ContentServer/Controllers/CreatureDataController.cs
@@ -11,86 +11,13 @@ using UnityEngine;
 namespace GladMMO
 {
 	[Route("api/[controller]")]
-	public class CreatureDataController : AuthorizationReadyController
+	public class CreatureDataController : InstanceObjectDataController<CreatureEntryModel, CreatureInstanceModel, ICreatureEntryRepository>
 	{
 		/// <inheritdoc />
 		public CreatureDataController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
 			: base(claimsReader, logger)
 		{
 
-		}
-
-		//TODO: We need to do authorization so players can't just change anyone's map.
-		[HttpPut("instance/{id}")]
-		public async Task<IActionResult> UpdateCreatureInstance(
-			[FromBody] CreatureInstanceModel model,
-			[FromRoute(Name = "id")] int creatureId,
-			[FromServices] ICreatureEntryRepository creatureEntryRepository)
-		{
-			if (model.Guid.EntryId != creatureId)
-				return BadRequest();
-
-			CreatureEntryModel entryModel = await creatureEntryRepository.RetrieveAsync(creatureId);
-			entryModel.CreatureTemplateId = model.TemplateId;
-			entryModel.InitialOrientation = model.YAxisRotation;
-			entryModel.SpawnPosition = new Vector3<float>(model.InitialPosition.x, model.InitialPosition.y, model.InitialPosition.z);
-
-			await creatureEntryRepository.UpdateAsync(creatureId, entryModel);
-
-			return Ok();
-		}
-
-		[ProducesJson]
-		[HttpGet("{world}/instance")]
-		public async Task<IActionResult> GetCreatureEntries([FromRoute(Name = "world")] long worldId,
-			[FromServices] ICreatureEntryRepository creatureEntryRepository,
-			[FromServices] ITypeConverterProvider<CreatureEntryModel, CreatureInstanceModel> tableToNetworkModelConverter)
-		{
-			IReadOnlyCollection<CreatureEntryModel> models = await creatureEntryRepository.RetrieveAllWorldIdAsync((int)worldId);
-
-			if (models.Count == 0)
-				return BuildFailedResponseModel(CreatureCollectionResponseCode.NoneFound);
-
-			CreatureInstanceModel[] instanceModels = models
-				.Select(tableToNetworkModelConverter.Convert)
-				.ToArray();
-
-			return BuildSuccessfulResponseModel(new InstanceObjectEntryCollectionModel<CreatureInstanceModel>(instanceModels));
-		}
-
-		//TODO: Eventually we need to require authorization, because they need to own the world.
-		//[AuthorizeJwt]
-		[ProducesJson]
-		[HttpPost("{world}/instance")]
-		public async Task<IActionResult> CreateCreatureInstance([FromRoute(Name = "world")] long worldId,
-			[FromServices] ICreatureEntryRepository creatureEntryRepository)
-		{
-			CreatureEntryModel creatureEntryModel = new CreatureEntryModel(1, new Vector3<float>(0, 0, 0), 0, worldId);
-			bool result = await creatureEntryRepository.TryCreateAsync(creatureEntryModel);
-
-			//No known reason that this should fail.
-			if (!result)
-				return BuildFailedResponseModel(SceneContentQueryResponseCode.GeneralServerError);
-
-			//Otherwise, it has been created so let's get it
-			return await GetCreatureInstance(creatureEntryModel.CreatureEntryId, creatureEntryRepository, new CreatureInstanceTableToNetworkTypeConverter());
-		}
-
-		[ProducesJson]
-		[HttpGet("instance/{id}")]
-		public async Task<IActionResult> GetCreatureInstance([FromRoute(Name = "id")] int creatureId,
-			[FromServices] ICreatureEntryRepository creatureEntryRepository,
-			[FromServices] ITypeConverterProvider<CreatureEntryModel, CreatureInstanceModel> tableToNetworkModelConverter)
-		{
-			//If unknown templateId, then just indicate such.
-			if(!await creatureEntryRepository.ContainsAsync(creatureId))
-				return BuildFailedResponseModel(SceneContentQueryResponseCode.TemplateUnknown);
-
-			//Load the model, convert and send back.
-			CreatureEntryModel entryModel = await creatureEntryRepository.RetrieveAsync(creatureId);
-			CreatureInstanceModel instanceModel = tableToNetworkModelConverter.Convert(entryModel);
-
-			return BuildSuccessfulResponseModel(instanceModel);
 		}
 
 		[ProducesJson]
@@ -129,24 +56,5 @@ namespace GladMMO
 
 			return BuildSuccessfulResponseModel(new CreatureTemplateCollectionModel(templateModels));
 		}
-
-		//TODO: We should make it so it requires ZoneServer authorization to query this
-		//[AuthorizeJwt(GuardianApplicationRole.ZoneServer)]
-		/*[ProducesJson]
-		[ResponseCache(Duration = int.MaxValue)]
-		[HttpGet("Map/{id}")]
-		public async Task<IActionResult> GetNpcsOnMap([FromRoute(Name = "id")] int mapId, [FromServices] ICreatureEntryRepository entryRepository)
-		{
-			if(entryRepository == null) throw new ArgumentNullException(nameof(entryRepository));
-
-			IReadOnlyCollection<CreatureEntryModel> entryModels = await entryRepository.RetrieveAllWithMapIdAsync(mapId)
-				.ConfigureAwait(false);
-
-			//TODO: Should this be an OK?
-			if(entryModels.Count == 0)
-				return Ok(new CreatureEntryCollectionResponse(CreatureEntryCollectionResponseCode.NoneFound));
-
-			return base.Ok(new CreatureEntryCollectionResponse(entryModels.Select(npc => BuildDatabaseNPCEntryToTransportNPC(npc)).ToArray()));
-		}*/
 	}
 }
