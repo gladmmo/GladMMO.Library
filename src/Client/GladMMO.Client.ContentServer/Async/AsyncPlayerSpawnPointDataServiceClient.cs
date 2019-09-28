16727fdc928a7922c4331b466505cfe2150e4b51
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 17:00:53 -0500

    Added AsyncPlayerSpawnPointDataServiceClient

---
+++ src/Client/GladMMO.Client.ContentServer/Async/AsyncPlayerSpawnPointDataServiceClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncPlayerSpawnPointDataServiceClient : BaseAsyncEndpointService<IPlayerSpawnPointDataServiceClient>, IPlayerSpawnPointDataServiceClient
+	{
+		/// <inheritdoc />
+		public AsyncPlayerSpawnPointDataServiceClient(Task<string> futureEndpoint) 
+			: base(futureEndpoint)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public AsyncPlayerSpawnPointDataServiceClient(Task<string> futureEndpoint, RefitSettings settings) 
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		public async Task<ResponseModel<PlayerSpawnPointInstanceModel, SceneContentQueryResponseCode>> GetSpawnPointInstance(int spawnPointId)
+		{
+			return await (await GetService().ConfigureAwait(false)).GetSpawnPointInstance(spawnPointId).ConfigureAwait(false);
+		}
+
+		public async Task<ResponseModel<PlayerSpawnPointInstanceModel, SceneContentQueryResponseCode>> CreateSpawnPointInstance(long worldId)
+		{
+			return await (await GetService().ConfigureAwait(false)).CreateSpawnPointInstance(worldId).ConfigureAwait(false);
+		}
+
+		public async Task UpdateSpawnPointInstance(int spawnPointId, PlayerSpawnPointInstanceModel model)
+		{
+			await (await GetService().ConfigureAwait(false)).UpdateSpawnPointInstance(spawnPointId, model).ConfigureAwait(false);
+		}
+
+		public async Task<ResponseModel<ObjectEntryCollectionModel<PlayerSpawnPointInstanceModel>, ContentEntryCollectionResponseCode>> GetSpawnPointEntriesByWorld(long worldId)
+		{
+			return await (await GetService().ConfigureAwait(false)).GetSpawnPointEntriesByWorld(worldId).ConfigureAwait(false);
+		}
+	}
+}
