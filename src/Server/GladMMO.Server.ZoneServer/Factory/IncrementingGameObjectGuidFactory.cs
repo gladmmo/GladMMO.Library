5b2c6871ea88667bb370226e795f72b5046cab73
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 00:44:33 -0500

    Added IncrementingGameObjectGuidFactory

---
+++ src/Server/GladMMO.Server.ZoneServer/Factory/IncrementingGameObjectGuidFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace GladMMO
+{
+	public sealed class IncrementingGameObjectGuidFactory : IFactoryCreatable<NetworkEntityGuid, GameObjectInstanceModel>
+	{
+		/// <summary>
+		/// The atomically incremented (or should be) counter for unique gameobject guid issueing requests.
+		/// </summary>
+		private int GameObjectCount = 0;
+
+		public NetworkEntityGuid Create(GameObjectInstanceModel context)
+		{
+			if(!context.Guid.isTemplateGuid)
+				throw new InvalidOperationException($"Cannot create instance {nameof(NetworkEntityGuid)} from non-Template {nameof(NetworkEntityGuid)}.");
+
+			NetworkEntityGuidBuilder builder = new NetworkEntityGuidBuilder();
+
+			return builder.WithId(Interlocked.Increment(ref GameObjectCount))
+				.WithType(EntityType.GameObject)
+				.WithEntryId(context.Guid.EntryId)
+				.Build();
+		}
+	}
+}
