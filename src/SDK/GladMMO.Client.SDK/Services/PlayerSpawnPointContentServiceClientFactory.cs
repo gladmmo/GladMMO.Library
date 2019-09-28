d98eb05493faa5651f9dd8bae3794a4f68926976
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 17:01:42 -0500

    Added PlayerSpawnPointContentServiceClientFactory

---
+++ src/SDK/GladMMO.Client.SDK/Services/PlayerSpawnPointContentServiceClientFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+
+namespace GladMMO.SDK
+{
+	public sealed class PlayerSpawnPointContentServiceClientFactory : IFactoryCreatable<IPlayerSpawnPointDataServiceClient, EmptyFactoryContext>
+	{
+		public IPlayerSpawnPointDataServiceClient Create(EmptyFactoryContext context)
+		{
+			return Refit.RestService.For<IPlayerSpawnPointDataServiceClient>("http://72.190.177.214:5005/", new RefitSettings() { HttpMessageHandlerFactory = () => new AuthenticatedHttpClientHandler(new ContentSDKAuthenticationTokenRepository()) });
+		}
+	}
+}
