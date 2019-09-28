84a8ec11af865a10eb8f0a873137bec4344b1abc
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 01:44:37 -0500

    Added IObjectTemplateModel

---
+++ src/GladMMO.Models.ContentServer/Models/IObjectTemplateModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for types that are a data model
+	/// for object templates.
+	/// </summary>
+	public interface IObjectTemplateModel
+	{
+		/// <summary>
+		/// The unique identifier for the template
+		/// the model represents.
+		/// </summary>
+		int TemplateId { get; }
+	}
+}
