4867b44fcdbaac981eda0d1a572d0fa89365aa7b
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 22:28:56 -0500

    Added GameObjectContentServiceClientFactory

---
+++ src/SDK/GladMMO.Client.SDK/Services/GameObjectContentServiceClientFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+
+namespace GladMMO.SDK
+{
+	public sealed class GameObjectContentServiceClientFactory : IFactoryCreatable<IGameObjectDataServiceClient, EmptyFactoryContext>
+	{
+		public IGameObjectDataServiceClient Create(EmptyFactoryContext context)
+		{
+			return Refit.RestService.For<IGameObjectDataServiceClient>("http://72.190.177.214:5005/", new RefitSettings() { HttpMessageHandlerFactory = () => new AuthenticatedHttpClientHandler(new ContentSDKAuthenticationTokenRepository()) });
+		}
+	}
+}
