a326f2f54ee77190869f2ffd83fc29d3dea0b7a3
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 16:31:51 -0500

    Added PlayerSpawnPointInstanceModel

---
+++ src/GladMMO.Models.ContentServer/Models/PlayerSpawnPointInstanceModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class PlayerSpawnPointInstanceModel
+	{
+		[JsonProperty]
+		[JsonRequired]
+		public int SpawnPointId { get; private set; }
+
+		/// <summary>
+		/// The initial spawn position for the entry.
+		/// </summary>
+		[JsonRequired]
+		[JsonProperty]
+		[JsonConverter(typeof(Vector3Converter))] //TODO: Make custom attribute
+		public Vector3 InitialPosition { get; private set; }
+
+		/// <summary>
+		/// Creature's initial rotation around the Y-Axis.
+		/// </summary>
+		[JsonRequired]
+		[JsonProperty]
+		public float YAxisRotation { get; private set; }
+
+		/// <inheritdoc />
+		public PlayerSpawnPointInstanceModel(int spawnPointId, Vector3 initialPosition, float yRotation)
+		{
+			if (spawnPointId <= 0) throw new ArgumentOutOfRangeException(nameof(spawnPointId));
+
+			SpawnPointId = spawnPointId;
+			InitialPosition = initialPosition;
+			YAxisRotation = yRotation;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		protected PlayerSpawnPointInstanceModel()
+		{
+			
+		}
+	}
+}
