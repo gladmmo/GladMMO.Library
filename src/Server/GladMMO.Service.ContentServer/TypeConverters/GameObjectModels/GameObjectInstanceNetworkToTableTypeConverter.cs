8b6fc0f59de58ca59cc68c9ab9c3f20215f658f2
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 23:46:44 -0500

    Added GameObjectInstance DB to Transfer DTO type converters

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/GameObjectModels/GameObjectInstanceNetworkToTableTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladMMO.Database;
+using UnityEngine;
+
+namespace GladMMO
+{
+	
+	public sealed class GameObjectInstanceNetworkToTableTypeConverter : ITypeConverterProvider<GameObjectInstanceModel, GameObjectEntryModel>
+	{
+		public GameObjectEntryModel Convert([NotNull] GameObjectInstanceModel fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			//TODO: Kinda hacky, we don't have a valid world id.
+			//TODO: better handle position crap
+			return new GameObjectEntryModel(fromObject.TemplateId, new Vector3<float>(fromObject.InitialPosition.x, fromObject.InitialPosition.y, fromObject.InitialPosition.z), fromObject.YAxisRotation, int.MaxValue);
+		}
+	}
+}
---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/GameObjectModels/GameObjectInstanceTableToNetworkTypeConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class GameObjectInstanceTableToNetworkTypeConverter : ITypeConverterProvider<GameObjectEntryModel, GameObjectInstanceModel>
+	{
+		public GameObjectInstanceModel Convert([NotNull] GameObjectEntryModel fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			NetworkEntityGuid guid = new NetworkEntityGuidBuilder()
+				.WithId(0) //0 means that it's not an instance.
+				.WithType(EntityType.Creature)
+				.WithEntryId(fromObject.GameObjectEntryId)
+				.Build();
+
+			//TODO: better handle position crap
+			return new GameObjectInstanceModel(guid, fromObject.GameObjectTemplateId, new Vector3(fromObject.SpawnPosition.X, fromObject.SpawnPosition.Y, fromObject.SpawnPosition.Z), fromObject.InitialOrientation);
+		}
+	}
+}
