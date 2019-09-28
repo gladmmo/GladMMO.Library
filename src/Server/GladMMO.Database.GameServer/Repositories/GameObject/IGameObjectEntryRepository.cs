efbc110f2d8645cf0518f2730b6a51cc44f78a5f
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 22:09:11 -0500

    Added IGameObjectEntryRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/GameObject/IGameObjectEntryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public interface IGameObjectEntryRepository : IGenericRepositoryCrudable<int, GameObjectEntryModel>, IInstanceableWorldObjectRepository<GameObjectEntryModel>
+	{
+
+	}
+}
