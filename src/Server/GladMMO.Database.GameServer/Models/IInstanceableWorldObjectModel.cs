3049953c28482d707d1d8340ede6cd88209683ad
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 20:40:34 -0500

    Added Rotation and Position to IInstanceableWorldObjectModel

--- src/Server/GladMMO.Database.GameServer/Models/IInstanceableWorldObjectModel.cs
+++ src/Server/GladMMO.Database.GameServer/Models/IInstanceableWorldObjectModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using GladMMO.Database;
 
 namespace GladMMO
 {
@@ -10,5 +13,16 @@ namespace GladMMO
 		/// The unique identifier for the instance.
 		/// </summary>
 		int ObjectInstanceId { get; }
+
+		/// <summary>
+		/// The spawn position of the GameObject.
+		/// </summary>
+		Vector3<float> SpawnPosition { get; set; }
+
+		/// <summary>
+		/// The initial Y-axis orientation/rotation of the GameObject when spawned.
+		/// Especially important for stationary GameObjects.
+		/// </summary>
+		float InitialOrientation { get; set; }
 	}
 }
