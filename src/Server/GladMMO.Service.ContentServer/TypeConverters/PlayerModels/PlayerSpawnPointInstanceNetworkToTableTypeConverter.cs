868015d9ee3fb179918ab5ed21d36773e4f5f683
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 16:37:34 -0500

    Added PlayerSpawnPoint type converters

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/PlayerModels/PlayerSpawnPointInstanceNetworkToTableTypeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladMMO.Database;
+using UnityEngine;
+
+namespace GladMMO
+{
+	
+	public sealed class PlayerSpawnPointInstanceNetworkToTableTypeConverter : ITypeConverterProvider<PlayerSpawnPointInstanceModel, PlayerSpawnPointEntryModel>
+	{
+		public PlayerSpawnPointEntryModel Convert([NotNull] PlayerSpawnPointInstanceModel fromObject)
+		{
+			if(fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			//TODO: Kinda hacky, we don't have a valid world id.
+			//TODO: better handle position crap
+			return new PlayerSpawnPointEntryModel(new Vector3<float>(fromObject.InitialPosition.x, fromObject.InitialPosition.y, fromObject.InitialPosition.z), fromObject.YAxisRotation, fromObject.SpawnPointId, fromObject.isReserved);
+		}
+	}
+}
