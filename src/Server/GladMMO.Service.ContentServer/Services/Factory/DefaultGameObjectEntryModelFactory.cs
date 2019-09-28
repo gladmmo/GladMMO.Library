edf98ac7cec8dcd2ecf8b089ed675091f5623725
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 23:43:19 -0500

    Added DefaultGameObjectEntryModelFactory

---
+++ src/Server/GladMMO.Service.ContentServer/Services/Factory/DefaultGameObjectEntryModelFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladMMO.Database;
+
+namespace GladMMO
+{
+	public sealed class DefaultGameObjectEntryModelFactory : IFactoryCreatable<GameObjectEntryModel, WorldInstanceableEntryModelCreationContext>
+	{
+		public GameObjectEntryModel Create([NotNull] WorldInstanceableEntryModelCreationContext context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+
+			return new GameObjectEntryModel(1, new Vector3<float>(0, 0, 0), 0, context.WorldId);
+		}
+	}
+}
