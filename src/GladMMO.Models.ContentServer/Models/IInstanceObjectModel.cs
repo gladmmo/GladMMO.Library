6f7b560377c622b9e49dff7635c2ee490fe0db9c
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 01:44:29 -0500

    Added IInstanceObjectModel

---
+++ src/GladMMO.Models.ContentServer/Models/IInstanceObjectModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IInstanceObjectModel
+	{
+		/// <summary>
+		/// The object template id of the entry.
+		/// </summary>
+		int TemplateId { get;  }
+	}
+}
