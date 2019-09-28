3f076d9d392054af4d696a658335d4bb6f4853cc
HelloKitty <andrew.blakely@ymail.com>
2019-09-27 22:54:11 -0500

    Added GameObjectStaticSpawnPointDefinition

---
+++ src/SDK/GladMMO.Common.SDK/Models/GameObject/GameObjectStaticSpawnPointDefinition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class GameObjectStaticSpawnPointDefinition : StaticSpawnPointDefinition
+	{
+		public override EntityType EntitySpawnType => EntityType.GameObject;
+
+		[HideInInspector]
+		[Tooltip("This should be the ID of the gameobject template that holds information about what gameobject is suppose to spawn.")]
+		[SerializeField]
+		private int _GameObjectTemplateId = -1; //default to -1 so it's not known.
+
+		//We don't show this in the inspector because the user should not be changing this.
+		[HideInInspector]
+		[SerializeField]
+		private int _GameObjectInstanceId = -1; //default to -1 so it's not known.
+
+		/// <summary>
+		/// The ID of the GameObject template to use for spawning.
+		/// </summary>
+		public int GameObjectTemplateId
+		{
+			get => _GameObjectTemplateId;
+			set => _GameObjectTemplateId = value; //TODO: Make internal
+		}
+
+		/// <summary>
+		/// The ID of the GameObject instance. Basically the global instance id of a GameObject.
+		/// </summary>
+		public int GameObjectInstanceId
+		{
+			get => _GameObjectInstanceId;
+			set => _GameObjectInstanceId = value; //TODO: Make internal
+		}
+	}
+}
