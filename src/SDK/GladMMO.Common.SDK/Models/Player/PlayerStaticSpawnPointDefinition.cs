3570f723df877fa7ed549724d7f388a944f36481
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 16:52:00 -0500

    Added PlayerSpawnPointId to to PlayerStaticSpawnPointDefinition

--- src/SDK/GladMMO.Common.SDK/Models/Player/PlayerStaticSpawnPointDefinition.cs
+++ src/SDK/GladMMO.Common.SDK/Models/Player/PlayerStaticSpawnPointDefinition.cs
@@ -1,11 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace GladMMO
 {
 	public sealed class PlayerStaticSpawnPointDefinition : StaticSpawnPointDefinition
 	{
 		public override EntityType EntitySpawnType => EntityType.Player;
+
+		[HideInInspector]
+		[SerializeField]
+		private int _PlayerSpawnPointId = -1; //default to -1 so it's not known.
+
+		/// <summary>
+		/// The ID of the spawnpoint.
+		/// </summary>
+		public int PlayerSpawnPointId
+		{
+			get => _PlayerSpawnPointId;
+			set => _PlayerSpawnPointId = value; //TODO: Make internal
+		}
 	}
 }
