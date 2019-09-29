1b0929b03c822fe955f884b6789e5a6379a2dfce
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 22:32:07 -0500

    Added IWorldTeleporterGameObjectEntryRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/GameObject/IWorldTeleporterGameObjectEntryRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IWorldTeleporterGameObjectEntryRepository : IGenericRepositoryCrudable<int, GameObjectWorldTeleporterEntryModel>
+	{
+
+	}
+}
