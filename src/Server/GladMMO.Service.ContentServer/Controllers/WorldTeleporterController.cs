b525cb7f53a93e6cf489c878530a0432df5beb61
HelloKitty <andrew.blakely@ymail.com>
2019-09-29 15:32:06 -0500

    Fixed generic params of WorldTeleporterController

--- src/Server/GladMMO.Service.ContentServer/Controllers/WorldTeleporterController.cs
+++ src/Server/GladMMO.Service.ContentServer/Controllers/WorldTeleporterController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace GladMMO
 {
 	[Route("api/[controller]")]
-	public sealed class WorldTeleporterController : InstanceObjectDataController<GameObjectWorldTeleporterEntryModel, object, IWorldTeleporterGameObjectEntryRepository>
+	public sealed class WorldTeleporterController : InstanceObjectDataController<GameObjectWorldTeleporterEntryModel, WorldTeleporterInstanceModel, IWorldTeleporterGameObjectEntryRepository>
 	{
 		public WorldTeleporterController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
 			: base(claimsReader, logger)
@@ -16,7 +16,7 @@ namespace GladMMO
 
 		}
 
-		public override Task<IActionResult> CreateObjectInstance(long worldId, IWorldTeleporterGameObjectEntryRepository instanceObjectRepository, IFactoryCreatable<GameObjectWorldTeleporterEntryModel, WorldInstanceableEntryModelCreationContext> defaultInstanceEntryModelFactory, ITypeConverterProvider<GameObjectWorldTeleporterEntryModel, object> tableToNetworkModelConverter)
+		public override Task<IActionResult> CreateObjectInstance(long worldId, IWorldTeleporterGameObjectEntryRepository instanceObjectRepository, IFactoryCreatable<GameObjectWorldTeleporterEntryModel, WorldInstanceableEntryModelCreationContext> defaultInstanceEntryModelFactory, ITypeConverterProvider<GameObjectWorldTeleporterEntryModel, WorldTeleporterInstanceModel> tableToNetworkModelConverter)
 		{
 			return Task.FromResult((IActionResult)BadRequest("This request is not valid for World Teleporters."));
 		}
