b501aad40bfa6f5224e5efdda602f9b006c8cf9a
HelloKitty <andrew.blakely@ymail.com>
2019-09-29 14:45:45 -0500

    Created intial content controller for WorldTeleporter

---
+++ src/Server/GladMMO.Service.ContentServer/Controllers/WorldTeleporterController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	[Route("api/[controller]")]
+	public sealed class WorldTeleporterController : InstanceObjectDataController<GameObjectWorldTeleporterEntryModel, object, IWorldTeleporterGameObjectEntryRepository>
+	{
+		public WorldTeleporterController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger)
+		{
+
+		}
+	}
+}
