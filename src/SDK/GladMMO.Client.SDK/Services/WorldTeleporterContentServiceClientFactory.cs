4e4fc8ff5466dd93e465cf1dd6fef54c3e50a759
HelloKitty <andrew.blakely@ymail.com>
2019-09-29 16:51:28 -0500

    Added WorldTeleporterContentServiceClientFactory

---
+++ src/SDK/GladMMO.Client.SDK/Services/WorldTeleporterContentServiceClientFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+
+namespace GladMMO.SDK
+{
+	public sealed class WorldTeleporterContentServiceClientFactory : IFactoryCreatable<IWorldTeleporterDataServiceClient, EmptyFactoryContext>
+	{
+		public IWorldTeleporterDataServiceClient Create(EmptyFactoryContext context)
+		{
+			return Refit.RestService.For<IWorldTeleporterDataServiceClient>("http://72.190.177.214:5005/", new RefitSettings() { HttpMessageHandlerFactory = () => new AuthenticatedHttpClientHandler(new ContentSDKAuthenticationTokenRepository()) });
+		}
+	}
+}
