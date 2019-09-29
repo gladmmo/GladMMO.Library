33a319c07b60291279849aa0bad96dcd0034310c
HelloKitty <andrew.blakely@ymail.com>
2019-09-29 17:10:53 -0500

    Added WorldTeleporter typeconverters

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/WorldTeleporters/WorldTeleporterInstanceNetworkToTableTypeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladMMO.Database;
+using UnityEngine;
+
+namespace GladMMO
+{
+	
+	public sealed class WorldTeleporterInstanceNetworkToTableTypeConverter : ITypeConverterProvider<WorldTeleporterInstanceModel, GameObjectWorldTeleporterEntryModel>
+	{
+		public GameObjectWorldTeleporterEntryModel Convert([NotNull] WorldTeleporterInstanceModel fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			return new GameObjectWorldTeleporterEntryModel(fromObject.TargetGameObjectId, fromObject.LocalSpawnPointId, fromObject.RemoteSpawnPointId);
+		}
+	}
+}
---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/WorldTeleporters/WorldTeleporterInstanceTableToNetworkTypeConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class WorldTeleporterInstanceTableToNetworkTypeConverter : ITypeConverterProvider<GameObjectWorldTeleporterEntryModel, WorldTeleporterInstanceModel>
+	{
+		public WorldTeleporterInstanceModel Convert([NotNull] GameObjectWorldTeleporterEntryModel fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+			
+			return new WorldTeleporterInstanceModel(fromObject.TargetGameObjectId, fromObject.LocalSpawnPointId, fromObject.RemoteSpawnPointId);
+		}
+	}
+}
