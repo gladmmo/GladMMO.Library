0d7e118c632c23bfdcd6b038e2f4ca85ecccbf32
HelloKitty <andrew.blakely@ymail.com>
2019-09-29 15:35:33 -0500

    Implemented WorldTeleporterController.UpdateObjectInstance override

--- src/Server/GladMMO.Service.ContentServer/Controllers/WorldTeleporterController.cs
+++ src/Server/GladMMO.Service.ContentServer/Controllers/WorldTeleporterController.cs
@@ -20,5 +20,23 @@ namespace GladMMO
 		{
 			return Task.FromResult((IActionResult)BadRequest("This request is not valid for World Teleporters."));
 		}
+
+		public override async Task<IActionResult> UpdateObjectInstance(WorldTeleporterInstanceModel model, int objectId, IWorldTeleporterGameObjectEntryRepository instanceObjectRepository, ITypeConverterProvider<WorldTeleporterInstanceModel, GameObjectWorldTeleporterEntryModel> networkToTableConverter)
+		{
+			//So, this one is abit different since there is no created instance.
+			//The client needs to create it, we don't assign a default behavior.
+			//So it's possible it DOESN'T exist.
+			if(await instanceObjectRepository.ContainsAsync(objectId))
+				return await base.UpdateObjectInstance(model, objectId, instanceObjectRepository, networkToTableConverter);
+			else
+			{
+				//it doesn't exist, we need to try to create it.
+				GameObjectWorldTeleporterEntryModel entryModel = networkToTableConverter.Convert(model);
+
+				await instanceObjectRepository.TryCreateAsync(entryModel);
+			}
+
+			return Ok();
+		}
 	}
 }
