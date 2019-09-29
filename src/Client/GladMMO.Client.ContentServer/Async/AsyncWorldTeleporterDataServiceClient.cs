f2661ca69a90eebebceae0541900af83bf1cbcbd
HelloKitty <andrew.blakely@ymail.com>
2019-09-29 15:53:43 -0500

    Added AsyncWorldTeleporterDataServiceClient

---
+++ src/Client/GladMMO.Client.ContentServer/Async/AsyncWorldTeleporterDataServiceClient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncWorldTeleporterDataServiceClient : BaseAsyncEndpointService<IWorldTeleporterDataServiceClient>, IWorldTeleporterDataServiceClient
+	{
+		/// <inheritdoc />
+		public AsyncWorldTeleporterDataServiceClient(Task<string> futureEndpoint) 
+			: base(futureEndpoint)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public AsyncWorldTeleporterDataServiceClient(Task<string> futureEndpoint, RefitSettings settings) 
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+
+		public async Task<ResponseModel<WorldTeleporterInstanceModel, SceneContentQueryResponseCode>> GetWorldTeleporterInstance(int gameObjectId)
+		{
+			return await (await GetService().ConfigureAwait(false)).GetWorldTeleporterInstance(gameObjectId).ConfigureAwait(false);
+		}
+
+		public async Task UpdateGameObjectInstance(int gameObjectId, WorldTeleporterInstanceModel model)
+		{
+			await (await GetService().ConfigureAwait(false)).UpdateGameObjectInstance(gameObjectId, model).ConfigureAwait(false);
+		}
+
+		public async Task<ResponseModel<ObjectEntryCollectionModel<WorldTeleporterInstanceModel>, ContentEntryCollectionResponseCode>> GetWorldTeleporterEntriesByWorld(long worldId)
+		{
+			return await (await GetService().ConfigureAwait(false)).GetWorldTeleporterEntriesByWorld(worldId).ConfigureAwait(false);
+		}
+	}
+}
