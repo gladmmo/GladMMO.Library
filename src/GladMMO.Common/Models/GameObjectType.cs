7857e0b410b364c6ce86de7cfa6352a61a4d9a34
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 19:51:46 -0500

    Added GameObjectType enum

---
+++ src/GladMMO.Common/Models/GameObjectType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Enumeration of all known game object types.
+	/// </summary>
+	public enum GameObjectType
+	{
+		/// <summary>
+		/// Represents just a visual gameobject
+		/// with no other functionality.
+		/// </summary>
+		Visual = 0,
+
+		/// <summary>
+		/// Represents a gameobject that teleports
+		/// players between worlds.
+		/// </summary>
+		WorldTeleporter = 1,
+	}
+}
