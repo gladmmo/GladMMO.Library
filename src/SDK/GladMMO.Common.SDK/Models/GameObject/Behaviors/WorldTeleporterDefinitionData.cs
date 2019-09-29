9e044470efedc45a9c5b5e1e6db62d5f0398bd77
HelloKitty <andrew.blakely@ymail.com>
2019-09-28 22:15:01 -0500

    Added WorldTeleporterDefinitionData

---
+++ src/SDK/GladMMO.Common.SDK/Models/GameObject/Behaviors/WorldTeleporterDefinitionData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO.SDK
+{
+	public sealed class WorldTeleporterDefinitionData : GladMMOSDKMonoBehaviour, INetworkGameObjectBehaviour
+	{
+		public GameObjectType BehaviorType => GameObjectType.WorldTeleporter;
+
+		[HideInInspector]
+		[SerializeField]
+		private int _TargetTeleportWorldId;
+
+		[HideInInspector]
+		[SerializeField]
+		private PlayerStaticSpawnPointDefinition _LocalSpawnPointId;
+
+		[HideInInspector]
+		[SerializeField]
+		private int _RemoteSpawnPointId;
+
+		public PlayerStaticSpawnPointDefinition LocalSpawnPointId
+		{
+			get => _LocalSpawnPointId;
+			set => _LocalSpawnPointId = value;
+		}
+
+		public int RemoteSpawnPointId
+		{
+			get => _RemoteSpawnPointId;
+			set => _RemoteSpawnPointId = value;
+		}
+
+		public int TargetTeleportWorldId
+		{
+			get => _TargetTeleportWorldId;
+			set => _TargetTeleportWorldId = value;
+		}
+	}
+}
