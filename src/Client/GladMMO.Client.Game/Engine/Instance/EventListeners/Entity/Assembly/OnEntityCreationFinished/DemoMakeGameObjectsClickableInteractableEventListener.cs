0c860fb04bac0fd9c2075f7e783cd7d0d6839a3f
HelloKitty <andrew.blakely@ymail.com>
2019-10-03 04:10:12 -0500

    Added demo gameobject interaction

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/Assembly/OnEntityCreationFinished/DemoMakeGameObjectsClickableInteractableEventListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GladMMO.Component;
+using GladNet;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class DemoMakeGameObjectsClickableInteractableEventListener : GameObjectCreationFinishedEventListener
+	{
+		private IReadonlyEntityGuidMappable<GameObject> GameObjectMappable { get; }
+
+		private IPeerPayloadSendService<GameClientPacketPayload> SendService { get; }
+
+		public DemoMakeGameObjectsClickableInteractableEventListener(IEntityCreationFinishedEventSubscribable subscriptionService,
+			[NotNull] IReadonlyEntityGuidMappable<GameObject> gameObjectMappable,
+			[NotNull] IPeerPayloadSendService<GameClientPacketPayload> sendService) 
+			: base(subscriptionService)
+		{
+			GameObjectMappable = gameObjectMappable ?? throw new ArgumentNullException(nameof(gameObjectMappable));
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+		}
+
+		protected override void OnEntityCreationFinished(EntityCreationFinishedEventArgs args)
+		{
+			GameObject entity = GameObjectMappable.RetrieveEntity(args.EntityGuid);
+
+			//Adding a test/demo collider to allow for a clicking volume.
+			BoxCollider collider = entity.AddComponent<BoxCollider>();
+			collider.isTrigger = true;
+			entity.AddComponent<OnMouseClickedComponent>().OnMouseClicked += (sender, eventArgs) =>
+			{
+				SendService.SendMessage(new ClientInteractGameObjectRequestPayload(args.EntityGuid));
+			};
+		}
+	}
+}
