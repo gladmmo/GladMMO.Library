c2aba175c21cba2f638e3c7cc9f73400472bb677
HelloKitty <andrew.blakely@ymail.com>
2019-10-03 01:23:05 -0500

    Added initial interface IWorldInteractable

---
+++ src/Server/GladMMO.Server.ZoneServer/EntityComponents/IWorldInteractable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for world objects that are interactable.
+	/// </summary>
+	public interface IWorldInteractable
+	{
+		/// <summary>
+		/// Interacts with the <see cref="IWorldInteractable"/>.
+		/// </summary>
+		/// <param name="entityInteracting">The guid of the entity attempting to interact with the interactable.</param>
+		void Interact(NetworkEntityGuid entityInteracting);
+	}
+}
