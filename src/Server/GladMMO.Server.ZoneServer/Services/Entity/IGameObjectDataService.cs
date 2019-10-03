aa09f4c0cd0593c1a554d682b01632bc33bb5fdb
HelloKitty <andrew.blakely@ymail.com>
2019-10-02 20:24:34 -0500

    Added IGameObjectDataService

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Entity/IGameObjectDataService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public interface IGameObjectDataService
+	{
+		/// <summary>
+		/// Loads all the GameObject data async.
+		/// This may have to make many remote service calls.
+		/// </summary>
+		/// <returns>An awaitable.</returns>
+		Task LoadDataAsync();
+
+		/// <summary>
+		/// All loaded <see cref="GameObjectTemplateModel"/>s.
+		/// This will be empty if <see cref="LoadDataAsync"/> is not called.
+		/// </summary>
+		IReadonlyEntityGuidMappable<GameObjectTemplateModel> GameObjectTemplateMappable { get; }
+
+		/// <summary>
+		/// All loaded <see cref="GameObjectInstanceModel"/>s.
+		/// This will be empty if <see cref="LoadDataAsync"/> is not called.
+		/// </summary>
+		IReadonlyEntityGuidMappable<GameObjectInstanceModel> GameObjectInstanceMappable { get; }
+
+		/// <summary>
+		/// All loaded <see cref="WorldTeleporterInstanceModel"/>s.
+		/// This will be empty if <see cref="LoadDataAsync"/> is not called.
+		/// </summary>
+		IReadonlyEntityGuidMappable<WorldTeleporterInstanceModel> WorldTeleporterInstanceMappable { get; }
+	}
+}
