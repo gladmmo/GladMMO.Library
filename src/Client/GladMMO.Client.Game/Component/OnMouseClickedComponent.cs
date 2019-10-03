5763edc78909b9d03e24f7bd8e04ab003afce9bc
HelloKitty <andrew.blakely@ymail.com>
2019-10-03 04:05:51 -0500

    Added demo OnMouseClickedComponent

---
+++ src/Client/GladMMO.Client.Game/Component/OnMouseClickedComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO.Component
+{
+	public sealed class OnMouseClickedComponent : MonoBehaviour
+	{
+		public event EventHandler OnMouseClicked;
+
+		private void OnMouseDown()
+		{
+			OnMouseClicked?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
