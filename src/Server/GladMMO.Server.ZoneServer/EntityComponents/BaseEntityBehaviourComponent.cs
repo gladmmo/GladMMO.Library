fdc947448a12306049042141bce8689c65e650be
HelloKitty <andrew.blakely@ymail.com>
2019-10-02 23:46:47 -0500

    Added BaseEntityBehaviourComponent

---
+++ src/Server/GladMMO.Server.ZoneServer/EntityComponents/BaseEntityBehaviourComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class BaseEntityBehaviourComponent
+	{
+		protected NetworkEntityGuid TargetEntity { get; }
+
+		public BaseEntityBehaviourComponent([NotNull] NetworkEntityGuid targetEntity)
+		{
+			TargetEntity = targetEntity ?? throw new ArgumentNullException(nameof(targetEntity));
+		}
+	}
+}
