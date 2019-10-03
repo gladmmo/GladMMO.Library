d558f7e21fa47934ecba99a24a9e5eb06188883f
HelloKitty <andrew.blakely@ymail.com>
2019-10-02 20:18:24 -0500

    Added InstanceStaticEntityDataCollection

---
+++ src/Server/GladMMO.Server.ZoneServer/Collections/InstanceStaticEntityDataCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	//TODO: Verify against entity type.
+	/// <summary>
+	/// <see cref="IEntityGuidMappable{TValue}"/> implementation that shares static <typeparamref name="TInstanceLinkedDataType"/> component
+	/// data across all <see cref="NetworkEntityGuid"/> that share an entry.
+	/// </summary>
+	/// <typeparam name="TInstanceLinkedDataType">The linked/shared component data type.</typeparam>
+	public sealed class InstanceStaticEntityDataCollection<TInstanceLinkedDataType> : IEntityGuidMappable<TInstanceLinkedDataType>
+	{
+		protected Dictionary<int, TInstanceLinkedDataType> InstanceLinkedDataMap { get; }
+
+		public InstanceStaticEntityDataCollection()
+		{
+			InstanceLinkedDataMap = new Dictionary<int, TInstanceLinkedDataType>();
+		}
+
+		public bool ContainsKey(NetworkEntityGuid key)
+		{
+			return InstanceLinkedDataMap.ContainsKey(key.EntryId);
+		}
+
+		public void Add(NetworkEntityGuid key, TInstanceLinkedDataType value)
+		{
+			InstanceLinkedDataMap.Add(key.EntryId, value);
+		}
+
+		public TInstanceLinkedDataType this[NetworkEntityGuid key]
+		{
+			get => InstanceLinkedDataMap[key.EntryId];
+			set => InstanceLinkedDataMap[key.EntryId] = value;
+		}
+
+		public bool RemoveEntityEntry(NetworkEntityGuid entityGuid)
+		{
+			return InstanceLinkedDataMap.Remove(entityGuid.EntryId);
+		}
+	}
+}
