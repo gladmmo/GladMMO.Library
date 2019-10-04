bcd88e1a2918f936f922fcda1bf2a36391a41179
HelloKitty <andrew.blakely@ymail.com>
2019-10-03 19:45:10 -0500

    Added EntitySaveableConfiguration model

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Entity/Models/EntitySaveableConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class EntitySaveableConfiguration
+	{
+		private readonly object syncObj = new object();
+
+		private readonly bool _isWorldTeleporting = false;
+
+		/// <summary>
+		/// Indicates if the entity position is saveable.
+		/// </summary>
+		public bool isCurrentPositionSaveable => !isWorldTeleporting;
+
+		/// <summary>
+		/// Indicates if the entity is world teleporting.
+		/// </summary>
+		public bool isWorldTeleporting
+		{
+			get
+			{
+				lock (syncObj)
+				{
+					return _isWorldTeleporting;
+				}
+			}
+		}
+	}
+}
