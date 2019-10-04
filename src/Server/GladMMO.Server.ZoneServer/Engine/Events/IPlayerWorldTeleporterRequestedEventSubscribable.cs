55c21d20f956d5111ec0379624c9c0ecc9a5a169
HelloKitty <andrew.blakely@ymail.com>
2019-10-03 20:00:04 -0500

    Added IPlayerWorldTeleporterRequestedEventSubscribable

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/Events/IPlayerWorldTeleporterRequestedEventSubscribable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IPlayerWorldTeleporterRequestedEventSubscribable
+	{
+		event EventHandler<PlayerSessionClaimedEventArgs> OnWorldTeleporterRequested;
+	}
+
+	public sealed class PlayerWorldTeleporterRequestEventArgs : EventArgs
+	{
+		public int WorldTeleporterEntryId { get; }
+
+		public PlayerWorldTeleporterRequestEventArgs(int worldTeleporterEntryId)
+		{
+			if (worldTeleporterEntryId <= 0) throw new ArgumentOutOfRangeException(nameof(worldTeleporterEntryId));
+
+			WorldTeleporterEntryId = worldTeleporterEntryId;
+		}
+	}
+}
