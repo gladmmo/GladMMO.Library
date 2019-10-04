8780a8cb7828a4333a1d6b0e13aad32b3613949c
HelloKitty <andrew.blakely@ymail.com>
2019-10-04 18:49:31 -0500

    Added ReturnToWorldDownloadScreenOnDisconnectionEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnNetworkDisconnect/ReturnToWorldDownloadScreenOnDisconnectionEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ReturnToWorldDownloadScreenOnDisconnectionEventListener : ThreadUnSafeBaseSingleEventListenerInitializable<INetworkClientDisconnectedEventSubscribable>
+	{
+		public ReturnToWorldDownloadScreenOnDisconnectionEventListener(INetworkClientDisconnectedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+
+		protected override void OnThreadUnSafeEventFired(object source, EventArgs args)
+		{
+			//TODO: Use the scene manager service.
+			//TODO: Don't hardcode scene ids. Don't load scenes directly.
+			SceneManager.LoadSceneAsync(2).allowSceneActivation = true;
+		}
+	}
+}
