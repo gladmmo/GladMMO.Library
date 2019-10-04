0d699d646a961db0f56986df1ff85440990436db
HelloKitty <andrew.blakely@ymail.com>
2019-10-03 20:04:24 -0500

    Added PlayerWorldTeleporterRequestEventPublisher

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventPublishers/PlayerWorldTeleporterRequestEventPublisher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(IEventPublisher<IPlayerWorldTeleporterRequestedEventSubscribable, PlayerSessionClaimedEventArgs>))]
+	[AdditionalRegisterationAs(typeof(IPlayerWorldTeleporterRequestedEventSubscribable))]
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class PlayerWorldTeleporterRequestEventPublisher : IEventPublisher<IPlayerWorldTeleporterRequestedEventSubscribable, PlayerSessionClaimedEventArgs>, IPlayerWorldTeleporterRequestedEventSubscribable, IGameInitializable
+	{
+		public event EventHandler<PlayerSessionClaimedEventArgs> OnWorldTeleporterRequested;
+
+		public void PublishEvent(object sender, [NotNull] PlayerSessionClaimedEventArgs eventArgs)
+		{
+			if (eventArgs == null) throw new ArgumentNullException(nameof(eventArgs));
+
+			OnWorldTeleporterRequested?.Invoke(sender, eventArgs);
+		}
+
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
