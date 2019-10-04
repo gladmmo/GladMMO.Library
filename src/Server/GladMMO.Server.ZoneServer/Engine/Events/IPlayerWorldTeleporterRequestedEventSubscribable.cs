e6bd5bf84dc936990ce7057b63c5aafc9f803638
HelloKitty <andrew.blakely@ymail.com>
2019-10-04 00:12:54 -0500

    Extended PlayerWorldTeleporterRequestEventArgs with player GUID

--- src/Server/GladMMO.Server.ZoneServer/Engine/Events/IPlayerWorldTeleporterRequestedEventSubscribable.cs
+++ src/Server/GladMMO.Server.ZoneServer/Engine/Events/IPlayerWorldTeleporterRequestedEventSubscribable.cs
@@ -11,13 +11,16 @@ namespace GladMMO
 
 	public sealed class PlayerWorldTeleporterRequestEventArgs : EventArgs
 	{
+		public NetworkEntityGuid TeleportingPlayer { get; }
+
 		public int WorldTeleporterEntryId { get; }
 
-		public PlayerWorldTeleporterRequestEventArgs(int worldTeleporterEntryId)
+		public PlayerWorldTeleporterRequestEventArgs(int worldTeleporterEntryId, [NotNull] NetworkEntityGuid teleportingPlayer)
 		{
 			if (worldTeleporterEntryId <= 0) throw new ArgumentOutOfRangeException(nameof(worldTeleporterEntryId));
 
 			WorldTeleporterEntryId = worldTeleporterEntryId;
+			TeleportingPlayer = teleportingPlayer ?? throw new ArgumentNullException(nameof(teleportingPlayer));
 		}
 	}
 }
