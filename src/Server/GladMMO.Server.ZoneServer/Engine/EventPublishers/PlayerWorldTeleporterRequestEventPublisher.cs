b0380e01b7db5ce3a18ba16695332e5a09b31eee
HelloKitty <andrew.blakely@ymail.com>
2019-10-04 00:11:37 -0500

    Fixed world teleport event publisher signature

--- src/Server/GladMMO.Server.ZoneServer/Engine/EventPublishers/PlayerWorldTeleporterRequestEventPublisher.cs
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventPublishers/PlayerWorldTeleporterRequestEventPublisher.cs
@@ -6,14 +6,14 @@ using Glader.Essentials;
 
 namespace GladMMO
 {
-	[AdditionalRegisterationAs(typeof(IEventPublisher<IPlayerWorldTeleporterRequestedEventSubscribable, PlayerSessionClaimedEventArgs>))]
+	[AdditionalRegisterationAs(typeof(IEventPublisher<IPlayerWorldTeleporterRequestedEventSubscribable, PlayerWorldTeleporterRequestEventArgs>))]
 	[AdditionalRegisterationAs(typeof(IPlayerWorldTeleporterRequestedEventSubscribable))]
 	[ServerSceneTypeCreate(ServerSceneType.Default)]
-	public sealed class PlayerWorldTeleporterRequestEventPublisher : IEventPublisher<IPlayerWorldTeleporterRequestedEventSubscribable, PlayerSessionClaimedEventArgs>, IPlayerWorldTeleporterRequestedEventSubscribable, IGameInitializable
+	public sealed class PlayerWorldTeleporterRequestEventPublisher : IEventPublisher<IPlayerWorldTeleporterRequestedEventSubscribable, PlayerWorldTeleporterRequestEventArgs>, IPlayerWorldTeleporterRequestedEventSubscribable, IGameInitializable
 	{
-		public event EventHandler<PlayerSessionClaimedEventArgs> OnWorldTeleporterRequested;
+		public event EventHandler<PlayerWorldTeleporterRequestEventArgs> OnWorldTeleporterRequested;
 
-		public void PublishEvent(object sender, [NotNull] PlayerSessionClaimedEventArgs eventArgs)
+		public void PublishEvent(object sender, [NotNull] PlayerWorldTeleporterRequestEventArgs eventArgs)
 		{
 			if (eventArgs == null) throw new ArgumentNullException(nameof(eventArgs));
 
