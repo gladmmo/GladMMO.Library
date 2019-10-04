99b799ceea810f4dce0695f7c3b0f1f57d12d38a
HelloKitty <andrew.blakely@ymail.com>
2019-10-03 23:18:44 -0500

    Added ZoneServerWorldTeleportCharacterRequest model

---
+++ src/GladMMO.Models.Server.ZoneServer/Models/ZoneServerWorldTeleportCharacterRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class ZoneServerWorldTeleportCharacterRequest
+	{
+		[JsonProperty]
+		public NetworkEntityGuid CharacterGuid { get; private set; }
+
+		[JsonProperty]
+		public int WorldTeleporterId { get; private set; }
+
+		public ZoneServerWorldTeleportCharacterRequest([NotNull] NetworkEntityGuid characterGuid, int worldTeleporterId)
+		{
+			if (worldTeleporterId <= 0) throw new ArgumentOutOfRangeException(nameof(worldTeleporterId));
+
+			CharacterGuid = characterGuid ?? throw new ArgumentNullException(nameof(characterGuid));
+			WorldTeleporterId = worldTeleporterId;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		protected ZoneServerWorldTeleportCharacterRequest()
+		{
+
+		}
+	}
+}
