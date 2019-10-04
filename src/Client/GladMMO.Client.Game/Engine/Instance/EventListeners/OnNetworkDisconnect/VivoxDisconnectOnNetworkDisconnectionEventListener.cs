f7a1b0e0f1b818f67f94bf199a923b85ad84d5ae
HelloKitty <andrew.blakely@ymail.com>
2019-10-04 18:49:07 -0500

    Added VivoxDisconnectOnNetworkDisconnectionEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnNetworkDisconnect/VivoxDisconnectOnNetworkDisconnectionEventListener.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class VivoxDisconnectOnNetworkDisconnectionEventListener : BaseSingleEventListenerInitializable<INetworkClientDisconnectedEventSubscribable>
+	{
+		private VivoxUnity.Client VoiceClient { get; }
+
+		public VivoxDisconnectOnNetworkDisconnectionEventListener(INetworkClientDisconnectedEventSubscribable subscriptionService,
+			[NotNull] VivoxUnity.Client voiceClient) : base(subscriptionService)
+		{
+			VoiceClient = voiceClient ?? throw new ArgumentNullException(nameof(voiceClient));
+		}
+
+		protected override void OnEventFired(object source, EventArgs args)
+		{
+			VoiceClient.Uninitialize();
+		}
+	}
+}
