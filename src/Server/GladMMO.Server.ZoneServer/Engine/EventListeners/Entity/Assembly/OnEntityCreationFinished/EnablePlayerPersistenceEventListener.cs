2365d3bb748ad073b605a022ec84d3c7de09491f
HelloKitty <andrew.blakely@ymail.com>
2019-10-03 19:45:25 -0500

    Added PlayerSpawn listener for persitence configuration

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/Assembly/OnEntityCreationFinished/EnablePlayerPersistenceEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class EnablePlayerPersistenceEventListener : PlayerCreationFinishedEventListener
+	{
+		private IEntityGuidMappable<EntitySaveableConfiguration> PlayerPersistenceConfigMappable { get; }
+
+		public EnablePlayerPersistenceEventListener(IEntityCreationFinishedEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<EntitySaveableConfiguration> playerPersistenceConfigMappable) 
+			: base(subscriptionService)
+		{
+			PlayerPersistenceConfigMappable = playerPersistenceConfigMappable ?? throw new ArgumentNullException(nameof(playerPersistenceConfigMappable));
+		}
+
+		protected override void OnEntityCreationFinished(EntityCreationFinishedEventArgs args)
+		{
+			//Right now it's just default.
+			PlayerPersistenceConfigMappable.AddObject(args.EntityGuid, new EntitySaveableConfiguration());
+		}
+	}
+}
