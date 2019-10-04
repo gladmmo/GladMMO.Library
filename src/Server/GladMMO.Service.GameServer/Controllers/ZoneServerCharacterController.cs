05f7448411d347b7da6d8106649f08181448cf57
HelloKitty <andrew.blakely@ymail.com>
2019-10-03 23:40:10 -0500

    Added ZoneServerCharacterController

---
+++ src/Server/GladMMO.Service.GameServer/Controllers/ZoneServerCharacterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Controller for ZoneServer serverside actions involving characters
+	/// or character data.
+	/// </summary>
+	[Route("api/[controller]")]
+	public class ZoneServerCharacterController : AuthorizationReadyController
+	{
+		/// <inheritdoc />
+		public ZoneServerCharacterController(IClaimsPrincipalReader claimsReader, 
+			ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger)
+		{
+
+		}
+	}
+}
