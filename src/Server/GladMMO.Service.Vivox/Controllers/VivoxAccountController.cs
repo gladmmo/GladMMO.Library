e41def71c0324f01590941113d8890f429dcd14f
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 06:26:45 -0500

    Added VivoxAccountController

---
+++ src/Server/GladMMO.Service.Vivox/Controllers/VivoxAccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[Route("api/[controller]")]
+	public class VivoxAccountController : AuthorizationReadyController
+	{
+		public VivoxAccountController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger)
+		{
+
+		}
+
+	}
+}
