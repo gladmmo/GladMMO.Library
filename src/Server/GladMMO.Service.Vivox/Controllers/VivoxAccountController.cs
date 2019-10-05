0c143794ce3e6021696f03e5e39906cc23ba8689
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 06:26:53 -0500

    Added VivoxAccountController.LoginVivox

--- src/Server/GladMMO.Service.Vivox/Controllers/VivoxAccountController.cs
+++ src/Server/GladMMO.Service.Vivox/Controllers/VivoxAccountController.cs
@@ -17,5 +17,36 @@ namespace GladMMO
 
 		}
 
+		[AuthorizeJwt]
+		[NoResponseCache]
+		[HttpPost("Login")]
+		public async Task<IActionResult> LoginVivox([FromServices] ICharacterSessionRepository characterSessionRepository,
+			[FromServices] IFactoryCreatable<VivoxTokenClaims, VivoxTokenClaimsCreationContext> claimsFactory,
+			[FromServices] IVivoxTokenSignService signService)
+		{
+			int accountId = this.ClaimsReader.GetUserIdInt(User);
+
+			//If the user doesn't actually have a claimed session in the game
+			//then we shouldn't log them into Vivox.
+			if (!await characterSessionRepository.AccountHasActiveSession(accountId))
+				return BuildFailedResponseModel(VivoxLoginResponseCode.NoActiveCharacterSession);
+
+			int characterId = await RetrieveSessionCharacterIdAsync(characterSessionRepository, accountId);
+
+			VivoxTokenClaims claims = claimsFactory.Create(new VivoxTokenClaimsCreationContext(characterId, VivoxAction.Login));
+
+			//We don't send it back in a JSON form even though it's technically a JSON object
+			//because the client just needs it as a raw string anyway to put through the Vivox client API.
+			return Ok(signService.CreateSignature(claims));
+		}
+
+		private static async Task<int> RetrieveSessionCharacterIdAsync(ICharacterSessionRepository characterSessionRepository, int accountId)
+		{
+			//TODO: Technically a race condition here.
+			//Now let's actually get the character id of the session that the account has
+			ClaimedSessionsModel session = await characterSessionRepository.RetrieveClaimedSessionByAccountId(accountId);
+			int characterId = session.CharacterId;
+			return characterId;
+		}
 	}
 }
