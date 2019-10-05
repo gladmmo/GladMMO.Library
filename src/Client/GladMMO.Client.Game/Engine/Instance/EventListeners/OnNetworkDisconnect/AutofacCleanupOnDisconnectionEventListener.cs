12ffa584dad3f0b854148ff0e7d6f00a1ce6570c
HelloKitty <andrew.blakely@ymail.com>
2019-10-04 19:15:55 -0500

    Added AutofacCleanupOnDisconnectionEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnNetworkDisconnect/AutofacCleanupOnDisconnectionEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class AutofacCleanupOnDisconnectionEventListener : ThreadUnSafeBaseSingleEventListenerInitializable<INetworkClientDisconnectedEventSubscribable>
+	{
+		private ILifetimeScope AutofacScope { get; }
+
+		public AutofacCleanupOnDisconnectionEventListener(INetworkClientDisconnectedEventSubscribable subscriptionService,
+			[NotNull] ILifetimeScope autofacScope) 
+			: base(subscriptionService)
+		{
+			AutofacScope = autofacScope ?? throw new ArgumentNullException(nameof(autofacScope));
+		}
+
+		protected override void OnThreadUnSafeEventFired(object source, EventArgs args)
+		{
+			//Most importantly, this will dipose of the resources of
+			//all currently loaded asset bundles.
+			AutofacScope.Dispose();
+		}
+	}
+}
