4e595d5bb2ddd909fde8c4a5ef984a509093b9fe
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 05:51:15 -0500

    Added GeneralServerError to VivoxLoginResponseCode

--- src/GladMMO.Models.Vivox/ResultCodes/VivoxLoginResponseCode.cs
+++ src/GladMMO.Models.Vivox/ResultCodes/VivoxLoginResponseCode.cs
@@ -14,9 +14,11 @@ namespace GladMMO
 		/// </summary>
 		Success = 1,
 
+		GeneralServerError = 2,
+
 		/// <summary>
 		/// Indicates that no session character session is actually active for the account.
 		/// </summary>
-		NoActiveCharacterSession = 2,
+		NoActiveCharacterSession = 3,
 	}
 }
