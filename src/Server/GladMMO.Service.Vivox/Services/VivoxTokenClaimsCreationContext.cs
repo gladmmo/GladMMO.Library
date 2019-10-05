85473d56ed72f42a4d77e4eff30439e810637457
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 05:51:38 -0500

    Added VivoxTokenClaimsCreationContext

---
+++ src/Server/GladMMO.Service.Vivox/Services/VivoxTokenClaimsCreationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class VivoxTokenClaimsCreationContext
+	{
+		public int CharacterId { get; }
+
+		public VivoxAction Action { get; }
+
+		public VivoxTokenClaimsCreationContext(int characterId, VivoxAction action)
+		{
+			if (characterId <= 0) throw new ArgumentOutOfRangeException(nameof(characterId));
+			if (!Enum.IsDefined(typeof(VivoxAction), action)) throw new InvalidEnumArgumentException(nameof(action), (int)action, typeof(VivoxAction));
+
+			CharacterId = characterId;
+			Action = action;
+		}
+	}
+}
