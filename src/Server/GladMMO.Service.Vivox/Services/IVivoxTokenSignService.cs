de948024ba1fef1363cf200297a0374e90678ea6
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 05:53:08 -0500

    Added IVivoxTokenSignService

---
+++ src/Server/GladMMO.Service.Vivox/Services/IVivoxTokenSignService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for service that signs a Vivox claims.
+	/// </summary>
+	public interface IVivoxTokenSignService
+	{
+		/// <summary>
+		/// Creates a signed token authorizing it as officially
+		/// useable.
+		/// </summary>
+		/// <param name="claims">The token claims</param>
+		/// <returns>The signed authorization token in string form.</returns>
+		string CreateSignature(VivoxTokenClaims claims);
+	}
+}
