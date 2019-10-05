9a22212d1a20c544889881ffd09ca122205ac6b3
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 07:13:29 -0500

    Added IVivoxAuthorizationService

---
+++ src/Client/GladMMO.Client.VivoxService/Async/AsyncVivoxAuthorizationServiceClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncVivoxAuthorizationServiceClient : BaseAsyncEndpointService<IVivoxAuthorizationService>, IVivoxAuthorizationService
+	{
+		/// <inheritdoc />
+		public AsyncVivoxAuthorizationServiceClient(Task<string> futureEndpoint)
+			: base(futureEndpoint)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public AsyncVivoxAuthorizationServiceClient(Task<string> futureEndpoint, RefitSettings settings)
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		public async Task<ResponseModel<string, VivoxLoginResponseCode>> LoginAsync()
+		{
+			return await (await GetService().ConfigureAwait(false)).LoginAsync().ConfigureAwait(false);
+		}
+	}
+}
