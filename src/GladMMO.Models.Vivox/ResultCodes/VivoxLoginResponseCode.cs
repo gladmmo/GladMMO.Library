9b13282ce549635a3fa185ddb794ced30ef08609
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 05:26:16 -0500

    Added VivoxLoginResponseCode

---
+++ src/GladMMO.Models.Vivox/ResultCodes/VivoxLoginResponseCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Enumeration of all responses for a vivox login attempt.
+	/// </summary>
+	public enum VivoxLoginResponseCode
+	{
+		/// <summary>
+		/// Indicates the request was successful.
+		/// </summary>
+		Success = 1,
+
+		/// <summary>
+		/// Indicates that no session character session is actually active for the account.
+		/// </summary>
+		NoActiveCharacterSession = 2,
+	}
+}
