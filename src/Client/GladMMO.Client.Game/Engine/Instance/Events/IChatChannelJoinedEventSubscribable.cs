d0c3a4df0fca9dd28808778b43b09cfb564d3cab
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 22:43:24 -0500

    Added IChatChannelJoinedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/IChatChannelJoinedEventSubscribable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using VivoxUnity;
+
+namespace GladMMO
+{
+	public interface IChatChannelJoinedEventSubscribable
+	{
+		event EventHandler<ChatChannelJoinedEventArgs> OnChatChannelJoined;
+	}
+
+	public sealed class ChatChannelJoinedEventArgs : EventArgs
+	{
+		public ChatChannelType ChannelType { get; }
+
+		public ChatChannelJoinedEventArgs(ChatChannelType channelType)
+		{
+			if(!Enum.IsDefined(typeof(ChatChannelType), channelType)) throw new InvalidEnumArgumentException(nameof(channelType), (int)channelType, typeof(ChatChannelType));
+
+			ChannelType = channelType;
+		}
+	}
+}
