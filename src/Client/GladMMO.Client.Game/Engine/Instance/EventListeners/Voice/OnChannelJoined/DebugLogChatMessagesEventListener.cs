e6e25284636cd4130343ceab5815ec57807d7b21
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 23:21:52 -0500

    Added text message reciever debug listener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Voice/OnChannelJoined/DebugLogChatMessagesEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	//This is just a debugging component.
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class DebugLogChatMessagesEventListener : BaseSingleEventListenerInitializable<IChatChannelJoinedEventSubscribable, ChatChannelJoinedEventArgs>
+	{
+		private ILog Logger { get; }
+
+		public DebugLogChatMessagesEventListener(IChatChannelJoinedEventSubscribable subscriptionService,
+			[NotNull] ILog logger) 
+			: base(subscriptionService)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		protected override void OnEventFired(object source, ChatChannelJoinedEventArgs args)
+		{
+			args.Channel.OnChannelTextMessageRecieved += (s, message) => Logger.Debug($"Recieved Message. Sender: {message.Sender.Name} Content: {message.Message}")
+		}
+	}
+}
