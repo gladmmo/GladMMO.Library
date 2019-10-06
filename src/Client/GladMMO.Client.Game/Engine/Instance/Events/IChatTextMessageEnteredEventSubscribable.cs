540f90b894cf4ba17adebfc5ed4d6570313d3057
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 23:31:54 -0500

    Added IChatTextMessageEnteredEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/IChatTextMessageEnteredEventSubscribable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IChatTextMessageEnteredEventSubscribable
+	{
+		event EventHandler<ChatTextMessageEnteredEventArgs> OnChatMessageEntered;
+	}
+
+	public sealed class ChatTextMessageEnteredEventArgs : EventArgs
+	{
+		/// <summary>
+		/// The requested channel type of the message.
+		/// </summary>
+		public ChatChannelType RequestedChannelType { get; }
+
+		public string Content { get; }
+
+		public ChatTextMessageEnteredEventArgs(ChatChannelType requestedChannelType, [NotNull] string content)
+		{
+			if (!Enum.IsDefined(typeof(ChatChannelType), requestedChannelType)) throw new InvalidEnumArgumentException(nameof(requestedChannelType), (int) requestedChannelType, typeof(ChatChannelType));
+			if (string.IsNullOrWhiteSpace(content)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(content));
+
+			RequestedChannelType = requestedChannelType;
+			Content = content;
+		}
+	}
+}
