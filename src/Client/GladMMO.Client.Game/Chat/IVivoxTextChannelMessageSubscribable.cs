88713a89f85e5cf133cfef92abb3208a476ca908
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 23:16:21 -0500

    Added IVivoxTextChannelMessageSubscribable

---
+++ src/Client/GladMMO.Client.Game/Chat/IVivoxTextChannelMessageSubscribable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VivoxUnity;
+
+namespace GladMMO.Chat
+{
+	public interface IVivoxTextChannelMessageSubscribable
+	{
+		event EventHandler<IChannelTextMessage> OnChannelTextMessageRecieved;
+	}
+
+	public sealed class DefaultVivoxTextChannelSubscribableAdapter : IVivoxTextChannelMessageSubscribable
+	{
+		public event EventHandler<IChannelTextMessage> OnChannelTextMessageRecieved;
+
+		public DefaultVivoxTextChannelSubscribableAdapter([NotNull] IChannelSession channel)
+		{
+			if (channel == null) throw new ArgumentNullException(nameof(channel));
+
+			channel.MessageLog.AfterItemAdded += MessageLogOnAfterItemAdded;
+		}
+
+		private void MessageLogOnAfterItemAdded(object sender, [NotNull] QueueItemAddedEventArgs<IChannelTextMessage> e)
+		{
+			if (e == null) throw new ArgumentNullException(nameof(e));
+
+			OnChannelTextMessageRecieved?.Invoke(sender, e.Value);
+		}
+	}
+}
