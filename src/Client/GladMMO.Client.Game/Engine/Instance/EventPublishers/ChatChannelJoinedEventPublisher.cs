a1c9ce67bdb7b001793d6432e6a166f8ac9cf1f3
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 22:59:45 -0500

    Added ChatChannelJoinedEventPublisher

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventPublishers/ChatChannelJoinedEventPublisher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(IEventPublisher<IChatChannelJoinedEventSubscribable, ChatChannelJoinedEventArgs>))]
+	[AdditionalRegisterationAs(typeof(IChatChannelJoinedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ChatChannelJoinedEventPublisher : IEventPublisher<IChatChannelJoinedEventSubscribable, ChatChannelJoinedEventArgs>, IChatChannelJoinedEventSubscribable, IGameInitializable
+	{
+		public event EventHandler<ChatChannelJoinedEventArgs> OnChatChannelJoined;
+
+		public void PublishEvent(object sender, [NotNull] ChatChannelJoinedEventArgs eventArgs)
+		{
+			if (eventArgs == null) throw new ArgumentNullException(nameof(eventArgs));
+
+			OnChatChannelJoined?.Invoke(sender, eventArgs);
+		}
+
+		public async Task OnGameInitialized()
+		{
+
+		}
+	}
+}
