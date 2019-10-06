42eba8784b34f604a03187cdff85c3aede298c54
HelloKitty <andrew.blakely@ymail.com>
2019-10-06 00:46:42 -0500

    Added new IChatChannelSender

---
+++ src/Client/GladMMO.Client.Game/Chat/IChatChannelSender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+using Nito.AsyncEx;
+using VivoxUnity;
+
+namespace GladMMO
+{
+	public interface IChatChannelSender
+	{
+		Task SendAsync(string messageContent);
+	}
+
+	public sealed class DefaultVivoxChatChannelSenderAdapter : IChatChannelSender
+	{
+		private IChannelSession Channel { get; }
+
+		public DefaultVivoxChatChannelSenderAdapter([NotNull] IChannelSession channel)
+		{
+			Channel = channel ?? throw new ArgumentNullException(nameof(channel));
+		}
+
+		public Task SendAsync(string messageContent)
+		{
+			return Task.Factory.FromAsync(BeginSendText, Channel.EndSendText, messageContent);
+		}
+
+		//Adapt the the expected signature.
+		public IAsyncResult BeginSendText(AsyncCallback callback, object state)
+		{
+			string message = (string)state;
+			return Channel.BeginSendText(message, callback);
+		}
+	}
+}
