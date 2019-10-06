290e0a63bdd3cd13d5e280fece833122bf7e5859
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 23:47:04 -0500

    Added IChatChannelRoutable

---
+++ src/Client/GladMMO.Client.Game/Chat/IChatChannelRoutable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO.Chat
+{
+	public interface IChatChannelRoutable
+	{
+		ChatChannelType ChannelType { get; }
+	}
+}
