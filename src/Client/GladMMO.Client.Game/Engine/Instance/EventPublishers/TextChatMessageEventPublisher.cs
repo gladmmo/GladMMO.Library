af987dea832d23fd2132d383c9f4f131c1903ac1
HelloKitty <andrew.blakely@ymail.com>
2019-10-05 22:56:13 -0500

    Added TextChatMessageEventPublisher

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventPublishers/TextChatMessageEventPublisher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(IEventPublisher<IChatTextMessageRecievedEventSubscribable, TextChatEventArgs>))]
+	[AdditionalRegisterationAs(typeof(IChatTextMessageRecievedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class TextChatMessageEventPublisher : IEventPublisher<IChatTextMessageRecievedEventSubscribable, TextChatEventArgs>, IChatTextMessageRecievedEventSubscribable, IGameInitializable
+	{
+		public event EventHandler<TextChatEventArgs> OnTextChatMessageRecieved;
+
+		public void PublishEvent(object sender, [NotNull] TextChatEventArgs eventArgs)
+		{
+			if (eventArgs == null) throw new ArgumentNullException(nameof(eventArgs));
+
+			OnTextChatMessageRecieved?.Invoke(sender, eventArgs);
+		}
+
+		public async Task OnGameInitialized()
+		{
+
+		}
+	}
+}
