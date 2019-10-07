6b0a9837bcc483ccbc42d175a8e81890172b898b
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 05:38:13 -0500

    Added initial character creation dependency types

---
+++ src/GladMMO.Client.Common/IoC/Modules/CharacterCreationDependencyAutofacModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class CharacterCreationDependencyAutofacModule : NetworkServiceDiscoveryableAutofaceModule
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterModule<CharacterServiceDependencyAutofacModule>();
+		}
+	}
+}
