8d6e12daf522f5f9a5fe6c32cb5aebcec6f4391e
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 08:33:06 -0500

    Added ICreateNewCharacterButtonClickedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterSelection/Initializables/CreateNewCharacterButtonGlueStartable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(ICreateNewCharacterButtonClickedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	public sealed class CreateNewCharacterButtonGlueStartable : BaseButtonClickGlueStartable, ICreateNewCharacterButtonClickedEventSubscribable
+	{
+		public CreateNewCharacterButtonGlueStartable([KeyFilter(UnityUIRegisterationKey.CharacterCreateButton)] IUIButton referenceButton) 
+			: base(referenceButton)
+		{
+		}
+	}
+}
