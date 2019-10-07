9497ecfc4e76963466e95cf0cf2afa102a473dca
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 06:40:35 -0500

    Added base ButtonClickedEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/Events/ButtonClickedEventListener.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class ButtonClickedEventListener<TButtonClickedEventType> : BaseSingleEventListenerInitializable<TButtonClickedEventType, ButtonClickedEventArgs>
+		where TButtonClickedEventType : class, IButtonClickedEventSubscribable
+	{
+		protected ButtonClickedEventListener(TButtonClickedEventType subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
