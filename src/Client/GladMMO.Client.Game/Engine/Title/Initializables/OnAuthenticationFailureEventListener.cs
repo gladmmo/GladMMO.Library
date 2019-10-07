2ad25152ee63663eccb809b88ccdbddfa8782a32
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 01:48:23 -0500

    Added support for renabling login box after failured attempt

---
+++ src/Client/GladMMO.Client.Game/Engine/Title/Initializables/OnAuthenticationFailureEventListener.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+using Nito.AsyncEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.TitleScreen)]
+	public sealed class OnAuthenticationFailureEventListener : BaseSingleEventListenerInitializable<IAuthenticationResultRecievedEventSubscribable, AuthenticationResultEventArgs>
+	{
+		private IUIButton LoginButton { get; }
+
+		private IUIText PasswordInput { get; }
+
+		/// <inheritdoc />
+		public OnAuthenticationFailureEventListener(IAuthenticationResultRecievedEventSubscribable subscriptionService, 
+			[KeyFilter(UnityUIRegisterationKey.Login)] [NotNull] IUIButton loginButton,
+			[KeyFilter(UnityUIRegisterationKey.PasswordTextBox)] [NotNull] IUIText passwordInput)
+			: base(subscriptionService)
+		{
+			LoginButton = loginButton ?? throw new ArgumentNullException(nameof(loginButton));
+			PasswordInput = passwordInput ?? throw new ArgumentNullException(nameof(passwordInput));
+		}
+
+		protected override void OnEventFired(object source, AuthenticationResultEventArgs args)
+		{
+			//Only interested in failure.
+			if (args.isSuccessful)
+				return;
+
+			//Just renable the login button on failure
+			//and clear the password as it's likely what is wrong.
+			LoginButton.IsInteractable = true;
+			PasswordInput.Text = "";
+		}
+	}
+}
