6afe4337fd64db1148e0ea919c4d20bbc2403599
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 06:41:07 -0500

    Added ToggleInteractableCharacterCreationButtonOnClickEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterCreation/EventListeners/CharacterCreationButtonClicked/ToggleInteractableCharacterCreationButtonOnClickEventListener.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterCreationScreen)]
+	public sealed class ToggleInteractableCharacterCreationButtonOnClickEventListener : ButtonClickedEventListener<ICharacterCreationButtonClickedEventSubscribable>
+	{
+		public ToggleInteractableCharacterCreationButtonOnClickEventListener(ICharacterCreationButtonClickedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+
+		protected override void OnEventFired(object source, ButtonClickedEventArgs args)
+		{
+			args.Button.IsInteractable = false;
+		}
+	}
+}
