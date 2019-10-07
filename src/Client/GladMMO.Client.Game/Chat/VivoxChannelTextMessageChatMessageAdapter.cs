718053d5acd5feb09cdde569596055ce7fa0625f
HelloKitty <andrew.blakely@ymail.com>
2019-10-06 20:03:15 -0500

    Added VivoxChannelTextMessageChatMessageAdapter

---
+++ src/Client/GladMMO.Client.Game/Chat/VivoxChannelTextMessageChatMessageAdapter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using VivoxUnity;
+
+namespace GladMMO.Chat
+{
+	public sealed class VivoxChannelTextMessageChatMessageAdapter : IChatChannelRoutable, ITextMessageContainable
+	{
+		private IChannelTextMessage ChannelMessage { get; }
+
+		public ChatChannelType ChannelType { get; }
+
+		public string Message => ChannelMessage.Message;
+
+		public VivoxChannelTextMessageChatMessageAdapter([NotNull] IChannelTextMessage channelMessage, ChatChannelType channelType)
+		{
+			if (!Enum.IsDefined(typeof(ChatChannelType), channelType)) throw new InvalidEnumArgumentException(nameof(channelType), (int) channelType, typeof(ChatChannelType));
+
+			ChannelMessage = channelMessage ?? throw new ArgumentNullException(nameof(channelMessage));
+			ChannelType = channelType;
+		}
+	}
+}
