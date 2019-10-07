6625be93ac4bd1a1d45501712d8a140d8b3f68c2
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 05:50:19 -0500

    Added general IButtonClickedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/Events/IButtonClickedEventSubscribable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IButtonClickedEventSubscribable
+	{
+		event EventHandler<ButtonClickedEventArgs> OnButtonClicked;
+	}
+
+	public sealed class ButtonClickedEventArgs : EventArgs
+	{
+		public IUIButton Button { get; }
+
+		public ButtonClickedEventArgs([NotNull] IUIButton button)
+		{
+			Button = button ?? throw new ArgumentNullException(nameof(button));
+		}
+	}
+}
