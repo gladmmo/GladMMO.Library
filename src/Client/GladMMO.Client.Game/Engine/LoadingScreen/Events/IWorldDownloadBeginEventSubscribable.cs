b9c67457ec759eede361fdc96aea27f3857164b6
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 03:24:26 -0500

    Added IWorldDownloadBeginEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/LoadingScreen/Events/IWorldDownloadBeginEventSubscribable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public interface IWorldDownloadBeginEventSubscribable
+	{
+		event EventHandler<WorldDownloadBeginEventArgs> OnWorldDownloadBegins;
+	}
+
+	public sealed class WorldDownloadBeginEventArgs : EventArgs
+	{
+		public AsyncOperation DownloadOperation { get; }
+
+		public WorldDownloadBeginEventArgs([NotNull] AsyncOperation downloadOperation)
+		{
+			DownloadOperation = downloadOperation ?? throw new ArgumentNullException(nameof(downloadOperation));
+		}
+	}
+}
