e6a8ca5c12e0ceed04b765996ccac7982ba70d71
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 08:19:31 -0500

    Added ReturnToCharacterSelectionOnSuccessEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterCreation/EventListeners/CharacterCreationAttempted/ReturnToCharacterSelectionOnSuccessEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterCreationScreen)]
+	public sealed class ReturnToCharacterSelectionOnSuccessEventListener : BaseSingleEventListenerInitializable<ICharacterCreationAttemptedEventSubscribable, CharacterCreationAttemptedEventArgs>
+	{
+		public IUIButton BackButton { get; }
+
+		public ReturnToCharacterSelectionOnSuccessEventListener(ICharacterCreationAttemptedEventSubscribable subscriptionService,
+			[KeyFilter(UnityUIRegisterationKey.BackButton)] [NotNull] IUIButton backButton) 
+			: base(subscriptionService)
+		{
+			BackButton = backButton ?? throw new ArgumentNullException(nameof(backButton));
+		}
+
+		protected override void OnEventFired(object source, CharacterCreationAttemptedEventArgs args)
+		{
+			//Just simulate a back button press.
+			BackButton.SimulateClick(true);
+		}
+	}
+}
