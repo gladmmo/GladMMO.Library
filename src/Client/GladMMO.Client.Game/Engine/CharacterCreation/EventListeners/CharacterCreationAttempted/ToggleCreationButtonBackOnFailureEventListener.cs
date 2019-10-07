11da68b3d867e0a810bef63ec073ebe30583b088
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 07:51:18 -0500

    Added ToggleCreationButtonBackOnFailureEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterCreation/EventListeners/CharacterCreationAttempted/ToggleCreationButtonBackOnFailureEventListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterCreationScreen)]
+	public sealed class ToggleCreationButtonBackOnFailureEventListener : BaseSingleEventListenerInitializable<ICharacterCreationAttemptedEventSubscribable, CharacterCreationAttemptedEventArgs>
+	{
+		private IUIButton CreationButton { get; }
+
+		public ToggleCreationButtonBackOnFailureEventListener(ICharacterCreationAttemptedEventSubscribable subscriptionService,
+			[KeyFilter(UnityUIRegisterationKey.CharacterCreateButton)] [NotNull]
+			IUIButton creationButton) 
+			: base(subscriptionService)
+		{
+			CreationButton = creationButton ?? throw new ArgumentNullException(nameof(creationButton));
+		}
+
+		protected override void OnEventFired(object source, CharacterCreationAttemptedEventArgs args)
+		{
+			//Only on failure.
+			if (args.isSuccessful)
+				return;
+
+			CreationButton.IsInteractable = true;
+		}
+	}
+}
