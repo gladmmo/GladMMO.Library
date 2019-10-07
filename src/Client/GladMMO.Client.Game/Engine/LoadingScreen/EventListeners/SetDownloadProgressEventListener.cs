2642d889bf1a539ffca863c24e6cf4724679970a
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 03:49:25 -0500

    Added download progress tickable

---
+++ src/Client/GladMMO.Client.Game/Engine/LoadingScreen/EventListeners/SetDownloadProgressEventListener.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.PreZoneBurstingScreen)]
+	public sealed class SetDownloadProgressEventListener : BaseSingleEventListenerInitializable<IWorldDownloadBeginEventSubscribable, WorldDownloadBeginEventArgs>, IGameTickable
+	{
+		public IUIFillableImage DownloadFillImage { get; }
+
+		public IUIText DownloadText { get; }
+
+		private AsyncOperation CurrentDownloadOperation { get; set; } = null;
+
+		public SetDownloadProgressEventListener(IWorldDownloadBeginEventSubscribable subscriptionService,
+			[KeyFilter(UnityUIRegisterationKey.WorldDownloadProgress)] [NotNull] IUIFillableImage downloadFillImage,
+			[KeyFilter(UnityUIRegisterationKey.WorldDownloadProgress)] [NotNull] IUIText downloadText) 
+			: base(subscriptionService)
+		{
+			DownloadFillImage = downloadFillImage ?? throw new ArgumentNullException(nameof(downloadFillImage));
+			DownloadText = downloadText ?? throw new ArgumentNullException(nameof(downloadText));
+		}
+
+		protected override void OnEventFired(object source, WorldDownloadBeginEventArgs args)
+		{
+			CurrentDownloadOperation = args.DownloadOperation;
+		}
+
+		public void Tick()
+		{
+			//Can't do anything without world downloading operation.
+			if (CurrentDownloadOperation == null)
+				return;
+
+			DownloadText.Text = $"{CurrentDownloadOperation.progress * 100}%";
+			DownloadFillImage.FillAmount = CurrentDownloadOperation.progress;
+		}
+	}
+}
