178133830479c8358cb13b528ece3abbf72e0e7d
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 08:33:33 -0500

    Added LoadCharacterCreationSceneEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterSelection/EventListeners/OnCreateNewCharacterButtonClicked/LoadCharacterCreationSceneEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	public sealed class LoadCharacterCreationSceneEventListener : ButtonClickedEventListener<ICharacterCreationButtonClickedEventSubscribable>
+	{
+		public LoadCharacterCreationSceneEventListener(ICharacterCreationButtonClickedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+		}
+
+		protected override void OnEventFired(object source, ButtonClickedEventArgs args)
+		{
+			//When the button is clicked we should disable interaction
+			//then we can load the scene async.
+			//We should probably disabled the enter-world button too, but we don't have exclusive control over it.
+			args.Button.IsInteractable = false;
+
+			//Don't load it async, because then they may click on Enter World or some wacky
+			//stuff and who knows what will happen.
+			SceneManager.LoadScene(GladMMOClientConstants.CHARACTER_CREATION_SCENE_NAME);
+		}
+	}
+}
