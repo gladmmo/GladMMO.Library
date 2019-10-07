101566b5f02c0410886b4a42a1cc771a97f0579c
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 06:05:10 -0500

    Added button click event general dispatching glue component

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/Initializables/BaseButtonClickGlueStartable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class BaseButtonClickGlueStartable : IGameStartable, IButtonClickedEventSubscribable
+	{
+		public event EventHandler<ButtonClickedEventArgs> OnButtonClicked;
+
+		private IUIButton ReferenceButton { get; }
+
+		public BaseButtonClickGlueStartable([NotNull] IUIButton referenceButton)
+		{
+			ReferenceButton = referenceButton ?? throw new ArgumentNullException(nameof(referenceButton), "Maybe you're missing autofac key filter.");
+		}
+
+		public async Task OnGameStart()
+		{
+			ReferenceButton.AddOnClickListener(() => { OnButtonClicked?.Invoke(ReferenceButton, new ButtonClickedEventArgs(ReferenceButton)); });
+		}
+	}
+}
