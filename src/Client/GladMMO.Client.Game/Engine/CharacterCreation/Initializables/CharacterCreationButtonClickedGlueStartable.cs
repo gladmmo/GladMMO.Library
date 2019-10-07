655d8be1c1e4a4a2a528195e0f49c0035f947a90
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 06:14:57 -0500

    Added CharacterCreationButtonClickedGlueStartable

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterCreation/Initializables/CharacterCreationButtonClickedGlueStartable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(ICharacterCreationButtonClickedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterCreationScreen)]
+	public sealed class CharacterCreationButtonClickedGlueStartable : BaseButtonClickGlueStartable, ICharacterCreationButtonClickedEventSubscribable
+	{
+		public const UnityUIRegisterationKey ButtonKey = UnityUIRegisterationKey.BackButton;
+
+		public CharacterCreationButtonClickedGlueStartable([KeyFilter(ButtonKey)] [NotNull] IUIButton createButton) 
+			: base(createButton)
+		{
+
+		}
+	}
+}
