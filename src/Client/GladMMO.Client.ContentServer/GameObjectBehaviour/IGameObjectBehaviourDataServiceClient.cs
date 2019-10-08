bb8449e7435d1b1bce70e6db5d5246b76220380b
HelloKitty <andrew.blakely@ymail.com>
2019-10-08 13:57:25 -0500

    Added generic IGameObjectBehaviourDataServiceClient

---
+++ src/Client/GladMMO.Client.ContentServer/GameObjectBehaviour/IGameObjectBehaviourDataServiceClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO.SDK
+{
+	[Headers("User-Agent: SDK 0.0.1")]
+	public interface IGameObjectBehaviourDataServiceClient<TBehaviourModelType> 
+		where TBehaviourModelType : class
+	{
+		[Get("/instance/{id}")]
+		Task<ResponseModel<TBehaviourModelType, SceneContentQueryResponseCode>> GetBehaviourInstance([AliasAs("id")] int gameObjectId);
+
+		[Put("/instance/{id}")]
+		Task UpdateBehaviourInstance([AliasAs("id")] int gameObjectId, [JsonBody] TBehaviourModelType model);
+
+		/// <summary>
+		/// REST endpoint that yields the entire collection of behaviour entries for a provided/specified <see cref="worldId"/>.
+		/// </summary>
+		/// <param name="worldId">The id of the world.</param>
+		/// <returns>A non-null response model indicating the success or result.</returns>
+		[Get("/{worldId}/instance")]
+		Task<ResponseModel<ObjectEntryCollectionModel<TBehaviourModelType>, ContentEntryCollectionResponseCode>> GetBehaviourEntriesByWorld(long worldId);
+	}
+}
