92f53c83a4a81862a80598a222b686323e222e37
HelloKitty <andrew.blakely@ymail.com>
2019-10-08 15:36:33 -0500

    Added IRemoteModelUpdateable

---
+++ src/SDK/GladMMO.Common.SDK/Models/IRemoteModelUpdateable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for data types that can be updated from a remote model.
+	/// </summary>
+	/// <typeparam name="TRemoteModelType">The remote model type.</typeparam>
+	public interface IRemoteModelUpdateable<in TRemoteModelType>
+	{
+		/// <summary>
+		/// Updates the model with the remote <see cref="model"/> counterpart's data.
+		/// </summary>
+		/// <param name="model"></param>
+		void Update(TRemoteModelType model);
+	}
+}
