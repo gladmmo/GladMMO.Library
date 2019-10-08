a87c0f7527abbed95cdfa754bca9c6d725a9200b
HelloKitty <andrew.blakely@ymail.com>
2019-10-08 15:46:24 -0500

    Added WorldTeleporterBehaviourToTransportModelTypeConverter

---
+++ src/SDK/GladMMO.Client.SDK/Services/ContentBehaviourTypeConverters/WorldTeleporterBehaviourToTransportModelTypeConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO.SDK
+{
+	public sealed class WorldTeleporterBehaviourToTransportModelTypeConverter : ITypeConverterProvider<WorldTeleporterDefinitionData, WorldTeleporterInstanceModel>
+	{
+		public WorldTeleporterInstanceModel Convert(WorldTeleporterDefinitionData fromObject)
+		{
+			return new WorldTeleporterInstanceModel(fromObject.gameObject.GetComponent<GameObjectStaticSpawnPointDefinition>().GameObjectInstanceId, fromObject.LocalSpawnPointId.PlayerSpawnPointId, fromObject.RemoteSpawnPointId);
+		}
+	}
+}
