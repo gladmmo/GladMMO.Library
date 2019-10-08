62c2a6ec7006ee9a91d2e143e6d9513e4868d995
HelloKitty <andrew.blakely@ymail.com>
2019-10-08 08:55:59 -0500

    Added AvatarPedestalDataController

---
+++ src/Server/GladMMO.Service.ContentServer/Controllers/GameObjectBehaviorControllers/AvatarPedestalDataController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	[Route("api/[controller]")]
+	public sealed class AvatarPedestalDataController : BaseGameObjectBehaviourDataController<GameObjectAvatarPedestalEntryModel, AvatarPedestalInstanceModel, IAvatarPedestalGameObjectEntryRepository>
+	{
+		public AvatarPedestalDataController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger)
+		{
+
+		}
+	}
+}
