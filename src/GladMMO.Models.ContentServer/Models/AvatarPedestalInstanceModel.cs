cce9d42640bb6ad98206a5817aba5c8543f38fb5
HelloKitty <andrew.blakely@ymail.com>
2019-10-08 08:55:11 -0500

    Added AvatarPedestalInstanceModel

---
+++ src/GladMMO.Models.ContentServer/Models/AvatarPedestalInstanceModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public class AvatarPedestalInstanceModel : IGameObjectLinkable
+	{
+		//TODO: Combine the TargetGameObjectId and the LinkedGameObjectId. They are conceptually the same, legacy stuff.
+		[JsonIgnore]
+		public int LinkedGameObjectId => TargetGameObjectId;
+
+		/// <summary>
+		/// Defines the GameObject instance that
+		/// this behavior is attached to. It is the primary
+		/// and foreign key to the instance it's attached to.
+		/// </summary>
+		[JsonRequired]
+		[JsonProperty]
+		public int TargetGameObjectId { get; private set; }
+
+		[JsonRequired]
+		[JsonProperty]
+		public int AvatarModelId { get; private set; }
+
+		public AvatarPedestalInstanceModel(int targetGameObjectId, int avatarModelId)
+		{
+			if(targetGameObjectId <= 0) throw new ArgumentOutOfRangeException(nameof(targetGameObjectId));
+			if(avatarModelId <= 0) throw new ArgumentOutOfRangeException(nameof(avatarModelId));
+
+			TargetGameObjectId = targetGameObjectId;
+			AvatarModelId = avatarModelId;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		protected AvatarPedestalInstanceModel()
+		{
+
+		}
+	}
+}
