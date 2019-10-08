1c8a161c7d65af7be087815a7e8351e22d1aecb7
HelloKitty <andrew.blakely@ymail.com>
2019-10-08 08:02:58 -0500

    Made WorldTeleporterDataController inherit generic base behavior controller

--- src/Server/GladMMO.Service.ContentServer/Controllers/GameObjectBehaviorControllers/WorldTeleporterDataController.cs
+++ src/Server/GladMMO.Service.ContentServer/Controllers/GameObjectBehaviorControllers/WorldTeleporterDataController.cs
@@ -8,39 +8,12 @@ using Microsoft.Extensions.Logging;
 namespace GladMMO
 {
 	[Route("api/[controller]")]
-	public sealed class WorldTeleporterDataController : InstanceObjectDataController<GameObjectWorldTeleporterEntryModel, WorldTeleporterInstanceModel, IWorldTeleporterGameObjectEntryRepository>
+	public sealed class WorldTeleporterDataController : BaseGameObjectBehaviourDataController<GameObjectWorldTeleporterEntryModel, WorldTeleporterInstanceModel, IWorldTeleporterGameObjectEntryRepository>
 	{
 		public WorldTeleporterDataController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
 			: base(claimsReader, logger)
 		{
 
-		}
-
-		//TODO: Fix parameters annotations
-		public override Task<IActionResult> CreateObjectInstance(long worldId, IWorldTeleporterGameObjectEntryRepository instanceObjectRepository, IFactoryCreatable<GameObjectWorldTeleporterEntryModel, WorldInstanceableEntryModelCreationContext> defaultInstanceEntryModelFactory, ITypeConverterProvider<GameObjectWorldTeleporterEntryModel, WorldTeleporterInstanceModel> tableToNetworkModelConverter)
-		{
-			return Task.FromResult((IActionResult)BadRequest("This request is not valid for World Teleporters."));
-		}
-
-		public override async Task<IActionResult> UpdateObjectInstance([FromBody] [NotNull] WorldTeleporterInstanceModel model,
-			[FromRoute(Name = "id")] int objectId,
-			[FromServices] [NotNull] IWorldTeleporterGameObjectEntryRepository instanceObjectRepository,
-			[FromServices] [NotNull] ITypeConverterProvider<WorldTeleporterInstanceModel, GameObjectWorldTeleporterEntryModel> networkToTableConverter)
-		{
-			//So, this one is abit different since there is no created instance.
-			//The client needs to create it, we don't assign a default behavior.
-			//So it's possible it DOESN'T exist.
-			if(await instanceObjectRepository.ContainsAsync(objectId))
-				return await base.UpdateObjectInstance(model, objectId, instanceObjectRepository, networkToTableConverter);
-			else
-			{
-				//it doesn't exist, we need to try to create it.
-				GameObjectWorldTeleporterEntryModel entryModel = networkToTableConverter.Convert(model);
-
-				await instanceObjectRepository.TryCreateAsync(entryModel);
-			}
-
-			return Ok();
 		}
 	}
 }
