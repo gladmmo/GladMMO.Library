a1d8a5174849465d5ac63ca3a7e996af12c80d98
HelloKitty <andrew.blakely@ymail.com>
2019-10-08 15:15:25 -0500

    Added GenericGameObjectBehaviourServiceClientFactory

---
+++ src/SDK/GladMMO.Client.SDK/Services/GenericGameObjectBehaviourServiceClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GladMMO.SDK;
+
+namespace GladMMO
+{
+	public interface IGameObjectBehaviourDataClientFactory<TBehaviourType> : IFactoryCreatable<IGameObjectBehaviourDataServiceClient<TBehaviourType>, EmptyFactoryContext>
+		where TBehaviourType : class
+	{
+
+	}
+
+	public abstract class GenericGameObjectBehaviourServiceClientFactory<TBehaviourType> : IGameObjectBehaviourDataClientFactory<TBehaviourType> 
+		where TBehaviourType : class
+	{
+		public abstract IGameObjectBehaviourDataServiceClient<TBehaviourType> Create(EmptyFactoryContext context);
+	}
+}
