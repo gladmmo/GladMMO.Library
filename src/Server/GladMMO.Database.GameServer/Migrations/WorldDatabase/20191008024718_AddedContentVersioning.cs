32341798e5790ec868ffe7160b62b59d42450366
HelloKitty <andrew.blakely@ymail.com>
2019-10-07 21:48:21 -0500

    Added content versioning DB migration

---
+++ src/Server/GladMMO.Database.GameServer/Migrations/WorldDatabase/20191008024718_AddedContentVersioning.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GladMMO.Database.GameServer.Migrations.WorldDatabase
+{
+    public partial class AddedContentVersioning : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "world_entry",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "gameobject_model",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "creature_model",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Version",
+                table: "avatar_entry",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "world_entry");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "gameobject_model");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "creature_model");
+
+            migrationBuilder.DropColumn(
+                name: "Version",
+                table: "avatar_entry");
+        }
+    }
+}
