8f214da68951177b4eaa9db5b848274fa527b9c2
HelloKitty <andrew.blakely@ymail.com>
2019-10-08 11:28:26 -0500

    Added AvatarPedestalDefinitionData

---
+++ src/SDK/GladMMO.Common.SDK/Models/GameObject/Behaviors/AvatarPedestalDefinitionData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO.SDK
+{
+	public sealed class AvatarPedestalDefinitionData : GladMMOSDKMonoBehaviour, INetworkGameObjectBehaviour
+	{
+		public GameObjectType BehaviorType => GameObjectType.AvatarPedestal;
+
+		/// <summary>
+		/// The Avatar's Model ID.
+		/// </summary>
+		[HideInInspector]
+		[SerializeField]
+		private int _TargetAvatarModelId;
+
+		public int TargetAvatarModelId
+		{
+			get => _TargetAvatarModelId;
+			set => _TargetAvatarModelId = value;
+		}
+	}
+}
