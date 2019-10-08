c99c8f166dd4ea242b78f9285098f87231c0c79d
HelloKitty <andrew.blakely@ymail.com>
2019-10-08 08:42:00 -0500

    Refactored WorldTeleporter repository to use generic behavior repository.

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/GameObject/Behavior/DatabaseBackedWorldTeleporterEntryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	public sealed class DatabaseBackedWorldTeleporterEntryRepository : GenericDatabaseBackedGameObjectBehaviorEntryRepository<GameObjectWorldTeleporterEntryModel>, IWorldTeleporterGameObjectEntryRepository
+	{
+		public DatabaseBackedWorldTeleporterEntryRepository([JetBrains.Annotations.NotNull] ContentDatabaseContext context) 
+			: base(context)
+		{
+
+		}
+	}
+}
