ad2796febbefc18cc55f92b46fce790fded8565a
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 02:54:22 -0500

    Added AvatarPedestalBehaviourToTransportModelTypeConverter

---
+++ src/SDK/GladMMO.Client.SDK/Services/ContentBehaviourTypeConverters/AvatarPedestalBehaviourToTransportModelTypeConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO.SDK
+{
+	public sealed class AvatarPedestalBehaviourToTransportModelTypeConverter : ITypeConverterProvider<AvatarPedestalDefinitionData, AvatarPedestalInstanceModel>
+	{
+		public AvatarPedestalInstanceModel Convert([NotNull] AvatarPedestalDefinitionData fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			return new AvatarPedestalInstanceModel(fromObject.gameObject.GetComponent<GameObjectStaticSpawnPointDefinition>().GameObjectInstanceId, fromObject.TargetAvatarModelId);
+		}
+	}
+}
