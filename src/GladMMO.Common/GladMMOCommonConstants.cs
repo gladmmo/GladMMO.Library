dbc41787f12a97770b133dc66a0e248eeff86173
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 09:37:41 -0500

    Fixed data field size constants

--- src/GladMMO.Common/GladMMOCommonConstants.cs
+++ src/GladMMO.Common/GladMMOCommonConstants.cs
@@ -6,8 +6,19 @@ namespace GladMMO
 {
 	public static class GladMMOCommonConstants
 	{
-		public const int PLAYER_DATA_FIELD_SIZE = 5;
+		//These are just 8 right now because that's the minimum size.
+		public const int PLAYER_DATA_FIELD_SIZE = 16;
 
-		public const int GAMEOBJECT_DATA_FIELD_SIZE = 3;
+		public const int GAMEOBJECT_DATA_FIELD_SIZE = 8;
+
+		static GladMMOCommonConstants()
+		{
+			//If these throws highlight then you've fucked up the above.
+			if ((GAMEOBJECT_DATA_FIELD_SIZE % 8) != 0)
+				throw new InvalidOperationException($"Cannot have {nameof(GAMEOBJECT_DATA_FIELD_SIZE)} with field length that isn't a multiple of 8.");
+
+			if ((PLAYER_DATA_FIELD_SIZE % 8) != 0)
+				throw new InvalidOperationException($"Cannot have {nameof(PLAYER_DATA_FIELD_SIZE)} with field length that isn't a multiple of 8.");
+		}
 	}
 }
