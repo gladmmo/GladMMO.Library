d71d9c197e9e930b9b816ea5b9395572936bfee7
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 07:25:05 -0500

    Added InitializeGameObjectBehaviourTickable

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/OnGameObjectBehaviourCreated/InitializeGameObjectBehaviourTickable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using Glader.Essentials;
+using Nito.AsyncEx;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class InitializeGameObjectBehaviourTickable : EventQueueBasedTickable<IGameObjectBehaviourCreatedEventSubscribable, GameObjectBehaviourCreatedEventArgs>
+	{
+		public InitializeGameObjectBehaviourTickable(IGameObjectBehaviourCreatedEventSubscribable subscriptionService, ILog logger) 
+			: base(subscriptionService, true, logger)
+		{
+
+		}
+
+		protected override void HandleEvent(GameObjectBehaviourCreatedEventArgs args)
+		{
+			if (args.Component is IBehaviourComponentInitializable gi)
+				gi.Initialize();
+		}
+	}
+}
