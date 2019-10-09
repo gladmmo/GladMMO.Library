ea8b81583a01162691ee9547bf12ffa48eef1d36
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 09:14:32 -0500

    Added GladMMOCommonConstants

---
+++ src/GladMMO.Common/GladMMOCommonConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public static class GladMMOCommonConstants
+	{
+		public const int PLAYER_DATA_FIELD_SIZE = 5;
+
+		public const int GAMEOBJECT_DATA_FIELD_SIZE = 3;
+	}
+}
