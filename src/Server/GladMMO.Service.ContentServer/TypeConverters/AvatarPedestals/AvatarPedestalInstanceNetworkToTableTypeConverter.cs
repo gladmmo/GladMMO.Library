02028a62491b5a0b957c99170a78d07627dd3809
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 03:08:06 -0500

    Added backend avatarpedestal DB to Transport type converters

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/AvatarPedestals/AvatarPedestalInstanceNetworkToTableTypeConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladMMO.Database;
+using UnityEngine;
+
+namespace GladMMO
+{
+	
+	public sealed class AvatarPedestalInstanceNetworkToTableTypeConverter : ITypeConverterProvider<AvatarPedestalInstanceModel, GameObjectAvatarPedestalEntryModel>
+	{
+		public GameObjectAvatarPedestalEntryModel Convert([NotNull] AvatarPedestalInstanceModel fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			return new GameObjectAvatarPedestalEntryModel(fromObject.TargetGameObjectId, fromObject.AvatarModelId);
+		}
+	}
+}
