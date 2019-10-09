69180b0c48e34983f95d971cadd4f0f9224dccf8
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 02:56:23 -0500

    Added AvatarPedestalContentServiceClientFactory

---
+++ src/SDK/GladMMO.Client.SDK/Services/AvatarPedestalContentServiceClientFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Refit;
+
+namespace GladMMO.SDK
+{
+	public sealed class AvatarPedestalContentServiceClientFactory : GenericGameObjectBehaviourServiceClientFactory<AvatarPedestalInstanceModel>
+	{
+		public override IGameObjectBehaviourDataServiceClient<AvatarPedestalInstanceModel> Create(EmptyFactoryContext context)
+		{
+			//api/AvatarPedestalData
+			return Refit.RestService.For<IGameObjectBehaviourDataServiceClient<AvatarPedestalInstanceModel>>("http://72.190.177.214:5005/api/AvatarPedestalData/", new RefitSettings() { HttpMessageHandlerFactory = () => new AuthenticatedHttpClientHandler(new ContentSDKAuthenticationTokenRepository()) });
+		}
+	}
+}
