f0471a60fc1b629a1462192002717aea67cf5ec3
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 03:40:10 -0500

    Added initial AvatarPedestal serverside implementation

---
+++ src/Server/GladMMO.Server.ZoneServer/EntityComponents/DefaultAvatarPedestalInteractableGameObjectBehaviourComponent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+
+namespace GladMMO
+{
+	public sealed class DefaultAvatarPedestalInteractableGameObjectBehaviourComponent : BaseDefinedGameObjectEntityBehaviourComponent<AvatarPedestalInstanceModel>, IWorldInteractable
+	{
+		private ILog Logger { get; }
+
+		private IReadonlyEntityGuidMappable<IEntityDataFieldContainer> EntityDataContainer { get; }
+
+		public DefaultAvatarPedestalInteractableGameObjectBehaviourComponent(NetworkEntityGuid targetEntity, 
+			GameObjectInstanceModel instanceData, 
+			GameObjectTemplateModel templateData,
+			AvatarPedestalInstanceModel behaviourData,
+			[NotNull] ILog logger,
+			[NotNull] IReadonlyEntityGuidMappable<IEntityDataFieldContainer> entityDataContainer) 
+			: base(targetEntity, instanceData, templateData, behaviourData)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			EntityDataContainer = entityDataContainer ?? throw new ArgumentNullException(nameof(entityDataContainer));
+		}
+
+		public void Interact([NotNull] NetworkEntityGuid entityInteracting)
+		{
+			if(entityInteracting == null) throw new ArgumentNullException(nameof(entityInteracting));
+
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Entity: {entityInteracting} interacted with Entity: {TargetEntity}.");
+
+			//Only players should be able to interact with this.
+			if (entityInteracting.EntityType != EntityType.Player)
+				return;
+
+			IEntityDataFieldContainer dataFieldContainer = EntityDataContainer.RetrieveEntity(entityInteracting);
+
+			dataFieldContainer.SetFieldValue(EUnitFields.UNIT_FIELD_DISPLAYID, BehaviourData.AvatarModelId);
+		}
+	}
+}
