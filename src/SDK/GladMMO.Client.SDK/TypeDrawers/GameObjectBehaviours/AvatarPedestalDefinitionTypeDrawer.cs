35c0a331cd6b75758a3e020996238d451bb1b8c0
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 02:57:30 -0500

    Added AvatarPedestalDefinitionTypeDrawer

---
+++ src/SDK/GladMMO.Client.SDK/TypeDrawers/GameObjectBehaviours/AvatarPedestalDefinitionTypeDrawer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Glader.Essentials;
+using UnityEditor;
+using UnityEngine;
+
+namespace GladMMO.SDK
+{
+	[CustomEditor(typeof(AvatarPedestalDefinitionData))]
+	public sealed class AvatarPedestalDefinitionTypeDrawer : BaseBehaviourDefinitionTypeDrawer<AvatarPedestalDefinitionData, AvatarPedestalInstanceModel>
+	{
+
+		public AvatarPedestalDefinitionTypeDrawer() 
+			: base(new AvatarPedestalBehaviourToTransportModelTypeConverter(), new AvatarPedestalContentServiceClientFactory())
+		{
+
+		}
+
+		protected override void GatherConfigurableData([NotNull] AvatarPedestalDefinitionData target)
+		{
+			if (target == null) throw new ArgumentNullException(nameof(target));
+
+			target.TargetAvatarModelId = EditorGUILayout.IntField("Avatar Model Id:", GetTarget().TargetAvatarModelId);
+		}
+	}
+}
