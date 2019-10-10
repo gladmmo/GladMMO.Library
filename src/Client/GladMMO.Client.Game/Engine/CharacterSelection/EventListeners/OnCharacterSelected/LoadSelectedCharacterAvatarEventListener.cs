06b041f5cdb3253cd83f835cde2bd39af0930f0a
HelloKitty <andrew.blakely@ymail.com>
2019-10-10 06:32:04 -0500

    Added Character Avatar downloading to char select

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterSelection/EventListeners/OnCharacterSelected/LoadSelectedCharacterAvatarEventListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	public sealed class LoadSelectedCharacterAvatarEventListener : BaseSingleEventListenerInitializable<ICharacterSelectionButtonClickedEventSubscribable, CharacterButtonClickedEventArgs>
+	{
+		private IReadonlyEntityGuidMappable<CharacterAppearanceResponse> CharacterAppearanceMappable { get; }
+
+		private IFactoryCreatable<CustomModelLoaderCancelable, CustomModelLoaderCreationContext> AvatarLoaderFactory { get; }
+
+		public LoadSelectedCharacterAvatarEventListener(ICharacterSelectionButtonClickedEventSubscribable subscriptionService,
+			[NotNull] IReadonlyEntityGuidMappable<CharacterAppearanceResponse> characterAppearanceMappable,
+			[NotNull] IFactoryCreatable<CustomModelLoaderCancelable, CustomModelLoaderCreationContext> avatarLoaderFactory) 
+			: base(subscriptionService)
+		{
+			CharacterAppearanceMappable = characterAppearanceMappable ?? throw new ArgumentNullException(nameof(characterAppearanceMappable));
+			AvatarLoaderFactory = avatarLoaderFactory ?? throw new ArgumentNullException(nameof(avatarLoaderFactory));
+		}
+
+		protected override void OnEventFired(object source, CharacterButtonClickedEventArgs args)
+		{
+			CharacterAppearanceResponse data = CharacterAppearanceMappable.RetrieveEntity(args.CharacterGuid);
+
+			CustomModelLoaderCancelable loader = AvatarLoaderFactory.Create(new CustomModelLoaderCreationContext(data.AvatarModelId, args.CharacterGuid));
+			loader.StartLoading();
+		}
+	}
+}
