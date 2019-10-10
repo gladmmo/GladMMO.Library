3179adfe1b404536f00dc148c54aae58167be9a1
HelloKitty <andrew.blakely@ymail.com>
2019-10-10 06:04:25 -0500

    Added character appearence DTO models

---
+++ src/GladMMO.Models.GameServer/Models/CharacterAppearanceResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class CharacterAppearanceResponse
+	{
+		//We may have more stuff eventually, right now we only have the model id.
+		[JsonProperty]
+		public int AvatarModelId { get; }
+
+		public CharacterAppearanceResponse(int avatarModelId)
+		{
+			if (avatarModelId <= 0) throw new ArgumentOutOfRangeException(nameof(avatarModelId));
+
+			AvatarModelId = avatarModelId;
+		}
+
+		//Serializer ctor
+		private CharacterAppearanceResponse()
+		{
+
+		}
+	}
+}
---
+++ src/GladMMO.Models.GameServer/ResultCodes/CharacterDataQueryReponseCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public enum CharacterDataQueryReponseCode
+	{
+		/// <summary>
+		/// Indicates the creation was successful.
+		/// </summary>
+		Success = 1,
+
+		GeneralServerError = 2,
+
+		CharacterNotFound = 3,
+	}
+}
