9ce27224a167831a32152e2a360496fccae0fc5e
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 23:01:53 -0500

    Added client gameobject behaviour stubs

---
+++ src/Client/GladMMO.Client.Game/EntityComponents/StubAvatarPedestalGameObjectBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class StubAvatarPedestalGameObjectBehaviour : BaseClientGameObjectEntityBehaviourComponent
+	{
+		public StubAvatarPedestalGameObjectBehaviour(NetworkEntityGuid targetEntity, 
+			GameObject rootSceneObject, 
+			IEntityDataFieldContainer data) 
+			: base(targetEntity, rootSceneObject, data)
+		{
+
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/EntityComponents/StubWorldTeleporterGameObjectBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class StubWorldTeleporterGameObjectBehaviour : BaseClientGameObjectEntityBehaviourComponent
+	{
+		public StubWorldTeleporterGameObjectBehaviour(NetworkEntityGuid targetEntity, 
+			GameObject rootSceneObject, 
+			IEntityDataFieldContainer data) 
+			: base(targetEntity, rootSceneObject, data)
+		{
+
+		}
+	}
+}
