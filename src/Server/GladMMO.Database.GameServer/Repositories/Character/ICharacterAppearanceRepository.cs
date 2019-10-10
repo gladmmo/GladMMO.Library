9a7de6871105e07e78d3c71509c33f58c79b8043
HelloKitty <andrew.blakely@ymail.com>
2019-10-10 04:11:34 -0500

    Added ICharacterAppearanceRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Character/ICharacterAppearanceRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ICharacterAppearanceRepository : IGenericRepositoryCrudable<int, CharacterAppearanceModel>
+	{
+	}
+}
