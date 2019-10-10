5ef60e7606da0d7b624076ba319f02717d949d93
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 22:47:29 -0500

    Added BaseClientGameObjectEntityBehaviourComponent

---
+++ src/Client/GladMMO.Client.Game/EntityComponents/Base/BaseClientGameObjectEntityBehaviourComponent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public abstract class BaseClientGameObjectEntityBehaviourComponent : BaseEntityBehaviourComponent
+	{
+		protected GameObject RootSceneObject { get; }
+
+		//TODO: We should move this to base class, server objects should have access to their data too.
+		protected IEntityDataFieldContainer Data { get; }
+
+		public BaseClientGameObjectEntityBehaviourComponent(NetworkEntityGuid targetEntity, 
+			[NotNull] GameObject rootSceneObject,
+			[NotNull] IEntityDataFieldContainer data) 
+			: base(targetEntity)
+		{
+			RootSceneObject = rootSceneObject ?? throw new ArgumentNullException(nameof(rootSceneObject));
+			Data = data ?? throw new ArgumentNullException(nameof(data));
+		}
+	}
+}
