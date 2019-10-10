65babdb57ad7a046897b2cac81caed6ab53c5a0a
HelloKitty <andrew.blakely@ymail.com>
2019-10-10 06:36:03 -0500

    Added CreateSelectedCharacterAvatarDisplayEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterSelection/EventListeners/OnAvatarDownloaded/CreateSelectedCharacterAvatarDisplayEventListener.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	public sealed class CreateSelectedCharacterAvatarDisplayEventListener : ThreadUnSafeBaseSingleEventListenerInitializable<IContentPrefabCompletedDownloadEventSubscribable, ContentPrefabCompletedDownloadEventArgs>
+	{
+		private Action OnContentPrefabRecieved;
+
+		public CreateSelectedCharacterAvatarDisplayEventListener(IContentPrefabCompletedDownloadEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+		}
+
+		protected override void OnThreadUnSafeEventFired(object source, ContentPrefabCompletedDownloadEventArgs args)
+		{
+			//We really should only get this for players, so we should just assume this will work.
+			OnContentPrefabRecieved?.Invoke();
+			OnContentPrefabRecieved = null;
+
+			GameObject avatar = GameObject.Instantiate(args.DownloadedPrefabObject);
+
+			OnContentPrefabRecieved += () =>
+			{
+				GameObject.Destroy(avatar);
+				args.PrefabHandle.Release();
+			};
+		}
+	}
+}
