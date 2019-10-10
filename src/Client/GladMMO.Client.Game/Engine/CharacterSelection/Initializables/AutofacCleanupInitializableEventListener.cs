3e2563f4a1d5ded0ddacc150473db5b1c593b530
HelloKitty <andrew.blakely@ymail.com>
2019-10-10 08:45:35 -0500

    Added character selection AutofacCleanupInitializableEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterSelection/Initializables/AutofacCleanupInitializableEventListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Glader.Essentials;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	public sealed class AutofacCleanupInitializableEventListener : IGameInitializable
+	{
+		private ILifetimeScope AutofacScope { get; }
+
+		public AutofacCleanupInitializableEventListener([NotNull] ILifetimeScope autofacScope)
+		{
+			AutofacScope = autofacScope ?? throw new ArgumentNullException(nameof(autofacScope));
+		}
+
+		public async Task OnGameInitialized()
+		{
+			SceneManager.sceneLoaded += CleanupAutofac;
+		}
+
+		public void CleanupAutofac(Scene scene, LoadSceneMode mode)
+		{
+			AutofacScope.Dispose();
+			SceneManager.sceneLoaded -= CleanupAutofac;
+		}
+	}
+}
