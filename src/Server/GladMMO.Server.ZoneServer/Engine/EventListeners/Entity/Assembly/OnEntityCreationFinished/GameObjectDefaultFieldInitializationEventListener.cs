f2de0b69045aec1c07064391f3b012a6e0f10ff8
HelloKitty <andrew.blakely@ymail.com>
2019-10-09 20:58:35 -0500

    Added GameObjectDefaultFieldInitializationEventListener

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/Assembly/OnEntityCreationFinished/GameObjectDefaultFieldInitializationEventListener.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class GameObjectDefaultFieldInitializationEventListener : GameObjectCreationFinishedEventListener
+	{
+		private IReadonlyEntityGuidMappable<IEntityDataFieldContainer> EntityDataMappable { get; }
+
+		private IReadonlyEntityGuidMappable<GameObjectTemplateModel> GameObjectTemplateMappable { get; }
+
+		public GameObjectDefaultFieldInitializationEventListener(IEntityCreationFinishedEventSubscribable subscriptionService,
+			[NotNull] IReadonlyEntityGuidMappable<IEntityDataFieldContainer> entityDataMappable,
+			[NotNull] IReadonlyEntityGuidMappable<GameObjectTemplateModel> gameObjectTemplateMappable) 
+			: base(subscriptionService)
+		{
+			EntityDataMappable = entityDataMappable ?? throw new ArgumentNullException(nameof(entityDataMappable));
+			GameObjectTemplateMappable = gameObjectTemplateMappable ?? throw new ArgumentNullException(nameof(gameObjectTemplateMappable));
+		}
+
+		protected override void OnEntityCreationFinished(EntityCreationFinishedEventArgs args)
+		{
+			IEntityDataFieldContainer entityData = EntityDataMappable.RetrieveEntity(args.EntityGuid);
+			GameObjectTemplateModel objectTemplateModel = GameObjectTemplateMappable.RetrieveEntity(args.EntityGuid);
+
+			entityData.SetFieldValue(GameObjectField.GAMEOBJECT_TYPE_ID, objectTemplateModel.ObjectType);
+		}
+	}
+}
