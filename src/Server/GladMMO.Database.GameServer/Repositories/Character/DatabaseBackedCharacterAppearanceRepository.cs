a550dcafdb5fb22958686bcf670c71900f03652b
HelloKitty <andrew.blakely@ymail.com>
2019-10-10 04:13:50 -0500

    Added DatabaseBackedCharacterAppearanceRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Character/DatabaseBackedCharacterAppearanceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DatabaseBackedCharacterAppearanceRepository : BaseGenericBackedDatabaseRepository<CharacterDatabaseContext, int, CharacterAppearanceModel>, ICharacterAppearanceRepository
+	{
+		public DatabaseBackedCharacterAppearanceRepository([JetBrains.Annotations.NotNull] CharacterDatabaseContext context) 
+			: base(context)
+		{
+
+		}
+	}
+}
