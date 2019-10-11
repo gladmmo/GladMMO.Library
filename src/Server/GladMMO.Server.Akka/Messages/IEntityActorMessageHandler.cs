680c6a21566f4735f56b5371c616759d786c9335
HelloKitty <andrew.blakely@ymail.com>
2019-10-11 18:25:53 -0500

    Added IEntityActorMessageHandler

---
+++ src/Server/GladMMO.Server.Akka/Messages/IEntityActorMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IEntityActorMessageHandler<in TEntityStateType, in TEntityMessageType>
+		where TEntityStateType : IEntityActorStateContainable
+		where TEntityMessageType : EntityActorMessage
+	{
+		void HandleMessage(EntityActorMessageContext messageContext, TEntityStateType state, TEntityMessageType message);
+	}
+}
