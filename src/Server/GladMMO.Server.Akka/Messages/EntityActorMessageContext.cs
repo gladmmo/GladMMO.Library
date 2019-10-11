0ee54b46f8ef6ad820bc2583551dd03ddf8b639e
HelloKitty <andrew.blakely@ymail.com>
2019-10-11 17:25:47 -0500

    Added EntityActorMessageContext

---
+++ src/Server/GladMMO.Server.Akka/Messages/EntityActorMessageContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// The context of the entity actor message sent.
+	/// </summary>
+	public sealed class EntityActorMessageContext
+	{
+		/// <summary>
+		/// Actor that sent the corresponding <see cref="EntityActorMessage"/>.
+		/// </summary>
+		public IActorRef Sender { get; }
+
+		/// <summary>
+		/// Actor that is receiving this message.
+		/// </summary>
+		public IActorRef Entity { get; }
+
+		public EntityActorMessageContext(IActorRef sender, IActorRef entity)
+		{
+			Sender = sender ?? throw new ArgumentNullException(nameof(sender));
+			Entity = entity ?? throw new ArgumentNullException(nameof(entity));
+		}
+	}
+}
