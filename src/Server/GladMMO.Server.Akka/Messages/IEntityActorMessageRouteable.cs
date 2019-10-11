e8bd2bb80f0d2ab80f5c2071e4a3cbbd27b509c1
HelloKitty <andrew.blakely@ymail.com>
2019-10-11 18:37:26 -0500

    Added IEntityActorMessageRouteable

---
+++ src/Server/GladMMO.Server.Akka/Messages/IEntityActorMessageRouteable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for types that can route messages.
+	/// </summary>
+	/// <typeparam name="TEntityStateType"></typeparam>
+	public interface IEntityActorMessageRouteable<in TEntityStateType>
+		where TEntityStateType : IEntityActorStateContainable
+	{
+		void RouteMessage(EntityActorMessageContext messageContext, TEntityStateType state, EntityActorMessage message);
+	}
+}
