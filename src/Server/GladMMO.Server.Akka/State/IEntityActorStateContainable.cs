c5aa3c9e5f74b5ab1f8de1a8f444f8aefe3fff6e
HelloKitty <andrew.blakely@ymail.com>
2019-10-11 17:25:54 -0500

    Added IEntityActorStateContainable

---
+++ src/Server/GladMMO.Server.Akka/State/IEntityActorStateContainable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Base contract for actors' state.
+	/// </summary>
+	public interface IEntityActorStateContainable
+	{
+		/// <summary>
+		/// Reference to Entity/Actor raw replicatable entity data.
+		/// </summary>
+		IEntityDataFieldContainer EntityData { get; }
+	}
+}
