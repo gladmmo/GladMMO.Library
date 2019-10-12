d8123d5d5c12538a6a8edbbd49c0aa8949afa468
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 15:27:59 -0500

    Added IEntityActorStateInitializable

---
+++ src/Server/GladMMO.Server.Akka/State/IEntityActorStateInitializable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IEntityActorStateInitializable<in TActorStateType>
+	{
+		/// <summary>
+		/// Indicates if the state has been initialized.
+		/// </summary>
+		bool isInitialized { get; }
+
+		/// <summary>
+		/// Initializes the actor's state.
+		/// </summary>
+		/// <exception cref="InvalidOperationException">Throws if <see cref="isInitialized"/> is true.</exception>
+		/// <param name="state">The state.</param>
+		void InitializeState(TActorStateType state);
+	}
+}
