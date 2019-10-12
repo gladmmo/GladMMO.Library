bf84dd322fa9cadb044e47a36219f5dc8fbd11dd
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 16:40:21 -0500

    Added EntityActorStateInitializeMessage

---
+++ src/Server/GladMMO.Server.Akka/Messages/EntityActorStateInitializeMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class EntityActorStateInitializeMessage<TStateType> : EntityActorMessage
+		where TStateType : class, IEntityActorStateContainable
+	{
+		public TStateType State { get; }
+
+		public EntityActorStateInitializeMessage(TStateType state)
+		{
+			State = state ?? throw new ArgumentNullException(nameof(state));
+		}
+	}
+}
