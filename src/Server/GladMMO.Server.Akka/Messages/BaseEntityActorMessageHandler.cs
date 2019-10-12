19a4dc15cb0512054e0c74617ceb6d86eccdd441
HelloKitty <andrew.blakely@ymail.com>
2019-10-11 22:17:22 -0500

    Added BaseEntityActorMessageHandler

---
+++ src/Server/GladMMO.Server.Akka/Messages/BaseEntityActorMessageHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	//Reflection will search for all: IEntityActorMessageHandler<TEntityStateType, EntityActorMessage>
+	//Meaning it will discover all known message handlers and will grab them if they are marked with the appropriate attribute type.
+	public abstract class BaseEntityActorMessageHandler<TEntityStateType, TEntityMessageType> : IEntityActorMessageHandler<TEntityStateType, EntityActorMessage>
+		where TEntityStateType : IEntityActorStateContainable 
+		where TEntityMessageType : EntityActorMessage
+	{
+		public void HandleMessage(EntityActorMessageContext messageContext, TEntityStateType state, EntityActorMessage message)
+		{
+			//Assume caller has verified this will work
+			//We downcast so the API consumed is simplier/easier.
+			//We need the interface to not be specific so it can be discovered, registered and handled.
+			HandleMessage(messageContext, state, (TEntityMessageType) message);
+		}
+
+		protected abstract void HandleMessage(EntityActorMessageContext messageContext, TEntityStateType state, TEntityMessageType message);
+	}
+}
