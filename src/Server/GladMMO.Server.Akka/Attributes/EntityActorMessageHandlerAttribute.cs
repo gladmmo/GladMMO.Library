18a36035d59dc7b93c9a8b39d45b9851a6670b87
HelloKitty <andrew.blakely@ymail.com>
2019-10-11 22:17:07 -0500

    Added EntityActorMessageHandlerAttribute

---
+++ src/Server/GladMMO.Server.Akka/Attributes/EntityActorMessageHandlerAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	public class EntityActorMessageHandlerAttribute : Attribute
+	{
+		public Type TargetActorType { get; }
+
+		public EntityActorMessageHandlerAttribute(Type targetActorType)
+		{
+			TargetActorType = targetActorType ?? throw new ArgumentNullException(nameof(targetActorType));
+
+			if (!typeof(IEntityActor).IsAssignableFrom(targetActorType))
+				throw new InvalidOperationException($"Provided Type: {targetActorType.Name} is not a valid Entity Actor.");
+		}
+	}
+}
