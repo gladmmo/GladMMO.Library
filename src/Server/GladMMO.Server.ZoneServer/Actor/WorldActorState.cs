e5cc30a18de4c11137abcc47177c437ea18d4a0a
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 16:30:25 -0500

    Added WorldActorState

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/WorldActorState.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	public sealed class WorldActorState : IEntityActorStateContainable
+	{
+		//Kinda a hack, but we can use entity data container for World too maybe.
+		public IEntityDataFieldContainer EntityData { get; } = new EntityFieldDataCollection(8);
+
+		//TODO: Eventually we should create a world entity guid.
+		public NetworkEntityGuid EntityGuid { get; } = NetworkEntityGuid.Empty;
+
+		/// <summary>
+		/// Factory for actor creation for the world.
+		/// </summary>
+		public IActorRefFactory WorldActorFactory { get; }
+
+		public WorldActorState([NotNull] IActorRefFactory actorFactory)
+		{
+			WorldActorFactory = actorFactory ?? throw new ArgumentNullException(nameof(actorFactory));
+		}
+	}
+}
