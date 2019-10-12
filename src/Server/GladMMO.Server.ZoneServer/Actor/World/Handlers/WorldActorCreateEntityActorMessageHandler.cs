b326ac7c24833a4acb5c0ed20cc31611afa09ae1
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 17:50:39 -0500

    Added demo worldactor messaging

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/World/Handlers/WorldActorCreateEntityActorMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultWorldActor))]
+	public sealed class WorldActorCreateEntityActorMessageHandler : BaseEntityActorMessageHandler<WorldActorState, CreateEntityActorMessage>
+	{
+		private ILog Logger { get; }
+
+		public WorldActorCreateEntityActorMessageHandler([NotNull] ILog logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		protected override void HandleMessage(EntityActorMessageContext messageContext, WorldActorState state, CreateEntityActorMessage message)
+		{
+			if(Logger.IsWarnEnabled)
+				Logger.Warn($"WorldActor recieved entity actor creation request for Actor: {message.EntityGuid}");
+		}
+	}
+}
---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/EventListeners/Entity/Assembly/OnEntityCreationFinished/WorldActorEntityActorCreationEventListener.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class WorldActorEntityActorCreationEventListener : EntityCreationFinishedEventListener
+	{
+		private IWorldActorRef WorldActor { get; }
+
+		public WorldActorEntityActorCreationEventListener(IEntityCreationFinishedEventSubscribable subscriptionService,
+			[NotNull] IWorldActorRef worldActor) 
+			: base(subscriptionService)
+		{
+			WorldActor = worldActor ?? throw new ArgumentNullException(nameof(worldActor));
+		}
+
+		public WorldActorEntityActorCreationEventListener(IEntityCreationFinishedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+
+		protected override void OnEventFired(object source, EntityCreationFinishedEventArgs args)
+		{
+			WorldActor.Tell(new CreateEntityActorMessage(args.EntityGuid));
+		}
+
+		protected override void OnEntityCreationFinished(EntityCreationFinishedEventArgs args)
+		{
+			
+		}
+	}
+}
