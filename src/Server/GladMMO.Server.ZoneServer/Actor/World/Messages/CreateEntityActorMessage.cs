384f976e8cbfeb98273885b11ba810f482f5f024
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 17:50:19 -0500

    Added CreateEntityActorMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/World/Messages/CreateEntityActorMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class CreateEntityActorMessage : EntityActorMessage
+	{
+		public NetworkEntityGuid EntityGuid { get; }
+
+		public CreateEntityActorMessage([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
