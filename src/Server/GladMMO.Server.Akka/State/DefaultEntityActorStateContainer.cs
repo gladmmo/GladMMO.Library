e5063a456b309d6d271c02e166b9027b900d2372
HelloKitty <andrew.blakely@ymail.com>
2019-10-11 21:53:28 -0500

    Added DefaultEntityActorStateContainer

---
+++ src/Server/GladMMO.Server.Akka/State/DefaultEntityActorStateContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DefaultEntityActorStateContainer : IEntityActorStateContainable
+	{
+		public IEntityDataFieldContainer EntityData { get; }
+
+		public NetworkEntityGuid EntityGuid { get; }
+
+		public DefaultEntityActorStateContainer(IEntityDataFieldContainer entityData, NetworkEntityGuid entityGuid)
+		{
+			EntityData = entityData ?? throw new ArgumentNullException(nameof(entityData));
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
