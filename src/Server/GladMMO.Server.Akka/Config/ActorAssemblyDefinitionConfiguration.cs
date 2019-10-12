a7e4a5074131d7696a95337907f91b8836c12909
HelloKitty <andrew.blakely@ymail.com>
2019-10-11 23:15:14 -0500

    Added ActorAssemblyDefinitionConfiguration

---
+++ src/Server/GladMMO.Server.Akka/Config/ActorAssemblyDefinitionConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public class ActorAssemblyDefinitionConfiguration
+	{
+		[JsonRequired]
+		[JsonProperty]
+		public string[] AssemblyNames { get; }
+
+		public ActorAssemblyDefinitionConfiguration(string[] assemblyNames)
+		{
+			AssemblyNames = assemblyNames ?? throw new ArgumentNullException(nameof(assemblyNames));
+			AssemblyNames = assemblyNames.Select(s => s.ToLower()).ToArray();
+
+			if (!AssemblyNames.Contains("gladmmo.server.zoneserver"))
+				AssemblyNames = assemblyNames.ToList().Concat(new string[] {"GladMMO.Server.ZoneServer"}).ToArray();
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		public ActorAssemblyDefinitionConfiguration()
+		{
+
+		}
+	}
+}
