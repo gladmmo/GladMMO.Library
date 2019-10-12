a6ce1fb552914031bce200263bba28c1b176195a
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 16:30:36 -0500

    Added DefaultWorldActor

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/DefaultWorldActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+
+namespace GladMMO
+{
+	public sealed class DefaultWorldActor : BaseEntityActor<DefaultWorldActor, WorldActorState>
+	{
+		public DefaultWorldActor(IEntityActorMessageRouteable<DefaultWorldActor, WorldActorState> messageRouter, ILog logger) 
+			: base(messageRouter, logger)
+		{
+
+		}
+	}
+}
