068ea1b1f74f2e8f67adea7de58e893287da8df1
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 15:28:12 -0500

    Impl IEntityActorStateInitializable on BaseEntityActor

--- src/Server/GladMMO.Server.Akka/BaseEntityActor.cs
+++ src/Server/GladMMO.Server.Akka/BaseEntityActor.cs
@@ -11,24 +11,27 @@ namespace GladMMO
 	/// </summary>
 	/// <typeparam name="TActorStateType"></typeparam>
 	/// <typeparam name="TChildActorType"></typeparam>
-	public abstract class BaseEntityActor<TChildActorType, TActorStateType> : Akka.Actor.UntypedActor, IEntityActor
+	public abstract class BaseEntityActor<TChildActorType, TActorStateType> : Akka.Actor.UntypedActor, IEntityActor, IEntityActorStateInitializable<TActorStateType>
 		where TActorStateType : class, IEntityActorStateContainable
 		where TChildActorType : BaseEntityActor<TChildActorType, TActorStateType>
 	{
+		private readonly object SyncObj = new object();
+
 		/// <summary>
 		/// Potentially mutable state for the actor.
 		/// </summary>
-		protected TActorStateType ActorState { get; }
+		protected TActorStateType ActorState { get; private set; }
 
 		private IEntityActorMessageRouteable<TChildActorType, TActorStateType> MessageRouter { get; }
 
 		protected ILog Logger { get; }
 
-		protected BaseEntityActor(TActorStateType actorState, IEntityActorMessageRouteable<TChildActorType, TActorStateType> messageRouter, ILog logger)
+		public bool isInitialized { get; private set; } = false;
+
+		protected BaseEntityActor(IEntityActorMessageRouteable<TChildActorType, TActorStateType> messageRouter, ILog logger)
 		{
 			MessageRouter = messageRouter ?? throw new ArgumentNullException(nameof(messageRouter));
 			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-			ActorState = actorState ?? throw new ArgumentNullException(nameof(actorState));
 		}
 
 		protected sealed override void OnReceive(object message)
@@ -41,6 +44,15 @@ namespace GladMMO
 			if(!MessageRouter.RouteMessage(context, ActorState, castedMessage))
 				if(Logger.IsWarnEnabled)
 					Logger.Warn($"EntityActor encountered unhandled MessageType: {message.GetType().Name}");
+		}
+
+		public void InitializeState(TActorStateType state)
+		{
+			lock (SyncObj)
+			{
+				ActorState = state;
+				isInitialized = true;
+			}
 		}
 	}
 }
