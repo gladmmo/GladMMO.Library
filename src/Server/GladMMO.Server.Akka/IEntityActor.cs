a596638df35ca9738d7c1cb32066be5fe5414db7
HelloKitty <andrew.blakely@ymail.com>
2019-10-11 22:16:52 -0500

    Added IEntityActor

---
+++ src/Server/GladMMO.Server.Akka/IEntityActor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Marker interface for entity actor types.
+	/// </summary>
+	public interface IEntityActor
+	{
+
+	}
+}
