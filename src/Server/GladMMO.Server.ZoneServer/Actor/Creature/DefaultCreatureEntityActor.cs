882b05e94241d97e432a9255b10ee6ac1bcab320
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 21:42:39 -0500

    Added DefaultCreatureEntityActor

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Creature/DefaultCreatureEntityActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+
+namespace GladMMO
+{
+	public sealed class DefaultCreatureEntityActor : BaseEntityActor<DefaultCreatureEntityActor, DefaultEntityActorStateContainer>
+	{
+		public DefaultCreatureEntityActor(IEntityActorMessageRouteable<DefaultCreatureEntityActor, DefaultEntityActorStateContainer> messageRouter, ILog logger) 
+			: base(messageRouter, logger)
+		{
+
+		}
+	}
+}
