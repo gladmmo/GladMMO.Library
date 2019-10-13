55ea40e0bd8c7244cf9de0ecc03ab944d55af85b
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 19:50:37 -0500

    Added EntityActorCreationResult

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Creation/EntityActorCreationResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class EntityActorCreationResult
+	{
+		public Type DesiredActorType { get; }
+
+		public IEntityActorStateInitializeMessage<IEntityActorStateContainable> InitializationMessage { get; }
+
+		public EntityActorCreationResult([NotNull] Type desiredActorType, [NotNull] IEntityActorStateInitializeMessage<IEntityActorStateContainable> initializationMessage)
+		{
+			DesiredActorType = desiredActorType ?? throw new ArgumentNullException(nameof(desiredActorType));
+			InitializationMessage = initializationMessage ?? throw new ArgumentNullException(nameof(initializationMessage));
+		}
+	}
+}
