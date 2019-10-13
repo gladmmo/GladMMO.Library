ec39fc73fe3be38837e47c54b313e518c6011643
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 21:54:40 -0500

    Added InteractWithEntityActorMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/InteractWithEntityActorMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class InteractWithEntityActorMessage : EntityActorMessage
+	{
+		public NetworkEntityGuid EntityInteracting { get; }
+
+		public InteractWithEntityActorMessage([NotNull] NetworkEntityGuid entityInteracting)
+		{
+			EntityInteracting = entityInteracting ?? throw new ArgumentNullException(nameof(entityInteracting));
+		}
+	}
+}
