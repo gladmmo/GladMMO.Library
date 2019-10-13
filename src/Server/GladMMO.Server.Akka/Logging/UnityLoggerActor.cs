73efa037d2ccd4595f9808d0c7e0e8fb6506eefc
HelloKitty <andrew.blakely@ymail.com>
2019-10-13 01:47:46 -0500

    Added UnityLoggerActor

---
+++ src/Server/GladMMO.Server.Akka/Logging/UnityLoggerActor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using Akka.Event;
+using Common.Logging;
+
+namespace GladMMO
+{
+	public sealed class UnityLoggerActor : ReceiveActor, ILogReceive
+	{
+		private ILog Logger { get; }
+
+		public UnityLoggerActor(ILog logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+			Receive<Debug>(e => Logger.Debug(e.ToString()));
+			Receive<Info>(e => Logger.Info(e.ToString()));
+			Receive<Warning>(e => Logger.Warn(e.ToString()));
+			Receive<Error>(e => Logger.Error(e.ToString()));
+			Receive<InitializeLogger>(_ => Sender.Tell(new LoggerInitialized()));
+		}
+	}
+}
