f2532d0b54761ee7c1589fdef0d7c6a1a46259ae
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 19:31:32 -0500

    Added DefaultGameObjectActorState

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/GameObjects/DefaultGameObjectActorState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DefaultGameObjectActorState : IEntityActorStateContainable
+	{
+		public IEntityDataFieldContainer EntityData { get; }
+
+		public NetworkEntityGuid EntityGuid { get; }
+
+		public GameObjectInstanceModel InstanceModel { get; }
+
+		public GameObjectTemplateModel TemplateModel { get; }
+
+		public DefaultGameObjectActorState([NotNull] IEntityDataFieldContainer entityData,
+			[NotNull] NetworkEntityGuid entityGuid,
+			[NotNull] GameObjectInstanceModel instanceModel,
+			[NotNull] GameObjectTemplateModel templateModel)
+		{
+			EntityData = entityData ?? throw new ArgumentNullException(nameof(entityData));
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+			InstanceModel = instanceModel ?? throw new ArgumentNullException(nameof(instanceModel));
+			TemplateModel = templateModel ?? throw new ArgumentNullException(nameof(templateModel));
+		}
+	}
+}
