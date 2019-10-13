29d6708ea5c6bd01c4f5e02fac1729db2586a12b
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 19:56:57 -0500

    Added WorldTeleporterGameObjectState

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/GameObjects/GameObjectWorldTeleporterEntityActor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+
+namespace GladMMO
+{
+	public sealed class GameObjectWorldTeleporterEntityActor :  BaseEntityActor<GameObjectWorldTeleporterEntityActor, WorldTeleporterGameObjectState>
+	{
+		public GameObjectWorldTeleporterEntityActor(IEntityActorMessageRouteable<GameObjectWorldTeleporterEntityActor, WorldTeleporterGameObjectState> messageRouter, ILog logger) 
+			: base(messageRouter, logger)
+		{
+
+		}
+	}
+}
