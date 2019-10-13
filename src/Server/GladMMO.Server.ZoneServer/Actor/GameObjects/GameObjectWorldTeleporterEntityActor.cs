0b691809b587540c66c924a7f8db57dbef49e7f7
HelloKitty <andrew.blakely@ymail.com>
2019-10-12 19:59:13 -0500

    Made behaviour game objects state generic

--- src/Server/GladMMO.Server.ZoneServer/Actor/GameObjects/GameObjectWorldTeleporterEntityActor.cs
+++ src/Server/GladMMO.Server.ZoneServer/Actor/GameObjects/GameObjectWorldTeleporterEntityActor.cs
@@ -5,9 +5,9 @@ using Common.Logging;
 
 namespace GladMMO
 {
-	public sealed class GameObjectWorldTeleporterEntityActor :  BaseEntityActor<GameObjectWorldTeleporterEntityActor, WorldTeleporterGameObjectState>
+	public sealed class GameObjectWorldTeleporterEntityActor :  BaseEntityActor<GameObjectWorldTeleporterEntityActor, BehaviourGameObjectState<WorldTeleporterInstanceModel>>
 	{
-		public GameObjectWorldTeleporterEntityActor(IEntityActorMessageRouteable<GameObjectWorldTeleporterEntityActor, WorldTeleporterGameObjectState> messageRouter, ILog logger) 
+		public GameObjectWorldTeleporterEntityActor(IEntityActorMessageRouteable<GameObjectWorldTeleporterEntityActor, BehaviourGameObjectState<WorldTeleporterInstanceModel>> messageRouter, ILog logger) 
 			: base(messageRouter, logger)
 		{
 
---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/GameObjects/State/BehaviourGameObjectState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class BehaviourGameObjectState<TBehaviourType> : DefaultGameObjectActorState
+		where TBehaviourType : class, IGameObjectLinkable
+	{
+		public TBehaviourType BehaviourData { get; }
+
+		public BehaviourGameObjectState(IEntityDataFieldContainer entityData, NetworkEntityGuid entityGuid, GameObjectInstanceModel instanceModel, GameObjectTemplateModel templateModel,
+			[NotNull] TBehaviourType behaviourData) 
+			: base(entityData, entityGuid, instanceModel, templateModel)
+		{
+			BehaviourData = behaviourData ?? throw new ArgumentNullException(nameof(behaviourData));
+		}
+	}
+}
