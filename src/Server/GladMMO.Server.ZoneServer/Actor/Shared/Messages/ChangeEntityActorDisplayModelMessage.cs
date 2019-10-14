3a688afaff6b6d9fc66fe16964897699a5d665e9
HelloKitty <andrew.blakely@ymail.com>
2019-10-13 21:40:27 -0500

    Added ChangeEntityActorDisplayModelMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/ChangeEntityActorDisplayModelMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class ChangeEntityActorDisplayModelMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The new model/display ID to be set.
+		/// </summary>
+		public int NewModelId { get; }
+
+		public ChangeEntityActorDisplayModelMessage(int newModelId)
+		{
+			if (newModelId <= 0) throw new ArgumentOutOfRangeException(nameof(newModelId));
+
+			NewModelId = newModelId;
+		}
+	}
+}
