383e65ecba13c0f1ae57bd7a94659301ef26a437
HelloKitty <andrew.blakely@ymail.com>
2019-10-14 06:41:51 -0500

    Added GeneralErrorEncounteredEventPublisher

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/EventPublishers/GeneralErrorEncounteredEventPublisher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IGeneralErrorEncounteredEventPublisher
+	{
+
+	}
+
+	//I need a better way to specify "all scenes" lol.
+	[AdditionalRegisterationAs(typeof(IEventPublisher<IGeneralErrorEncounteredEventListener, GeneralErrorEncounteredEventArgs>))]
+	[AdditionalRegisterationAs(typeof(IGeneralErrorEncounteredEventPublisher))]
+	[AdditionalRegisterationAs(typeof(IGeneralErrorEncounteredEventListener))]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterCreationScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterCreationScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.TitleScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.PreZoneBurstingScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class GeneralErrorEncounteredEventPublisher : IEventPublisher<IGeneralErrorEncounteredEventListener, GeneralErrorEncounteredEventArgs>, IGeneralErrorEncounteredEventListener
+	{
+		public event EventHandler<GeneralErrorEncounteredEventArgs> OnErrorEncountered;
+
+		public void PublishEvent(object sender, GeneralErrorEncounteredEventArgs eventArgs)
+		{
+			OnErrorEncountered?.Invoke(sender, eventArgs);
+		}
+	}
+}
