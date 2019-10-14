aac78d623ab53b9ee99ddeea17fdd65b48ec1596
HelloKitty <andrew.blakely@ymail.com>
2019-10-14 06:35:42 -0500

    Added IGeneralErrorEncounteredEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/Events/IGeneralErrorEncounteredEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IGeneralErrorEncounteredEventListener
+	{
+		event EventHandler<GeneralErrorEncounteredEventArgs> OnErrorEncountered;
+	}
+
+	public sealed class GeneralErrorEncounteredEventArgs : EventArgs
+	{
+		public string ErrorTitle { get; }
+
+		public string ErrorMessage { get; }
+
+		public GeneralErrorEncounteredEventArgs([NotNull] string errorTitle, [NotNull] string errorMessage)
+		{
+			if (string.IsNullOrEmpty(errorTitle)) throw new ArgumentException("Value cannot be null or empty.", nameof(errorTitle));
+			if (string.IsNullOrEmpty(errorMessage)) throw new ArgumentException("Value cannot be null or empty.", nameof(errorMessage));
+
+			ErrorTitle = errorTitle;
+			ErrorMessage = errorMessage;
+		}
+	}
+}
