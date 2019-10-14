1e7a1af627ea70d5d5b448c61b2bbfd613c1298d
HelloKitty <andrew.blakely@ymail.com>
2019-10-14 01:07:20 -0500

    Added DisconnectNetworkPlayerEntityActorMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Player/Messages/DisconnectNetworkPlayerEntityActorMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DisconnectNetworkPlayerEntityActorMessage : EntityActorMessage
+	{
+		//TODO: Add a reason code.
+
+		public DisconnectNetworkPlayerEntityActorMessage()
+		{
+			
+		}
+	}
+}
