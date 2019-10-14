bb0eda6a30e14c88c9703e8ce34792f33842c1f5
HelloKitty <andrew.blakely@ymail.com>
2019-10-13 21:57:11 -0500

    Added AvatarPedestalChangeResponse

---
+++ src/GladMMO.Models.Server.ZoneServer/Models/AvatarPedestalChangeResponse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class AvatarPedestalChangeResponse
+	{
+		/// <summary>
+		/// This is the absolute backend truth about the entity's model id.
+		/// This is the correct one, and can be verified against in-memory.
+		/// </summary>
+		[JsonProperty]
+		public int PersistedModelId { get; private set; }
+
+		public AvatarPedestalChangeResponse(int persistedModelId)
+		{
+			if (persistedModelId <= 0) throw new ArgumentOutOfRangeException(nameof(persistedModelId));
+
+			PersistedModelId = persistedModelId;
+		}
+	}
+}
