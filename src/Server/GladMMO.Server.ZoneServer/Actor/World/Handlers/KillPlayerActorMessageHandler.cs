cecdfbe05c8a80d7e67b73a2484b18b7c7081f61
HelloKitty <andrew.blakely@ymail.com>
2019-10-14 04:42:28 -0500

    Added KillPlayerActorMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/World/Handlers/KillPlayerActorMessageHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultWorldActor))]
+	public sealed class KillPlayerActorMessageHandler : BaseEntityActorMessageHandler<WorldActorState, KillPlayerActorMessage>
+	{
+		private IEntityGuidMappable<IActorRef> ActorReferenceMappable { get; }
+
+		public KillPlayerActorMessageHandler([NotNull] IEntityGuidMappable<IActorRef> actorReferenceMappable)
+		{
+			ActorReferenceMappable = actorReferenceMappable ?? throw new ArgumentNullException(nameof(actorReferenceMappable));
+		}
+
+		protected override void HandleMessage(EntityActorMessageContext messageContext, WorldActorState state, KillPlayerActorMessage message)
+		{
+			//The player's actor has been killed.
+			//Let's count ourselves as nobody now.
+			ActorReferenceMappable.ReplaceObject(message.EntityGuid, Nobody.Instance);
+		}
+	}
+}
