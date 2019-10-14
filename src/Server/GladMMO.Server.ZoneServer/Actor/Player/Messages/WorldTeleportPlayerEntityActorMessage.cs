45c6ae56c3a088b059dbda4b3b375266997f4e4b
HelloKitty <andrew.blakely@ymail.com>
2019-10-14 00:57:17 -0500

    Added WorldTeleportPlayerEntityActorMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Player/Messages/WorldTeleportPlayerEntityActorMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class WorldTeleportPlayerEntityActorMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The spawnpoint of the ID the player should end up at after teleporting.
+		/// </summary>
+		public int WorldSpawnPointId { get; }
+
+		public WorldTeleportPlayerEntityActorMessage(int worldSpawnPointId)
+		{
+			if (worldSpawnPointId <= 0) throw new ArgumentOutOfRangeException(nameof(worldSpawnPointId));
+
+			WorldSpawnPointId = worldSpawnPointId;
+		}
+	}
+}
