b9d3d3dccc5ffc81a58972e96b6e7117d1f9c998
HelloKitty <andrew.blakely@ymail.com>
2019-10-14 01:23:00 -0500

    Added some logging to disconnect message handling

--- src/Server/GladMMO.Server.ZoneServer/Actor/Player/Handlers/DisconnectNetworkPlayerEntityActorMessageHandler.cs
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Player/Handlers/DisconnectNetworkPlayerEntityActorMessageHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Akka.Actor;
+using Common.Logging;
 using GladNet;
 
 namespace GladMMO
@@ -12,13 +13,20 @@ namespace GladMMO
 	{
 		private IReadonlyEntityGuidMappable<IConnectionService> ConnectionServiceMappable { get; }
 
-		public DisconnectNetworkPlayerEntityActorMessageHandler([NotNull] IReadonlyEntityGuidMappable<IConnectionService> connectionServiceMappable)
+		private ILog Logger { get; }
+
+		public DisconnectNetworkPlayerEntityActorMessageHandler([NotNull] IReadonlyEntityGuidMappable<IConnectionService> connectionServiceMappable,
+			[NotNull] ILog logger)
 		{
 			ConnectionServiceMappable = connectionServiceMappable ?? throw new ArgumentNullException(nameof(connectionServiceMappable));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
 		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, DisconnectNetworkPlayerEntityActorMessage message)
 		{
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Entity: {state.EntityGuid} actor handling disconnection message.");
+
 			//TODO: This can throw, but the entity will die. We should make it so that entity data always exists if the actor is alive.
 			ConnectionServiceMappable.RetrieveEntity(state.EntityGuid).Disconnect();
 		}
