280fbafc7a0cf04d9918dee7c703eaf0381bd60d
HelloKitty <andrew.blakely@ymail.com>
2019-10-14 04:37:56 -0500

    Added KillPlayerActorMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/World/Messages/KillPlayerActorMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// This has nothing to do with health/combat.
+	/// Don't use this externally.
+	/// </summary>
+	public sealed class KillPlayerActorMessage : EntityActorMessage
+	{
+		public NetworkEntityGuid EntityGuid { get; }
+
+		public KillPlayerActorMessage([NotNull] NetworkEntityGuid entityGuid)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+		}
+	}
+}
