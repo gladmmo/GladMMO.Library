74c629bf6bdda1e18613ddaefba20e6f8d601736
HelloKitty <andrew.blakely@ymail.com>
2019-10-15 01:31:41 -0500

    Added CreatureSetPathMovementMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Creature/Messages/CreatureSetPathMovementMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class CreatureSetPathMovementMessage : EntityActorMessage
+	{
+		public Vector3[] PathPoints { get; }
+
+		public CreatureSetPathMovementMessage([NotNull] Vector3[] pathPoints)
+		{
+			PathPoints = pathPoints ?? throw new ArgumentNullException(nameof(pathPoints));
+		}
+	}
+}
