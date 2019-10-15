a2d3236ec10ab1a65ed3b562c7571b82d2524897
HelloKitty <andrew.blakely@ymail.com>
2019-10-15 15:04:10 -0500

    Added SetEntityActorTargetMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/SetEntityActorTargetMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class SetEntityActorTargetMessage : EntityActorMessage
+	{
+		public NetworkEntityGuid TargetGuid { get; }
+
+		public SetEntityActorTargetMessage([NotNull] NetworkEntityGuid targetGuid)
+		{
+			TargetGuid = targetGuid ?? throw new ArgumentNullException(nameof(targetGuid));
+		}
+	}
+}
