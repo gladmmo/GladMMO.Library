8dc9f7d44a373e481734cc6595708cefb8be99a2
HelloKitty <andrew.blakely@ymail.com>
2019-10-15 15:10:10 -0500

    Added EntityActorSetTargetMessageMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/EntityActorSetTargetMessageMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultCreatureEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	public sealed class EntityActorSetTargetMessageMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, SetEntityActorTargetMessage>
+	{
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, SetEntityActorTargetMessage message)
+		{
+			//Simple, just set our target.
+			state.EntityData.SetFieldValue(EntityObjectField.UNIT_FIELD_TARGET, message.TargetGuid);
+		}
+	}
+}
