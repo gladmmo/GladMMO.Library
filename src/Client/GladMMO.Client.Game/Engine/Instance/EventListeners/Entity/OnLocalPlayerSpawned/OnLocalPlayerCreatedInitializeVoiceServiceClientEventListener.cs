67687ce6b506bd856471f70297a244bfb6dc5065
HelloKitty <andrew.blakely@ymail.com>
2019-10-15 16:42:53 -0500

    Added gracefully handleing of voice connection failure

--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnLocalPlayerSpawned/OnLocalPlayerCreatedInitializeVoiceServiceClientEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnLocalPlayerSpawned/OnLocalPlayerCreatedInitializeVoiceServiceClientEventListener.cs
@@ -19,20 +19,34 @@ namespace GladMMO
 
 		public event EventHandler OnVoiceNetworkInitialized;
 
+		private IChatTextMessageRecievedEventPublisher TextChatPublisher { get; }
+
 		public OnLocalPlayerCreatedInitializeVoiceServiceClientEventListener(ILocalPlayerSpawnedEventSubscribable subscriptionService,
 			[NotNull] VivoxUnity.Client voiceClient,
-			[NotNull] ILog logger) 
+			[NotNull] ILog logger,
+			[NotNull] IChatTextMessageRecievedEventPublisher textChatPublisher) 
 			: base(subscriptionService)
 		{
 			VoiceClient = voiceClient ?? throw new ArgumentNullException(nameof(voiceClient));
 			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			TextChatPublisher = textChatPublisher ?? throw new ArgumentNullException(nameof(textChatPublisher));
 		}
 
 		protected override void OnEventFired(object source, LocalPlayerSpawnedEventArgs args)
 		{
-			//It may seem small and simple, or dumb to seperate all this stuff, but a lot of stuff is going to happen with voice related events.
-			VoiceClient.Initialize();
-			OnVoiceNetworkInitialized?.Invoke(this, EventArgs.Empty);
+			try
+			{
+				//It may seem small and simple, or dumb to seperate all this stuff, but a lot of stuff is going to happen with voice related events.
+				VoiceClient.Initialize();
+				OnVoiceNetworkInitialized?.Invoke(this, EventArgs.Empty);
+			}
+			catch (Exception e)
+			{
+				if(Logger.IsErrorEnabled)
+					Logger.Error($"Vivox initialization failed. Reason: {e.Message}.\n\nStack: {e.StackTrace}");
+
+				TextChatPublisher.PublishEvent(this, new TextChatEventArgs("Failed to connect to voice service.", ChatChannelType.System));
+			}
 		}
 	}
 }
