b92d2e27e1abf80e20f324049adcf2919952a153
HelloKitty <andrew.blakely@ymail.com>
2019-10-16 03:01:30 -0500

    Added ILocalPlayerTargetChangedEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/ILocalPlayerTargetChangedEventListener.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ILocalPlayerTargetChangedEventListener
+	{
+		event EventHandler<LocalPlayerTargetChangedEventArgs> OnPlayerTargetChanged;
+	}
+
+	public class LocalPlayerTargetChangedEventArgs : EventArgs
+	{
+		public NetworkEntityGuid TargetedEntity { get; }
+
+		public LocalPlayerTargetChangedEventArgs([NotNull] NetworkEntityGuid targetedEntity)
+		{
+			TargetedEntity = targetedEntity ?? throw new ArgumentNullException(nameof(targetedEntity));
+		}
+	}
+}
