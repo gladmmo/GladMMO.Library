e6c2bc3750c5ccbcf23b47c5d28f23eaf981aa32
HelloKitty <andrew.blakely@ymail.com>
2019-10-16 03:29:50 -0500

    Added QueryEntityNameOnDiscoverEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnNetworkEntityVisible/QueryEntityNameOnDiscoverEventListener.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class QueryEntityNameOnDiscoverEventListener : BaseSingleEventListenerInitializable<INetworkEntityVisibleEventSubscribable, NetworkEntityNowVisibleEventArgs>
+	{
+		private IEntityNameQueryable NameQueryService { get; }
+
+		public QueryEntityNameOnDiscoverEventListener(INetworkEntityVisibleEventSubscribable subscriptionService,
+			[NotNull] IEntityNameQueryable nameQueryService) 
+			: base(subscriptionService)
+		{
+			NameQueryService = nameQueryService ?? throw new ArgumentNullException(nameof(nameQueryService));
+		}
+
+		protected override void OnEventFired(object source, NetworkEntityNowVisibleEventArgs args)
+		{
+			//Just fire and forget a retrieve so that hopefully when the player eventually interacts with the entity
+			//the name will be available already.
+			NameQueryService.RetrieveAsync(args.EntityGuid);
+		}
+	}
+}
