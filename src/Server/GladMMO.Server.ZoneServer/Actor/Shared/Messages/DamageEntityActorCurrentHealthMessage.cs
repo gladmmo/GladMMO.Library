5d703bd63a3dd28de9c09c8620727f0138db0398
HelloKitty <andrew.blakely@ymail.com>
2019-10-16 06:52:07 -0500

    Added DamageEntityActorCurrentHealthMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/DamageEntityActorCurrentHealthMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DamageEntityActorCurrentHealthMessage : EntityActorMessage
+	{
+		public int Damage { get; }
+
+		public DamageEntityActorCurrentHealthMessage(int damage)
+		{
+			if (damage <= 0) throw new ArgumentOutOfRangeException(nameof(damage));
+
+			Damage = damage;
+		}
+	}
+}
