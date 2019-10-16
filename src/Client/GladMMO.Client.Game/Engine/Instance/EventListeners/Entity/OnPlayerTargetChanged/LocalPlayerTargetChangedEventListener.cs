1e79c7d6f474361d7c905b2f328e47f9aaa102de
HelloKitty <andrew.blakely@ymail.com>
2019-10-16 03:15:48 -0500

    Added LocalPlayerTargetChangedEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnPlayerTargetChanged/LocalPlayerTargetChangedEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class LocalPlayerTargetChangedEventListener : BaseSingleEventListenerInitializable<ILocalPlayerTargetChangedEventListener, LocalPlayerTargetChangedEventArgs>
+	{
+		protected IUIUnitFrame TargetUnitFrame { get; }
+
+		protected LocalPlayerTargetChangedEventListener(ILocalPlayerTargetChangedEventListener subscriptionService,
+			[NotNull] [KeyFilter(UnityUIRegisterationKey.TargetUnitFrame)] IUIUnitFrame targetUnitFrame) 
+			: base(subscriptionService)
+		{
+			TargetUnitFrame = targetUnitFrame ?? throw new ArgumentNullException(nameof(targetUnitFrame));
+		}
+
+		protected override void OnEventFired(object source, LocalPlayerTargetChangedEventArgs args)
+		{
+			//Just forward so consumers AKA 3rd party users can have a cleaner API to implement.
+			OnLocalPlayerTargetChanged(args);
+		}
+
+		protected abstract void OnLocalPlayerTargetChanged(LocalPlayerTargetChangedEventArgs args);
+	}
+}
