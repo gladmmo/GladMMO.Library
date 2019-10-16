e7739f683396d02a8233d5687340b952c1dc0e26
HelloKitty <andrew.blakely@ymail.com>
2019-10-16 06:58:10 -0500

    Added EntityActorDamageMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/EntityActorDamageMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultCreatureEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	public sealed class EntityActorDamageMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, DamageEntityActorCurrentHealthMessage>
+	{
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, DamageEntityActorCurrentHealthMessage message)
+		{
+			//Simple, if the entity has health we just try to subtract from it.
+			if (state.EntityData.DataSetIndicationArray.Get((int) EntityObjectField.UNIT_FIELD_HEALTH))
+				state.EntityData.SetFieldValue(EntityObjectField.UNIT_FIELD_HEALTH, Math.Max(0, state.EntityData.GetFieldValue<int>(EntityObjectField.UNIT_FIELD_HEALTH) - message.Damage));
+		}
+	}
+}
