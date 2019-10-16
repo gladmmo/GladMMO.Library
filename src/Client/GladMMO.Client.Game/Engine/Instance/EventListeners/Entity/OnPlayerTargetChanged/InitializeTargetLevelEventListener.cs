ab5ee99a9649fd96d0ec96db8a748d3aa54b3b3c
HelloKitty <andrew.blakely@ymail.com>
2019-10-16 06:46:20 -0500

    Added InitializeTargetLevelEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnPlayerTargetChanged/InitializeTargetLevelEventListener.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Common.Logging;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class InitializeTargetLevelEventListener : LocalPlayerTargetChangedEventListener
+	{
+		private IReadonlyEntityGuidMappable<IEntityDataFieldContainer> EntityDataMappable { get; }
+
+		private IEntityDataChangeCallbackRegisterable EntityDataChangeCallbackService { get; }
+
+		public InitializeTargetLevelEventListener(ILocalPlayerTargetChangedEventListener subscriptionService,
+			ILog logger,
+			[KeyFilter(UnityUIRegisterationKey.TargetUnitFrame)] IUIUnitFrame targetUnitFrame,
+			[NotNull] IReadonlyEntityGuidMappable<IEntityDataFieldContainer> entityDataMappable,
+			[NotNull] IEntityDataChangeCallbackRegisterable entityDataChangeCallbackService) 
+			: base(subscriptionService, logger, targetUnitFrame)
+		{
+			EntityDataMappable = entityDataMappable ?? throw new ArgumentNullException(nameof(entityDataMappable));
+			EntityDataChangeCallbackService = entityDataChangeCallbackService ?? throw new ArgumentNullException(nameof(entityDataChangeCallbackService));
+		}
+
+		protected override void OnLocalPlayerTargetChanged(LocalPlayerTargetChangedEventArgs args)
+		{
+			IEntityDataFieldContainer entityData = EntityDataMappable.RetrieveEntity(args.TargetedEntity);
+
+			EntityDataChangeCallbackService.RegisterCallback<int>(args.TargetedEntity, (int)BaseObjectField.UNIT_FIELD_LEVEL, OnTargetEntityLevelChanged);
+
+			//Only initialize if we have their values
+			if (entityData.DataSetIndicationArray.Get((int)BaseObjectField.UNIT_FIELD_LEVEL))
+				OnTargetEntityLevelChanged(args.TargetedEntity, new EntityDataChangedArgs<int>(0, entityData.GetFieldValue<int>(BaseObjectField.UNIT_FIELD_LEVEL)));
+		}
+
+		private void OnTargetEntityLevelChanged(NetworkEntityGuid entity, EntityDataChangedArgs<int> args)
+		{
+			TargetUnitFrame.UnitLevel.Text = args.NewValue.ToString();
+		}
+	}
+}
