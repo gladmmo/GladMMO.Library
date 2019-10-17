f1987f2e65bf3bfcec3ce308332b189abe2beaed
HelloKitty <andrew.blakely@ymail.com>
2019-10-17 07:15:52 -0500

    Added CharacterDataRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Character/DatabaseBackedCharacterDataRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DatabaseBackedCharacterDataRepository : BaseGenericBackedDatabaseRepository<CharacterDatabaseContext, int, CharacterDataModel>, ICharacterDataRepository
+	{
+		public DatabaseBackedCharacterDataRepository([JetBrains.Annotations.NotNull] CharacterDatabaseContext context) 
+			: base(context)
+		{
+
+		}
+	}
+}
---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Character/ICharacterDataRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ICharacterDataRepository : IGenericRepositoryCrudable<int, CharacterDataModel>
+	{
+	}
+}
