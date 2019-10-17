4d4b8cb6c3f9d2dfff75fa2dfdfc398356014250
HelloKitty <andrew.blakely@ymail.com>
2019-10-17 10:14:05 -0500

    Added AddPlayerExperienceActorMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Player/Messages/AddPlayerExperienceActorMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class AddPlayerExperienceActorMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The amount of experience to add to the player.
+		/// </summary>
+		public int ExperienceAmount { get; }
+
+		public AddPlayerExperienceActorMessage(int experienceAmount)
+		{
+			if (experienceAmount < 0) throw new ArgumentOutOfRangeException(nameof(experienceAmount));
+
+			ExperienceAmount = experienceAmount;
+		}
+	}
+}
