f5c5133bba77c4db57d1ba253bc654a881ebc43d
HelloKitty <andrew.blakely@ymail.com>
2019-10-17 07:49:45 -0500

    Added CharacterDataResponse

---
+++ src/GladMMO.Models.GameServer/Models/CharacterDataResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class CharacterDataResponse
+	{
+		//We may have more stuff eventually, right now we only have the model id.
+		[JsonProperty]
+		public int Experience { get; private set; }
+
+		public CharacterDataResponse(int experience)
+		{
+			if (experience < 0) throw new ArgumentOutOfRangeException(nameof(experience));
+
+			Experience = experience;
+		}
+
+		//Serializer ctor
+		private CharacterDataResponse()
+		{
+
+		}
+	}
+}
