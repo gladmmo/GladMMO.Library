2253f908c2701aee77960df50f264f147ba94d39
HelloKitty <andrew.blakely@ymail.com>
2019-10-17 14:15:45 -0500

    Added ReinitializeEntityActorStatsMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/ReinitializeEntityActorStatsMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Message that tells an entity to reset or reinitialize the stats.
+	/// Ex: Health, mana and etc.
+	/// </summary>
+	public sealed class ReinitializeEntityActorStatsMessage : EntityActorMessage
+	{
+		public ReinitializeEntityActorStatsMessage()
+		{
+			
+		}
+	}
+}
