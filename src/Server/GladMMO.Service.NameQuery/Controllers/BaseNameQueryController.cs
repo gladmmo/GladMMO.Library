448adbeefb2efc6ba0cbbdde863f06359802c132
HelloKitty <andrew.blakely@ymail.com>
2019-10-17 01:57:31 -0500

    Added BaseNameQueryController

---
+++ src/Server/GladMMO.Service.NameQuery/Controllers/BaseNameQueryController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	public abstract class BaseNameQueryController : AuthorizationReadyController
+	{
+		/// <inheritdoc />
+		protected BaseNameQueryController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger)
+			: base(claimsReader, logger)
+		{
+
+		}
+
+		[AllowAnonymous]
+		[ProducesJson]
+		[ResponseCache(Duration = 360)] //We want to cache this for a long time. But it's possible with name changes that we want to not cache forever
+		[HttpGet("{id}/name")]
+		public async Task<IActionResult> EntityNameQuery([FromRoute(Name = "id")] ulong id)
+		{
+			//Since this is a GET we can't send a JSON model. We have to use this process instead, sending the raw guid value.
+			NetworkEntityGuid entityGuid = new NetworkEntityGuid(id);
+			return await EntityNameQuery(entityGuid);
+		}
+
+		protected abstract Task<JsonResult> EntityNameQuery(NetworkEntityGuid entityGuid);
+	}
+}
