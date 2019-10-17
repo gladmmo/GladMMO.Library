87704f9ef315796000687e28400d3e42f1e77342
HelloKitty <andrew.blakely@ymail.com>
2019-10-17 09:11:04 -0500

    Added IEntityExperienceLevelStrategy

---
+++ src/GladMMO.Common/EntityStats/DefaultEntityExperienceLevelStrategy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DefaultEntityExperienceLevelStrategy : IEntityExperienceLevelStrategy
+	{
+		public int ComputeLevelFromExperience(int experience)
+		{
+			if (experience < 0) throw new ArgumentOutOfRangeException(nameof(experience));
+
+			if (experience < 400)
+				return 1;
+			if (experience >= 400)
+				return 2;
+			else if (experience >= 1300)
+				return 3;
+
+			throw new NotImplementedException($"TODO: Haven't implemented full level/experience system.");
+		}
+
+		public int TotalExperienceRequiredForLevel(int level)
+		{
+			if (level < 0) throw new ArgumentOutOfRangeException(nameof(level));
+
+			switch (level)
+			{
+				case 1:
+					return 0;
+				case 2:
+					return 400;
+				case 3:
+					return 1300;
+			}
+
+			throw new NotImplementedException($"TODO: Haven't implemented full level/experience system.");
+		}
+	}
+}
---
+++ src/GladMMO.Common/EntityStats/IEntityExperienceLevelStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IEntityExperienceLevelStrategy
+	{
+		/// <summary>
+		/// Determines the entity level from the provided <see cref="experience"/> value.
+		/// </summary>
+		/// <param name="experience">The experience.</param>
+		/// <returns>The level from the experience.</returns>
+		int ComputeLevelFromExperience(int experience);
+
+		/// <summary>
+		/// Determines the total experience that is required for a given level.
+		/// </summary>
+		/// <param name="level">The level.</param>
+		/// <returns>Total experience required to reach a specific level.</returns>
+		int TotalExperienceRequiredForLevel(int level);
+	}
+}
