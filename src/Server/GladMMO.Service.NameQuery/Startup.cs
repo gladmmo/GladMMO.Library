aefe5b07c1fa72b8878d0c4b4f15164cb554ed41
HelloKitty <andrew.blakely@ymail.com>
2019-10-17 02:00:40 -0500

    Added default startup for Service.NameQuery

--- src/Server/GladMMO.Service.NameQuery/Startup.cs
+++ src/Server/GladMMO.Service.NameQuery/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -25,23 +27,55 @@ namespace GladMMO
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+				.RegisterHealthCheckController();
+
+			X509Certificate2 cert = null;
+			string certPath = "Certs/TestCert.pfx";
+
+			try
+			{
+				cert = X509Certificate2Loader.Create(certPath).Load();
+			}
+			catch(Exception e)
+			{
+				throw new System.InvalidOperationException($"Failed to load {nameof(X509Certificate2)} from Path: {certPath} \n\n StackTrace: {e.StackTrace}", e);
+			}
+
+			services.AddResponseCaching();
+			services.AddLogging();
+
+			//This provides JwtBearer support for Authorize attribute/header
+			services.AddJwtAuthorization(cert);
+
+			RegisterDatabaseServices(services);
+		}
+
+		private void RegisterDatabaseServices([JetBrains.Annotations.NotNull] IServiceCollection services)
+		{
+			if (services == null) throw new ArgumentNullException(nameof(services));
+
+			services.AddDbContext<CharacterDatabaseContext>(o =>
+			{
+				o.UseMySql("Server=127.0.0.1;Database=guardians.gameserver;Uid=root;Pwd=test;");
+			});
+
+			services.AddTransient<ICharacterRepository, DatabaseBackedCharacterRepository>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
 		{
-			if(env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseHsts();
-			}
+#warning Do not deploy exceptions page into production
+			app.UseDeveloperExceptionPage();
 
-			app.UseHttpsRedirection();
-			app.UseMvc();
+			app.UseResponseCaching();
+			app.UseAuthentication();
+
+			loggerFactory.RegisterGuardiansLogging(Configuration);
+			loggerFactory.AddDebug();
+
+			app.UseMvcWithDefaultRoute();
 		}
 	}
 }
