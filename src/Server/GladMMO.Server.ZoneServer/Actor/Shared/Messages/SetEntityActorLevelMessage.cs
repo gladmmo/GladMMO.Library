0439596a646d5d64311c11bb8241eff25d32476d
HelloKitty <andrew.blakely@ymail.com>
2019-10-17 10:35:18 -0500

    Added SetEntityActorLevelMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/SetEntityActorLevelMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class SetEntityActorLevelMessage : EntityActorMessage
+	{
+		public int NewLevel { get; }
+
+		public SetEntityActorLevelMessage(int newLevel)
+		{
+			if (newLevel <= 0) throw new ArgumentOutOfRangeException(nameof(newLevel));
+
+			NewLevel = newLevel;
+		}
+	}
+}
