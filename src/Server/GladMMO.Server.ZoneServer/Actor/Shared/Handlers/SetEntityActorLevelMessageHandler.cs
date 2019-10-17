f07e30452ee9df1f43dec19f21086f0892f9ecdd
HelloKitty <andrew.blakely@ymail.com>
2019-10-17 11:08:41 -0500

    Added SetEntityActorLevelMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/SetEntityActorLevelMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultGameObjectEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultCreatureEntityActor))]
+	public sealed class SetEntityActorLevelMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, SetEntityActorLevelMessage>
+	{
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, SetEntityActorLevelMessage message)
+		{
+			state.EntityData.SetFieldValue(BaseObjectField.UNIT_FIELD_LEVEL, message.NewLevel);
+		}
+	}
+}
