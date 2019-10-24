8c4e3139c151d2a5ec9764797ebc52156edfc1e6
HelloKitty <andrew.blakely@ymail.com>
2019-10-24 18:08:40 -0500

    Added RequestFriendsListEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnLocalPlayerSpawned/RequestFriendsListEventListener.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+using Nito.AsyncEx;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(ICharacterFriendAddedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class RequestFriendsListEventListener : OnLocalPlayerSpawnedEventListener, ICharacterFriendAddedEventSubscribable
+	{
+		private ISocialService SocialService { get; }
+
+		public event EventHandler<CharacterFriendAddedEventArgs> OnCharacterFriendAdded;
+
+		public RequestFriendsListEventListener(ILocalPlayerSpawnedEventSubscribable subscriptionService,
+			[NotNull] ISocialService socialService) 
+			: base(subscriptionService)
+		{
+			SocialService = socialService ?? throw new ArgumentNullException(nameof(socialService));
+		}
+
+		protected override void OnLocalPlayerSpawned(LocalPlayerSpawnedEventArgs args)
+		{
+			UnityAsyncHelper.UnityMainThreadContext.PostAsync(async () =>
+			{
+				CharacterFriendListResponseModel friendsResponse = await SocialService.GetCharacterListAsync();
+
+				foreach (int characterId in friendsResponse.CharacterFriendsId)
+				{
+					NetworkEntityGuid entityGuid = NetworkEntityGuidBuilder.New()
+						.WithType(EntityType.Player)
+						.WithId(characterId)
+						.Build();
+
+					OnCharacterFriendAdded?.Invoke(this, new CharacterFriendAddedEventArgs(entityGuid));
+				}
+			});
+		}
+	}
+}
