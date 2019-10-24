630782806760ae9ba4f0c88361c488f0fbd542b6
HelloKitty <andrew.blakely@ymail.com>
2019-10-24 16:35:14 -0500

    Added CharacterFriendListResponseModel

---
+++ src/GladMMO.Models.Social/Models/Server/CharacterFriendListResponseModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class CharacterFriendListResponseModel : BaseSocialModel
+	{
+		[JsonProperty]
+		private readonly int[] _CharacterFriendsId;
+
+		[JsonIgnore]
+		public IReadOnlyCollection<int> CharacterFriendsId => _CharacterFriendsId;
+
+		public CharacterFriendListResponseModel([JetBrains.Annotations.NotNull] int[] characterFriendsId)
+		{
+			_CharacterFriendsId = characterFriendsId ?? throw new ArgumentNullException(nameof(characterFriendsId));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		private CharacterFriendListResponseModel()
+		{
+			
+		}
+	}
+}
