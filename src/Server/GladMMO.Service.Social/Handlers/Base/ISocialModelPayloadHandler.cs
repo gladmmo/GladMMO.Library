78f944463b33f188a72293e5d6821c86c4f4af75
HelloKitty <andrew.blakely@ymail.com>
2019-10-23 22:23:37 -0500

    Added ISocialModelPayloadHandler

---
+++ src/Server/GladMMO.Service.Social/Handlers/Base/ISocialModelPayloadHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GladNet;
+
+namespace GladMMO
+{
+	//IPeerPayloadSpecificMessageHandler<TMessageType, object, HubConnectionMessageContext<TRemoteClientHubInterfaceType>>
+	public interface ISocialModelPayloadHandler<TRemoteClientHubInterfaceType> : IPeerMessageHandler<BaseSocialModel, BaseSocialModel, HubConnectionMessageContext<TRemoteClientHubInterfaceType>>
+	{
+		Type PayloadType { get; }
+	}
+}
