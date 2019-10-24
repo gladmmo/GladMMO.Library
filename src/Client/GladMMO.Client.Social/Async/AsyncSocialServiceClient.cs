4d29fd02c2429aa3ff414c95ed693052f3c5e06a
HelloKitty <andrew.blakely@ymail.com>
2019-10-24 17:53:06 -0500

    Added AsyncSocialServiceClient

---
+++ src/Client/GladMMO.Client.Social/Async/AsyncSocialServiceClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncSocialServiceClient : BaseAsyncEndpointService<ISocialService>, ISocialService
+	{
+		/// <inheritdoc />
+		public AsyncSocialServiceClient(Task<string> futureEndpoint)
+			: base(futureEndpoint)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public AsyncSocialServiceClient(Task<string> futureEndpoint, RefitSettings settings)
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		public async Task<CharacterFriendListResponseModel> GetCharacterListAsync()
+		{
+			return await (await GetService().ConfigureAwait(false)).GetCharacterListAsync().ConfigureAwait(false);
+		}
+	}
+}
