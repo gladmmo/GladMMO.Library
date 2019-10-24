f65d1d74eaf8623343b151f96076ea409d4bdb5d
HelloKitty <andrew.blakely@ymail.com>
2019-10-23 19:11:44 -0500

    Added simple character table migration

--- src/Server/GladMMO.Database.GameServer/Migrations/20191024000529_AddedSimpleFriendshipModel.Designer.cs
+++ src/Server/GladMMO.Database.GameServer/Migrations/20191024000529_AddedSimpleFriendshipModel.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using GladMMO;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GladMMO.Database.GameServer.Migrations
 {
     [DbContext(typeof(CharacterDatabaseContext))]
-    partial class CharacterDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20191024000529_AddedSimpleFriendshipModel")]
+    partial class AddedSimpleFriendshipModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Server/GladMMO.Database.GameServer/Migrations/20191024000529_AddedSimpleFriendshipModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GladMMO.Database.GameServer.Migrations
+{
+    public partial class AddedSimpleFriendshipModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "character_friends",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(nullable: false),
+                    FriendCharacterId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_character_friends", x => new { x.CharacterId, x.FriendCharacterId });
+                    table.ForeignKey(
+                        name: "FK_character_friends_characters_CharacterId",
+                        column: x => x.CharacterId,
+                        principalTable: "characters",
+                        principalColumn: "CharacterId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_character_friends_characters_FriendCharacterId",
+                        column: x => x.FriendCharacterId,
+                        principalTable: "characters",
+                        principalColumn: "CharacterId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_character_friends_CharacterId",
+                table: "character_friends",
+                column: "CharacterId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_character_friends_FriendCharacterId",
+                table: "character_friends",
+                column: "FriendCharacterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "character_friends");
+        }
+    }
+}
