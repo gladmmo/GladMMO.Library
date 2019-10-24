ff6583de44c71d1f610101c2827d0302de3c3014
HelloKitty <andrew.blakely@ymail.com>
2019-10-23 19:32:52 -0500

    Added ICharacterFriendRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Social/DatabaseBackedCharacterFriendRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	public sealed class DatabaseBackedCharacterFriendRepository : BaseGenericBackedDatabaseRepository<CharacterDatabaseContext, int, CharacterFriendModel>, ICharacterFriendRepository
+	{
+		public DatabaseBackedCharacterFriendRepository([JetBrains.Annotations.NotNull] CharacterDatabaseContext context)
+			: base(context)
+		{
+
+		}
+
+		public async Task<int[]> GetCharactersFriendsList(int characterId)
+		{
+			return await Context
+				.CharacterFriends
+				.Where(f => f.CharacterId == characterId)
+				.Select(f => f.FriendCharacterId)
+				.ToArrayAsync();
+		}
+	}
+}
---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Social/ICharacterFriendRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public interface ICharacterFriendRepository : IGenericRepositoryCrudable<int, CharacterFriendModel>
+	{
+		/// <summary>
+		/// Gets an array of characterIds that represent
+		/// the friend's list of the provided character <see cref="characterId"/>.
+		/// </summary>
+		/// <param name="characterId">The character to query friends list for.</param>
+		/// <returns>Array of friend characterIds</returns>
+		Task<int[]> GetCharactersFriendsList(int characterId);
+	}
+}
