413bc6186f298d2ccadbc42620d307d2121d4b0c
HelloKitty <andrew.blakely@ymail.com>
2019-10-24 18:07:10 -0500

    Added ICharacterFriendAddedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/ICharacterFriendAddedEventSubscribable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ICharacterFriendAddedEventSubscribable
+	{
+		event EventHandler<CharacterFriendAddedEventArgs> OnCharacterFriendAdded;
+	}
+
+	public class CharacterFriendAddedEventArgs : EventArgs
+	{
+		public NetworkEntityGuid FriendGuid { get; }
+
+		public CharacterFriendAddedEventArgs([NotNull] NetworkEntityGuid friendGuid)
+		{
+			FriendGuid = friendGuid ?? throw new ArgumentNullException(nameof(friendGuid));
+		}
+	}
+}
