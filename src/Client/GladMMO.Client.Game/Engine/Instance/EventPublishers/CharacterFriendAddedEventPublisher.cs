fc0faa24c19f35868d3bab7ac6a1bbfa56e00c61
HelloKitty <andrew.blakely@ymail.com>
2019-10-25 17:34:01 -0500

    Added CharacterFriendAddedEventPublisher

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventPublishers/CharacterFriendAddedEventPublisher.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface ICharacterFriendAddedEventPublisher : IEventPublisher<ICharacterFriendAddedEventSubscribable, CharacterFriendAddedEventArgs>
+	{
+
+	}
+
+	[AdditionalRegisterationAs(typeof(ICharacterFriendAddedEventSubscribable))]
+	[AdditionalRegisterationAs(typeof(ICharacterFriendAddedEventPublisher))]
+	[AdditionalRegisterationAs(typeof(IEventPublisher<ICharacterFriendAddedEventSubscribable, CharacterFriendAddedEventArgs>))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class CharacterFriendAddedEventPublisher : ICharacterFriendAddedEventSubscribable, IEventPublisher<ICharacterFriendAddedEventSubscribable, CharacterFriendAddedEventArgs>, IGameInitializable
+	{
+		public event EventHandler<CharacterFriendAddedEventArgs> OnCharacterFriendAdded;
+
+		public void PublishEvent(object sender, CharacterFriendAddedEventArgs eventArgs)
+		{
+			OnCharacterFriendAdded?.Invoke(sender, eventArgs);
+		}
+
+		//Hack
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
