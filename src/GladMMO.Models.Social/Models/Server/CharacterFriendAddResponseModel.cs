437d9d7dd517777c02ec433bf357b11c6f70b33d
HelloKitty <andrew.blakely@ymail.com>
2019-10-25 16:08:12 -0500

    Added CharacterFriendAddResponseModel

---
+++ src/GladMMO.Models.Social/Models/Server/CharacterFriendAddResponseModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class CharacterFriendAddResponseModel
+	{
+		/// <summary>
+		/// The GUID for the newly added player.
+		/// </summary>
+		public NetworkEntityGuid NewFriendEntityGuid { get; }
+
+		public CharacterFriendAddResponseModel([JetBrains.Annotations.NotNull] NetworkEntityGuid newFriendEntityGuid)
+		{
+			NewFriendEntityGuid = newFriendEntityGuid ?? throw new ArgumentNullException(nameof(newFriendEntityGuid));
+		}
+
+		[JsonConstructor]
+		private CharacterFriendAddResponseModel()
+		{
+			
+		}
+	}
+}
---
+++ src/GladMMO.Models.Social/ResultCodes/CharacterFriendAddResponseCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public enum CharacterFriendAddResponseCode
+	{
+		/// <summary>
+		/// Indicates the request was successful.
+		/// </summary>
+		Success = 1,
+
+		/// <summary>
+		/// Unknown serverside error occurred.
+		/// </summary>
+		GeneralServerError = 2,
+
+		/// <summary>
+		/// Indicates that the player is already friends with that player.
+		/// </summary>
+		AlreadyFriends = 3,
+
+		/// <summary>
+		/// The character is not found.
+		/// </summary>
+		CharacterNotFound = 4,
+	}
+}
