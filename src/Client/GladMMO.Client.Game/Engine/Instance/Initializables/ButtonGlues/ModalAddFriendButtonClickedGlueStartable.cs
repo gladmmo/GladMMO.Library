80aaeda1d897b9171a59a73e0af0fa7b78fef9fa
HelloKitty <andrew.blakely@ymail.com>
2019-10-25 12:57:05 -0500

    Added IAddFriendModalClickedEventSubscribable and Glue

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Initializables/ButtonGlues/ModalAddFriendButtonClickedGlueStartable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	//This is the button event for when they hit the "Add" button on the Add Friend popup modal.
+	[AdditionalRegisterationAs(typeof(IAddFriendModalClickedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ModalAddFriendButtonClickedGlueStartable : BaseButtonClickGlueStartable, IAddFriendModalClickedEventSubscribable
+	{
+		public const UnityUIRegisterationKey ButtonKey = UnityUIRegisterationKey.AddFriendModalWindow;
+
+		public ModalAddFriendButtonClickedGlueStartable([KeyFilter(ButtonKey)] [NotNull] IUIButton createButton)
+			: base(createButton)
+		{
+
+		}
+	}
+}
