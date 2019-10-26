264c54384334c27fb84707e1889c2bbb7603d858
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 02:30:17 -0500

    Refactored OnLocalPlayerGuildStatusChangedEventListener

--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Social/OnGuildStatusChanged/OnLocalPlayerGuildStatusChangedEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Social/OnGuildStatusChanged/OnLocalPlayerGuildStatusChangedEventListener.cs
@@ -7,10 +7,22 @@ namespace GladMMO
 {
 	public abstract class OnLocalPlayerGuildStatusChangedEventListener : BaseSingleEventListenerInitializable<IGuildStatusChangedEventSubscribable, GuildStatusChangedEventArgs>
 	{
-		protected OnLocalPlayerGuildStatusChangedEventListener(IGuildStatusChangedEventSubscribable subscriptionService) 
+		protected IReadonlyLocalPlayerDetails LocalPlayerDetails { get; }
+
+		protected OnLocalPlayerGuildStatusChangedEventListener(IGuildStatusChangedEventSubscribable subscriptionService,
+			[NotNull] IReadonlyLocalPlayerDetails localPlayerDetails) 
 			: base(subscriptionService)
 		{
-
+			LocalPlayerDetails = localPlayerDetails ?? throw new ArgumentNullException(nameof(localPlayerDetails));
 		}
+
+		protected sealed override void OnEventFired(object source, GuildStatusChangedEventArgs args)
+		{
+			//Only if it's matching the local player should we dispatch.
+			if(LocalPlayerDetails.LocalPlayerGuid == args.EntityGuid)
+				OnGuildStatusChanged(args);
+		}
+
+		protected abstract void OnGuildStatusChanged(GuildStatusChangedEventArgs changeArgs);
 	}
 }
