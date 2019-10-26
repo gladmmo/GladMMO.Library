8a45334e485ca22b5ed0246bfae0da259b907d75
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 17:05:17 -0500

    Added CharacterGuildListResponseModel

---
+++ src/GladMMO.Models.Social/Models/Server/CharacterGuildListResponseModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class CharacterGuildListResponseModel
+	{
+		[JsonProperty]
+		private int[] _GuildedCharacterIds { get; set; }
+
+		[JsonIgnore]
+		public IReadOnlyCollection<int> GuildedCharacterIds => _GuildedCharacterIds;
+
+		public CharacterGuildListResponseModel([JetBrains.Annotations.NotNull] int[] guildedCharacterIds)
+		{
+			_GuildedCharacterIds = guildedCharacterIds ?? throw new ArgumentNullException(nameof(guildedCharacterIds));
+		}
+
+		[JsonConstructor]
+		private CharacterGuildListResponseModel()
+		{
+			
+		}
+	}
+}
