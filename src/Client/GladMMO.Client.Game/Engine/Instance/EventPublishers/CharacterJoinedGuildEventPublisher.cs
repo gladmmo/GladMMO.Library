46833145394487b2e8144cab9d29bc0a76745841
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 18:01:36 -0500

    Added CharacterJoinedGuildEventPublisher

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventPublishers/CharacterJoinedGuildEventPublisher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface ICharacterJoinedGuildEventPublisher : IEventPublisher<ICharacterJoinedGuildEventSubscribable, CharacterJoinedGuildEventArgs>
+	{
+
+	}
+
+	[AdditionalRegisterationAs(typeof(ICharacterJoinedGuildEventPublisher))]
+	[AdditionalRegisterationAs(typeof(ICharacterJoinedGuildEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class CharacterJoinedGuildEventPublisher : ICharacterJoinedGuildEventSubscribable, ICharacterJoinedGuildEventPublisher, IGameInitializable
+	{
+		public event EventHandler<CharacterJoinedGuildEventArgs> OnCharacterJoinedGuild;
+
+		public void PublishEvent(object sender, CharacterJoinedGuildEventArgs eventArgs)
+		{
+			OnCharacterJoinedGuild?.Invoke(sender, eventArgs);
+		}
+
+		//Hack to get into the scene.
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
