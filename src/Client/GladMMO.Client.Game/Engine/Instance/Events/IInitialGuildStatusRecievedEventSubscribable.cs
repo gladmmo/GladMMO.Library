ac7547f6c7736f0bae92ee5ba9f321f7201ebeb1
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 02:07:22 -0500

    Added IInitialGuildStatusRecievedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/IInitialGuildStatusRecievedEventSubscribable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IInitialGuildStatusRecievedEventSubscribable
+	{
+		event EventHandler<InitialGuildStatusRecievedEventArgs> OnInitialGuildStatusRecieved;
+	}
+
+	public sealed class InitialGuildStatusRecievedEventArgs : EventArgs
+	{
+		public int GuildId { get; }
+
+		public string GuildName { get; }
+
+		public InitialGuildStatusRecievedEventArgs(int guildId, [NotNull] string guildName)
+		{
+			if (guildId <= 0) throw new ArgumentOutOfRangeException(nameof(guildId));
+			if (string.IsNullOrWhiteSpace(guildName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(guildName));
+
+			GuildId = guildId;
+			GuildName = guildName;
+		}
+	}
+}
