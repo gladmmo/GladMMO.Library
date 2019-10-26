82a0a1349a20a374b1db671e55845ed4f1da2c0b
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 02:48:28 -0500

    Added IGuildEntryRepository and DatabaseBackedGuildEntryRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Social/DatabaseBackedGuildEntryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	public sealed class DatabaseBackedGuildEntryRepository : BaseGenericBackedDatabaseRepository<CharacterDatabaseContext, int, GuildEntryModel>, IGuildEntryRepository
+	{
+		public DatabaseBackedGuildEntryRepository([JetBrains.Annotations.NotNull] CharacterDatabaseContext context)
+			: base(context)
+		{
+
+		}
+	}
+}
