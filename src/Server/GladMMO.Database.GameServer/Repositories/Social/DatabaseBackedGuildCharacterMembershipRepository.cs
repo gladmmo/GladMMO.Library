54991b8de72b1f8fececffb5277305276c2cb808
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 17:22:49 -0500

    Refactored DatabaseBackedGuildCharacterMembershipRepository

--- src/Server/GladMMO.Database.GameServer/Repositories/Social/DatabaseBackedGuildCharacterMembershipRepository.cs
+++ src/Server/GladMMO.Database.GameServer/Repositories/Social/DatabaseBackedGuildCharacterMembershipRepository.cs
@@ -5,43 +5,12 @@ using System.Threading.Tasks;
 
 namespace GladMMO
 {
-	public sealed class DatabaseBackedGuildCharacterMembershipRepository : IGuildCharacterMembershipRepository
+	public sealed class DatabaseBackedGuildCharacterMembershipRepository : BaseGenericBackedDatabaseRepository<CharacterDatabaseContext, int, CharacterGuildMemberRelationshipModel>, IGuildCharacterMembershipRepository
 	{
-		private IGenericRepositoryCrudable<int, CharacterGuildMemberRelationshipModel> GenericGuildRelationshipRepository { get; }
-
-		public DatabaseBackedGuildCharacterMembershipRepository(CharacterDatabaseContext context)
+		public DatabaseBackedGuildCharacterMembershipRepository([JetBrains.Annotations.NotNull] CharacterDatabaseContext context) 
+			: base(context)
 		{
-			GenericGuildRelationshipRepository = new GeneralGenericCrudRepositoryProvider<int, CharacterGuildMemberRelationshipModel>(context.GuildMembers, context);
-		}
 
-		/// <inheritdoc />
-		public Task<bool> ContainsAsync(int key)
-		{
-			return GenericGuildRelationshipRepository.ContainsAsync(key);
-		}
-
-		/// <inheritdoc />
-		public Task<bool> TryCreateAsync(CharacterGuildMemberRelationshipModel model)
-		{
-			return GenericGuildRelationshipRepository.TryCreateAsync(model);
-		}
-
-		/// <inheritdoc />
-		public Task<CharacterGuildMemberRelationshipModel> RetrieveAsync(int key, bool includeNavigationProperties = false)
-		{
-			return GenericGuildRelationshipRepository.RetrieveAsync(key, includeNavigationProperties);
-		}
-
-		/// <inheritdoc />
-		public Task<bool> TryDeleteAsync(int key)
-		{
-			return GenericGuildRelationshipRepository.TryDeleteAsync(key);
-		}
-
-		/// <inheritdoc />
-		public Task UpdateAsync(int key, CharacterGuildMemberRelationshipModel model)
-		{
-			return GenericGuildRelationshipRepository.UpdateAsync(key, model);
 		}
 	}
 }
