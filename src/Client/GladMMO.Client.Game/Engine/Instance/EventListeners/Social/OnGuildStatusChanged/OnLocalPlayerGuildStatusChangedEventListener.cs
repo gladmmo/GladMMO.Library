9abb8f24d0c064e5343b647ed8e2a94ebdc0347a
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 02:21:17 -0500

    Added OnLocalPlayerGuildStatusChangedEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Social/OnGuildStatusChanged/OnLocalPlayerGuildStatusChangedEventListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class OnLocalPlayerGuildStatusChangedEventListener : BaseSingleEventListenerInitializable<IGuildStatusChangedEventSubscribable, GuildStatusChangedEventArgs>
+	{
+		protected OnLocalPlayerGuildStatusChangedEventListener(IGuildStatusChangedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
