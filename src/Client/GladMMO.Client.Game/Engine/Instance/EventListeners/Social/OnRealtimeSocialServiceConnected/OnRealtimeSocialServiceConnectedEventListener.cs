3f972ae8aef45045a7483ae640bf115894d4925e
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 01:40:53 -0500

    Added OnRealtimeSocialServiceConnectedEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Social/OnRealtimeSocialServiceConnected/OnRealtimeSocialServiceConnectedEventListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class OnRealtimeSocialServiceConnectedEventListener : BaseSingleEventListenerInitializable<IRealtimeSocialServiceConnectedEventSubscribable>
+	{
+		protected OnRealtimeSocialServiceConnectedEventListener(IRealtimeSocialServiceConnectedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
