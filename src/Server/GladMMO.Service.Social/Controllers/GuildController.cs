2899ddcc60052bc93859a7683d044767927ee4b8
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 17:30:56 -0500

    Added GuildController.GetCharacterGuildList

--- src/Server/GladMMO.Service.Social/Controllers/GuildController.cs
+++ src/Server/GladMMO.Service.Social/Controllers/GuildController.cs
@@ -12,7 +12,7 @@ namespace GladMMO
 	public sealed class GuildController : AuthorizationReadyController
 	{
 		/// <inheritdoc />
-		public GuildController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+		public GuildController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger)
 			: base(claimsReader, logger)
 		{
 
@@ -24,13 +24,13 @@ namespace GladMMO
 		[AllowAnonymous]
 		[HttpGet("character/{id}")]
 		[NoResponseCache] //we don't want to cache this, if they are removed from a guild then we want this reflected immediately or they may be taking in a guild chat they aren't apart of due to a race condition
-		public async Task<IActionResult> GetCharacterMembershipGuildStatus([FromRoute(Name = "id")] int characterId, 
+		public async Task<IActionResult> GetCharacterMembershipGuildStatus([FromRoute(Name = "id")] int characterId,
 			[NotNull] [FromServices] IGuildCharacterMembershipRepository guildCharacterMembershipRepository)
 		{
-			if(guildCharacterMembershipRepository == null) throw new ArgumentNullException(nameof(guildCharacterMembershipRepository));
+			if (guildCharacterMembershipRepository == null) throw new ArgumentNullException(nameof(guildCharacterMembershipRepository));
 
 			//If guild membership repo doesn't have the character id as an entry then it means there is no guild associated with them.
-			if(!(await guildCharacterMembershipRepository.ContainsAsync(characterId).ConfigureAwait(false)))
+			if (!(await guildCharacterMembershipRepository.ContainsAsync(characterId).ConfigureAwait(false)))
 				return BuildFailedResponseModel(CharacterGuildMembershipStatusResponseCode.NoGuild);
 
 			//TODO: There is technically a race condition here. They could have just been kicked from a guild but the cached model may say they are in a guild
@@ -42,13 +42,36 @@ namespace GladMMO
 			{
 				return BuildSuccessfulResponseModel(new CharacterGuildMembershipStatusResponse((await guildCharacterMembershipRepository.RetrieveAsync(characterId).ConfigureAwait(false)).GuildId));
 			}
-			catch(Exception e)
+			catch (Exception e)
 			{
-				if(Logger.IsEnabled(LogLevel.Error))
+				if (Logger.IsEnabled(LogLevel.Error))
 					Logger.LogError($"Encountered error in expected guild membership status request. CharacterId: {characterId} Reason: {e.Message}\n\nStack: {e.StackTrace}");
 
 				return BuildFailedResponseModel(CharacterGuildMembershipStatusResponseCode.GeneralServerError);
 			}
+		}
+
+		[ProducesJson]
+		[AuthorizeJwt]
+		[HttpGet("{id}/list")]
+		public async Task<IActionResult> GetCharacterGuildList([FromRoute(Name = "id")] int guildId,
+			[NotNull] [FromServices] IGuildCharacterMembershipRepository guildCharacterMembershipRepository,
+			[FromServices] ISocialServiceToGameServiceClient socialToGameClient)
+		{
+			if(guildCharacterMembershipRepository == null) throw new ArgumentNullException(nameof(guildCharacterMembershipRepository));
+
+			CharacterSessionDataResponse session = await socialToGameClient.GetCharacterSessionDataByAccount(ClaimsReader.GetUserIdInt(User));
+
+			if (!session.isSuccessful)
+				return BuildFailedResponseModel(CharacterGuildMembershipStatusResponseCode.GeneralServerError);
+
+			//No guild check
+			if (!await guildCharacterMembershipRepository.ContainsAsync(session.CharacterId))
+				return BuildFailedResponseModel(CharacterGuildMembershipStatusResponseCode.NoGuild);
+
+			int[] roster = await guildCharacterMembershipRepository.GetEntireGuildRosterAsync(guildId);
+
+			return BuildSuccessfulResponseModel(new CharacterGuildListResponseModel(roster));
 		}
 	}
 }
