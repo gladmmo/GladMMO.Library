fd3b67ef18a89bc40b4d761d39fe326479685aa8
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 20:12:41 -0500

    Added guild invite UI events and glues

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Initializables/ButtonGlues/ManualInviteGuildMemberButtonClickedGlueStartable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(IManualInviteGuildMemberClickedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ManualInviteGuildMemberButtonClickedGlueStartable : BaseButtonClickGlueStartable, IManualInviteGuildMemberClickedEventSubscribable
+	{
+		public const UnityUIRegisterationKey ButtonKey = UnityUIRegisterationKey.SocialWindowAddGuildMember;
+
+		public ManualInviteGuildMemberButtonClickedGlueStartable([KeyFilter(ButtonKey)] [NotNull] IUIButton createButton)
+			: base(createButton)
+		{
+
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Initializables/ButtonGlues/ModalInviteGuildMemberButtonClickedGlueStartable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	//This is the button event for when they hit the "Add" button on the Invite GuildMember popup modal.
+	[AdditionalRegisterationAs(typeof(IInviteGuildMemberModalClickedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ModalInviteGuildMemberButtonClickedGlueStartable : BaseButtonClickGlueStartable, IInviteGuildMemberModalClickedEventSubscribable
+	{
+		public const UnityUIRegisterationKey ButtonKey = UnityUIRegisterationKey.AddGuildMemberModalWindow;
+
+		public ModalInviteGuildMemberButtonClickedGlueStartable([KeyFilter(ButtonKey)] [NotNull] IUIButton createButton)
+			: base(createButton)
+		{
+
+		}
+	}
+}
