e496183232a4f911ffe508be32b97057abc5fedd
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 20:37:16 -0500

    Added better failure handling for social models

--- src/Server/GladMMO.Service.Social/Handlers/SocialSignalRMessageRouter.cs
+++ src/Server/GladMMO.Service.Social/Handlers/SocialSignalRMessageRouter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GladNet;
+using Microsoft.Extensions.Logging;
 
 namespace GladMMO
 {
@@ -10,9 +11,13 @@ namespace GladMMO
 	{
 		private Dictionary<Type, ISocialModelPayloadHandler<TRemoteClientHubInterfaceType>> MessageRouteMap { get; }
 
-		public SocialSignalRMessageRouter([JetBrains.Annotations.NotNull] IEnumerable<ISocialModelPayloadHandler<TRemoteClientHubInterfaceType>> messageHandlers)
+		private ILogger<SocialSignalRMessageRouter<TRemoteClientHubInterfaceType>> Logger { get; }
+
+		public SocialSignalRMessageRouter([JetBrains.Annotations.NotNull] IEnumerable<ISocialModelPayloadHandler<TRemoteClientHubInterfaceType>> messageHandlers,
+			[JetBrains.Annotations.NotNull] ILogger<SocialSignalRMessageRouter<TRemoteClientHubInterfaceType>> logger)
 		{
 			if (messageHandlers == null) throw new ArgumentNullException(nameof(messageHandlers));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 
 			MessageRouteMap = new Dictionary<Type, ISocialModelPayloadHandler<TRemoteClientHubInterfaceType>>();
 
@@ -27,7 +32,13 @@ namespace GladMMO
 
 		public Task<bool> TryHandleMessage(HubConnectionMessageContext<TRemoteClientHubInterfaceType> context, NetworkIncomingMessage<BaseSocialModel> message)
 		{
-			return MessageRouteMap[message.Payload.GetType()].TryHandleMessage(context, message);
+			if(CanHandle(message))
+				return MessageRouteMap[message.Payload.GetType()].TryHandleMessage(context, message);
+
+			if(Logger.IsEnabled(LogLevel.Error))
+				Logger.LogError($"Unable to handle Social Model of Type: {message.Payload.GetType().Name}");
+
+			return Task.FromResult(false);
 		}
 	}
 }
