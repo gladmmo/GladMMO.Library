ce2b052f6e5979ebe945b09ce91b4ce6e81e8887
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 18:17:57 -0500

    Refactored BaseRoutableChatTextMessageEnteredEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Social/Chat/OnChatTextMessageEntered/BaseRoutableChatTextMessageEnteredEventListener.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+using Nito.AsyncEx;
+
+namespace GladMMO
+{
+	public class BaseRoutableChatTextMessageEnteredEventListener : IGameInitializable
+	{
+		private ILog Logger { get; }
+
+		protected ChatChannelType ChannelType { get; }
+
+		public BaseRoutableChatTextMessageEnteredEventListener(IChatTextMessageEnteredEventSubscribable subscriptionService,
+			[NotNull] IChatChannelJoinedEventSubscribable channelJoinedSubscriptionService,
+			[NotNull] ILog logger, 
+			ChatChannelType channelType)
+		{
+			if(channelJoinedSubscriptionService == null) throw new ArgumentNullException(nameof(channelJoinedSubscriptionService));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			ChannelType = channelType;
+
+			//This just registers and wires up that when we join a channel we'll start recieveing that channels messages.
+			channelJoinedSubscriptionService.OnChatChannelJoined += (o, e) =>
+			{
+				if(!CheckType(e))
+					return;
+
+				subscriptionService.OnChatMessageEntered += (o2, e2) =>
+				{
+					if(!CheckType(e2))
+						return;
+
+					OnEventFired(e.MessageSender, e2);
+				};
+			};
+		}
+
+		protected void OnEventFired(IChatChannelSender sender, [NotNull] ChatTextMessageEnteredEventArgs args)
+		{
+			if(args == null) throw new ArgumentNullException(nameof(args));
+
+			//Post it so we can get exception information.
+			UnityAsyncHelper.UnityMainThreadContext.PostAsync(async () =>
+			{
+				try
+				{
+					await sender.SendAsync(args.Content);
+				}
+				catch(Exception e)
+				{
+					if(Logger.IsErrorEnabled)
+						Logger.Error($"Failed to send message. Reason: {e.Message}");
+
+					throw;
+				}
+			});
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected bool CheckType([NotNull] IChatChannelRoutable routable)
+		{
+			if(routable == null) throw new ArgumentNullException(nameof(routable));
+
+			return routable.ChannelType == ChannelType;
+		}
+
+		//Hack to get into the scene.
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Social/Chat/OnChatTextMessageEntered/ProximityTextChatMessageManagerEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Social/Chat/OnChatTextMessageEntered/ProximityTextChatMessageManagerEventListener.cs
@@ -10,65 +10,14 @@ using Nito.AsyncEx;
 namespace GladMMO
 {
 	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
-	public sealed class ProximityTextChatMessageManagerEventListener : IGameInitializable
+	public sealed class ProximityTextChatMessageManagerEventListener : BaseRoutableChatTextMessageEnteredEventListener
 	{
-		private ILog Logger { get; }
-
 		public ProximityTextChatMessageManagerEventListener(IChatTextMessageEnteredEventSubscribable subscriptionService,
 			[NotNull] IChatChannelJoinedEventSubscribable channelJoinedSubscriptionService,
 			[NotNull] ILog logger)
+			: base(subscriptionService, channelJoinedSubscriptionService, logger, ChatChannelType.Proximity)
 		{
-			if (channelJoinedSubscriptionService == null) throw new ArgumentNullException(nameof(channelJoinedSubscriptionService));
-			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-
-			//This just registers and wires up that when we join a channel we'll start recieveing that channels messages.
-			channelJoinedSubscriptionService.OnChatChannelJoined += (o, e) =>
-			{
-				if (!CheckType(e))
-					return;
-
-				subscriptionService.OnChatMessageEntered += (o2, e2) =>
-				{
-					if (!CheckType(e2))
-						return;
-
-					OnEventFired(e.MessageSender, e2);
-				};
-			};
-		}
-
-		protected void OnEventFired(IChatChannelSender sender, [NotNull] ChatTextMessageEnteredEventArgs args)
-		{
-			if (args == null) throw new ArgumentNullException(nameof(args));
-
-			//Post it so we can get exception information.
-			UnityAsyncHelper.UnityMainThreadContext.PostAsync(async () =>
-			{
-				try
-				{
-					await sender.SendAsync(args.Content);
-				}
-				catch (Exception e)
-				{
-					if(Logger.IsErrorEnabled)
-						Logger.Error($"Failed to send message. Reason: {e.Message}");
-
-					throw;
-				}
-			});
-		}
-
-		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		private static bool CheckType([NotNull] IChatChannelRoutable routable)
-		{
-			if (routable == null) throw new ArgumentNullException(nameof(routable));
-
-			return routable.ChannelType == ChatChannelType.Proximity;
-		}
-
-		public async Task OnGameInitialized()
-		{
-
+			
 		}
 	}
 }
