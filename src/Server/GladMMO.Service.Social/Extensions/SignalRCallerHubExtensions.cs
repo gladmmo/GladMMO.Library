dbb39c527f2a0edb2e355f02013127024d5ac40d
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 22:45:20 -0500

    Added SignalRCallerHubExtensions

---
+++ src/Server/GladMMO.Service.Social/Extensions/SignalRCallerHubExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GladMMO
+{
+	public static class SignalRCallerHubExtensions
+	{
+		/// <summary>
+		/// Gets the <typeparamref name="TRemoteClientHubInterfaceType"/> associated with the provided entity guid <see cref="clientGuid"/>.
+		/// </summary>
+		/// <typeparam name="TRemoteClientHubInterfaceType">The remote interface type.</typeparam>
+		/// <param name="clientGroup">The client group.</param>
+		/// <param name="clientGuid">The entity.</param>
+		/// <returns>The remote client RPC interface.</returns>
+		public static TRemoteClientHubInterfaceType RetrievePlayerClient<TRemoteClientHubInterfaceType>(this IHubCallerClients<TRemoteClientHubInterfaceType> clientGroup, NetworkEntityGuid clientGuid)
+		{
+			return clientGroup.Client(clientGuid.EntityId.ToString());
+		}
+	}
+}
