968aefa7b2fecc44b1dad73e9b3c0d24a5157cf3
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 18:18:22 -0500

    Added GuildTextChatMessageManagerEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Social/Chat/OnChatTextMessageEntered/GuildTextChatMessageManagerEventListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+using Nito.AsyncEx;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class GuildTextChatMessageManagerEventListener : BaseRoutableChatTextMessageEnteredEventListener
+	{
+		public GuildTextChatMessageManagerEventListener(IChatTextMessageEnteredEventSubscribable subscriptionService,
+			[NotNull] IChatChannelJoinedEventSubscribable channelJoinedSubscriptionService,
+			[NotNull] ILog logger)
+			: base(subscriptionService, channelJoinedSubscriptionService, logger, ChatChannelType.Guild)
+		{
+			
+		}
+	}
+}
