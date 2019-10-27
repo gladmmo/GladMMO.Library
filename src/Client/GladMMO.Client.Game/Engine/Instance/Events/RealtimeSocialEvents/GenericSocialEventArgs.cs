bac82693daa8acf268f52d269741eac7abc15361
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 00:40:03 -0500

    Added GenericSocialEventArgs

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/RealtimeSocialEvents/GenericSocialEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Generic social event args.
+	/// </summary>
+	/// <typeparam name="TSocialModel">The social data model Type.</typeparam>
+	public sealed class GenericSocialEventArgs<TSocialModel> : EventArgs
+		where TSocialModel : BaseSocialModel
+	{
+		public TSocialModel Data { get; }
+
+		public GenericSocialEventArgs([NotNull] TSocialModel data)
+		{
+			Data = data ?? throw new ArgumentNullException(nameof(data));
+		}
+	}
+}
