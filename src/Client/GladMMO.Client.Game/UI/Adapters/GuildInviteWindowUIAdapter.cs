627323588587035c341680ed4762e84635ee937d
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 00:30:29 -0500

    Added IUIGuildInviteWindow

---
+++ src/Client/GladMMO.Client.Game/UI/Adapters/GuildInviteWindowUIAdapter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GladMMO
+{
+#pragma warning disable 649
+	public sealed class GuildInviteWindowUIAdapter : BaseUnityUI<IUIGuildInviteWindow>, IUIGuildInviteWindow
+	{
+		[SerializeField]
+		private Button _declineInviteButton;
+
+		[SerializeField]
+		private Button _acceptInviteButton;
+
+		[SerializeField]
+		private Text _invitationText;
+
+		[SerializeField]
+		private Text _guildNameText;
+
+		private Lazy<IUIButton> _DeclineInviteButton { get; }
+			    
+		private Lazy<IUIButton> _AcceptInviteButton { get; }
+			    
+		private Lazy<IUIText> _InvitationText { get; }
+			    
+		private Lazy<IUIText> _GuildNameText { get; }
+
+		public IUIButton DeclineInviteButton => _DeclineInviteButton.Value;
+
+		public IUIButton AcceptInviteButton => _AcceptInviteButton.Value;
+
+		public IUIText InvitationText => _InvitationText.Value;
+
+		public IUIText GuildNameText => _GuildNameText.Value;
+
+		public GuildInviteWindowUIAdapter()
+		{
+			_DeclineInviteButton = new Lazy<IUIButton>(() => new UnityButtonUIButtonAdapterImplementation(_declineInviteButton));
+			_AcceptInviteButton = new Lazy<IUIButton>(() => new UnityButtonUIButtonAdapterImplementation(_acceptInviteButton));
+
+			_InvitationText = new Lazy<IUIText>(() => new UnityTextUITextAdapterImplementation(_invitationText));
+			_GuildNameText = new Lazy<IUIText>(() => new UnityTextUITextAdapterImplementation(_guildNameText));
+		}
+
+		public void SetElementActive(bool state)
+		{
+			gameObject.SetActive(state);
+		}
+
+		public bool isActive => gameObject.activeSelf;
+	}
+#pragma warning restore 649
+}
---
+++ src/Client/GladMMO.Client.Game/UI/Interfaces/IUIGuildInviteWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IUIGuildInviteWindow : IUIElement //it's attached to the window.
+	{
+		IUIButton DeclineInviteButton { get; }
+
+		IUIButton AcceptInviteButton { get; }
+
+		IUIText InvitationText { get; }
+
+		IUIText GuildNameText { get; }
+	}
+}
