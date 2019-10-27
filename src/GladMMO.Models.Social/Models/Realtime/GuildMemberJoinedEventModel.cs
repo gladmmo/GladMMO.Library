ab5097394d1377400b280aa8ca02307bae60b2e1
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 01:35:18 -0500

    Added GuildMemberJoinedEventModel

---
+++ src/GladMMO.Models.Social/Models/Realtime/GuildMemberJoinedEventModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class GuildMemberJoinedEventModel : BaseSocialModel
+	{
+		/// <summary>
+		/// The guid of who joined the guild.
+		/// </summary>
+		[JsonProperty]
+		public NetworkEntityGuid JoineeGuid { get; private set; }
+
+		public GuildMemberJoinedEventModel([JetBrains.Annotations.NotNull] NetworkEntityGuid joineeGuid)
+		{
+			JoineeGuid = joineeGuid ?? throw new ArgumentNullException(nameof(joineeGuid));
+		}
+
+		[JsonConstructor]
+		private GuildMemberJoinedEventModel()
+		{
+			
+		}
+	}
+}
