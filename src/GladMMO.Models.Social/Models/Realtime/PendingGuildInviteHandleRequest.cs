a92dfe0b2cf7a0e218e5e2be2169c90526a9868b
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 01:05:17 -0500

    Added PendingGuildInviteHandleRequest

---
+++ src/GladMMO.Models.Social/Models/Realtime/PendingGuildInviteHandleRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Represents a player's response to a received <see cref="GuildMemberInviteEventModel"/>.
+	/// </summary>
+	public sealed class PendingGuildInviteHandleRequest : BaseSocialModel, ISucceedable
+	{
+		/// <summary>
+		/// Indicates if the pending invite was accepted.
+		/// </summary>
+		[JsonProperty]
+		public bool isSuccessful { get; private set; }
+
+		public PendingGuildInviteHandleRequest(bool isSuccessful)
+		{
+			this.isSuccessful = isSuccessful;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		private PendingGuildInviteHandleRequest()
+		{
+			
+		}
+	}
+}
