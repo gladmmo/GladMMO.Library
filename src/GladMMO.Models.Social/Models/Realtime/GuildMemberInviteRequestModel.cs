c35ce8320271367ace05b718b1c14e7bd0727017
HelloKitty <andrew.blakely@ymail.com>
2019-10-26 19:35:03 -0500

    Added GuildMemberInviteRequestModel

---
+++ src/GladMMO.Models.Social/Models/Realtime/GuildMemberInviteRequestModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class GuildMemberInviteRequestModel : BaseSocialModel
+	{
+		[JsonProperty]
+		public NetworkEntityGuid MemberToInviteEntityGuid { get; private set; }
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		private GuildMemberInviteRequestModel()
+		{
+			
+		}
+	}
+}
