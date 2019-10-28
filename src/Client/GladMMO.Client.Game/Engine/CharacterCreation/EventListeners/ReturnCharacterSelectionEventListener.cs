c6b8a4450092e517183ec2e0908d604c1e46bc48
HelloKitty <andrew.blakely@ymail.com>
2019-10-28 00:02:48 -0500

    Added character creation back button ReturnCharacterSelectionEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterCreation/EventListeners/ReturnCharacterSelectionEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterCreationScreen)]
+	public sealed class ReturnCharacterSelectionEventListener : ButtonClickedEventListener<ISceneBackButtonClickedSubscribable>
+	{
+		private ILog Logger { get; }
+
+		public ReturnCharacterSelectionEventListener(ISceneBackButtonClickedSubscribable subscriptionService,
+			[NotNull] ILog logger) 
+			: base(subscriptionService)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		protected override void OnEventFired(object source, ButtonClickedEventArgs args)
+		{
+			args.Button.IsInteractable = false;
+			SceneManager.LoadSceneAsync(GladMMOClientConstants.CHARACTER_SELECTION_SCENE_NAME).allowSceneActivation = true;
+		}
+	}
+}
