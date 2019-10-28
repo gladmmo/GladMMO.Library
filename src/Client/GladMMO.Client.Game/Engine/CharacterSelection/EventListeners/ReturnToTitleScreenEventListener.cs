d7f17bd22157d4b63894d477e647dac61fd19045
HelloKitty <andrew.blakely@ymail.com>
2019-10-28 00:01:50 -0500

    Added character select back button ReturnToTitleScreenEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterSelection/EventListeners/ReturnToTitleScreenEventListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	public sealed class ReturnToTitleScreenEventListener : ButtonClickedEventListener<ISceneBackButtonClickedSubscribable>
+	{
+		private ILog Logger { get; }
+
+		public ReturnToTitleScreenEventListener(ISceneBackButtonClickedSubscribable subscriptionService,
+			[NotNull] ILog logger) 
+			: base(subscriptionService)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		protected override void OnEventFired(object source, ButtonClickedEventArgs args)
+		{
+			args.Button.IsInteractable = false;
+			SceneManager.LoadSceneAsync(GladMMOClientConstants.TITLE_SCREEN_NAME).allowSceneActivation = true;
+		}
+	}
+}
