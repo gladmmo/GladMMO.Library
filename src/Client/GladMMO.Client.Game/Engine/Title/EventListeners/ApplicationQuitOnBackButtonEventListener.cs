55b3fe8db624391effe8c9861b1a9e1d65f55ea2
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 23:58:27 -0500

    Added titlescreen quit

---
+++ src/Client/GladMMO.Client.Game/Engine/Title/EventListeners/ApplicationQuitOnBackButtonEventListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.TitleScreen)]
+	public sealed class ApplicationQuitOnBackButtonEventListener : ButtonClickedEventListener<ISceneBackButtonClickedSubscribable>
+	{
+		private ILog Logger { get; }
+
+		public ApplicationQuitOnBackButtonEventListener(ISceneBackButtonClickedSubscribable subscriptionService,
+			[NotNull] ILog logger) 
+			: base(subscriptionService)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		protected override void OnEventFired(object source, ButtonClickedEventArgs args)
+		{
+			if(!Application.isEditor)
+				Application.Quit(0);
+			else
+			{
+				//In the editor, log so it's known to be working
+				if(Logger.IsDebugEnabled)
+					Logger.Debug($"Quit button pressed.");
+			}
+		}
+	}
+}
