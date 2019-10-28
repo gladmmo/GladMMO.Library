ef555e24d41c577c3a03edd3c33ca20fb36d6321
HelloKitty <andrew.blakely@ymail.com>
2019-10-28 00:39:33 -0500

    Refactored character selection Enter World handling

--- src/Client/GladMMO.Client.Game/Engine/CharacterSelection/Initializables/CharacterSelectionSelectCharacterImmediatelyOnButtonClickedEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/CharacterSelection/Initializables/CharacterSelectionSelectCharacterImmediatelyOnButtonClickedEventListener.cs
@@ -15,13 +15,11 @@ namespace GladMMO
 	//This just selects the character as soon as one is clicked.
 	[AdditionalRegisterationAs(typeof(IServerRequestedSceneChangeEventSubscribable))]
 	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
-	public sealed class CharacterSelectionSelectCharacterImmediatelyOnButtonClickedEventListener : BaseSingleEventListenerInitializable<ICharacterSelectionButtonClickedEventSubscribable, CharacterButtonClickedEventArgs>, IServerRequestedSceneChangeEventSubscribable
+	public sealed class CharacterSelectionSelectCharacterImmediatelyOnButtonClickedEventListener : ButtonClickedEventListener<IEnterWorldButtonClickedEventSubscribable>, IServerRequestedSceneChangeEventSubscribable
 	{
 		private ILocalCharacterDataRepository CharacterData { get; }
 
 		private ILog Logger { get; }
-
-		private IUIButton EnterWorldButton { get; }
 
 		/// <inheritdoc />
 		public event EventHandler<ServerRequestedSceneChangeEventArgs> OnServerRequestedSceneChange;
@@ -32,66 +30,60 @@ namespace GladMMO
 
 		/// <inheritdoc />
 		public CharacterSelectionSelectCharacterImmediatelyOnButtonClickedEventListener(
+			IEnterWorldButtonClickedEventSubscribable enterWorldButtonClickableEventSubscribable,
 			[NotNull] ICharacterSelectionButtonClickedEventSubscribable subscriptionService, 
 			[NotNull] ILocalCharacterDataRepository characterData, 
 			[NotNull] ILog logger,
-			[KeyFilter(UnityUIRegisterationKey.EnterWorld)] [NotNull] IUIButton enterWorldButton,
 			[NotNull] ICharacterService characterServiceQueryable)
-			: base(subscriptionService)
+			: base(enterWorldButtonClickableEventSubscribable)
 		{
-			//CharacterService = characterService ?? throw new ArgumentNullException(nameof(characterService));
 			CharacterData = characterData ?? throw new ArgumentNullException(nameof(characterData));
 			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-			EnterWorldButton = enterWorldButton ?? throw new ArgumentNullException(nameof(enterWorldButton));
 			CharacterServiceQueryable = characterServiceQueryable ?? throw new ArgumentNullException(nameof(characterServiceQueryable));
+
+			//Manually rig up.
+			subscriptionService.OnCharacterButtonClicked += OnCharacterButtonClicked;
 		}
 
 		/// <inheritdoc />
-		protected override void OnEventFired(object source, CharacterButtonClickedEventArgs args)
+		protected void OnCharacterButtonClicked(object source, CharacterButtonClickedEventArgs args)
 		{
 			SelectedCharacterGuid = args.CharacterGuid;
 		}
 
-		/// <inheritdoc />
-		public override Task OnGameInitialized()
+		protected override void OnEventFired(object source, ButtonClickedEventArgs args)
 		{
-			base.OnGameInitialized();
+			args.Button.IsInteractable = false;
 
-			EnterWorldButton.IsInteractable = false;
-			EnterWorldButton.AddOnClickListener(() => EnterWorldButton.IsInteractable = false);
-			EnterWorldButton.AddOnClickListenerAsync(OnEnterWorldButtonClicked);
-
-			return Task.CompletedTask;
-		}
-
-		private async Task OnEnterWorldButtonClicked()
-		{
-			if(SelectedCharacterGuid == null)
+			UnityAsyncHelper.UnityMainThreadContext.PostAsync(async () =>
 			{
-				Logger.Error($"Tried to enter the world without any selected character guid.");
-				return;
-			}
+				if(SelectedCharacterGuid == null)
+				{
+					Logger.Error($"Tried to enter the world without any selected character guid.");
+					return;
+				}
 
-			//We do this before sending the player login BECAUSE of a race condition that can be caused
-			//since I actually KNOW this event should disable networking. We should not handle messages in this scene after this point basically.
-			//TODO: Don't hardcode this scene.
-			OnServerRequestedSceneChange?.Invoke(this, new ServerRequestedSceneChangeEventArgs((PlayableGameScene)2));
+				//We do this before sending the player login BECAUSE of a race condition that can be caused
+				//since I actually KNOW this event should disable networking. We should not handle messages in this scene after this point basically.
+				//TODO: Don't hardcode this scene.
+				OnServerRequestedSceneChange?.Invoke(this, new ServerRequestedSceneChangeEventArgs((PlayableGameScene) 2));
 
-			CharacterSessionEnterResponse enterResponse = await CharacterServiceQueryable.TryEnterSession(SelectedCharacterGuid.EntityId);
+				CharacterSessionEnterResponse enterResponse = await CharacterServiceQueryable.TryEnterSession(SelectedCharacterGuid.EntityId);
 
-			if(Logger.IsDebugEnabled)
-				Logger.Debug($"Character Session Entry Response: {enterResponse.ResultCode}.");
+				if (Logger.IsDebugEnabled)
+					Logger.Debug($"Character Session Entry Response: {enterResponse.ResultCode}.");
 
-			if(!enterResponse.isSuccessful)
-				if(Logger.IsErrorEnabled)
-					Logger.Error($"Failed to enter CharacterSession for Entity: {SelectedCharacterGuid} Reason: {enterResponse.ResultCode}");
+				if (!enterResponse.isSuccessful)
+					if (Logger.IsErrorEnabled)
+						Logger.Error($"Failed to enter CharacterSession for Entity: {SelectedCharacterGuid} Reason: {enterResponse.ResultCode}");
 
-			//TODO: handle character session failure
-			CharacterData.UpdateCharacterId(SelectedCharacterGuid.EntityId);
+				//TODO: handle character session failure
+				CharacterData.UpdateCharacterId(SelectedCharacterGuid.EntityId);
 
-			//TODO: Use the scene manager service.
-			//TODO: Don't hardcode scene ids. Don't load scenes directly.
-			SceneManager.LoadSceneAsync(GladMMOClientConstants.WORLD_DOWNLOAD_SCENE_NAME).allowSceneActivation = true;
+				//TODO: Use the scene manager service.
+				//TODO: Don't hardcode scene ids. Don't load scenes directly.
+				SceneManager.LoadSceneAsync(GladMMOClientConstants.WORLD_DOWNLOAD_SCENE_NAME).allowSceneActivation = true;
+			});
 		}
 	}
 }
---
+++ src/Client/GladMMO.Client.Game/Engine/CharacterSelection/Initializables/EnterWorldButtonClickedGlueStartable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(IEnterWorldButtonClickedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	public sealed class EnterWorldButtonClickedGlueStartable : BaseButtonClickGlueStartable, IEnterWorldButtonClickedEventSubscribable
+	{
+		public const UnityUIRegisterationKey ButtonKey = UnityUIRegisterationKey.EnterWorld;
+
+		public EnterWorldButtonClickedGlueStartable([KeyFilter(ButtonKey)] [NotNull] IUIButton createButton)
+			: base(createButton)
+		{
+
+		}
+	}
+}
