63879015a18e87b3767ff5aac565c6a9e3c9035a
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 21:09:29 -0500

    Added IFactoryCreatableExtensions

---
+++ src/GladMMO.Common/Extensions/IFactoryCreatableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public static class IFactoryCreatableExtensions
+	{
+		/// <summary>
+		/// Simplified creation API that creates a type <typeparamref name="TConstructionType"/> with provided <see cref="factory"/>.
+		/// Only for creation contexts of <see cref="EmptyFactoryContext"/>.
+		/// </summary>
+		/// <typeparam name="TConstructionType">The construction type.</typeparam>
+		/// <param name="factory">The factory instance.</param>
+		/// <returns>An instance of the construction type.</returns>
+		public static TConstructionType Create<TConstructionType>(this IFactoryCreatable<TConstructionType, EmptyFactoryContext> factory)
+		{
+			if (factory == null) throw new ArgumentNullException(nameof(factory));
+
+			return factory.Create(EmptyFactoryContext.Instance);
+		}
+	}
+}
