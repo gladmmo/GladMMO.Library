c2b832dacf0ed166f22bcbe5dcd279cb3211f2cd
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 23:53:52 -0500

    Added SceneBackButtonClickedGlueStartable

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/Initializables/SceneBackButtonClickedGlueStartable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(ISceneBackButtonClickedSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.TitleScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterCreationScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class SceneBackButtonClickedGlueStartable : BaseButtonClickGlueStartable, ISceneBackButtonClickedSubscribable
+	{
+		public const UnityUIRegisterationKey ButtonKey = UnityUIRegisterationKey.BackButton;
+
+		public SceneBackButtonClickedGlueStartable([KeyFilter(ButtonKey)] [NotNull] IUIButton createButton)
+			: base(createButton)
+		{
+
+		}
+	}
+}
