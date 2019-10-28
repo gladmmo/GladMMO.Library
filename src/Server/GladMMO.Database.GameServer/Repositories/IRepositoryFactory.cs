e5128e6ea34c2152e4acbe7b9c57d69ee97528b6
HelloKitty <andrew.blakely@ymail.com>
2019-10-27 21:03:28 -0500

    Added IRepositoryFactory

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/IRepositoryFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IRepositoryFactory<out TRepositoryType> : IFactoryCreatable<TRepositoryType, EmptyFactoryContext>
+		where TRepositoryType : IDisposable
+	{
+
+	}
+
+	public sealed class RepositoryFactory<TRepositoryType> : IRepositoryFactory<TRepositoryType>
+		where TRepositoryType : IDisposable
+	{
+		/// <summary>
+		/// Likely provided by Autofac.
+		/// </summary>
+		private Func<TRepositoryType> RepositoryCreationFactory { get; }
+
+		public RepositoryFactory([JetBrains.Annotations.NotNull] Func<TRepositoryType> repositoryCreationFactory)
+		{
+			RepositoryCreationFactory = repositoryCreationFactory ?? throw new ArgumentNullException(nameof(repositoryCreationFactory));
+		}
+
+		public TRepositoryType Create(EmptyFactoryContext context)
+		{
+			return RepositoryCreationFactory();
+		}
+	}
+}
