f7429926cc41b38700c804abf2bc4c9a414ee4c4
HelloKitty <andrew.blakely@ymail.com>
2019-10-28 21:36:24 -0500

    Added ZoneServerApplicationUser

---
+++ src/Server/GladMMO.Service.ZoneAuthentication/Database/ZoneServerApplicationUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace GladMMO
+{
+	public class ZoneServerApplicationUser : IdentityUser<int>
+	{
+		//Unlike normal guardians users we DO have additional data.
+		[Required]
+		public int AccountId { get; private set; }
+
+		public ZoneServerApplicationUser(int accountId) 
+			: base()
+		{
+			if (accountId <= 0) throw new ArgumentOutOfRangeException(nameof(accountId));
+
+			AccountId = accountId;
+		}
+
+		public ZoneServerApplicationUser(int accountId, string userName)
+			: base(userName)
+		{
+			if(accountId <= 0) throw new ArgumentOutOfRangeException(nameof(accountId));
+
+			AccountId = accountId;
+		}
+
+		/// <summary>
+		/// EF Ctor.
+		/// </summary>
+		private ZoneServerApplicationUser()
+		{
+			
+		}
+	}
+}
