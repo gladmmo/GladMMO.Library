39706f5292bbefd54dc3e16f3ef4034649091e47
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 04:23:03 -0500

    Added ZoneServerRegistrationRequest

---
+++ src/GladMMO.Models.Server.ZoneServer/ZoneManager/Models/ZoneServerRegisterationRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class ZoneServerRegistrationRequest
+	{
+		//TODO: Better encapsulate this data
+		/// <summary>
+		/// The desired ID of the world the zone would like to register as.
+		/// </summary>
+		[JsonProperty]
+		public int WorldId { get; private set; }
+
+		/// <summary>
+		/// The network port the zone instance will be listening for connections on.
+		/// </summary>
+		[JsonProperty]
+		public short NetworkPort { get; private set; }
+
+		public ZoneServerRegistrationRequest(int worldId, short networkPort)
+		{
+			if (worldId <= 0) throw new ArgumentOutOfRangeException(nameof(worldId));
+			if (networkPort <= 0) throw new ArgumentOutOfRangeException(nameof(networkPort));
+
+			WorldId = worldId;
+			NetworkPort = networkPort;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		private ZoneServerRegistrationRequest()
+		{
+			
+		}
+	}
+}
