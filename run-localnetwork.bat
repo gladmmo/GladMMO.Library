277a807b8714d031516f68bcc389610114b9e8c3
HelloKitty <andrew.blakely@ymail.com>
2019-10-28 23:53:33 -0500

    Temp fix ZoneAuth listen url

--- run-localnetwork.bat
+++ run-localnetwork.bat
@@ -31,5 +31,5 @@ start "social" dotnet GladMMO.Service.Social.dll --url=http://192.168.0.12:5008
 cd ..
 
 cd zoneauth
-start "zoneauth" dotnet GladMMO.Service.ZoneAuthentication.dll --usehttps Certs/TLSCert.pfx --url=https://192.168.0.12:5009
+start "zoneauth" dotnet GladMMO.Service.ZoneAuthentication.dll --url=http://192.168.0.12:5009
 cd ..
