3ad438292fb6ede02b9ab6eaa8ac6fc4b64bb527
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 05:37:15 -0500

    Added Service.ZoneManager ZoneRegisterController.TryRegisterZoneServer

--- src/Server/GladMMO.Service.ZoneManager/Controllers/ZoneRegistryController.cs
+++ src/Server/GladMMO.Service.ZoneManager/Controllers/ZoneRegistryController.cs
@@ -20,5 +20,37 @@ namespace GladMMO
 		{
 			ZoneRepository = zoneRepository ?? throw new ArgumentNullException(nameof(zoneRepository));
 		}
+
+		//ZoneServerRegistrationResponse
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="request"></param>
+		/// <returns></returns>
+		[AuthorizeJwt(GuardianApplicationRole.ZoneServer)] //only zone servers obviously.
+		[HttpPost("register")]
+		[ProducesJson]
+		public async Task<IActionResult> TryRegisterZoneServer([FromBody] ZoneServerRegistrationRequest request)
+		{
+			if (!ModelState.IsValid)
+				return BuildFailedResponseModel(ZoneServerRegistrationResponseCode.GeneralServerError);
+
+			//This should really ever happen in normal circumstances.
+			if (await ZoneRepository.ContainsAsync(ClaimsReader.GetAccountIdInt(User)))
+			{
+				if(Logger.IsEnabled(LogLevel.Warning))
+					Logger.LogWarning($"UserId: {ClaimsReader.GetPlayerAccountId(User)} attempted to register ZoneId: {ClaimsReader.GetAccountId(User)} multiple times.");
+
+				return BuildFailedResponseModel(ZoneServerRegistrationResponseCode.ZoneAlreadyRegistered);
+			}
+
+			//TODO: Should we check world rights ownership here?
+			bool registerResult = await ZoneRepository.TryCreateAsync(new ZoneInstanceEntryModel(ClaimsReader.GetAccountIdInt(User), HttpContext.Connection.RemoteIpAddress.ToString(), request.NetworkPort, request.WorldId));
+
+			if (!registerResult)
+				return BuildFailedResponseModel(ZoneServerRegistrationResponseCode.GeneralServerError);
+
+			return BuildSuccessfulResponseModel(new ZoneServerRegistrationResponse(ClaimsReader.GetAccountIdInt(User)));
+		}
 	}
 }
