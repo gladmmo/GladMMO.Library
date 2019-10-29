0f468055bc9615245171b4c6f087f60d0e17dc40
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 16:49:32 -0500

    Added IZoneRegistryService client

---
+++ src/Client/GladMMO.Client.ZoneService.Server/Async/AsyncEndpointZoneRegistryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GladMMO;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncEndpointZoneRegistryService : BaseAsyncEndpointService<IZoneRegistryService>, IZoneRegistryService
+	{
+		/// <inheritdoc />
+		public AsyncEndpointZoneRegistryService(Task<string> futureEndpoint)
+			: base(futureEndpoint)
+		{
+		}
+
+		/// <inheritdoc />
+		public AsyncEndpointZoneRegistryService(Task<string> futureEndpoint, RefitSettings settings)
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		public async Task<ResponseModel<ZoneServerRegistrationResponse, ZoneServerRegistrationResponseCode>> TryRegisterZoneServerAsync(ZoneServerRegistrationRequest request)
+		{
+			return await (await GetService().ConfigureAwait(false)).TryRegisterZoneServerAsync(request).ConfigureAwait(false);
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.ZoneService.Server/IZoneRegistryService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Proxy interface for zone registry controller/service.
+	/// </summary>
+	[Headers("User-Agent: GuardiansServer")]
+	public interface IZoneRegistryService
+	{
+		[RequiresAuthentication]
+		[Post("/api/ZoneRegistry/register")]
+		Task<ResponseModel<ZoneServerRegistrationResponse, ZoneServerRegistrationResponseCode>> TryRegisterZoneServerAsync([JsonBody] ZoneServerRegistrationRequest request);
+	}
+}
