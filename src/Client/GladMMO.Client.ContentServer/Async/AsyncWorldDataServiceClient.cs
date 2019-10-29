848167113e68554a8aa2d0f4e2937cd63ada95cf
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 05:43:15 -0500

    Added IWorldDataServiceClient

---
+++ src/Client/GladMMO.Client.ContentServer/Async/AsyncWorldDataServiceClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncWorldDataServiceClient : BaseAsyncEndpointService<IWorldDataServiceClient>, IWorldDataServiceClient
+	{
+		/// <inheritdoc />
+		public AsyncWorldDataServiceClient(Task<string> futureEndpoint) 
+			: base(futureEndpoint)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public AsyncWorldDataServiceClient(Task<string> futureEndpoint, RefitSettings settings) 
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		public async Task<bool> CheckWorldExistsAsync(int worldId)
+		{
+			return await (await GetService().ConfigureAwait(false)).CheckWorldExistsAsync(worldId).ConfigureAwait(false);
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.ContentServer/IWorldDataServiceClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	//TODO: Automate user-agent SDK version headers
+	[Headers("User-Agent: SDK 0.0.1")]
+	public interface IWorldDataServiceClient
+	{
+		[Get("/api/World/{id}/exists")]
+		Task<bool> CheckWorldExistsAsync([AliasAs("id")] int worldId);
+	}
+}
