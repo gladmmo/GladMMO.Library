55c33267f1d930f65101c6f720c9467642bf976b
HelloKitty <andrew.blakely@ymail.com>
2019-10-28 21:37:21 -0500

    Added GuardiansZoneAuthenticationDbContext

---
+++ src/Server/GladMMO.Service.ZoneAuthentication/Database/GuardiansZoneAuthenticationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	//Different from authserver, it uses ZoneServerApplicationUser.
+	/// <summary>
+	/// See Documentation for details: https://github.com/openiddict/openiddict-core
+	/// </summary>
+	public class GuardiansZoneAuthenticationDbContext : IdentityDbContext<ZoneServerApplicationUser, GuardiansApplicationRole, int>
+	{
+		public GuardiansZoneAuthenticationDbContext(DbContextOptions<GuardiansZoneAuthenticationDbContext> options)
+			: base(options)
+		{
+
+		}
+	}
+}
