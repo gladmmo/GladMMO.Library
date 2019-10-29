1a96f2e5e498d2c59f427516a8b01d9642657ce6
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 04:23:12 -0500

    Added ZoneServerRegistrationResponse

---
+++ src/GladMMO.Models.Server.ZoneServer/ZoneManager/Models/ZoneServerRegistrationResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class ZoneServerRegistrationResponse
+	{
+		//TODO: Should the zone registry service indicate anything to the zone?
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		public ZoneServerRegistrationResponse()
+		{
+			
+		}
+	}
+}
