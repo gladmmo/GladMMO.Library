1051b26c66a9a9a2a6bda8277ed3813920ec60fa
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 00:30:29 -0500

    Added ZoneServerRegistrationResponse

---
+++ src/GladMMO.Models.ZoneAuthentication/Models/ZoneServerRegistrationResponse.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class ZoneServerRegistrationResponse
+	{
+		/// <summary>
+		/// The expected zoneId (after registering online status) which is also
+		/// the zone's registered account id.
+		/// </summary>
+		[JsonProperty]
+		public int ZoneId { get; private set; }
+
+		/// <summary>
+		/// One-time use ephemeral zone username.
+		/// </summary>
+		[JsonProperty]
+		public string ZoneUserName { get; private set; }
+
+		/// <summary>
+		/// One-time use ephemeral zone password.
+		/// </summary>
+		[JsonProperty]
+		public string ZonePassword { get; private set; }
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		private ZoneServerRegistrationResponse()
+		{
+			
+		}
+	}
+}
