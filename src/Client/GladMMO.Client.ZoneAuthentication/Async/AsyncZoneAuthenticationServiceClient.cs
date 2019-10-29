00f11d0edd466c9dd981e62906de2f178232595b
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 02:52:59 -0500

    Added AsyncZoneAuthenticationServiceClient

---
+++ src/Client/GladMMO.Client.ZoneAuthentication/Async/AsyncZoneAuthenticationServiceClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncZoneAuthenticationServiceClient : BaseAsyncEndpointService<IZoneAuthenticationService>, IZoneAuthenticationService
+	{
+		/// <inheritdoc />
+		public AsyncZoneAuthenticationServiceClient(Task<string> futureEndpoint)
+			: base(futureEndpoint)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public AsyncZoneAuthenticationServiceClient(Task<string> futureEndpoint, RefitSettings settings)
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		public async Task<ZoneServerRegistrationResponse> CreateZoneServerAccount(ZoneServerRegistrationRequest request)
+		{
+			return await (await GetService().ConfigureAwait(false)).CreateZoneServerAccount(request).ConfigureAwait(false);
+		}
+
+		public async Task<JWTModel> TryAuthenticate(AuthenticationRequestModel request)
+		{
+			return await (await GetService().ConfigureAwait(false)).TryAuthenticate(request).ConfigureAwait(false);
+		}
+	}
+}
