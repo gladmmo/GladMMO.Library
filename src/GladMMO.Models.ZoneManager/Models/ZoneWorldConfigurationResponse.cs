224d8a400249695c0131fb8b1ccad471c0aa08cf
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 03:23:33 -0500

    Added ZoneWorldConfigurationResponse

---
+++ src/GladMMO.Models.ZoneManager/Models/ZoneWorldConfigurationResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class ZoneWorldConfigurationResponse
+	{
+		/// <summary>
+		/// The WorldId associated with the zone.
+		/// </summary>
+		[JsonProperty]
+		public int WorldId { get; private set; }
+
+		//We may have more stuff here. Such as name
+		//or instance owner or if there are like configurations
+		//such as speed or who knows what.
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		private ZoneWorldConfigurationResponse()
+		{
+			
+		}
+	}
+}
---
+++ src/GladMMO.Models.ZoneManager/ResultCodes/ZoneWorldConfigurationResponseCode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public enum ZoneWorldConfigurationResponseCode
+	{
+		/// <summary>
+		/// Query successful.
+		/// </summary>
+		Success = 1,
+
+		/// <summary>
+		/// Indicates that there was an unknown general error.
+		/// </summary>
+		GeneralServerError = 2,
+
+		/// <summary>
+		/// Indicates that the requested zone does not exist.
+		/// </summary>
+		ZoneDoesntExist = 3,
+
+		/// <summary>
+		/// Indicates that the client was not authorized to
+		/// see zone world configuration.
+		/// </summary>
+		NotAuthorized = 4,
+	}
+}
