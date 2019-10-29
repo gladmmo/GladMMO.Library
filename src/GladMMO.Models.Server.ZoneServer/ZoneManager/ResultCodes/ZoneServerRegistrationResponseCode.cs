54ae8d30f8925d34c0e955b592f89480957c3a9d
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 04:45:32 -0500

    Added ZoneServerRegistrationResponseCode

---
+++ src/GladMMO.Models.Server.ZoneServer/ZoneManager/ResultCodes/ZoneServerRegistrationResponseCode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Enumeration of response codes associated with <see cref="ZoneServerRegistrationResponse"/>
+	/// </summary>
+	public enum ZoneServerRegistrationResponseCode
+	{
+		/// <summary>
+		/// Indicates the registration was successful.
+		/// </summary>
+		Success = 1,
+
+		GeneralServerError = 2,
+
+		/// <summary>
+		/// Indicates that the zoneserver has already been registered.
+		/// It can only be registered once ever (nothing ensures this yet though)
+		/// </summary>
+		ZoneAlreadyRegistered = 3,
+	}
+}
