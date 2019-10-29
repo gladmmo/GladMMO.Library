3f55e070c77454862fec777bdd1d46940366dcab
HelloKitty <andrew.blakely@ymail.com>
2019-10-28 23:04:32 -0500

    Added GladMMO.Models.ZoneAuthentication proj

---
+++ src/GladMMO.Models.ZoneAuthentication/Models/ZoneServerRegisterationRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class ZoneServerRegistrationRequest
+	{
+		//TODO: Add something here if we ever need it.
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		public ZoneServerRegistrationRequest()
+		{
+			
+		}
+	}
+}
