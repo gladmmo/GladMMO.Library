5d8c88ddaa21a072949b316ad32541809659468b
HelloKitty <andrew.blakely@ymail.com>
2019-10-28 23:15:30 -0500

    Added GladMMOAuthConstants

---
+++ src/GladMMO.Common/GladMMOAuthConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public static class GladMMOAuthConstants
+	{
+		/// <summary>
+		/// JWT claim entry type for account id.
+		/// </summary>
+		public const string ACCOUNT_ID_OWNER_CLAIM_NAME = "vrgid"; //vrgid is VRGuardians User Identifier.
+	}
+}
