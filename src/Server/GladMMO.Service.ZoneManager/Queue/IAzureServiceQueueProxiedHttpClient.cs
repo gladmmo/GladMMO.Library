cf3116f262594d909ef91c4675659b7f85f4a73b
HelloKitty <andrew.blakely@ymail.com>
2019-10-30 01:42:25 -0500

    Added IAzureServiceQueueProxiedHttpClient

---
+++ src/Server/GladMMO.Service.ZoneManager/Queue/IAzureServiceQueueProxiedHttpClient.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Refit;
+using UnityEngine.Scripting;
+
+namespace GladMMO
+{
+	[Headers("User-Agent: AzureServiceBus")]
+	public interface IAzureServiceQueueProxiedHttpClient
+	{
+		[Post("{route}")]
+		[Headers("Content-Type: application/json")]
+		Task SendProxiedPostAsync(string jsonBodyContent, [AliasAs("route")] string requestPath, [Header("Authorization")] string authorizationToken);
+
+		[Patch("{route}")]
+		[Headers("Content-Type: application/json")]
+		Task SendProxiedPatchAsync(string jsonBodyContent, [AliasAs("route")] string requestPath, [Header("Authorization")] string authorizationToken);
+
+		[Put("{route}")]
+		[Headers("Content-Type: application/json")]
+		Task SendProxiedPutAsync(string jsonBodyContent, [AliasAs("route")] string requestPath, [Header("Authorization")] string authorizationToken);
+	}
+}
