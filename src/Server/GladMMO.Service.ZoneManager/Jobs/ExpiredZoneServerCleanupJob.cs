8b57cd4cffc07ba158c4990f2259e6361f6502d2
HelloKitty <andrew.blakely@ymail.com>
2019-10-29 22:30:23 -0500

    Added ExpiredZoneServerCleanupJob

---
+++ src/Server/GladMMO.Service.ZoneManager/Jobs/ExpiredZoneServerCleanupJob.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	public sealed class ExpiredZoneServerCleanupJob : ReOccurringTimedJob
+	{
+		private IZoneServerRepository ZoneServerRepository { get; }
+
+		public ExpiredZoneServerCleanupJob(TimedJobConfig<ExpiredZoneServerCleanupJob> jobConfig, 
+			ILogger<ReOccurringTimedJob> logger,
+			[NotNull] IZoneServerRepository zoneServerRepository) 
+			: base(jobConfig, logger)
+		{
+			ZoneServerRepository = zoneServerRepository ?? throw new ArgumentNullException(nameof(zoneServerRepository));
+		}
+
+		protected override async Task ExecuteJobAsync(CancellationToken cancellationToken)
+		{
+			//All we need to do is cleanup the expired zones
+			try
+			{
+				await ZoneServerRepository.CleanupExpiredZonesAsync(cancellationToken);
+			}
+			catch (Exception e)
+			{
+				if(Logger.IsEnabled(LogLevel.Error))
+					Logger.LogError($"Failed to cleanup expired zones. Reason: {e.ToString()}");
+
+				throw;
+			}
+		}
+	}
+}
