96f4bdc7e44a231107df335d43274f1ab99f4c0b
HelloKitty <andrew.blakely@ymail.com>
2019-10-30 01:57:12 -0500

    Added missing Route value to ProxiedHttpRequestModel

--- src/GladMMO.Models.Common/Models/ProxiedHttpRequestModel.cs
+++ src/GladMMO.Models.Common/Models/ProxiedHttpRequestModel.cs
@@ -26,15 +26,20 @@ namespace GladMMO
 		[JsonProperty]
 		public string AuthorizationToken { get; private set; }
 
-		public ProxiedHttpRequestModel(ProxiedHttpMethod method, [NotNull] string serializedJsonBody, [NotNull] string authorizationToken)
+		[JsonProperty]
+		public string Route { get; private set; }
+
+		public ProxiedHttpRequestModel(ProxiedHttpMethod method, [NotNull] string serializedJsonBody, [NotNull] string authorizationToken, [NotNull] string route)
 		{
 			if(!Enum.IsDefined(typeof(ProxiedHttpMethod), method)) throw new InvalidEnumArgumentException(nameof(method), (int)method, typeof(ProxiedHttpMethod));
 			if (string.IsNullOrWhiteSpace(serializedJsonBody)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(serializedJsonBody));
 			if (string.IsNullOrWhiteSpace(authorizationToken)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(authorizationToken));
+			if (string.IsNullOrWhiteSpace(route)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(route));
 
 			Method = method;
 			SerializedJsonBody = serializedJsonBody;
 			AuthorizationToken = authorizationToken;
+			Route = route;
 		}
 	}
 }
