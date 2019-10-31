6b5cabcd7253f9592d5ff796cbad24cc685dd316
HelloKitty <andrew.blakely@ymail.com>
2019-10-31 02:15:13 -0500

    Added FullCharacterDataSaveRequest

---
+++ src/GladMMO.Models.Server.ZoneServer/Models/FullCharacterDataSaveRequestModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Transferable model of entire savable character data.
+	/// </summary>
+	[JsonObject]
+	public sealed class FullCharacterDataSaveRequest
+	{
+		/// <summary>
+		/// Indicates if the position should be saved.
+		/// </summary>
+		[JsonProperty]
+		public bool isPositionSaved { get; private set; }
+
+		//Always sent for simplicity. Reciever may want to do something with it anyway.
+		//Even if isPositionSaved is false.
+		/// <summary>
+		/// The character location data.
+		/// </summary>
+		[JsonProperty]
+		public ZoneServerCharacterLocationSaveRequest CharacterLocationData { get; private set; }
+
+		/// <summary>
+		/// The raw entity data field collection snapshot in time when the entity despawned.
+		/// </summary>
+		[JsonProperty]
+		public EntityFieldDataCollection PlayerDataSnapshot { get; private set; }
+
+		
+		public FullCharacterDataSaveRequest(bool isPositionSaved, [NotNull] ZoneServerCharacterLocationSaveRequest characterLocationData, [NotNull] EntityFieldDataCollection playerDataSnapshot)
+		{
+			this.isPositionSaved = isPositionSaved;
+			CharacterLocationData = characterLocationData ?? throw new ArgumentNullException(nameof(characterLocationData));
+			PlayerDataSnapshot = playerDataSnapshot ?? throw new ArgumentNullException(nameof(playerDataSnapshot));
+		}
+
+		[JsonConstructor]
+		private FullCharacterDataSaveRequest()
+		{
+			
+		}
+	}
+}
