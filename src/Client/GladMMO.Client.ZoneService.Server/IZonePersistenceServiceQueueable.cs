bc202377e14aabafe4a7609221d46ccb78e58284
HelloKitty <andrew.blakely@ymail.com>
2019-10-31 02:43:40 -0500

    Added IZonePersistenceServiceQueueable

---
+++ src/Client/GladMMO.Client.ZoneService.Server/IZonePersistenceServiceQueueable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	[Headers("User-Agent: AzureServiceBus")]
+	public interface IZonePersistenceServiceQueueable
+	{
+		[RequiresAuthentication]
+		[Patch("/api/ZonePersistence/{id}/data")]
+		Task SaveFullCharacterDataAsync([AliasAs("id")] int characterId, [JsonBody] FullCharacterDataSaveRequest saveRequest);
+
+		[RequiresAuthentication]
+		[Patch("/api/ZonePersistence/{id}/location")]
+		Task SaveCharacterLocation([JsonBody] ZoneServerCharacterLocationSaveRequest saveRequest);
+	}
+}
