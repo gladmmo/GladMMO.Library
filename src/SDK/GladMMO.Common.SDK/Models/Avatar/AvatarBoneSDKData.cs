e2d6dbf12f32b17e3a58283d9e6b1a92049d0bb8
HelloKitty <andrew.blakely@ymail.com>
2019-10-30 22:06:21 -0500

    Added head and floating name high computation to avatar bone data

--- src/SDK/GladMMO.Common.SDK/Models/Avatar/AvatarBoneSDKData.cs
+++ src/SDK/GladMMO.Common.SDK/Models/Avatar/AvatarBoneSDKData.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using GladMMO.FinalIK;
 
 namespace GladMMO.SDK
@@ -18,6 +19,13 @@ namespace GladMMO.SDK
 		[Tooltip("Bone mapping. Right-click on the component header and select 'Auto-detect References' of fill in manually if not a Humanoid character. Chest, neck, shoulder and toe bones are optional. Also supports legless characters. If you do not wish to use legs, leave all leg references empty.")]
 		public CustomVRIKReferences _references = new CustomVRIKReferences();
 
+		[Header("Height settings")]
+		[SerializeField]
+		public float FloatingNameHeight = 1.0f;
+
+		[SerializeField]
+		public float HeadHeight = 1.6f;
+
 		/// <summary>
 		/// Auto-detects bone references for this Avatar. Works with a Humanoid Animator on the gameobject only.
 		/// </summary>
@@ -32,6 +40,29 @@ namespace GladMMO.SDK
 			_references.LocalHeadRotation = _references.head.eulerAngles;
 			_references.LocalLeftHandRotation = _references.leftHand.eulerAngles;
 			_references.LocalRightHandRotation = _references.rightHand.eulerAngles;
+
+			//Approximate the height of the head by the bone, but leave it as configurable for the user.
+			//We also adjust for worldspace height of the root.
+			if (_references.head != null)
+				HeadHeight = _references.head.position.y - gameObject.transform.position.y;
+		}
+
+		[Button]
+		public void ComputeNameHeight()
+		{
+			//This should be the maximum height bounds
+			//of all renderers included in the avatar.
+			FloatingNameHeight = gameObject.GetComponentsInChildren<Renderer>()
+				.Concat(gameObject.GetComponents<Renderer>())
+				.Select(r => r.bounds.max)
+				.Max(vector3 => vector3.y);
+
+			//We should consider the current worldspace height of the root object though.
+			//as this can skew the results if it's not at 0,0,0 etc.
+			FloatingNameHeight -= gameObject.transform.position.y;
+
+			if(FloatingNameHeight > HeadHeight * 3.0f)
+				Debug.LogWarning($"Floating name height is significantly higher than head height. Client may ignore this data.");
 		}
 
 		/// <inheritdoc />
