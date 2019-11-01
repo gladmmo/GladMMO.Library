cf4250080fb1c173dd440d299fb48b4165ce2fc8
HelloKitty <andrew.blakely@ymail.com>
2019-11-01 05:41:18 -0500

    Added CHARACTER_DATABASE_CONNECTION_STRING enviroment variable

--- src/Server/GladMMO.Service.Common/GladMMOServiceConstants.cs
+++ src/Server/GladMMO.Service.Common/GladMMOServiceConstants.cs
@@ -15,5 +15,10 @@ namespace GladMMO
 		/// The environment variable path for the content database connection string.
 		/// </summary>
 		public const string CONTENT_DATABASE_CONNECTION_STRING_ENV_VAR_PATH = "CONTENT_DATABASE_CONNECTION_STRING";
+
+		/// <summary>
+		/// The environment variable path for the character database connection string.
+		/// </summary>
+		public const string CHARACTER_DATABASE_CONNECTION_STRING_ENV_VAR_PATH = "CHARACTER_DATABASE_CONNECTION_STRING";
 	}
 }
