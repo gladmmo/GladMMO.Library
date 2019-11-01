89c4e2593a79b5545d139afd1d5e0e59a7d04b3d
HelloKitty <andrew.blakely@ymail.com>
2019-11-01 01:05:39 -0500

    Added GladMMOServiceConstants

---
+++ src/Server/GladMMO.Service.Common/GladMMOServiceConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public static class GladMMOServiceConstants
+	{
+		/// <summary>
+		/// The environment variable path for the auth database connection string.
+		/// </summary>
+		public const string AUTHENTICATION_DATABASE_CONNECTION_STRING_ENV_VAR_PATH = "AUTHENTICATION_DATABASE_CONNECTION_STRING";
+	}
+}
