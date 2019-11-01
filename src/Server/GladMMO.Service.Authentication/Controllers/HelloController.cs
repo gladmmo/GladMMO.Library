fbcef7d8c65cffcb868b50a3d3818995c79918d8
HelloKitty <andrew.blakely@ymail.com>
2019-10-31 22:40:49 -0500

    Added HelloController to Auth service for Azure test

---
+++ src/Server/GladMMO.Service.Authentication/Controllers/HelloController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GladMMO
+{
+	//Queryable endpoint for browser-level online status testing.
+	[Route("api/[controller]")]
+	public class HelloController : Controller
+	{
+		[ResponseCache]
+		[HttpGet]
+		public Task<string> Hello()
+		{
+			return Task.FromResult("Hello");
+		}
+	}
+}
