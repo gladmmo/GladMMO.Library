55f9ad1e4827b022e15612d152c6568fa7afe879
HelloKitty <andrew.blakely@ymail.com>
2019-11-03 05:47:36 -0600

    Added RegisterAccountButtonClickedGlueStartable

---
+++ src/Client/GladMMO.Client.Game/Engine/Title/Initializables/RegisterAccountButtonClickedGlueStartable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(IRegisterAccountButtonClickedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.TitleScreen)]
+	public sealed class RegisterAccountButtonClickedGlueStartable : BaseButtonClickGlueStartable, IRegisterAccountButtonClickedEventSubscribable
+	{
+		public const UnityUIRegisterationKey ButtonKey = UnityUIRegisterationKey.Registeration;
+
+		public RegisterAccountButtonClickedGlueStartable([KeyFilter(ButtonKey)] [NotNull] IUIButton createButton) 
+			: base(createButton)
+		{
+
+		}
+	}
+}
