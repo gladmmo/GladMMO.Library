930e54fc80900642fb9014d3f33e74140c5b4b5c
HelloKitty <andrew.blakely@ymail.com>
2019-11-03 01:21:10 -0500

    Added DeploymentModeConfigurationComponent

---
+++ src/Client/GladMMO.Client.Game/Component/DeploymentModeConfigurationComponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class DeploymentModeConfigurationComponent : MonoBehaviour
+	{
+		private static DeploymentModeConfigurationComponent Instance;
+
+		private static readonly Lazy<DeploymentMode> LazyInitializedMode;
+
+		static DeploymentModeConfigurationComponent()
+		{
+			LazyInitializedMode = new Lazy<DeploymentMode>(() => DeploymentModeConfigurationComponent.Instance._Mode);
+		}
+
+		public static DeploymentMode Mode => LazyInitializedMode.Value;
+
+		[SerializeField]
+		private DeploymentMode _Mode;
+
+		public DeploymentModeConfigurationComponent()
+		{
+			Instance = this;
+		}
+	}
+}
