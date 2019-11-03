25c328b8dabfba8eb24b35ad1a671edc9bd77145
HelloKitty <andrew.blakely@ymail.com>
2019-11-03 08:47:25 -0600

    Added new Akka EntityActorInitializationSuccessMessage

---
+++ src/Server/GladMMO.Server.Akka/Messages/EntityActorInitializationSuccessMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Empty message that indicates an Entity actor's state has been successfully initialized.
+	/// Entity actors can used this to do some specialized initialization here
+	/// or they can startup some long running logic or AI.
+	/// </summary>
+	public sealed class EntityActorInitializationSuccessMessage : EntityActorMessage
+	{
+		public EntityActorInitializationSuccessMessage()
+		{
+			
+		}
+	}
+}
