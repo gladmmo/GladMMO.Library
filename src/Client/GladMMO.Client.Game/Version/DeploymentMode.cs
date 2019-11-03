974f54af6603124cd21a9c5ada23eb26ae957943
HelloKitty <andrew.blakely@ymail.com>
2019-11-03 01:16:34 -0500

    Added DeploymentMode enum

---
+++ src/Client/GladMMO.Client.Game/Version/DeploymentMode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Enumeration of client deployment modes.
+	/// </summary>
+	public enum DeploymentMode
+	{
+		/// <summary>
+		/// Indicates the client is deployed locally and will connect to local services.
+		/// </summary>
+		Local = 1,
+
+		/// <summary>
+		/// Indicates the client is deployed to connect to Azure's GladMMO test servers.
+		/// </summary>
+		AzureTest = 2,
+
+		/// <summary>
+		/// Indicates the client is deployed to connect to Azure's GladMMO production servers.
+		/// </summary>
+		AzureProduction = 3,
+	}
+}
