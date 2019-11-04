eddeadfcfd4b0f862c1e673361d55ee42fa88d96
HelloKitty <andrew.blakely@ymail.com>
2019-11-04 01:48:33 -0600

    Added InitializeUnityEngineConfigurationInitializable

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/Initializables/InitializeUnityEngineConfigurationInitializable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class InitializeUnityEngineConfigurationInitializable : IGameInitializable
+	{
+		public async Task OnGameInitialized()
+		{
+			//Get onto the main thread.
+			await new UnityYieldAwaitable();
+			Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.None);
+		}
+	}
+}
