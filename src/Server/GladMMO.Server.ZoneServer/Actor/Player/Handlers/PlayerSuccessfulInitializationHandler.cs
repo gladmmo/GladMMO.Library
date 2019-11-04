bc4d24655e545f9d0ac7edc0cada535ebe5f40f1
HelloKitty <andrew.blakely@ymail.com>
2019-11-04 08:10:58 -0600

    Added PlayerSuccessfulInitializationHandler for Experience init

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Player/Handlers/PlayerSuccessfulInitializationHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Common.Logging;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	public sealed class PlayerSuccessfulInitializationHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, EntityActorInitializationSuccessMessage>
+	{
+		private ILog Logger { get; }
+
+		private IReadonlyEntityGuidMappable<CharacterDataInstance> InitialCharacterDataMappable { get; }
+
+		private IEntityExperienceLevelStrategy LevelStrategy { get; }
+
+		public PlayerSuccessfulInitializationHandler([NotNull] ILog logger,
+			[NotNull] IReadonlyEntityGuidMappable<CharacterDataInstance> initialCharacterDataMappable,
+			[NotNull] IEntityExperienceLevelStrategy levelStrategy)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			InitialCharacterDataMappable = initialCharacterDataMappable ?? throw new ArgumentNullException(nameof(initialCharacterDataMappable));
+			LevelStrategy = levelStrategy ?? throw new ArgumentNullException(nameof(levelStrategy));
+		}
+
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, EntityActorInitializationSuccessMessage message)
+		{
+			//We should send ourselves the level initialization message because it takes care of stats
+			CharacterDataInstance entity = InitialCharacterDataMappable.RetrieveEntity(state.EntityGuid);
+			IEntityDataFieldContainer entityDataFieldContainer = state.EntityData;
+
+			entityDataFieldContainer.SetFieldValue((int)PlayerObjectField.PLAYER_TOTAL_EXPERIENCE, entity.Experience);
+
+			//Set the level from the initial experience data.
+			messageContext.Entity.Tell(new SetEntityActorLevelMessage(LevelStrategy.ComputeLevelFromExperience(entity.Experience)));
+		}
+	}
+}
