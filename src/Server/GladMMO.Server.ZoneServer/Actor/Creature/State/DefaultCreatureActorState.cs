d51ba6a435a7b1ee9e686603425d8ba8c637f1e4
HelloKitty <andrew.blakely@ymail.com>
2019-11-04 07:18:36 -0600

    Added DefaultCreatureActorState Akka Actor State

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Creature/State/DefaultCreatureActorState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public class DefaultCreatureActorState : DefaultEntityActorStateContainer
+	{
+		public CreatureInstanceModel InstanceModel { get; }
+
+		public CreatureTemplateModel TemplateModel { get; }
+
+		public DefaultCreatureActorState([NotNull] IEntityDataFieldContainer entityData,
+			[NotNull] NetworkEntityGuid entityGuid,
+			[NotNull] CreatureInstanceModel instanceModel,
+			[NotNull] CreatureTemplateModel templateModel)
+			: base(entityData, entityGuid)
+		{
+			InstanceModel = instanceModel ?? throw new ArgumentNullException(nameof(instanceModel));
+			TemplateModel = templateModel ?? throw new ArgumentNullException(nameof(templateModel));
+		}
+	}
+}
