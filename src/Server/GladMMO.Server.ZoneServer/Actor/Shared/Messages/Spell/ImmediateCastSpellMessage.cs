152e70416f6352569a13efa10e0baa00665fb96c
HelloKitty <andrew.blakely@ymail.com>
2019-11-04 23:40:03 -0600

    Added ImmediateCastSpellMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/Spell/ImmediateCastSpellMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Message for an entity actor that should begin a cast
+	/// of a spell immediately.
+	/// </summary>
+	public sealed class ImmediateCastSpellMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The ID of the spell to cast.
+		/// </summary>
+		public int SpellId { get; private set; }
+
+		public ImmediateCastSpellMessage(int spellId)
+		{
+			if(spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			SpellId = spellId;
+		}
+	}
+}
