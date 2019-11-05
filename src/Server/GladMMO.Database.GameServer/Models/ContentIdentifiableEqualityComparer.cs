bed1a132c9a68151110bb0b0f5efd114ee080f19
HelloKitty <andrew.blakely@ymail.com>
2019-11-05 15:19:49 -0600

    Added ContentIdentifiableEqualityComparer

---
+++ src/Server/GladMMO.Database.GameServer/Models/ContentIdentifiableEqualityComparer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public class ContentIdentifiableEqualityComparer<TContentType> : IEqualityComparer<TContentType>
+		where TContentType : IContentIdentifiable
+	{
+		public bool Equals(TContentType x, TContentType y)
+		{
+			if(x == null)
+				return y == null;
+			else if(y == null)
+				return false;
+			else
+				return x.ContentId == y.ContentId;
+		}
+
+		public int GetHashCode(TContentType obj)
+		{
+			if(obj == null)
+				return -1;
+
+			return obj.ContentId;
+		}
+	}
+}
