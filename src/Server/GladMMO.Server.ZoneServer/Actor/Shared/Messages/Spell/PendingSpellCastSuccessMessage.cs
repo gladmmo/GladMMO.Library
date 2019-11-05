baf36772c30265ae2c69636406d9b440dce76fcc
HelloKitty <andrew.blakely@ymail.com>
2019-11-04 23:14:03 -0600

    Added PendingSpellCastSuccessMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/Spell/PendingSpellCastSuccessMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Message that indicates the successful cast of a pending spell.
+	/// </summary>
+	public sealed class PendingSpellCastSuccessMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The pending spell cast data.
+		/// </summary>
+		public PendingSpellCastData Pending { get; private set; }
+
+		public PendingSpellCastSuccessMessage([NotNull] PendingSpellCastData pending)
+		{
+			Pending = pending ?? throw new ArgumentNullException(nameof(pending));
+		}
+	}
+}
