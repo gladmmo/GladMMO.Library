446df31f9b811535422dad300dbbe6e50f309fb5
HelloKitty <andrew.blakely@ymail.com>
2019-11-04 21:57:51 -0600

    Added debug/test EntityTrySpellCastMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/EntityTrySpellCastMessageHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultCreatureEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	public sealed class EntityTrySpellCastMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, TryCastSpellMessage>
+	{
+		private IReadonlyNetworkTimeService TimeService { get; }
+
+		public EntityTrySpellCastMessageHandler([NotNull] INetworkTimeService timeService)
+		{
+			TimeService = timeService ?? throw new ArgumentNullException(nameof(timeService));
+		}
+
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, TryCastSpellMessage message)
+		{
+			//This is just a debug/testing implementation.
+			//TODO: Eventually we need a pending cast system.
+			//TODO: Check they are not casting or can cast the spell.
+			//These must both be set at the same time, to avoid dispatching in seperate field updates.
+			lock (state.EntityData.SyncObj)
+			{
+				state.EntityData.SetFieldValue(EntityObjectField.UNIT_FIELD_CASTING_SPELLID, message.SpellId);
+				state.EntityData.SetFieldValue(EntityObjectField.UNIT_FIELD_CASTING_STARTTIME, TimeService.CurrentLocalTime);
+			}
+		}
+	}
+}
