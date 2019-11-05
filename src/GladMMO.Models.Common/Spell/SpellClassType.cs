cfeee73898bf6e6e7a8623e01b4ba76fa8971c5c
HelloKitty <andrew.blakely@ymail.com>
2019-11-05 12:22:39 -0600

    Added SpellClassType

---
+++ src/GladMMO.Models.Common/Spell/SpellClassType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	//Based on WoW.
+	//See: https://raw.githubusercontent.com/FreecraftCore/FreecraftCore.DBCTools/7600c69732d9c31f0762304cd3ab8757a2859314/src/FreecraftCore.DBC.Common/Spell/SpellDamageClassType.cs
+	public enum SpellClassType
+	{
+		Normal = 0,
+		Magic = 1,
+		Melee = 2,
+		Ranged = 3
+	};
+}
