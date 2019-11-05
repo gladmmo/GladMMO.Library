1f4b5f7025c13a5e5c4e6bd54020c6643d5fc08e
HelloKitty <andrew.blakely@ymail.com>
2019-11-04 20:36:52 -0600

    Added TryCastSpellMessage Akka message

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/Spell/TryCastSpellMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Message for an entity actor that requests it cast a spell.
+	/// There is no promise the spell will go off.
+	/// </summary>
+	public sealed class TryCastSpellMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The ID of the requested spell to cast.
+		/// </summary>
+		public int SpellId { get; private set; }
+
+		public TryCastSpellMessage(int spellId)
+		{
+			if(spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			SpellId = spellId;
+		}
+	}
+}
