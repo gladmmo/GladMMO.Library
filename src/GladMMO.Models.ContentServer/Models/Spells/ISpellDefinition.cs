99e4e5e0c1f9c8deb0c7091ca8fb8815ee6c82df
HelloKitty <andrew.blakely@ymail.com>
2019-11-05 15:43:33 -0600

    Added ISpellDefinition in ContentServer models proj

---
+++ src/GladMMO.Models.ContentServer/Models/Spells/ISpellDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ISpellDefinition
+	{
+		int SpellId { get; }
+
+		//We don't make this unique because users may want to have
+		//spells of differing ids but with the same name, they might be different spells.
+		string SpellName { get; }
+
+		/// <summary>
+		/// Indicates the type of spell: Spell, melee or ranged.
+		/// </summary>
+		SpellClassType SpellType { get; }
+
+		/// <summary>
+		/// Cast time in milliseconds.
+		/// </summary>
+		int CastTime { get; }
+
+		/// <summary>
+		/// The ID of the first spell effect.
+		/// </summary>
+		int SpellEffectIdOne { get; }
+	}
+}
