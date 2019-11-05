de967a4bb3fc65bf4f46b62a1d67e8691237fe6c
HelloKitty <andrew.blakely@ymail.com>
2019-11-05 01:05:21 -0600

    Added some spellcast debug testing

--- src/Client/GladMMO.Client.Game/DebugComponents/DebugTestModelChangeRequest.cs
+++ src/Client/GladMMO.Client.Game/DebugComponents/DebugTestModelChangeRequest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac.Features.AttributeFilters;
 using Glader.Essentials;
 using GladNet;
 using UnityEngine;
@@ -13,9 +14,18 @@ namespace GladMMO
 	{
 		private IPeerPayloadSendService<GameClientPacketPayload> SendService { get; }
 
-		public DebugTestModelChangeRequest([NotNull] IPeerPayloadSendService<GameClientPacketPayload> sendService)
+		private IUIButton TestButton { get; }
+
+		public DebugTestModelChangeRequest([NotNull] IPeerPayloadSendService<GameClientPacketPayload> sendService, 
+			[KeyFilter(UnityUIRegisterationKey.LocalPlayerCastBar)] IUIButton testButton)
 		{
 			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+			TestButton = testButton;
+
+			testButton.AddOnClickListener(() =>
+			{
+				SendService.SendMessage(new SpellCastRequestPayload(1));
+			});
 		}
 
 		public void Tick()
@@ -25,6 +35,11 @@ namespace GladMMO
 
 			//if(Input.GetKeyDown(KeyCode.J))
 			//	SendService.SendMessage(new PlayerModelChangeRequestPayload(7));
+
+			if (Input.GetKeyDown(KeyCode.L))
+			{
+				SendService.SendMessage(new SpellCastRequestPayload(1));
+			}
 		}
 	}
 }
