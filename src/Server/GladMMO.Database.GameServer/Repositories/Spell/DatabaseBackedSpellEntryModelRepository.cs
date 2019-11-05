af4de9fdd1641133ad85c289463c84f1c4d225da
HelloKitty <andrew.blakely@ymail.com>
2019-11-05 14:18:32 -0600

    Added ISpellEntryRepository

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Spell/DatabaseBackedSpellEntryModelRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	public sealed class DatabaseBackedSpellEntryModelRepository : BaseGenericBackedDatabaseRepository<ContentDatabaseContext, int, SpellEntryModel>, ISpellEntryRepository
+	{
+		public DatabaseBackedSpellEntryModelRepository(ContentDatabaseContext databaseContext) 
+			: base(databaseContext)
+		{
+
+		}
+
+		public Task<SpellEntryModel[]> RetrieveAllDefaultAsync(CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return Context
+				.SpellEntries
+				.Where(e => e.isDefault)
+				.ToArrayAsync(cancellationToken);
+		}
+	}
+}
---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Spell/ISpellEntryRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public interface ISpellEntryRepository : IGenericRepositoryCrudable<int, SpellEntryModel>
+	{
+		Task<SpellEntryModel[]> RetrieveAllDefaultAsync(CancellationToken cancellationToken = default(CancellationToken));
+	}
+}
