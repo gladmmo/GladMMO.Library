35cf99f2e56a36bd9e5a19b5d93cfb5ed665995f
HelloKitty <andrew.blakely@ymail.com>
2019-11-05 01:04:26 -0600

    Added EntityImmediateCastSpellMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/Spell/EntityImmediateCastSpellMessageHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	//This is all demo code.
+	[EntityActorMessageHandler(typeof(DefaultCreatureEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	public sealed class EntityImmediateCastSpellMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, ImmediateCastSpellMessage>
+	{
+		private IReadonlyEntityGuidMappable<IActorRef> ActorReferenceMappable { get; }
+
+		public EntityImmediateCastSpellMessageHandler([NotNull] IReadonlyEntityGuidMappable<IActorRef> actorReferenceMappable)
+		{
+			ActorReferenceMappable = actorReferenceMappable ?? throw new ArgumentNullException(nameof(actorReferenceMappable));
+		}
+
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, ImmediateCastSpellMessage message)
+		{
+			//TODO: This is just demo code.
+			//TODO: Handle targeting at the spell level.
+			NetworkEntityGuid targetGuid = state.EntityData.GetEntityGuidValue(EntityObjectField.UNIT_FIELD_TARGET);
+
+			if (targetGuid.isEmpty)
+				return;
+
+			if (!ActorReferenceMappable.ContainsKey(targetGuid))
+				return;
+
+			IActorRef targetActor = ActorReferenceMappable.RetrieveEntity(targetGuid);
+
+			targetActor.Tell(new DamageEntityActorCurrentHealthMessage(1));
+		}
+	}
+}
