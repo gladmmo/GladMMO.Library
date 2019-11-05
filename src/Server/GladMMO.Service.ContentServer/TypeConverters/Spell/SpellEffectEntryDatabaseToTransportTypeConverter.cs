e04598813aa6e25f9d41a13b9e1c89d60eb4acd1
HelloKitty <andrew.blakely@ymail.com>
2019-11-05 17:05:49 -0600

    Added SpellEffectEntryDatabaseToTransportTypeConverter

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/Spell/SpellEffectEntryDatabaseToTransportTypeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class SpellEffectEntryDatabaseToTransportTypeConverter : ITypeConverterProvider<SpellEffectEntryModel, SpellEffectDefinitionDataModel>
+	{
+		public SpellEffectDefinitionDataModel Convert([NotNull] SpellEffectEntryModel effect)
+		{
+			if (effect == null) throw new ArgumentNullException(nameof(effect));
+
+			return new SpellEffectDefinitionDataModel(effect.SpellEffectId, effect.EffectType, effect.BasePointsAdditiveLevelModifier, effect.EffectBasePoints, effect.EffectPointsDiceRange, effect.EffectTargetingType, effect.AdditionalEffectTargetingType);
+		}
+	}
+}
