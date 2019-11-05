943bd3d8680d745eea68d30766fc524e65f5c218
HelloKitty <andrew.blakely@ymail.com>
2019-11-05 16:28:44 -0600

    Added SpellDefinitionDataModel

---
+++ src/GladMMO.Models.ContentServer/Models/Spells/SpellDefinitionDataModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class SpellDefinitionDataModel : ISpellDefinition
+	{
+		[JsonProperty]
+		public int SpellId { get; private set; }
+
+		[JsonProperty]
+		public string SpellName { get; private set; }
+
+		[JsonProperty]
+		public SpellClassType SpellType { get; private set; }
+
+		[JsonProperty]
+		public int CastTime { get; private set; }
+
+		[JsonProperty]
+		public int SpellEffectIdOne { get; private set; }
+
+		public SpellDefinitionDataModel(int spellId, string spellName, SpellClassType spellType, int castTime, int spellEffectIdOne)
+		{
+			if (spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+			if (spellEffectIdOne < 0) throw new ArgumentOutOfRangeException(nameof(spellEffectIdOne));
+			if (castTime < 0) throw new ArgumentOutOfRangeException(nameof(castTime));
+			if (!Enum.IsDefined(typeof(SpellClassType), spellType)) throw new InvalidEnumArgumentException(nameof(spellType), (int) spellType, typeof(SpellClassType));
+
+			SpellId = spellId;
+			SpellName = spellName;
+			SpellType = spellType;
+			CastTime = castTime;
+			SpellEffectIdOne = spellEffectIdOne;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		private SpellDefinitionDataModel()
+		{
+			
+		}
+	}
+}
