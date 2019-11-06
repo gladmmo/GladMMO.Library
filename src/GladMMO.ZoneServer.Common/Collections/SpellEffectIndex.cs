1b719e5f6b41f1901b21faf2206d87ee7b90da0c
HelloKitty <andrew.blakely@ymail.com>
2019-11-06 17:25:17 -0600

    Added SpellEffectIndex

---
+++ src/GladMMO.ZoneServer.Common/Collections/SpellEffectIndex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public enum SpellEffectIndex
+	{
+		SpellEffectIndexOne = 0,
+
+		SpellEffectIndexTwo = 1,
+
+		SpellEffectIndexThree = 2
+	}
+}
