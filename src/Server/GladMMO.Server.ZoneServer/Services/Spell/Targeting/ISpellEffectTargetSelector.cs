247cad22596358c9ee7d3f0a495ba35eb46242a4
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 13:18:55 -0600

    Added ISpellEffectTargetSelector

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/ISpellEffectTargetSelector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ISpellEffectTargetSelector
+	{
+		SpellEffectTargetContext CalculateTargets(SpellDefinitionDataModel spellDefinition, SpellEffectDefinitionDataModel spellEffect, DefaultEntityActorStateContainer actorState);
+	}
+}
