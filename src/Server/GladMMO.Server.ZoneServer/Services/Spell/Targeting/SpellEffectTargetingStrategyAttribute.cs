df2555fc676892021fdf52a0b113f2d973532c23
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 09:56:56 -0600

    Added SpellEffectTargetingStrategyAttribute

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/SpellEffectTargetingStrategyAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GladMMO
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+	public sealed class SpellEffectTargetingStrategyAttribute : Attribute
+	{
+		public SpellEffectTargetType EffectTargetingType { get; private set; }
+
+		public SpellEffectTargetType AdditionalEffectTargetingType { get; private set; }
+
+		public SpellEffectTargetingStrategyAttribute(SpellEffectTargetType effectTargetingType, SpellEffectTargetType additionalEffectTargetingType)
+		{
+			if (!Enum.IsDefined(typeof(SpellEffectTargetType), effectTargetingType)) throw new InvalidEnumArgumentException(nameof(effectTargetingType), (int) effectTargetingType, typeof(SpellEffectTargetType));
+			if (!Enum.IsDefined(typeof(SpellEffectTargetType), additionalEffectTargetingType)) throw new InvalidEnumArgumentException(nameof(additionalEffectTargetingType), (int) additionalEffectTargetingType, typeof(SpellEffectTargetType));
+
+			EffectTargetingType = effectTargetingType;
+			AdditionalEffectTargetingType = additionalEffectTargetingType;
+		}
+	}
+}
