59e994b8e753a08985368a92f9d68a0c5b431c0e
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 14:03:18 -0600

    Added BaseTargetSpellEffectSelector

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/Selectors/BaseTargetSpellEffectSelector.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public abstract class BaseTargetSpellEffectSelector : ISpellEffectTargetSelector
+	{
+		public abstract SpellEffectTargetContext CalculateTargets(SpellDefinitionDataModel spellDefinition, SpellEffectDefinitionDataModel spellEffect, DefaultEntityActorStateContainer actorState)
+
+		protected NetworkEntityGuid GetEntityTarget([NotNull] DefaultEntityActorStateContainer state)
+		{
+			if(state == null) throw new ArgumentNullException(nameof(state));
+
+			return state.EntityData.GetEntityGuidValue(EntityObjectField.UNIT_FIELD_TARGET); ;
+		}
+	}
+}
