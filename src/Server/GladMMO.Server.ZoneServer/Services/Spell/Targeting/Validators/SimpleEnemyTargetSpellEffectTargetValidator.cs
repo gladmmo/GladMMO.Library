431430d69a285b0baa890b3992c51b3d7e4b2d5a
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 12:50:38 -0600

    Added better target validation for single target enemy

--- src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/Validators/SimpleEnemyTargetSpellEffectTargetValidator.cs
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/Validators/SimpleEnemyTargetSpellEffectTargetValidator.cs
@@ -7,6 +7,13 @@ namespace GladMMO
 	[SpellEffectTargetingStrategy(SpellEffectTargetType.TARGET_UNIT_TARGET_ENEMY, SpellEffectTargetType.NO_TARGET)]
 	public sealed class SimpleEnemyTargetSpellEffectTargetValidator : BaseTargetSpellEffectValidator
 	{
+		private IReadonlyKnownEntitySet KnownEntitySet { get; }
+
+		public SimpleEnemyTargetSpellEffectTargetValidator([NotNull] IReadonlyKnownEntitySet knownEntitySet)
+		{
+			KnownEntitySet = knownEntitySet ?? throw new ArgumentNullException(nameof(knownEntitySet));
+		}
+
 		protected override bool ValidateTargeting(SpellDefinitionDataModel spellDefinition, SpellEffectDefinitionDataModel spellEffect, DefaultEntityActorStateContainer actorState)
 		{
 			if(spellDefinition == null) throw new ArgumentNullException(nameof(spellDefinition));
@@ -15,8 +22,13 @@ namespace GladMMO
 
 			NetworkEntityGuid guid = GetEntityTarget(actorState);
 
+			//Does the entity exist, small window of time between valid cast start and now where entity can despawn
+			//so minor chance it doesn't exist anymore.
+			if (!KnownEntitySet.isEntityKnown(guid))
+				return false;
+
 			//TODO: We shouldn't assume they are enemies just because they aren't use. We need a system for hostility masking for entities
-			return guid != actorState.EntityGuid;
+			return guid != NetworkEntityGuid.Empty && guid != actorState.EntityGuid;
 		}
 	}
 }
