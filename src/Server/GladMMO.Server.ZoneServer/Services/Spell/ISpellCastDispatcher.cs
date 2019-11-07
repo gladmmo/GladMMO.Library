97d072f055de05ac13ec7edaae4b48c16e30c4cc
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 13:42:52 -0600

    Added ISpellCastDispatcher

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/ISpellCastDispatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for types that can dispatch a spell cast.
+	/// </summary>
+	public interface ISpellCastDispatcher
+	{
+		/// <summary>
+		/// Dispatches the provided prepared <see cref="pendingSpellCast"/> data.
+		/// </summary>
+		/// <param name="pendingSpellCast">The pending spell to cast.</param>
+		void DispatchSpellCast(IPendingSpellCastData pendingSpellCast);
+	}
+}
