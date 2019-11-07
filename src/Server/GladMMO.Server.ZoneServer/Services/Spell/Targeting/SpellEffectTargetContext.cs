f1fecbe60effec08f1621dd641c32cb9117cf020
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 13:11:01 -0600

    Added SpellEffectTargetContext

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/SpellEffectTargetContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	public sealed class SpellEffectTargetContext
+	{
+		public IEnumerable<IActorRef> SpellEffectTargets { get; }
+
+		public SpellEffectTargetContext([NotNull] IEnumerable<IActorRef> spellEffectTargets)
+		{
+			SpellEffectTargets = spellEffectTargets ?? throw new ArgumentNullException(nameof(spellEffectTargets));
+		}
+	}
+}
