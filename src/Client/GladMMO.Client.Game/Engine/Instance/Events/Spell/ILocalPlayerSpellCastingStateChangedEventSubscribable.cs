057963291aa488a8a5a1d5dfe323dac693f42d7a
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 16:29:02 -0600

    Added CastingStartTimeStamp to SpellCastingStateChange event

--- src/Client/GladMMO.Client.Game/Engine/Instance/Events/Spell/ILocalPlayerSpellCastingStateChangedEventSubscribable.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Spell/ILocalPlayerSpellCastingStateChangedEventSubscribable.cs
@@ -11,15 +11,26 @@ namespace GladMMO
 
 	public sealed class SpellCastingStateChangedEventArgs : EventArgs
 	{
+		/// <summary>
+		/// Indicates the timestamp of the change if <see cref="isCasting"/> is true.
+		/// Potentially 0 if <see cref="isCasting"/> is false.
+		/// </summary>
+		public long CastingStartTimeStamp { get; }
+
+		/// <summary>
+		/// The spell id. If <see cref="isCasting"/> is false then 0.
+		/// </summary>
 		public int CastingSpellId { get; }
 
 		public bool isCasting => CastingSpellId != 0;
 
-		public SpellCastingStateChangedEventArgs(int castingSpellId)
+		public SpellCastingStateChangedEventArgs(int castingSpellId, long castingTimeStamp)
 		{
 			if (castingSpellId < 0) throw new ArgumentOutOfRangeException(nameof(castingSpellId));
+			if (castingTimeStamp < 0) throw new ArgumentOutOfRangeException(nameof(castingTimeStamp));
 
 			CastingSpellId = castingSpellId;
+			CastingStartTimeStamp = castingTimeStamp;
 		}
 	}
 }
