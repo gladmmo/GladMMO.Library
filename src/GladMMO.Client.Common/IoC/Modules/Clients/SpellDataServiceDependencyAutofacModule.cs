87f0ba1073f79d2e37ddf68d0ee7d824b8033f1d
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 07:12:39 -0600

    Added SpellDataServiceDependencyAutofacModule

---
+++ src/GladMMO.Client.Common/IoC/Modules/Clients/SpellDataServiceDependencyAutofacModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class SpellDataServiceDependencyAutofacModule : NetworkServiceDiscoveryableAutofaceModule
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.Register<ISpellEntryDataServiceClient>(context =>
+			{
+				IServiceDiscoveryService serviceDiscovery = context.Resolve<IServiceDiscoveryService>();
+				IReadonlyAuthTokenRepository tokenRepository = context.Resolve<IReadonlyAuthTokenRepository>();
+
+				return new AsyncSpellEntryDataServiceClient(QueryForRemoteServiceEndpoint(serviceDiscovery, "ContentServer"), new RefitSettings() { HttpMessageHandlerFactory = () => new AuthenticatedHttpClientHandler(tokenRepository) });
+			});
+		}
+	}
+}
