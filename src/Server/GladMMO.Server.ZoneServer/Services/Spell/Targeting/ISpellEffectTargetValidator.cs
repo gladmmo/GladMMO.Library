0b2f0fd9cab9126e0558b8fdbd2665c968e576d2
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 09:57:05 -0600

    Added ISpellEffectTargetValidator

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/ISpellEffectTargetValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ISpellEffectTargetValidator
+	{
+		bool ValidateTargetContext(SpellDefinitionDataModel spellDefinition, SpellEffectDefinitionDataModel spellEffect, DefaultEntityActorStateContainer actorState);
+	}
+}
