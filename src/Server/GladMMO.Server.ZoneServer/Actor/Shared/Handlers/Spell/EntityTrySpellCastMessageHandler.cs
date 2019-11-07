8bb80e0a3f14975a4e16e23fd813193ad73d86b1
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 10:31:49 -0600

    Added spell target validation for EntityTrySpellCastMessageHandler

--- src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/Spell/EntityTrySpellCastMessageHandler.cs
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/Spell/EntityTrySpellCastMessageHandler.cs
@@ -14,13 +14,17 @@ namespace GladMMO
 
 		private IPendingSpellCastFactory PendingSpellFactory { get; }
 
+		private ISpellTargetValidator TargetValidator { get; }
+
 		public EntityTrySpellCastMessageHandler([NotNull] INetworkTimeService timeService,
 			[NotNull] IEntityGuidMappable<PendingSpellCastData> pendingSpellCastMappable,
-			[NotNull] IPendingSpellCastFactory pendingSpellFactory)
+			[NotNull] IPendingSpellCastFactory pendingSpellFactory,
+			[NotNull] ISpellTargetValidator targetValidator)
 		{
 			TimeService = timeService ?? throw new ArgumentNullException(nameof(timeService));
 			PendingSpellCastMappable = pendingSpellCastMappable ?? throw new ArgumentNullException(nameof(pendingSpellCastMappable));
 			PendingSpellFactory = pendingSpellFactory ?? throw new ArgumentNullException(nameof(pendingSpellFactory));
+			TargetValidator = targetValidator ?? throw new ArgumentNullException(nameof(targetValidator));
 		}
 
 		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, TryCastSpellMessage message)
@@ -36,14 +40,39 @@ namespace GladMMO
 				}
 			}
 
+			//TODO: Send packet spell response.
+			if (!TargetValidator.isSpellTargetViable(message.SpellId, state))
+				return;
+
+			PendingSpellCastData castData = CreatePendingSpellData(state, message);
+
+			SetCastingEntityState(state, message);
+
+			DispatchPendingSpellCast(messageContext, castData);
+		}
+
+		private PendingSpellCastData CreatePendingSpellData(DefaultEntityActorStateContainer state, TryCastSpellMessage message)
+		{
 			PendingSpellCastData castData = PendingSpellFactory.Create(new PendingSpellCastCreationContext(message.SpellId));
 
 			if (PendingSpellCastMappable.ContainsKey(state.EntityGuid))
 				PendingSpellCastMappable.ReplaceObject(state.EntityGuid, castData);
 			else
 				PendingSpellCastMappable.AddObject(state.EntityGuid, castData);
+			return castData;
+		}
 
+		private static void DispatchPendingSpellCast(EntityActorMessageContext messageContext, PendingSpellCastData castData)
+		{
+			//Instant casts can just directly send.
+			if (castData.isInstantCast)
+				messageContext.Entity.TellSelf(new PendingSpellCastFinishedWaitingMessage(castData));
+			else
+				messageContext.ContinuationScheduler.ScheduleTellOnce(castData.CastTime, messageContext.Entity, new PendingSpellCastFinishedWaitingMessage(castData), messageContext.Entity);
+		}
 
+		private void SetCastingEntityState(DefaultEntityActorStateContainer state, TryCastSpellMessage message)
+		{
 			//This is just a debug/testing implementation.
 			//TODO: Eventually we need a pending cast system.
 			//TODO: Check they are not casting or can cast the spell.
@@ -53,12 +82,6 @@ namespace GladMMO
 				state.EntityData.SetFieldValue(EntityObjectField.UNIT_FIELD_CASTING_SPELLID, message.SpellId);
 				state.EntityData.SetFieldValue(EntityObjectField.UNIT_FIELD_CASTING_STARTTIME, TimeService.CurrentLocalTime);
 			}
-
-			//Instant casts can just directly send.
-			if (castData.isInstantCast)
-				messageContext.Entity.TellSelf(new PendingSpellCastFinishedWaitingMessage(castData));
-			else
-				messageContext.ContinuationScheduler.ScheduleTellOnce(castData.CastTime, messageContext.Entity, new PendingSpellCastFinishedWaitingMessage(castData), messageContext.Entity);
 		}
 	}
 }
