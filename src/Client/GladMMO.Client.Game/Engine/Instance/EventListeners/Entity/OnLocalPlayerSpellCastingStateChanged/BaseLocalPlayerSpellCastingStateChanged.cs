722f4c24a84aa741503207b1c6bf68f1cbac0272
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 16:15:39 -0600

    Added BaseLocalPlayerSpellCastingStateChanged

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnLocalPlayerSpellCastingStateChanged/BaseLocalPlayerSpellCastingStateChanged.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class BaseLocalPlayerSpellCastingStateChanged : BaseSingleEventListenerInitializable<ILocalPlayerSpellCastingStateChangedEventSubscribable, SpellCastingStateChangedEventArgs>
+	{
+		protected BaseLocalPlayerSpellCastingStateChanged(ILocalPlayerSpellCastingStateChangedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
