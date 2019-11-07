35ceba3620131b7cf64a179a727dc485486f92e7
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 13:12:35 -0600

    Added ISpellEffectTargetFactory

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Factory/ISpellEffectTargetFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ISpellEffectTargetFactory : IFactoryCreatable<SpellEffectTargetContext, SpellEffectTargetCreationContext>
+	{
+
+	}
+}
