4fd95377ccf157c8505cc27e9fe07f04099f5234
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 10:19:55 -0600

    Added StrategyBasedSpellTargetValidator

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/StrategyBasedSpellTargetValidator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class StrategyBasedSpellTargetValidator : ISpellTargetValidator
+	{
+		private IReadonlySpellDataCollection SpellDataCollection { get; }
+
+		private ISpellEffectTargetValidatorFactory ValidatorFactory { get; }
+
+		public StrategyBasedSpellTargetValidator([NotNull] IReadonlySpellDataCollection spellDataCollection,
+			[NotNull] ISpellEffectTargetValidatorFactory validatorFactory)
+		{
+			SpellDataCollection = spellDataCollection ?? throw new ArgumentNullException(nameof(spellDataCollection));
+			ValidatorFactory = validatorFactory ?? throw new ArgumentNullException(nameof(validatorFactory));
+		}
+
+		public bool isSpellTargetViable(int spellId, [NotNull] DefaultEntityActorStateContainer actorState)
+		{
+			if (actorState == null) throw new ArgumentNullException(nameof(actorState));
+			if (spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			SpellDefinitionDataModel definition = SpellDataCollection.GetSpellDefinition(spellId);
+
+			foreach (var effect in SpellDataCollection.GetEffectsForSpell(spellId))
+			{
+				ISpellEffectTargetValidator effectTargetValidator = ValidatorFactory.Create(effect);
+
+				//One effect's targets failed given the context. So this fails.
+				if (!effectTargetValidator.ValidateTargetContext(definition, effect, actorState))
+					return false;
+			}
+
+			return true;
+		}
+	}
+}
