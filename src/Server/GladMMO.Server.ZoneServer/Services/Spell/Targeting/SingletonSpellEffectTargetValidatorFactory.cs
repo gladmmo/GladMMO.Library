5296b35a9b8b495db8fd1589f25d9920841b7416
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 10:17:29 -0600

    Added SingletonSpellEffectTargetValidatorFactory

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/SingletonSpellEffectTargetValidatorFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace GladMMO
+{
+	public sealed class SingletonSpellEffectTargetValidatorFactory : ISpellEffectTargetValidatorFactory
+	{
+		private Dictionary<long, ISpellEffectTargetValidator> ValidatorMap { get; } = new Dictionary<long, ISpellEffectTargetValidator>(200);
+
+		public SingletonSpellEffectTargetValidatorFactory([NotNull] IEnumerable<ISpellEffectTargetValidator> validators)
+		{
+			if (validators == null) throw new ArgumentNullException(nameof(validators));
+
+			foreach (var validator in validators)
+			foreach (var validatorAttribute in validator.GetType().GetCustomAttributes<SpellEffectTargetingStrategyAttribute>())
+				ValidatorMap.Add(validatorAttribute.TargetTypeKey, validator);
+		}
+
+		public ISpellEffectTargetValidator Create([NotNull] SpellEffectDefinitionDataModel context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+
+			long computedKeyValue = SpellEffectTargetingStrategyAttribute.ComputeKey(context.EffectTargetingType, context.AdditionalEffectTargetingType);
+
+			if (ValidatorMap.ContainsKey(computedKeyValue))
+				return ValidatorMap[computedKeyValue];
+			else
+				throw new KeyNotFoundException($"Key {computedKeyValue} for Effect target validator: {context.SpellEffectId} with TargetType: {context.EffectTargetingType}:{context.AdditionalEffectTargetingType} not found.");
+		}
+	}
+}
