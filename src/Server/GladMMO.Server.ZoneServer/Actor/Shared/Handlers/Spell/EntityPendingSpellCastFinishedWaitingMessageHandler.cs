39d7e26d7c3b096947cfb30a85cbdb5f03ecabc0
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 12:35:06 -0600

    Simplified EntityPendingSpellCastFinishedWaitingMessageHandler

--- src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/Spell/EntityPendingSpellCastFinishedWaitingMessageHandler.cs
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/Spell/EntityPendingSpellCastFinishedWaitingMessageHandler.cs
@@ -8,22 +8,8 @@ namespace GladMMO
 	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
 	public sealed class EntityPendingSpellCastFinishedWaitingMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, PendingSpellCastFinishedWaitingMessage>
 	{
-		private IReadonlyEntityGuidMappable<PendingSpellCastData> PendingSpellCastMappable { get; }
-
-		public EntityPendingSpellCastFinishedWaitingMessageHandler([NotNull] IEntityGuidMappable<PendingSpellCastData> pendingSpellCastMappable)
-		{
-			PendingSpellCastMappable = pendingSpellCastMappable ?? throw new ArgumentNullException(nameof(pendingSpellCastMappable));
-		}
-
 		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, PendingSpellCastFinishedWaitingMessage message)
 		{
-			//This is the CURRENT pending cast.
-			PendingSpellCastData castData = PendingSpellCastMappable.RetrieveEntity(state.EntityGuid);
-
-			//TODO: Handle the canceled casts. This i NOT how.
-			if (castData.StartTime != message.Pending.StartTime)
-				return;
-
 			//This indicates that the spell cast has finished.
 			state.EntityData.SetFieldValue(EntityObjectField.UNIT_FIELD_CASTING_SPELLID, 0);
 
