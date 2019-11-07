27fcdccf363935e55a598065a8d7dce30be15f14
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 07:22:17 -0600

    Added serverside LoadDefaultSpellDataContentInitializable

---
+++ src/Server/GladMMO.Server.ZoneServer/Engine/Initializables/LoadDefaultSpellDataContentInitializable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[ServerSceneTypeCreate(ServerSceneType.Default)]
+	public sealed class LoadDefaultSpellDataContentInitializable : IGameInitializable
+	{
+		private ISpellEntryDataServiceClient SpellDataService { get; }
+
+		private ISpellDataCollection SpellDataCollection { get; }
+
+		public LoadDefaultSpellDataContentInitializable([NotNull] ISpellEntryDataServiceClient spellDataService, [NotNull] ISpellDataCollection spellDataCollection)
+		{
+			SpellDataService = spellDataService ?? throw new ArgumentNullException(nameof(spellDataService));
+			SpellDataCollection = spellDataCollection ?? throw new ArgumentNullException(nameof(spellDataCollection));
+		}
+
+		public async Task OnGameInitialized()
+		{
+			SpellDefinitionCollectionResponseModel model = await SpellDataService.GetDefaultSpellDataAsync();
+
+			foreach (var spell in model.SpellEntries)
+				SpellDataCollection.AddSpellDefinition(spell);
+
+			foreach (var spellEffect in model.SpellEffects)
+				SpellDataCollection.AddSpellEffectDefinition(spellEffect);
+		}
+	}
+}
