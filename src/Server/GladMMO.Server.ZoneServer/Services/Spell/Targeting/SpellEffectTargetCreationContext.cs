624d617aee6147ea2fa50c465e19cb83c73b33fa
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 13:12:18 -0600

    Added SpellEffectTargetCreationContext

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/SpellEffectTargetCreationContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class SpellEffectTargetCreationContext
+	{
+		public int SpellId { get; }
+
+		public SpellEffectIndex EffectIndex { get; }
+
+		public DefaultEntityActorStateContainer ActorState { get; }
+
+		public SpellEffectTargetCreationContext(int spellId, SpellEffectIndex effectIndex, [NotNull] DefaultEntityActorStateContainer actorState)
+		{
+			if (spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			SpellId = spellId;
+			EffectIndex = effectIndex;
+			ActorState = actorState ?? throw new ArgumentNullException(nameof(actorState));
+		}
+	}
+}
