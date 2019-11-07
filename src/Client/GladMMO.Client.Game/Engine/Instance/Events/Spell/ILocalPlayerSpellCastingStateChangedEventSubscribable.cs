9886b88262e0fa8da78fe7fe42a86996bf1367c6
HelloKitty <andrew.blakely@ymail.com>
2019-11-07 16:12:00 -0600

    Added ILocalPlayerSpellCastingStateChangedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Spell/ILocalPlayerSpellCastingStateChangedEventSubscribable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ILocalPlayerSpellCastingStateChangedEventSubscribable
+	{
+		event EventHandler<SpellCastingStateChangedEventArgs> OnSpellCastingStateChanged;
+	}
+
+	public sealed class SpellCastingStateChangedEventArgs : EventArgs
+	{
+		public int CastingSpellId { get; }
+
+		public bool isCasting => CastingSpellId != 0;
+
+		public SpellCastingStateChangedEventArgs(int castingSpellId)
+		{
+			if (castingSpellId < 0) throw new ArgumentOutOfRangeException(nameof(castingSpellId));
+
+			CastingSpellId = castingSpellId;
+		}
+	}
+}
