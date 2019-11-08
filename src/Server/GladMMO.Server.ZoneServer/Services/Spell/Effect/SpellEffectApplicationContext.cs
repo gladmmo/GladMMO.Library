c74f989ff33bf62f90a592ba0b03dd754f0c8d73
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 09:48:19 -0600

    Added SpellEffectApplicationContext

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Effect/SpellEffectApplicationContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class SpellEffectApplicationContext
+	{
+		/// <summary>
+		/// The source guid of the spell.
+		/// (Ex. the caster).
+		/// </summary>
+		public NetworkEntityGuid SpellSource { get; }
+
+		/// <summary>
+		/// The target guid of the spell.
+		/// </summary>
+		public NetworkEntityGuid ApplicationTarget { get; }
+
+		/// <summary>
+		/// The spell data for the application.
+		/// </summary>
+		public ISpellEffectPairable SpellEffectData { get; }
+
+		public SpellEffectApplicationContext([NotNull] NetworkEntityGuid spellSource, [NotNull] NetworkEntityGuid applicationTarget, [NotNull] ISpellEffectPairable spellEffectData)
+		{
+			SpellSource = spellSource ?? throw new ArgumentNullException(nameof(spellSource));
+			ApplicationTarget = applicationTarget ?? throw new ArgumentNullException(nameof(applicationTarget));
+			SpellEffectData = spellEffectData ?? throw new ArgumentNullException(nameof(spellEffectData));
+		}
+	}
+}
