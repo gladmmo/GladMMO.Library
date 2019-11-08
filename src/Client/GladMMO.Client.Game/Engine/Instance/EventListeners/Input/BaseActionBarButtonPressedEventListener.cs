b8953b524e52caccceff9a17921a020fadcf1f1d
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 15:30:38 -0600

    Added BaseActionBarButtonPressedEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Input/BaseActionBarButtonPressedEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class BaseActionBarButtonPressedEventListener : BaseSingleEventListenerInitializable<IActionBarButtonPressedEventSubscribable, ActionBarButtonPressedEventArgs>
+	{
+		protected BaseActionBarButtonPressedEventListener(IActionBarButtonPressedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+
+		protected override void OnEventFired(object source, [NotNull] ActionBarButtonPressedEventArgs args)
+		{
+			if (args == null) throw new ArgumentNullException(nameof(args));
+
+			OnActionBarButtonPressed(args.Index);
+		}
+
+		protected abstract void OnActionBarButtonPressed(ActionBarIndex index);
+	}
+}
