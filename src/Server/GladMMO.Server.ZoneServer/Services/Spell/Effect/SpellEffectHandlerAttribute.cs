63b2fb3ebbdc7f7445edfe105e6fb54702767da4
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 09:40:03 -0600

    Added SpellEffectHandlerAttribute

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Effect/SpellEffectHandlerAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GladMMO
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
+	public sealed class SpellEffectHandlerAttribute : Attribute
+	{
+		public SpellEffectType EffectType { get; }
+
+		public SpellEffectHandlerAttribute(SpellEffectType effectType)
+		{
+			if (!Enum.IsDefined(typeof(SpellEffectType), effectType)) throw new InvalidEnumArgumentException(nameof(effectType), (int) effectType, typeof(SpellEffectType));
+
+			EffectType = effectType;
+		}
+	}
+}
