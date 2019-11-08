72ceee504c99b5d1fe079c4f5b3eb4a2be75c7d8
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 15:20:45 -0600

    Added ActionBarButtonPressedEventPublisher

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventPublishers/Input/ActionBarButtonPressedEventPublisher.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IActionBarButtonPressedEventPublisher : IEventPublisher<IActionBarButtonPressedEventSubscribable, ActionBarButtonPressedEventArgs>
+	{
+
+	}
+
+	[AdditionalRegisterationAs(typeof(IActionBarButtonPressedEventSubscribable))]
+	[AdditionalRegisterationAs(typeof(IActionBarButtonPressedEventPublisher))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ActionBarButtonPressedEventPublisher : IActionBarButtonPressedEventPublisher, IActionBarButtonPressedEventSubscribable
+	{
+		public event EventHandler<ActionBarButtonPressedEventArgs> OnActionBarButtonPressed;
+
+		public void PublishEvent(object sender, ActionBarButtonPressedEventArgs eventArgs)
+		{
+			//Just forward event.
+			OnActionBarButtonPressed?.Invoke(sender, eventArgs);
+		}
+	}
+}
