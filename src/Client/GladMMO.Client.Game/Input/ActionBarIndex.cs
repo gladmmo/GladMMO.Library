c4be32cbb91dae6aa5ab245237082d04826368e7
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 15:05:15 -0600

    Added ActionBarIndex

---
+++ src/Client/GladMMO.Client.Game/Input/ActionBarIndex.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public enum ActionBarIndex
+	{
+		ActionBarIndex_01 = 0,
+		ActionBarIndex_02 = 1,
+		ActionBarIndex_03 = 2,
+		ActionBarIndex_04 = 3,
+		ActionBarIndex_05 = 4,
+		ActionBarIndex_06 = 5,
+		ActionBarIndex_07 = 6,
+		ActionBarIndex_08 = 7,
+		ActionBarIndex_09 = 8,
+		ActionBarIndex_10 = 9,
+		ActionBarIndex_11 = 10,
+		ActionBarIndex_12 = 11,
+	}
+}
