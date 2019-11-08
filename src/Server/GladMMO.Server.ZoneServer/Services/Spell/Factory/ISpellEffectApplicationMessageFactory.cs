964c4457aa1942c0cd7e51efb3e4fce61bb97365
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 07:53:32 -0600

    Added ISpellEffectApplicationMessageFactory

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Factory/ISpellEffectApplicationMessageFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class SpellEffectApplicationMessageCreationContext
+	{
+		/// <summary>
+		/// The source of the spell effect application.
+		/// Basically the casted of the linked spell.
+		/// </summary>
+		public NetworkEntityGuid ApplicationSource { get; }
+
+		/// <summary>
+		/// Spell id to cast.
+		/// </summary>
+		public int SpellId { get; }
+
+		/// <summary>
+		/// The effect index to create an application message for.
+		/// </summary>
+		public SpellEffectIndex EffectIndex { get; }
+
+		public SpellEffectApplicationMessageCreationContext([NotNull] NetworkEntityGuid applicationSource, int spellId, SpellEffectIndex effectIndex)
+		{
+			if (spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+			ApplicationSource = applicationSource ?? throw new ArgumentNullException(nameof(applicationSource));
+			SpellId = spellId;
+			EffectIndex = effectIndex;
+		}
+	}
+
+	public interface ISpellEffectApplicationMessageFactory : IFactoryCreatable<ApplySpellEffectMessage, SpellEffectApplicationMessageCreationContext>
+	{
+
+	}
+}
