e4595523bc30d95fa7085d26f6b72bdfbc614e69
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 11:35:45 -0600

    Added EntityApplySpellEffectMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/Spell/EntityApplySpellEffectMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultCreatureEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	public sealed class EntityApplySpellEffectMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, ApplySpellEffectMessage>
+	{
+		private ISpellEffectHandlerFactory SpellEffectHandlerFactory { get; }
+
+		public EntityApplySpellEffectMessageHandler([NotNull] ISpellEffectHandlerFactory spellEffectHandlerFactory)
+		{
+			SpellEffectHandlerFactory = spellEffectHandlerFactory ?? throw new ArgumentNullException(nameof(spellEffectHandlerFactory));
+		}
+
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, ApplySpellEffectMessage message)
+		{
+			SpellEffectHandlerFactory.Create(message)
+				.ApplySpellEffect(new SpellEffectApplicationContext(message.SourceCaster, state.EntityGuid, message, state.EntityData));
+		}
+	}
+}
