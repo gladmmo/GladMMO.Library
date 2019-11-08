0b637245eceb4d35515a2bfe1e0232d7490c9d24
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 12:51:00 -0600

    Added EntityActorHealMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/EntityActorHealMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultCreatureEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	public sealed class EntityActorHealMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, HealEntityActorCurrentHealthMessage>
+	{
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, HealEntityActorCurrentHealthMessage message)
+		{
+			//Increases health clamped by current max health.
+			if (state.EntityData.DataSetIndicationArray.Get((int) EntityObjectField.UNIT_FIELD_HEALTH))
+				state.EntityData.SetFieldValue(EntityObjectField.UNIT_FIELD_HEALTH, Math.Min(state.EntityData.GetFieldValue<int>(EntityObjectField.UNIT_FIELD_MAXHEALTH), state.EntityData.GetFieldValue<int>(EntityObjectField.UNIT_FIELD_HEALTH) + message.HealAmount));
+		}
+	}
+}
