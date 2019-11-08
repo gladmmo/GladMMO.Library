f72d7dc53ea98d5101d2796ed2a7bc8b00ef5cfa
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 15:17:10 -0600

    Added IActionBarButtonPressedEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Input/IActionBarButtonPressedEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IActionBarButtonPressedEventListener
+	{
+		event EventHandler<ActionBarButtonPressedEventArgs> OnActionBarButtonPressed;
+	}
+
+	public sealed class ActionBarButtonPressedEventArgs : EventArgs
+	{
+		public ActionBarIndex Index { get; }
+
+		public ActionBarButtonPressedEventArgs(ActionBarIndex index)
+		{
+			if (!Enum.IsDefined(typeof(ActionBarIndex), index)) throw new InvalidEnumArgumentException(nameof(index), (int) index, typeof(ActionBarIndex));
+
+			Index = index;
+		}
+	}
+}
