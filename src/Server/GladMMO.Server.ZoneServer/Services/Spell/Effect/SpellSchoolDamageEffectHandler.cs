f2156dd4e13292c1e945d034e51781e23cab84cb
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 10:13:43 -0600

    Added SpellSchoolDamageEffectHandler for SPELL_EFFECT_SCHOOL_DAMAGE

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Effect/SpellSchoolDamageEffectHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	[SpellEffectHandler(SpellEffectType.SPELL_EFFECT_SCHOOL_DAMAGE)]
+	public sealed class SpellSchoolDamageEffectHandler : BaseSpellEffectHandler
+	{
+		public SpellSchoolDamageEffectHandler(IReadonlyEntityGuidMappable<IActorRef> actorReferenceMappable) 
+			: base(actorReferenceMappable)
+		{
+
+		}
+
+		public override void ApplySpellEffect([NotNull] SpellEffectApplicationContext context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+
+			//TODO: Support spell schools AKA Frost damage, nature, physical and etc.
+			int damageValue = RollEffectValue(context.SpellEffectData.SpellEffect);
+			ApplyDamage(context.ApplicationTarget, damageValue, context.SpellSource);
+		}
+	}
+}
