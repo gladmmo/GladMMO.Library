726fed6062360405e5637ff6d93df0ec8f70dc1b
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 09:46:35 -0600

    Added ISpellEffectPairable

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Effect/ISpellEffectPairable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for types that expose a Spell and SpellEffect pair.
+	/// </summary>
+	public interface ISpellEffectPairable
+	{
+		/// <summary>
+		/// The actual spell that attached <see cref="SpellEffect"></see>
+		/// </summary>
+		SpellDefinitionDataModel Spell { get; }
+
+		/// <summary>
+		/// The linked spell effect.
+		/// </summary>
+		SpellEffectDefinitionDataModel SpellEffect { get; }
+	}
+}
