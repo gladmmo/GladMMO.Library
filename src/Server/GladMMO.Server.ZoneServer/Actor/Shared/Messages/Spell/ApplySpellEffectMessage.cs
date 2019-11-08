b5bee67a20a8e01f59749f8c2c3892ab9d8820f4
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 07:46:29 -0600

    Added ApplySpellEffectMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/Spell/ApplySpellEffectMessage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class ApplySpellEffectMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The GUID of the casting source.
+		/// Could indicate self or could indicate an enemy player or creature.
+		/// </summary>
+		public NetworkEntityGuid SourceCaster { get; }
+
+		/// <summary>
+		/// The actual spell that attached <see cref="SpellEffect"/> is attempting
+		/// to apply to the entity actor.
+		/// </summary>
+		public SpellDefinitionDataModel Spell { get; }
+
+		/// <summary>
+		/// The effect to apply.
+		/// </summary>
+		public SpellEffectDefinitionDataModel SpellEffect { get; }
+
+		public ApplySpellEffectMessage([NotNull] NetworkEntityGuid sourceCaster, [NotNull] SpellDefinitionDataModel spell, [NotNull] SpellEffectDefinitionDataModel spellEffect)
+		{
+			SourceCaster = sourceCaster ?? throw new ArgumentNullException(nameof(sourceCaster));
+			Spell = spell ?? throw new ArgumentNullException(nameof(spell));
+			SpellEffect = spellEffect ?? throw new ArgumentNullException(nameof(spellEffect));
+
+			//TODO: Expand this to validate additional effects.
+			if(spell.SpellEffectIdOne != SpellEffect.SpellEffectId)
+				throw new InvalidOperationException($"Cannot apply spell effect for Spell: {spell.SpellId} with Effect: {spellEffect.SpellEffectId} because the spell is not linked to that effect.");
+		}
+	}
+}
