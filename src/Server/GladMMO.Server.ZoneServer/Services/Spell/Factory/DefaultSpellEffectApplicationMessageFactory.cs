5ea6426bfd3ae9ba7659925b692adbd17d4e9196
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 08:00:54 -0600

    Added DefaultSpellEffectApplicationMessageFactory

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Factory/DefaultSpellEffectApplicationMessageFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DefaultSpellEffectApplicationMessageFactory : ISpellEffectApplicationMessageFactory
+	{
+		private IReadonlySpellDataCollection SpellDataCollection { get; }
+
+		public DefaultSpellEffectApplicationMessageFactory([NotNull] IReadonlySpellDataCollection spellDataCollection)
+		{
+			SpellDataCollection = spellDataCollection ?? throw new ArgumentNullException(nameof(spellDataCollection));
+		}
+
+		public ApplySpellEffectMessage Create([NotNull] SpellEffectApplicationMessageCreationContext context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+
+			SpellDefinitionDataModel spellDefinition = SpellDataCollection.GetSpellDefinition(context.SpellId);
+			SpellEffectDefinitionDataModel effectDefinition = SpellDataCollection.GetSpellEffectDefinition(spellDefinition.GetSpellEffectId(context.EffectIndex));
+
+			return new ApplySpellEffectMessage(context.ApplicationSource, spellDefinition, effectDefinition);
+		}
+	}
+}
