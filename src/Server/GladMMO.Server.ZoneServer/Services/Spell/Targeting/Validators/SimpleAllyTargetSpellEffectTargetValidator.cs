77f148d06186860006fe5b15bf80e3314c1e3119
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 13:07:04 -0600

    Added SimpleAllyTargetSpellEffectTargetValidator

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Targeting/Validators/SimpleAllyTargetSpellEffectTargetValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[SpellEffectTargetingStrategy(SpellEffectTargetType.TARGET_UNIT_TARGET_ALLY, SpellEffectTargetType.NO_TARGET)]
+	public sealed class SimpleAllyTargetSpellEffectTargetValidator : BaseTargetSpellEffectValidator
+	{
+		protected override bool ValidateTargeting(SpellDefinitionDataModel spellDefinition, SpellEffectDefinitionDataModel spellEffect, DefaultEntityActorStateContainer actorState)
+		{
+			//Ally targeting is always valid. Even if targeting an enemy
+			//This is because we'll just cast it on ourselves if we're
+			//targeting an enemy.
+			return true;
+		}
+	}
+}
