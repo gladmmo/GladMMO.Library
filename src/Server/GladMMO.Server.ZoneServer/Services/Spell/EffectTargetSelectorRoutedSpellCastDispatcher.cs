7c8db57cc7769c1344bcf4070f64e5ea93a4a676
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 08:24:51 -0600

    Added full effect and target handling for spell dispatching

--- src/Server/GladMMO.Server.ZoneServer/Services/Spell/EffectTargetSelectorRoutedSpellCastDispatcher.cs
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/EffectTargetSelectorRoutedSpellCastDispatcher.cs
@@ -10,17 +10,25 @@ namespace GladMMO
 	{
 		private IReadonlySpellDataCollection SpellDataCollection { get; }
 
-		private ISpellEffectTargetSelectorFactory EffectSelectorFactory { get; }
+		private ISpellEffectTargetSelectorFactory EffectTargetSelectorFactory { get; }
 
 		private ILog Logger { get; }
 
+		private ISpellEffectApplicationMessageFactory SpellEffectApplicationMessageFactory { get; }
+
+		private IReadonlyEntityGuidMappable<IActorRef> ActorReferenceMappable { get; }
+
 		public EffectTargetSelectorRoutedSpellCastDispatcher([NotNull] IReadonlySpellDataCollection spellDataCollection, 
 			[NotNull] ISpellEffectTargetSelectorFactory effectSelectorFactory,
-			[NotNull] ILog logger)
+			[NotNull] ILog logger,
+			[NotNull] ISpellEffectApplicationMessageFactory spellEffectApplicationMessageFactory,
+			[NotNull] IReadonlyEntityGuidMappable<IActorRef> actorReferenceMappable)
 		{
 			SpellDataCollection = spellDataCollection ?? throw new ArgumentNullException(nameof(spellDataCollection));
-			EffectSelectorFactory = effectSelectorFactory ?? throw new ArgumentNullException(nameof(effectSelectorFactory));
+			EffectTargetSelectorFactory = effectSelectorFactory ?? throw new ArgumentNullException(nameof(effectSelectorFactory));
 			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			SpellEffectApplicationMessageFactory = spellEffectApplicationMessageFactory ?? throw new ArgumentNullException(nameof(spellEffectApplicationMessageFactory));
+			ActorReferenceMappable = actorReferenceMappable ?? throw new ArgumentNullException(nameof(actorReferenceMappable));
 		}
 
 		public void DispatchSpellCast([NotNull] IPendingSpellCastData pendingSpellCast, DefaultEntityActorStateContainer casterData)
@@ -30,23 +38,31 @@ namespace GladMMO
 			if (!SpellDataCollection.ContainsSpellDefinition(pendingSpellCast.SpellId))
 				throw new InvalidOperationException($"Tried to cast Spell: {pendingSpellCast.SpellId} but no definition exists.");
 
+			IActorRef casterActorReference = ActorReferenceMappable.RetrieveEntity(casterData.EntityGuid);
 			SpellDefinitionDataModel spellDefinition = SpellDataCollection.GetSpellDefinition(pendingSpellCast.SpellId);
 
-			//No effect exists. We have nothing to do.
-			if (spellDefinition.SpellEffectIdOne == 0)
+			//Each spell can have N effects with individual unique targeting attributes.
+			//So we need to handle each spell effect seperately, compute their effects/targets
+			//and send an effect application message to the involved actors.
+			foreach (SpellEffectIndex effectIndex in spellDefinition.EnumerateSpellEffects())
 			{
-				return;
-			}
+				SpellEffectDefinitionDataModel effectDefinition = SpellDataCollection.GetSpellEffectDefinition(spellDefinition.GetSpellEffectId(effectIndex));
 
-			//TODO: Handle MULTIPLE effects.
-			SpellEffectDefinitionDataModel effectDefinition = SpellDataCollection.GetSpellEffectDefinition(spellDefinition.SpellEffectIdOne);
-			ISpellEffectTargetSelector targetSelector = EffectSelectorFactory.Create(effectDefinition);
-			SpellEffectTargetContext targetContext = targetSelector.CalculateTargets(spellDefinition, effectDefinition, casterData, pendingSpellCast);
+				SpellEffectTargetContext targetContext = EffectTargetSelectorFactory
+					.Create(effectDefinition)
+					.CalculateTargets(spellDefinition, effectDefinition, casterData, pendingSpellCast);
 
-			foreach (var target in targetContext.SpellEffectTargets)
-			{
-				//TODO: Send spell cast result to them.
-				Logger.Info($"Entity: {casterData.EntityGuid} casted spell with effect that targets Target: {target.Path.Name}");
+				ApplySpellEffectMessage spellEffectApplicationMessage = SpellEffectApplicationMessageFactory.Create(new SpellEffectApplicationMessageCreationContext(casterData.EntityGuid, pendingSpellCast.SpellId, effectIndex));
+
+				//For each actor target in the target context
+				//we need to send the spell application message for handling
+				foreach(var target in targetContext.SpellEffectTargets)
+				{
+					if(Logger.IsDebugEnabled)
+						Logger.Debug($"Entity: {casterData.EntityGuid} casted spell with effect that targets Target: {target.Path.Name}");
+
+					target.Tell(spellEffectApplicationMessage, casterActorReference);
+				}
 			}
 		}
 	}
