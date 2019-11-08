ba4c4322f32f4a59da56f6c334765ae8a0fabfb0
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 10:04:01 -0600

    Added ApplyDamage to BaseSpellEffectHandler

--- src/Server/GladMMO.Server.ZoneServer/Services/Spell/Effect/BaseSpellEffectHandler.cs
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Effect/BaseSpellEffectHandler.cs
@@ -18,5 +18,28 @@ namespace GladMMO
 		}
 
 		public abstract void ApplySpellEffect(SpellEffectApplicationContext context);
+
+		//TODO: Support spell school definition.
+		/// <summary>
+		/// Applies damage to the <see cref="entity"/> associated with the provided guid.
+		/// Applies <see cref="damageAmount"/> of the damage.
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <param name="damageAmount"></param>
+		protected void ApplyDamage([NotNull] NetworkEntityGuid entity, int damageAmount, NetworkEntityGuid damageSourceEntity = null)
+		{
+			if (entity == null) throw new ArgumentNullException(nameof(entity));
+			if (damageAmount < 0) throw new ArgumentOutOfRangeException(nameof(damageAmount));
+
+			IActorRef actorRef = ActorReferenceMappable.RetrieveEntity(entity);
+
+			if(damageSourceEntity == null || damageSourceEntity != NetworkEntityGuid.Empty)
+				actorRef.Tell(new DamageEntityActorCurrentHealthMessage(damageAmount));
+			else
+			{
+				IActorRef sourceRef = ActorReferenceMappable.RetrieveEntity(damageSourceEntity);
+				actorRef.Tell(new DamageEntityActorCurrentHealthMessage(damageAmount), sourceRef);
+			}
+		}
 	}
 }
