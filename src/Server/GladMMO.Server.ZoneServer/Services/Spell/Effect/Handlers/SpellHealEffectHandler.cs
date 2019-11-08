33d3ac6d4af87e5bd05921c31495889fe3d88e58
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 13:04:28 -0600

    Implemented SpellHealEffectHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Effect/Handlers/SpellHealEffectHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	[SpellEffectHandler(SpellEffectType.SPELL_EFFECT_HEAL)]
+	public sealed class SpellHealEffectHandler : BaseSpellEffectHandler
+	{
+		public SpellHealEffectHandler(IReadonlyEntityGuidMappable<IActorRef> actorReferenceMappable) 
+			: base(actorReferenceMappable)
+		{
+
+		}
+
+		public override void ApplySpellEffect([NotNull] SpellEffectApplicationContext context)
+		{
+			if (context == null) throw new ArgumentNullException(nameof(context));
+
+			//TODO: Implement healing power influence.
+			//TODO: Support spell schools AKA Holy heal, nature, shadow/dark and etc.
+			int healAmount = RollEffectValue(context.SpellEffectData.SpellEffect) + ComputeAdditiveLevelScaling(context);
+			ApplyHeal(context.ApplicationTarget, healAmount, context.SpellSource);
+		}
+	}
+}
