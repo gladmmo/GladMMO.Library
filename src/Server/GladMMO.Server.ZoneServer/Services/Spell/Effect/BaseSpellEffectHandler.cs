db5fff664c9f4337432013ccf4a24a56dd54ae1b
HelloKitty <andrew.blakely@ymail.com>
2019-11-08 09:54:07 -0600

    Added BaseSpellEffectHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/Effect/BaseSpellEffectHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Base handler for spell effects.
+	/// </summary>
+	public abstract class BaseSpellEffectHandler : ISpellEffectHandler
+	{
+		protected IReadonlyEntityGuidMappable<IActorRef> ActorReferenceMappable { get; }
+
+		protected BaseSpellEffectHandler([NotNull] IReadonlyEntityGuidMappable<IActorRef> actorReferenceMappable)
+		{
+			ActorReferenceMappable = actorReferenceMappable ?? throw new ArgumentNullException(nameof(actorReferenceMappable));
+		}
+
+		public abstract void ApplySpellEffect(SpellEffectApplicationContext context);
+	}
+}
