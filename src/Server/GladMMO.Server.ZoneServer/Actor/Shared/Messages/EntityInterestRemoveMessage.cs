e6f785bd17af4eb82f157c18f6b89a976d27cdd9
HelloKitty <andrew.blakely@ymail.com>
2019-11-09 12:10:10 -0600

    Added EntityInterestRemoveMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/EntityInterestRemoveMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Message for actors to indicate interest is lost in an entity.
+	/// </summary>
+	public sealed class EntityInterestRemoveMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The entity to remove interest for.
+		/// </summary>
+		public NetworkEntityGuid Entity { get; }
+
+		public EntityInterestRemoveMessage([NotNull] NetworkEntityGuid entity)
+		{
+			Entity = entity ?? throw new ArgumentNullException(nameof(entity));
+		}
+	}
+}
