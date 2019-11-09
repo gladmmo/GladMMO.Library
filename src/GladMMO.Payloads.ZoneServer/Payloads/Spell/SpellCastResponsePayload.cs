f1c9b68fccc900cd4869f817817a0a8444b0dd63
HelloKitty <andrew.blakely@ymail.com>
2019-11-09 17:59:43 -0600

    Added SpellCastResponsePayload

---
+++ src/GladMMO.Payloads.ZoneServer/Payloads/Spell/SpellCastResponsePayload.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using ProtoBuf;
+
+namespace GladMMO
+{
+	[ProtoContract]
+	[GamePayload(GamePayloadOperationCode.RequestSpellCast)]
+	public sealed class SpellCastResponsePayload : GameServerPacketPayload, ISucceedable
+	{
+		/// <summary>
+		/// Indicates the result of the spell cast.
+		/// </summary>
+		[ProtoMember(1)]
+		public SpellCastResult Result { get; private set; }
+
+		/// <summary>
+		/// The ID of the spell this is a result for attempted casting.
+		/// </summary>
+		[ProtoMember(2)]
+		public int SpellId { get; private set; }
+
+		/// <summary>
+		/// Indicates if the spell cast is successful.
+		/// </summary>
+		[ProtoIgnore]
+		public bool isSuccessful => Result == SpellCastResult.SPELL_FAILED_SUCCESS;
+
+		public SpellCastResponsePayload(SpellCastResult result, int spellId)
+		{
+			if (!Enum.IsDefined(typeof(SpellCastResult), result)) throw new InvalidEnumArgumentException(nameof(result), (int) result, typeof(SpellCastResult));
+			if (spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			Result = result;
+			SpellId = spellId;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private SpellCastResponsePayload()
+		{
+
+		}
+	}
+}
