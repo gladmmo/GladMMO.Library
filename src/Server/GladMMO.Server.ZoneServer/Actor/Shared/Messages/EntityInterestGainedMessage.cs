761f8dcb61797bf5ea4bc15b53e4112dc5ccaf95
HelloKitty <andrew.blakely@ymail.com>
2019-11-09 12:11:04 -0600

    Added EntityInterestGainedMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/EntityInterestGainedMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Message for actors to indicate interest is gained in an entity.
+	/// </summary>
+	public sealed class EntityInterestGainedMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// The entity to gain interest for.
+		/// </summary>
+		public NetworkEntityGuid Entity { get; }
+
+		public EntityInterestGainedMessage([NotNull] NetworkEntityGuid entity)
+		{
+			Entity = entity ?? throw new ArgumentNullException(nameof(entity));
+		}
+	}
+}
