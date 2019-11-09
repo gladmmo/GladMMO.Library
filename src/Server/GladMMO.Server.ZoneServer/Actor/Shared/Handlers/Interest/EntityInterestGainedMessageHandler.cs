919c620697416e1aa813093c6ff55932d4f4acf4
HelloKitty <andrew.blakely@ymail.com>
2019-11-09 14:23:38 -0600

    Added skeleton interest message handlers

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/Interest/EntityInterestGainedMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultCreatureEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultGameObjectEntityActor))]
+	public sealed class EntityInterestGainedMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, EntityInterestGainedMessage>
+	{
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, EntityInterestGainedMessage message)
+		{
+
+		}
+	}
+}
---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Handlers/Interest/EntityInterestRemoveMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultCreatureEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	[EntityActorMessageHandler(typeof(DefaultGameObjectEntityActor))]
+	public sealed class EntityInterestRemoveMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, EntityInterestRemoveMessage>
+	{
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, EntityInterestRemoveMessage message)
+		{
+
+		}
+	}
+}
