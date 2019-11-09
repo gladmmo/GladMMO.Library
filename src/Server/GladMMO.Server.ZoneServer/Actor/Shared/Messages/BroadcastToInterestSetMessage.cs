a38a907cb38838c41036f01d081aeb2dc4302c7e
HelloKitty <andrew.blakely@ymail.com>
2019-11-09 16:53:35 -0600

    Added BroadcastToInterestSetMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/BroadcastToInterestSetMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Message for broadcasting a server packet to an entire entity actor's interest set.
+	/// </summary>
+	public sealed class BroadcastToInterestSetMessage : EntityActorMessage
+	{
+		/// <summary>
+		/// Server message to broadcast.
+		/// </summary>
+		public GameServerPacketPayload Message { get; }
+
+		/// <summary>
+		/// Indicates if the <see cref="Message"/> should be sent to the local actor's client.
+		/// </summary>
+		public bool SendToSelf { get; }
+
+		public BroadcastToInterestSetMessage([NotNull] GameServerPacketPayload message, bool sendToSelf)
+		{
+			Message = message ?? throw new ArgumentNullException(nameof(message));
+			SendToSelf = sendToSelf;
+		}
+	}
+}
