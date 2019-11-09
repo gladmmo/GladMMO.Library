09ce68f1723089db22a8e08336a7fd6dfe38372e
HelloKitty <andrew.blakely@ymail.com>
2019-11-09 14:48:57 -0600

    Added NetworkedObjectActorState

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/NetworkedObjectActorState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Base type for all networked object actor states.
+	/// </summary>
+	public class NetworkedObjectActorState : DefaultEntityActorStateContainer
+	{
+		/// <summary>
+		/// Entity's interest collection.
+		/// </summary>
+		private InterestCollection Interest { get; }
+
+		public NetworkedObjectActorState(IEntityDataFieldContainer entityData, NetworkEntityGuid entityGuid, [NotNull] InterestCollection interest) 
+			: base(entityData, entityGuid)
+		{
+			Interest = interest ?? throw new ArgumentNullException(nameof(interest));
+		}
+	}
+}
