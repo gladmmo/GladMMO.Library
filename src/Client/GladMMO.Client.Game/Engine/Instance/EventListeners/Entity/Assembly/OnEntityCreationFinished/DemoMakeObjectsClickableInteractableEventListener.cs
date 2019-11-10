9872293afd5e090b3b73798bdecb9cb2ebc12882
HelloKitty <andrew.blakely@ymail.com>
2019-11-10 11:44:08 -0600

    Added clientside predicition for targeting

--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/Assembly/OnEntityCreationFinished/DemoMakeObjectsClickableInteractableEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/Assembly/OnEntityCreationFinished/DemoMakeObjectsClickableInteractableEventListener.cs
@@ -14,13 +14,21 @@ namespace GladMMO
 
 		private IPeerPayloadSendService<GameClientPacketPayload> SendService { get; }
 
+		private IReadonlyLocalPlayerDetails LocalPlayerDetails { get; }
+
+		private IReadonlyEntityGuidMappable<IEntityDataFieldContainer> EntityDataFieldMappable { get; }
+
 		public DemoMakeObjectsClickableInteractableEventListener(IEntityCreationFinishedEventSubscribable subscriptionService,
 			[NotNull] IReadonlyEntityGuidMappable<GameObject> gameObjectMappable,
-			[NotNull] IPeerPayloadSendService<GameClientPacketPayload> sendService) 
+			[NotNull] IPeerPayloadSendService<GameClientPacketPayload> sendService,
+			[NotNull] IReadonlyLocalPlayerDetails localPlayerDetails,
+			[NotNull] IReadonlyEntityGuidMappable<IEntityDataFieldContainer> entityDataFieldMappable) 
 			: base(subscriptionService)
 		{
 			GameObjectMappable = gameObjectMappable ?? throw new ArgumentNullException(nameof(gameObjectMappable));
 			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+			LocalPlayerDetails = localPlayerDetails ?? throw new ArgumentNullException(nameof(localPlayerDetails));
+			EntityDataFieldMappable = entityDataFieldMappable ?? throw new ArgumentNullException(nameof(entityDataFieldMappable));
 		}
 
 		protected override void OnEntityCreationFinished(EntityCreationFinishedEventArgs args)
@@ -39,11 +47,22 @@ namespace GladMMO
 			{
 				if (eventArgs.Type == MouseButtonClickEventArgs.MouseType.Left)
 				{
-					SendService.SendMessage(new ClientInteractNetworkedObjectRequestPayload(args.EntityGuid, ClientInteractNetworkedObjectRequestPayload.InteractType.Selection));
+					//Check if they are selectable
+					if (!EntityDataFieldMappable.RetrieveEntity(args.EntityGuid).HasBaseObjectFieldFlag(BaseObjectFieldFlags.UNIT_FLAG_NOT_SELECTABLE))
+					{
+						SendService.SendMessage(new ClientInteractNetworkedObjectRequestPayload(args.EntityGuid, ClientInteractNetworkedObjectRequestPayload.InteractType.Selection));
+
+						//Client side prediction of player target
+						LocalPlayerDetails.EntityData.SetFieldValue(EntityObjectField.UNIT_FIELD_TARGET, args.EntityGuid);
+					}
 				}
 				else
 				{
-					SendService.SendMessage(new ClientInteractNetworkedObjectRequestPayload(args.EntityGuid, ClientInteractNetworkedObjectRequestPayload.InteractType.Interaction));
+					//Check if the entity is interactable before sending a packet.
+					if (EntityDataFieldMappable.RetrieveEntity(args.EntityGuid).HasBaseObjectFieldFlag(BaseObjectFieldFlags.UNIT_FLAG_INTERACTABLE))
+					{
+						SendService.SendMessage(new ClientInteractNetworkedObjectRequestPayload(args.EntityGuid, ClientInteractNetworkedObjectRequestPayload.InteractType.Interaction));
+					}
 				}
 			};
 		}
