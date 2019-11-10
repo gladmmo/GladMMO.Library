356cae1bf8b5596c682af91fbe8676042664d664
HelloKitty <andrew.blakely@ymail.com>
2019-11-09 18:19:56 -0600

    Added PlayerSpellCastFailedMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Player/Handlers/Spell/PlayerSpellCastFailedMessageHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	public sealed class PlayerSpellCastFailedMessageHandler : BaseEntityActorMessageHandler<DefaultPlayerEntityActorState, SpellCastFailedMessage>
+	{
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultPlayerEntityActorState state, SpellCastFailedMessage message)
+		{
+			state.SendService.SendMessage(new SpellCastResponsePayload(message.Result, message.SpellId));
+		}
+	}
+}
--- src/Server/GladMMO.Server.ZoneServer/Actor/Player/State/DefaultPlayerEntityActorState.cs
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Player/State/DefaultPlayerEntityActorState.cs
@@ -7,7 +7,7 @@ namespace GladMMO
 {
 	public class DefaultPlayerEntityActorState : NetworkedObjectActorState
 	{
-		private IPeerPayloadSendService<GameServerPacketPayload> SendService { get; }
+		public IPeerPayloadSendService<GameServerPacketPayload> SendService { get; }
 
 		public DefaultPlayerEntityActorState(IEntityDataFieldContainer entityData, NetworkEntityGuid entityGuid, InterestCollection interest, [NotNull] IPeerPayloadSendService<GameServerPacketPayload> sendService) 
 			: base(entityData, entityGuid, interest)
