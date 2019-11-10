5874dade18b6c4108ce2c13cb28651ab417e2fa3
HelloKitty <andrew.blakely@ymail.com>
2019-11-10 11:10:47 -0600

    Added EntityActorSelectedMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/EntityActorSelectedMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class EntityActorSelectedMessage
+	{
+		/// <summary>
+		/// Guid of the entity attempting to select us.
+		/// </summary>
+		public NetworkEntityGuid SelectorGuid { get; }
+
+		public EntityActorSelectedMessage([NotNull] NetworkEntityGuid selectorGuid)
+		{
+			SelectorGuid = selectorGuid ?? throw new ArgumentNullException(nameof(selectorGuid));
+		}
+	}
+}
