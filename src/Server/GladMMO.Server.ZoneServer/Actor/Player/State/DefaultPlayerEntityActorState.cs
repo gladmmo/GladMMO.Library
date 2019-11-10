ced5f14d4b42d53559b00e9344b4c82f451369a4
HelloKitty <andrew.blakely@ymail.com>
2019-11-09 18:12:45 -0600

    Added DefaultPlayerEntityActorState

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Player/State/DefaultPlayerEntityActorState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GladNet;
+
+namespace GladMMO
+{
+	public class DefaultPlayerEntityActorState : NetworkedObjectActorState
+	{
+		private IPeerPayloadSendService<GameServerPacketPayload> SendService { get; }
+
+		public DefaultPlayerEntityActorState(IEntityDataFieldContainer entityData, NetworkEntityGuid entityGuid, InterestCollection interest, [NotNull] IPeerPayloadSendService<GameServerPacketPayload> sendService) 
+			: base(entityData, entityGuid, interest)
+		{
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
+		}
+	}
+}
