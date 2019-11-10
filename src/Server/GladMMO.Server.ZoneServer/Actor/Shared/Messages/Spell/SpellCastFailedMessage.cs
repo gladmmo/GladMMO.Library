012d15eeb819a52f9dd30b376cf7a6143a3ec826
HelloKitty <andrew.blakely@ymail.com>
2019-11-09 18:17:51 -0600

    Added SpellCastFailedMessage actor message

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/Spell/SpellCastFailedMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class SpellCastFailedMessage : EntityActorMessage
+	{
+		public SpellCastResult Result { get; }
+
+		public int SpellId { get; }
+
+		public SpellCastFailedMessage(SpellCastResult result, int spellId)
+		{
+			if (!Enum.IsDefined(typeof(SpellCastResult), result)) throw new InvalidEnumArgumentException(nameof(result), (int) result, typeof(SpellCastResult));
+			if (spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			Result = result;
+			SpellId = spellId;
+		}
+	}
+}
