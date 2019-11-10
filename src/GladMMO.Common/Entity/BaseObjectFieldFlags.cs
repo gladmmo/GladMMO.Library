a89699d8ea6acbff5c7ed29354086a4fbc382bb2
HelloKitty <andrew.blakely@ymail.com>
2019-11-10 10:07:20 -0600

    Added BaseObjectFieldFlags

---
+++ src/GladMMO.Common/Entity/BaseObjectFieldFlags.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Enumeration for the <see cref="BaseObjectField"/>.UNIT_FIELD_FLAGS
+	/// </summary>
+	[Flags]
+	public enum BaseObjectFieldFlags : int
+	{
+		/// <summary>
+		/// Indicates no flags are set.
+		/// </summary>
+		None = 0,
+
+		/// <summary>
+		/// Indicates that the object cannot be target/selected by the client.
+		/// </summary>
+		UNIT_FLAG_NOT_SELECTABLE = 1 << 0,
+	}
+}
