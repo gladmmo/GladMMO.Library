74ddae42f96f90e51c1c37403939c91344923deb
HelloKitty <andrew.blakely@ymail.com>
2019-11-11 01:31:36 -0600

    Added CancelSpellCastMessage

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Shared/Messages/Spell/CancelSpellCastMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class CancelSpellCastMessage : EntityActorMessage
+	{
+		//TODO: Provide reason?
+
+		public CancelSpellCastMessage()
+		{
+			
+		}
+	}
+}
