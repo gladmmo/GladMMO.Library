529267399c55e1f387940352590123d12d0289e3
HelloKitty <andrew.blakely@ymail.com>
2019-11-11 01:32:30 -0600

    Added InterruptCastingOnMovementMessageHandler

---
+++ src/Server/GladMMO.Server.ZoneServer/Actor/Player/Handlers/Movement/InterruptCastingOnMovementMessageHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[EntityActorMessageHandler(typeof(DefaultPlayerEntityActor))]
+	public sealed class InterruptCastingOnMovementMessageHandler : BaseEntityActorMessageHandler<DefaultEntityActorStateContainer, PlayerMovementStateChangedMessage>
+	{
+		private IReadonlyEntityGuidMappable<PendingSpellCastData> PendingSpellCastMappable { get; }
+
+		private IReadonlyNetworkTimeService TimeService { get; }
+
+		public InterruptCastingOnMovementMessageHandler([NotNull] IReadonlyEntityGuidMappable<PendingSpellCastData> pendingSpellCastMappable,
+			[NotNull] IReadonlyNetworkTimeService timeService)
+		{
+			PendingSpellCastMappable = pendingSpellCastMappable ?? throw new ArgumentNullException(nameof(pendingSpellCastMappable));
+			TimeService = timeService ?? throw new ArgumentNullException(nameof(timeService));
+		}
+
+		protected override void HandleMessage(EntityActorMessageContext messageContext, DefaultEntityActorStateContainer state, PlayerMovementStateChangedMessage message)
+		{
+			//If we started moving then we should check the current spell cast.
+			if (message.isMoving)
+			{
+				if (PendingSpellCastMappable.ContainsKey(state.EntityGuid))
+				{
+					PendingSpellCastData castData = PendingSpellCastMappable.RetrieveEntity(state.EntityGuid);
+
+					if (castData.isInstantCast || castData.IsSpellcastFinished(TimeService.CurrentLocalTime))
+						return;
+
+					//Ok, so we're casting and moving. Let's cancel the cast.
+					messageContext.Entity.TellSelf(new CancelSpellCastMessage());
+				}
+			}
+		}
+	}
+}
