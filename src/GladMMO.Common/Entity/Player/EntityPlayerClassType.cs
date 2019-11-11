9056f32d8f8df2e063a338f56c09f80e8272910a
HelloKitty <andrew.blakely@ymail.com>
2019-11-11 17:35:42 -0600

    Added EntityPlayerClassType
    Just an initial version.

---
+++ src/GladMMO.Common/Entity/Player/EntityPlayerClassType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Enumeration of player selectable class types for characters.
+	/// </summary>
+	public enum EntityPlayerClassType
+	{
+		Unknown = 0,
+
+		Mage = 1,
+	}
+}
