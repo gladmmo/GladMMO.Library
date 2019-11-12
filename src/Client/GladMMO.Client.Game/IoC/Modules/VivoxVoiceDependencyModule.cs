e47e841604d937fda9c1c013aafacd01a61c05da
HelloKitty <andrew.blakely@ymail.com>
2019-11-12 17:29:06 -0600

    Added mobile/android module initialization for Vivox

--- src/Client/GladMMO.Client.Game/IoC/Modules/VivoxVoiceDependencyModule.cs
+++ src/Client/GladMMO.Client.Game/IoC/Modules/VivoxVoiceDependencyModule.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using System.Text;
 using Autofac;
+using UnityEngine;
 
 namespace GladMMO
 {
 	public sealed class VivoxVoiceDependencyModule : Module
 	{
+		public static bool isInternalAndroidModuleInitialized { get; set; } = false;
+
 		protected override void Load(ContainerBuilder builder)
 		{
 			builder.RegisterModule<VivoxAuthServiceDependencyAutofacModule>();
+
+			//Only call this once.
+			if (!isInternalAndroidModuleInitialized && Application.isMobilePlatform)
+			{
+				AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+				AndroidJavaObject activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+				AndroidJavaObject appContext = activity.Call<AndroidJavaObject>("getApplicationContext");
+				AndroidJavaClass pluginClass = new AndroidJavaClass("com.vivox.vivoxnative.VivoxNative");
+				pluginClass.CallStatic("init", appContext);
+
+				isInternalAndroidModuleInitialized = true;
+			}
 
 			builder.RegisterType<VivoxUnity.Client>()
 				.AsSelf()
