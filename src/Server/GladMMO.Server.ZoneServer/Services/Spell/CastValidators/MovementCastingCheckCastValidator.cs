c7ece9fb460ae9ded1c9a2c600165a7077ed5431
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 17:36:49 -0600

    Added MovementCastingCheckCastValidator

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/CastValidators/MovementCastingCheckCastValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SpellCastValidator]
+	public sealed class MovementCastingCheckCastValidator : ISpellCastValidator
+	{
+		private IReadonlyEntityGuidMappable<IMovementGenerator<GameObject>> MovementGeneratorMappable { get; }
+
+		public MovementCastingCheckCastValidator([NotNull] IReadonlyEntityGuidMappable<IMovementGenerator<GameObject>> movementGeneratorMappable)
+		{
+			MovementGeneratorMappable = movementGeneratorMappable ?? throw new ArgumentNullException(nameof(movementGeneratorMappable));
+		}
+
+		public SpellCastResult ValidateSpellCast(DefaultEntityActorStateContainer state, SpellDefinitionDataModel spellDefinition)
+		{
+			if (!MovementGeneratorMappable.RetrieveEntity(state.EntityGuid).isFinished)
+				return SpellCastResult.SPELL_FAILED_MOVING;
+
+			return SpellCastResult.SPELL_FAILED_SUCCESS;
+		}
+	}
+}
