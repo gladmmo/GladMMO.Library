51fac4b70d707748ac7bd24e057b6e8148c96747
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 14:33:07 -0600

    Added LevelLearnedSpellsDatabaseToTransportTypeConverter

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/Spell/LevelLearnedSpellsDatabaseToTransportTypeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class LevelLearnedSpellsDatabaseToTransportTypeConverter : ITypeConverterProvider<SpellLevelLearned, SpellLevelLearnedDefinition>
+	{
+		public SpellLevelLearnedDefinition Convert([NotNull] SpellLevelLearned fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			return new SpellLevelLearnedDefinition(fromObject.CharacterClassType, fromObject.LevelLearned, fromObject.SpellId);
+		}
+	}
+}
