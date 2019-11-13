2bc58580a65fd8a946e06b1d14b2d9191ecaa618
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 16:43:58 -0600

    Added client IKnownSpellSet

---
+++ src/Client/GladMMO.Client.Game/Collections/IKnownSpellSet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Collection for known spell ids.
+	/// </summary>
+	public interface IKnownSpellSet : IEnumerable<int>
+	{
+		bool Contains(int spellId);
+
+		bool Add(int spellId);
+	}
+}
