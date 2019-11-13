8c4f91293609c5d4b1c4e10973029acf44aafa13
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 17:12:37 -0600

    Added SpellCastValidatorAttribute

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/CastValidators/SpellCastValidatorAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GladMMO
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public sealed class SpellCastValidatorAttribute : Attribute
+	{
+		public SpellCastValidatorAttribute()
+		{
+			
+		}
+	}
+}
