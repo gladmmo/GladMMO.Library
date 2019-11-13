4d1da53d231d661c0faf046d477027537c2cad3f
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 17:03:33 -0600

    Added ILearnedSpellsCollection

---
+++ src/Server/GladMMO.Server.ZoneServer/Collections/DefaultLearnedSpellsCollection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DefaultLearnedSpellsCollection : ILearnedSpellsCollection
+	{
+		private Dictionary<long, SpellLevelLearnedDefinition> LevelLearnedDefinitions { get; } = new Dictionary<long, SpellLevelLearnedDefinition>(100);
+
+		public bool IsSpellKnown(int spellId, EntityPlayerClassType classType, int level)
+		{
+			long key = ComputeKey(spellId, classType);
+
+			if (LevelLearnedDefinitions.ContainsKey(key))
+				return LevelLearnedDefinitions[key].LevelLearned <= level;
+			else
+				return false; //not a level learned spell.
+		}
+
+		private long ComputeKey(int spellId, EntityPlayerClassType classType)
+		{
+			return ((long) spellId << 32) + (long)classType;
+		}
+	}
+}
---
+++ src/Server/GladMMO.Server.ZoneServer/Collections/ILearnedSpellsCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ILearnedSpellsCollection
+	{
+		/// <summary>
+		/// Indicates if a spell <see cref="spellId"/> is known/learned give the input composite key
+		/// <see cref="classType"/> and <see cref="level"/>.
+		/// </summary>
+		/// <param name="spellId">The ID of the spell to check.</param>
+		/// <param name="classType">The class type.</param>
+		/// <param name="level">The level to check for.</param>
+		/// <returns>True if the spell is learned/known.</returns>
+		bool IsSpellKnown(int spellId, EntityPlayerClassType classType, int level);
+	}
+}
