0d23f697db0c7b786d6e23f69362041fc5440df9
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 17:32:14 -0600

    Added SpellInProgressCastValidator

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/CastValidators/SpellInProgressCastValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[SpellCastValidator]
+	public sealed class SpellInProgressCastValidator : ISpellCastValidator
+	{
+		private IReadonlyEntityGuidMappable<PendingSpellCastData> PendingSpellCastMappable { get; }
+
+		private IReadonlyNetworkTimeService TimeService { get; }
+
+		public SpellInProgressCastValidator([NotNull] IReadonlyEntityGuidMappable<PendingSpellCastData> pendingSpellCastMappable, [NotNull] IReadonlyNetworkTimeService timeService)
+		{
+			PendingSpellCastMappable = pendingSpellCastMappable ?? throw new ArgumentNullException(nameof(pendingSpellCastMappable));
+			TimeService = timeService ?? throw new ArgumentNullException(nameof(timeService));
+		}
+
+		public SpellCastResult ValidateSpellCast(DefaultEntityActorStateContainer state, SpellDefinitionDataModel spellDefinition)
+		{
+			PendingSpellCastData pendingCast = PendingSpellCastMappable.RetrieveEntity(state.EntityGuid);
+
+			if(!pendingCast.IsSpellcastFinished(TimeService.CurrentLocalTime))
+				return SpellCastResult.SPELL_FAILED_SPELL_IN_PROGRESS;
+
+			return SpellCastResult.SPELL_FAILED_SUCCESS;
+		}
+	}
+}
