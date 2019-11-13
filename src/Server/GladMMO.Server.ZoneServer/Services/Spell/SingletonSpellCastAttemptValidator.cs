07aca61abc06af4b1c42a066e49e92398642f41d
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 17:25:44 -0600

    Added SingletonSpellCastAttemptValidator

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/SingletonSpellCastAttemptValidator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class SingletonSpellCastAttemptValidator : ISpellCastAttemptValidator
+	{
+		private ISpellCastValidator[] Validators { get; }
+
+		public SingletonSpellCastAttemptValidator([NotNull] IEnumerable<ISpellCastValidator> validators)
+		{
+			if (validators == null) throw new ArgumentNullException(nameof(validators));
+
+			Validators = validators.ToArray();
+		}
+
+		public SpellCastResult ValidateSpellCast(DefaultEntityActorStateContainer actorState, SpellDefinitionDataModel spellDefinition)
+		{
+			foreach (var validator in Validators)
+			{
+				SpellCastResult result = validator.ValidateSpellCast(actorState, spellDefinition);
+
+				//If not successful we failed and no longer need to validate. We know something is wrong.
+				if (result != SpellCastResult.SPELL_FAILED_SUCCESS)
+					return result;
+			}
+
+			return SpellCastResult.SPELL_FAILED_SUCCESS;
+		}
+	}
+}
