0f85846473980e561c4f9194927904b43fe49bbc
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 17:22:58 -0600

    Added ISpellCastAttemptValidator

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/ISpellCastAttemptValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ISpellCastAttemptValidator
+	{
+		/// <summary>
+		/// Indicates the spell cast result. Successful means it's fine.
+		/// Any other result indicates failure.
+		/// </summary>
+		/// <param name="actorState">The actor's state.</param>
+		/// <param name="spellDefinition">The spell definition.</param>
+		/// <returns>The potential failure result of spell cast validation.</returns>
+		SpellCastResult ValidateSpellCast(DefaultEntityActorStateContainer actorState, SpellDefinitionDataModel spellDefinition);
+	}
+}
