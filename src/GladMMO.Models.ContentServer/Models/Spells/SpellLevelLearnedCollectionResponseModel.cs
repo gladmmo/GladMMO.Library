abfb19c9878c35549be027831d26532cc6b27fa8
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 14:48:23 -0600

    Added SpellLevelLearnedCollectionResponseModel

---
+++ src/GladMMO.Models.ContentServer/Models/Spells/SpellLevelLearnedCollectionResponseModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class SpellLevelLearnedCollectionResponseModel
+	{
+		[JsonProperty(PropertyName = "LevelLearnedSpells")]
+		private SpellLevelLearnedDefinition[] _LevelLearnedSpells;
+
+		/// <summary>
+		/// Collection of all level learned spells.
+		/// </summary>
+		[JsonIgnore]
+		public IReadOnlyCollection<SpellLevelLearnedDefinition> LevelLearnedSpells => _LevelLearnedSpells;
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		[JsonConstructor]
+		internal SpellLevelLearnedCollectionResponseModel()
+		{
+
+		}
+	}
+}
