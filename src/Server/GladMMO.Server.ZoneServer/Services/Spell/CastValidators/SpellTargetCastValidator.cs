6ab07f9fa6286e1c953d8f56537cc6b596d621fc
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 17:37:00 -0600

    Added SpellTargetCastValidator

---
+++ src/Server/GladMMO.Server.ZoneServer/Services/Spell/CastValidators/SpellTargetCastValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[SpellCastValidator]
+	public sealed class SpellTargetCastValidator : ISpellCastValidator
+	{
+		private ISpellTargetValidator TargetValidator { get; }
+
+		public SpellTargetCastValidator([NotNull] ISpellTargetValidator targetValidator)
+		{
+			TargetValidator = targetValidator ?? throw new ArgumentNullException(nameof(targetValidator));
+		}
+
+		public SpellCastResult ValidateSpellCast(DefaultEntityActorStateContainer actorState, SpellDefinitionDataModel spellDefinition)
+		{
+			if (!TargetValidator.isSpellTargetViable(spellDefinition.SpellId, actorState))
+				return SpellCastResult.SPELL_FAILED_BAD_TARGETS;
+
+			return SpellCastResult.SPELL_FAILED_SUCCESS;
+		}
+	}
+}
