e3fcb29eb9abe3390ad7517033131960e619897b
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 22:00:50 -0600

    Added IMovementInputController

---
+++ src/Client/GladMMO.Client.Game/Services/Input/IMovementInputController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IMovementInputController
+	{
+		float CurrentHorizontal { get; }
+
+		float CurrentVertical { get; }
+	}
+}
