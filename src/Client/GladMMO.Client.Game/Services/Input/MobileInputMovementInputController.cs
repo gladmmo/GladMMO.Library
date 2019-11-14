2f9ccf5d7f7099a4ef0febee35e1a277b733b187
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 22:01:03 -0600

    Added MobileInputMovementInputController

---
+++ src/Client/GladMMO.Client.Game/Services/Input/MobileInputMovementInputController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class MobileInputMovementInputController : IMovementInputController
+	{
+		public float CurrentHorizontal => JoyStickController.JoystickAxis.x;
+
+		public float CurrentVertical => JoyStickController.JoystickAxis.y;
+
+		private MobileJoystickInputController JoyStickController { get; }
+
+		public MobileInputMovementInputController([NotNull] MobileJoystickInputController joyStickController)
+		{
+			JoyStickController = joyStickController ?? throw new ArgumentNullException(nameof(joyStickController));
+		}
+	}
+}
