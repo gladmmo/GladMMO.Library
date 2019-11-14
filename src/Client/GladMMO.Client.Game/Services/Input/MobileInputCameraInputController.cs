60b4d90e1cb2ab39f8b0898242c96afa09e10e8e
HelloKitty <andrew.blakely@ymail.com>
2019-11-14 15:26:47 -0600

    Added MobileInputCameraInputController

---
+++ src/Client/GladMMO.Client.Game/Services/Input/MobileInputCameraInputController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class MobileInputCameraInputController : ICameraInputController, IGameTickable
+	{
+		public Vector2 InputDelta { get; private set; } = Vector2.zero;
+
+		public float CurrentHorizontal => InputDelta.x;
+
+		public float CurrentVertical => InputDelta.y;
+
+		public void Tick()
+		{
+			Touch touch = Input.GetTouch(0);
+			
+			switch (touch.phase)
+			{
+				case TouchPhase.Began:
+					InputDelta = Vector2.zero;
+					break;
+				case TouchPhase.Moved:
+					InputDelta = touch.deltaPosition;
+					break;
+				case TouchPhase.Stationary:
+					InputDelta = Vector2.zero;
+					break;
+				case TouchPhase.Ended:
+					InputDelta = Vector2.zero;
+					break;
+				case TouchPhase.Canceled:
+					InputDelta = Vector2.zero;
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+	}
+}
