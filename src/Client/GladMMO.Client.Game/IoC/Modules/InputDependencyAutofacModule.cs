0f44d22dd02fef3df7efe53f8a4c17071538b22b
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 22:46:15 -0600

    Added InputDependencyAutofacModule

---
+++ src/Client/GladMMO.Client.Game/IoC/Modules/InputDependencyAutofacModule.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using GladMMO.Services.Input;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class InputDependencyAutofacModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			//If mobile we need to find the joystick
+			if (Application.isMobilePlatform)
+				RegisterMobileInput(builder);
+			else
+				RegisterDesktopInput(builder);
+		}
+
+		private void RegisterDesktopInput([NotNull] ContainerBuilder builder)
+		{
+			if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+			builder.RegisterType<DesktopInputMovementInputController>()
+				.As<IMovementInputController>()
+				.SingleInstance();
+		}
+
+		private static void RegisterMobileInput([NotNull] ContainerBuilder builder)
+		{
+			if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+			MobileJoystickInputController inputController = GameObject.FindObjectOfType<MobileJoystickInputController>();
+
+			builder.RegisterInstance(inputController)
+				.As<MobileJoystickInputController>();
+
+			builder.RegisterType<MobileInputMovementInputController>()
+				.As<IMovementInputController>()
+				.SingleInstance();
+		}
+	}
+}
