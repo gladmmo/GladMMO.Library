ae7ebc8bb8e95d9a014c32c84797de417fc29d5e
HelloKitty <andrew.blakely@ymail.com>
2019-11-14 15:17:08 -0600

    Added DesktopInputCameraInputController

---
+++ src/Client/GladMMO.Client.Game/Services/Input/DesktopInputCameraInputController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class DesktopInputCameraInputController : ICameraInputController
+	{
+		public float CurrentHorizontal => Input.GetAxis("Mouse X");
+
+		public float CurrentVertical => Input.GetAxis("Mouse Y");
+	}
+}
