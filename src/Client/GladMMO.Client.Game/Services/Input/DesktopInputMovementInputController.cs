1af1a5e013ac482868883d580f98d1cd4fad8f4c
HelloKitty <andrew.blakely@ymail.com>
2019-11-13 22:00:56 -0600

    Added DesktopInputMovementInputController

---
+++ src/Client/GladMMO.Client.Game/Services/Input/DesktopInputMovementInputController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO.Services.Input
+{
+	public sealed class DesktopInputMovementInputController : IMovementInputController
+	{
+		public float CurrentHorizontal => UnityEngine.Input.GetAxisRaw("Horizontal");
+
+		public float CurrentVertical => UnityEngine.Input.GetAxisRaw("Vertical");
+	}
+}
