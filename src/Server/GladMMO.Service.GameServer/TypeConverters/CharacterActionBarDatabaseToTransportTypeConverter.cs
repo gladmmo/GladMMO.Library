55d6325acb3f4a7fbbb768926c6750ef951aa79a
HelloKitty <andrew.blakely@ymail.com>
2019-11-16 14:59:41 -0600

    Added CharacterActionBarDatabaseToTransportTypeConverter

---
+++ src/Server/GladMMO.Service.GameServer/TypeConverters/CharacterActionBarDatabaseToTransportTypeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class CharacterActionBarDatabaseToTransportTypeConverter : ITypeConverterProvider<CharacterActionBarEntry, CharacterActionBarInstanceModel>
+	{
+		public CharacterActionBarInstanceModel Convert([NotNull] CharacterActionBarEntry fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			return new CharacterActionBarInstanceModel(fromObject.BarIndex, fromObject.ActionType, fromObject.ActionId);
+		}
+	}
+}
