062450ec6275450db6e25021288579b09c3e2f1b
HelloKitty <andrew.blakely@ymail.com>
2019-11-16 17:04:15 -0600

    Added ICharacterActionBarEntry

---
+++ src/Server/GladMMO.Database.GameServer/Models/Character/ICharacterActionBarEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ICharacterActionBarEntry
+	{
+		ActionBarIndex BarIndex { get; }
+
+		[CanBeNull]
+		int? LinkedSpellId { get; }
+
+		ActionBarIndexType ActionType { get; }
+	}
+}
