588352cd7a65af63b427a2fe5b3c014bcb1338a5
HelloKitty <andrew.blakely@ymail.com>
2019-11-18 17:09:17 -0600

    Added RequestPlayerActionBarsEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnLocalPlayerSpawned/RequestPlayerActionBarsEventListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using Nito.AsyncEx;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class RequestPlayerActionBarsEventListener : OnLocalPlayerSpawnedEventListener
+	{
+		private ICharacterService CharacterService { get; }
+
+		private IReadonlyLocalPlayerDetails PlayerDetails { get; }
+
+		private IActionBarCollection ActionBarCollection { get; }
+
+		public RequestPlayerActionBarsEventListener(ILocalPlayerSpawnedEventSubscribable subscriptionService,
+			[NotNull] ICharacterService characterService,
+			[NotNull] IReadonlyLocalPlayerDetails playerDetails,
+			[NotNull] IActionBarCollection actionBarCollection) 
+			: base(subscriptionService)
+		{
+			CharacterService = characterService ?? throw new ArgumentNullException(nameof(characterService));
+			PlayerDetails = playerDetails ?? throw new ArgumentNullException(nameof(playerDetails));
+			ActionBarCollection = actionBarCollection ?? throw new ArgumentNullException(nameof(actionBarCollection));
+		}
+
+		protected override void OnLocalPlayerSpawned(LocalPlayerSpawnedEventArgs args)
+		{
+			UnityAsyncHelper.UnityMainThreadContext.PostAsync(async () =>
+			{
+				CharacterActionBarInstanceModel[] barInstanceModels = await CharacterService.GetCharacterActionBarDataAsync(PlayerDetails.LocalPlayerGuid.EntityId);
+
+				foreach (CharacterActionBarInstanceModel actionBarModel in barInstanceModels)
+					ActionBarCollection.Add(actionBarModel);
+			});
+		}
+	}
+}
