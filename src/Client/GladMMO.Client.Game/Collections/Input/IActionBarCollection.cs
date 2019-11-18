46f5bfdca8cf3ea119982a63e69cdb57dc7f46b5
HelloKitty <andrew.blakely@ymail.com>
2019-11-18 16:00:01 -0600

    Added IActionBarCollection

---
+++ src/Client/GladMMO.Client.Game/Collections/Input/IActionBarCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO.Input
+{
+	public interface IReadonlyActionBarCollection : IReadOnlyCollection<CharacterActionBarInstanceModel>
+	{
+		/// <summary>
+		/// Indicates if an action bar index are set.
+		/// AKA non-empty.
+		/// </summary>
+		/// <param name="index">The actionbar index.</param>
+		/// <returns>True if the actionbar index is set.</returns>
+		bool IsSet(ActionBarIndex index);
+	}
+
+	public interface IActionBarCollection : IReadonlyActionBarCollection
+	{
+		void Add(CharacterActionBarInstanceModel actionBarModel);
+	}
+}
