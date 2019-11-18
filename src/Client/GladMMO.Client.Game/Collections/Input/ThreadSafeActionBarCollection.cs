17f9a487ab7875604c91519717191d2663564d65
HelloKitty <andrew.blakely@ymail.com>
2019-11-18 17:06:50 -0600

    Added ThreadSafeActionBarCollection

---
+++ src/Client/GladMMO.Client.Game/Collections/Input/ThreadSafeActionBarCollection.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class ThreadSafeActionBarCollection : IActionBarCollection
+	{
+		private ConcurrentDictionary<ActionBarIndex, CharacterActionBarInstanceModel> BackingCollection { get; }
+
+		public ThreadSafeActionBarCollection()
+		{
+			BackingCollection = new ConcurrentDictionary<ActionBarIndex, CharacterActionBarInstanceModel>();
+		}
+
+		public IEnumerator<CharacterActionBarInstanceModel> GetEnumerator()
+		{
+			return BackingCollection.Values.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public int Count => BackingCollection.Count;
+
+		public bool IsSet(ActionBarIndex index)
+		{
+			if (BackingCollection.ContainsKey(index))
+				return BackingCollection[index].Type != ActionBarIndexType.Empty;
+			else
+				return false;
+		}
+
+		public void Add([NotNull] CharacterActionBarInstanceModel actionBarModel)
+		{
+			if (actionBarModel == null) throw new ArgumentNullException(nameof(actionBarModel));
+
+			//TODO: Throw if fails.
+			BackingCollection.TryAdd(actionBarModel.BarIndex, actionBarModel);
+		}
+	}
+}
