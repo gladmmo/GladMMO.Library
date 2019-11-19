197b2f85481b415fced2af36eeb59bd24b69b37c
HelloKitty <andrew.blakely@ymail.com>
2019-11-19 05:00:26 -0600

    Fixed DatabaseBackedContentIconEntryRepository

--- src/Server/GladMMO.Database.GameServer/Repositories/ClientContentOnly/DatabaseBackedContentIconEntryRepository.cs
+++ src/Server/GladMMO.Database.GameServer/Repositories/ClientContentOnly/DatabaseBackedContentIconEntryRepository.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace GladMMO
 {
-	public sealed class DatabaseBackedContentIconEntryRepository : BaseGenericBackedDatabaseRepository<CharacterDatabaseContext, int, ContentIconEntryModel>, IContentIconEntryModelRepository
+	public sealed class DatabaseBackedContentIconEntryRepository : BaseGenericBackedDatabaseRepository<ContentDatabaseContext, int, ContentIconEntryModel>, IContentIconEntryModelRepository
 	{
-		public DatabaseBackedContentIconEntryRepository([JetBrains.Annotations.NotNull] CharacterDatabaseContext context)
+		public DatabaseBackedContentIconEntryRepository([JetBrains.Annotations.NotNull] ContentDatabaseContext context)
 			: base(context)
 		{
 
+		}
+
+		public Task<ContentIconEntryModel[]> RetrieveAllAsync(CancellationToken cancellationToken = default(CancellationToken))
+		{
+			return Context.ContentIcons
+				.ToArrayAsync(cancellationToken);
 		}
 	}
 }
