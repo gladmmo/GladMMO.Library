ab5ea136ac0afacd3d5a93b34c3843bc7b608ce9
HelloKitty <andrew.blakely@ymail.com>
2019-11-19 01:36:42 -0600

    Added ContentIconStaticClientOnlyController

---
+++ src/Server/GladMMO.Service.ContentServer/Controllers/StaticContent/ContentIconStaticClientOnlyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	[Route("api/ContentIcon")]
+	public sealed class ContentIconStaticClientOnlyController : StaticClientOnlyContentController<ContentIconEntryModel, IContentIconEntryModelRepository, ContentIconEntryModel>
+	{
+		public ContentIconStaticClientOnlyController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger)
+		{
+
+		}
+	}
+}
