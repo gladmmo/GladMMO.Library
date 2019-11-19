3185a3c65cc54e0d1cdc1ebf6a14295121e3b36a
HelloKitty <andrew.blakely@ymail.com>
2019-11-18 22:33:45 -0600

    Added IEntireTableQueryable

---
+++ src/Server/GladMMO.Database.Common/Repositories/IEntireTableQueryable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public interface IEntireTableQueryable<TContentType>
+	{
+		Task<TContentType[]> RetrieveAllAsync(CancellationToken cancellationToken = default(CancellationToken));
+	}
+}
