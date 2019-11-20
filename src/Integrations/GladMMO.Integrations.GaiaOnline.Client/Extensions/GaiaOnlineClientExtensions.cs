0a264bb6d6ff253fbc661d0da2193d7e008f8042
HelloKitty <andrew.blakely@ymail.com>
2019-11-19 18:14:27 -0600

    Added GaiaOnlineClientExtensions

---
+++ src/Integrations/GladMMO.Integrations.GaiaOnline.Client/Extensions/GaiaOnlineClientExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO.GaiaOnline
+{
+	public static class GaiaOnlineClientExtensions
+	{
+		/// <summary>
+		/// Calls <see cref="IGaiaOnlineImageCDNClient"/>.GetAvatarImageBytesAsync
+		/// </summary>
+		/// <param name="client">The gaia image CDN client.</param>
+		/// <param name="uniqueAvatarUrl">The Avatar URL.</param>
+		/// <returns>Texture2D wrapper</returns>
+		public static async Task<Texture2DWrapper> GetAvatarImageAsync([NotNull] this IGaiaOnlineImageCDNClient client, string uniqueAvatarUrl)
+		{
+			if (client == null) throw new ArgumentNullException(nameof(client));
+
+			byte[] imageBytes = await client.GetAvatarImageBytesAsync(uniqueAvatarUrl);
+
+			return new Texture2DWrapper(imageBytes); 
+		}
+	}
+}
