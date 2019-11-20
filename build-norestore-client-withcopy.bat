c3aa0e1c6b3ccd363f6a3614026091ec2437dfbd
HelloKitty <andrew.blakely@ymail.com>
2019-11-19 22:15:51 -0600

    Added client copy to GaiaOnline project

--- build-norestore-client-withcopy.bat
+++ build-norestore-client-withcopy.bat
@@ -2,4 +2,5 @@ call build-norestore-client.bat
 
 xcopy build\client  C:\Users\Glader\Documents\GitHub\GladMMO.Client\Assets\DLLs /Y /q /EXCLUDE:BuildExclude.txt
 xcopy build\client  C:\Users\Glader\Documents\GitHub\VRGuardians.Client\Assets\DLLs /Y /q /EXCLUDE:BuildExclude.txt
+xcopy build\client  "C:\Users\Glader\Documents\Unity Projects\VRGuardians.GaiaOnline\Assets\DLLs" /Y /q /EXCLUDE:BuildExclude.txt
 PAUSE
