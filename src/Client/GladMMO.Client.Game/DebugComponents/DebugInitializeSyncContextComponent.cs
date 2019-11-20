0e9cc902e8b0fcee7e6bf7ecd213647716f783bd
HelloKitty <andrew.blakely@ymail.com>
2019-11-19 22:31:31 -0600

    Added DebugInitializeSyncContextComponent

---
+++ src/Client/GladMMO.Client.Game/DebugComponents/DebugInitializeSyncContextComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class DebugInitializeSyncContextComponent : MonoBehaviour
+	{
+		private void Awake()
+		{
+			UnityAsyncHelper.InitializeSyncContext();
+		}
+	}
+}
