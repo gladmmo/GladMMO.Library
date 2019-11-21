9d8edc93d4f1300b49882c022f796510dc545db5
HelloKitty <andrew.blakely@ymail.com>
2019-11-20 21:35:42 -0600

    Removed LocalClientInterpolatedCorrectionMovementGenerator from client

--- src/Client/GladMMO.Client.Game/Services/Factory/ClientMovementGeneratorFactory.cs
+++ src/Client/GladMMO.Client.Game/Services/Factory/ClientMovementGeneratorFactory.cs
@@ -62,7 +62,7 @@ namespace GladMMO
 			if (context.Data is PositionChangeMovementData pcmd)
 			{
 				//Warning, this following code is ONLY for client. To smooth out serverside corrections
-				if (pcmd.Direction == Vector2.zero)
+				/*if (pcmd.Direction == Vector2.zero)
 				{
 					return new LocalClientInterpolatedCorrectionMovementGenerator(pcmd, BuildLazyControllerFactory(context), LocalPlayerDetails.LocalPlayerGuid != context.EntityGuid);
 				}
@@ -70,7 +70,10 @@ namespace GladMMO
 				{
 					//The reason we use a lazy here is because we can't promise that the character controller exists AT ALL at this point sadly.
 					return new ClientCharacterControllerInputMovementGenerator(pcmd, BuildLazyControllerFactory(context), LocalPlayerDetails.LocalPlayerGuid != context.EntityGuid);
-				}
+				}*/
+
+				//The reason we use a lazy here is because we can't promise that the character controller exists AT ALL at this point sadly.
+				return new ClientCharacterControllerInputMovementGenerator(pcmd, BuildLazyControllerFactory(context), LocalPlayerDetails.LocalPlayerGuid != context.EntityGuid);
 			}
 
 			throw new NotSupportedException($"TODO: Encountered unsupported movement data: {context.Data.GetType().Name}");
