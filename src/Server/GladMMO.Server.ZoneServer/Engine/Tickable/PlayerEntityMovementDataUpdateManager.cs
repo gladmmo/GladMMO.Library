d642ed00859ee52d0745531d9f232db4f066ef68
HelloKitty <andrew.blakely@ymail.com>
2019-11-20 21:56:04 -0600

    Updated locking in PlayerEntityMovementDataUpdateManagaer

--- src/Server/GladMMO.Server.ZoneServer/Engine/Tickable/PlayerEntityMovementDataUpdateManager.cs
+++ src/Server/GladMMO.Server.ZoneServer/Engine/Tickable/PlayerEntityMovementDataUpdateManager.cs
@@ -18,28 +18,23 @@ namespace GladMMO
 
 		private IDirtyableMovementDataCollection MovementCollection { get; }
 
-		/// <summary>
-		/// The collections locking policy.
-		/// </summary>
-		private GlobalEntityCollectionsLockingPolicy LockingPolicy { get; }
-
 		/// <inheritdoc />
 		public PlayerEntityMovementDataUpdateManager(
 			[NotNull] IPlayerEntityGuidEnumerable playerGuids, 
 			[NotNull] INetworkMessageSender<EntityMovementMessageContext> movementUpdateMessageSender,
-			[NotNull] IDirtyableMovementDataCollection movementCollection,
-			[NotNull] GlobalEntityCollectionsLockingPolicy lockingPolicy)
+			[NotNull] IDirtyableMovementDataCollection movementCollection)
 		{
 			PlayerGuids = playerGuids ?? throw new ArgumentNullException(nameof(playerGuids));
 			MovementUpdateMessageSender = movementUpdateMessageSender ?? throw new ArgumentNullException(nameof(movementUpdateMessageSender));
 			MovementCollection = movementCollection ?? throw new ArgumentNullException(nameof(movementCollection));
-			LockingPolicy = lockingPolicy ?? throw new ArgumentNullException(nameof(lockingPolicy));
 		}
 
 		/// <inheritdoc />
 		public void Tick()
 		{
-			using(LockingPolicy.ReaderLock(null, CancellationToken.None))
+			//TODO: Reduce lock contention.
+			MovementCollection.SyncObject.EnterReadLock();
+			try
 			{
 				//For every player we need to do some processing so that we can send a movement update
 				//packet for them.
@@ -52,6 +47,10 @@ namespace GladMMO
 
 				//After all movement is done we need to clear all tracked/dirty changes in the movement collection
 				MovementCollection.ClearDirty();
+			}
+			finally
+			{
+				MovementCollection.SyncObject.ExitReadLock();
 			}
 		}
 	}
