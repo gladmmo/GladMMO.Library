1698297260ace89716fd155c59da7adbe65e9d06
HelloKitty <andrew.blakely@ymail.com>
2019-11-21 01:08:16 -0600

    Added IAvatarInitializable

---
+++ src/Integrations/GladMMO.Integrations.Client.Common/Avatar/IAvatarInitializable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public interface IAvatarInitializable
+	{
+		/// <summary>
+		/// Initializes the avatar.
+		/// </summary>
+		/// <param name="entityGuidOwner">The entity guid of the avatar owner.</param>
+		/// <param name="entityName">The awaitable name of the entity. (Task because it may not be available immediately upon initialization)</param>
+		void InitializeAvatar(NetworkEntityGuid entityGuidOwner, Task<string> entityName);
+	}
+}
