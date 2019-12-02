1fb4bff4e6211f42ea886239116f5486153762e7
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 11:34:18 -0600

    Added StaticGameContentAutofacModule

---
+++ src/Client/GladMMO.Client.Game/IoC/Modules/StaticGameContentAutofacModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace GladMMO
+{
+	public sealed class StaticGameContentAutofacModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<DefaultContentIconDataCollection>()
+				.As<IContentIconDataCollection>()
+				.SingleInstance();
+		}
+	}
+}
