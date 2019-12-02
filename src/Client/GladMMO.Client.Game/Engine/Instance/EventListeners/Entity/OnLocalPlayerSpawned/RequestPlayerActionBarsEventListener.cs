c4ca960fa80deee9e4fc06c0b198b644b9c71786
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 09:51:21 -0600

    Added actionbar state change broadcasting to RequestPlayerActionBarsEventListener

--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnLocalPlayerSpawned/RequestPlayerActionBarsEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnLocalPlayerSpawned/RequestPlayerActionBarsEventListener.cs
@@ -15,15 +15,19 @@ namespace GladMMO
 
 		private IActionBarCollection ActionBarCollection { get; }
 
+		private IActionBarButtonStateChangedEventPublisher ActionBarStateChangePublisher { get; }
+
 		public RequestPlayerActionBarsEventListener(ILocalPlayerSpawnedEventSubscribable subscriptionService,
 			[NotNull] ICharacterService characterService,
 			[NotNull] IReadonlyLocalPlayerDetails playerDetails,
-			[NotNull] IActionBarCollection actionBarCollection) 
+			[NotNull] IActionBarCollection actionBarCollection,
+			[NotNull] IActionBarButtonStateChangedEventPublisher actionBarStateChangePublisher) 
 			: base(subscriptionService)
 		{
 			CharacterService = characterService ?? throw new ArgumentNullException(nameof(characterService));
 			PlayerDetails = playerDetails ?? throw new ArgumentNullException(nameof(playerDetails));
 			ActionBarCollection = actionBarCollection ?? throw new ArgumentNullException(nameof(actionBarCollection));
+			ActionBarStateChangePublisher = actionBarStateChangePublisher ?? throw new ArgumentNullException(nameof(actionBarStateChangePublisher));
 		}
 
 		protected override void OnLocalPlayerSpawned(LocalPlayerSpawnedEventArgs args)
@@ -33,7 +37,10 @@ namespace GladMMO
 				CharacterActionBarInstanceModel[] barInstanceModels = await CharacterService.GetCharacterActionBarDataAsync(PlayerDetails.LocalPlayerGuid.EntityId);
 
 				foreach (CharacterActionBarInstanceModel actionBarModel in barInstanceModels)
+				{
+					ActionBarStateChangePublisher.PublishEvent(this, new ActionBarButtonStateChangedEventArgs(actionBarModel.BarIndex, actionBarModel.Type, actionBarModel.ActionId));
 					ActionBarCollection.Add(actionBarModel);
+				}
 			});
 		}
 	}
