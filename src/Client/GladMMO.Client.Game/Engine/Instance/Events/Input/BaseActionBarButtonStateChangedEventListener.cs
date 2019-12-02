830c8ae94f645511f46a5e7506403c91bd2d2ffb
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 08:31:44 -0600

    Added BaseActionBarButtonStateChangedEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Input/BaseActionBarButtonStateChangedEventListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class BaseActionBarButtonStateChangedEventListener : BaseSingleEventListenerInitializable<IActionBarButtonStateChangedEventSubscribable, ActionBarButtonStateChangedEventArgs>
+	{
+		protected BaseActionBarButtonStateChangedEventListener(IActionBarButtonStateChangedEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
