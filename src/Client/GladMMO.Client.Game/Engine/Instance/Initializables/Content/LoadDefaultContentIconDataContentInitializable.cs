d6d2f04378e8b08103e349058c994f548bd083b6
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 11:35:23 -0600

    Added LoadDefaultContentIconDataContentInitializable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Initializables/Content/LoadDefaultContentIconDataContentInitializable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class LoadDefaultContentIconDataContentInitializable : IGameInitializable
+	{
+		private IStaticContentDataServiceClient DataClient { get; }
+
+		private IContentIconDataCollection ContentIconCollection { get; }
+
+		public LoadDefaultContentIconDataContentInitializable([NotNull] IStaticContentDataServiceClient dataClient,
+			[NotNull] IContentIconDataCollection contentIconCollection)
+		{
+			DataClient = dataClient ?? throw new ArgumentNullException(nameof(dataClient));
+			ContentIconCollection = contentIconCollection ?? throw new ArgumentNullException(nameof(contentIconCollection));
+		}
+
+		public async Task OnGameInitialized()
+		{
+			ContentIconInstanceModel[] iconModels = await DataClient.ContentIconsAsync();
+
+			foreach (var icon in iconModels)
+			{
+				ContentIconCollection.Add(icon.IconId, icon);
+			}
+		}
+	}
+}
