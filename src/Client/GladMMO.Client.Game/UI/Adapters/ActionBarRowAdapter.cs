d11b2e917f7e38a8ac654ca93d4ae49159131236
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 12:11:49 -0600

    Fixed ActionBarRowAdapter initialization order

--- src/Client/GladMMO.Client.Game/UI/Adapters/ActionBarRowAdapter.cs
+++ src/Client/GladMMO.Client.Game/UI/Adapters/ActionBarRowAdapter.cs
@@ -16,7 +16,7 @@ namespace GladMMO
 
 		//The buttons for the row.
 		[SerializeField]
-		private List<ActionBarButtonAdapter> ActionBarButtons = new List<ActionBarButtonAdapter>(0);
+		private List<ActionBarButtonAdapter> ActionBarButtons;
 
 		[SerializeField]
 		private ActionBarIndex _startIndex;
@@ -30,7 +30,7 @@ namespace GladMMO
 
 		private Dictionary<ActionBarIndex, IUIActionBarButton> ActionBarCollection { get; } = new Dictionary<ActionBarIndex, IUIActionBarButton>(13);
 
-		public ActionBarRowAdapter()
+		private void Start()
 		{
 			if (ActionBarButtons.Count <= 0)
 				throw new InvalidOperationException($"Cannot have actionbar row with 0 actionbar buttons.");
