0d4157c56e047f728c83ad6250fbcb4a60ba379e
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 09:55:45 -0600

    Added ActionBarButtonImageChangeEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Input/ActionBarButtonImageChangeEventListener.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Common.Logging;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ActionBarButtonImageChangeEventListener : EventQueueBasedTickable<IActionBarButtonStateChangedEventSubscribable, ActionBarButtonStateChangedEventArgs>
+	{
+		//TODO: Support multiple actionbars.
+		private IUIActionBarRow ActionBarRow { get; }
+
+		public ActionBarButtonImageChangeEventListener(IActionBarButtonStateChangedEventSubscribable subscriptionService,
+			[NotNull] ILog logger,
+			[KeyFilter(UnityUIRegisterationKey.ActionBarRow1)] [NotNull] IUIActionBarRow actionBarRow) 
+			: base(subscriptionService, false, logger)
+		{
+			ActionBarRow = actionBarRow ?? throw new ArgumentNullException(nameof(actionBarRow));
+		}
+
+		protected override void HandleEvent(ActionBarButtonStateChangedEventArgs args)
+		{
+			//TODO: Check actionbar index and handle multiple rows.
+			if (ActionBarRow.ContainsKey(args.Index))
+			{
+				IUIActionBarButton barButton = ActionBarRow[args.Index];
+
+				if (args.ActionType == ActionBarIndexType.Empty)
+				{
+					barButton.SetElementActive(false);
+				}
+				else
+				{
+					barButton.SetElementActive(true);
+
+					//TODO: Set the action bar icon image.
+					if(Logger.IsWarnEnabled)
+						Logger.Warn($"TODO: Icon handling for actionbar.");
+				}
+			}
+		}
+	}
+}
