f8e5d36779d8c33961e6c9fa7c39e2cc40a03258
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 05:54:28 -0600

    Added IUIActionBarRow

---
+++ src/Client/GladMMO.Client.Game/UI/Interfaces/IUIActionBarRow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public class ActionBarButtonComposite
+	{
+		public IUIButton ActionBarButton { get; }
+
+		public IUIImage ActionBarImageIcon { get; }
+
+		public ActionBarButtonComposite([NotNull] IUIButton actionBarButton, [NotNull] IUIImage actionBarImageIcon)
+		{
+			ActionBarButton = actionBarButton ?? throw new ArgumentNullException(nameof(actionBarButton));
+			ActionBarImageIcon = actionBarImageIcon ?? throw new ArgumentNullException(nameof(actionBarImageIcon));
+		}
+	}
+
+	public interface IUIActionBarRow : IReadOnlyDictionary<ActionBarIndex, ActionBarButtonComposite>
+	{
+		
+	}
+}
