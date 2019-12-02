0ecb0212a4321dc97b8e12d9087ff9d421b59368
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 09:38:36 -0600

    Added ActionBarButtonStateChangedEventPublisher

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventPublishers/Input/ActionBarButtonStateChangedEventPublisher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IActionBarStateChangedEventPublisher : IEventPublisher<IActionBarButtonStateChangedEventSubscribable, ActionBarButtonStateChangedEventArgs>
+	{
+
+	}
+
+	[AdditionalRegisterationAs(typeof(IActionBarButtonStateChangedEventSubscribable))]
+	[AdditionalRegisterationAs(typeof(IActionBarStateChangedEventPublisher))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ActionBarButtonStateChangedEventPublisher : IActionBarStateChangedEventPublisher, IActionBarButtonStateChangedEventSubscribable, IGameInitializable
+	{
+		public event EventHandler<ActionBarButtonStateChangedEventArgs> OnActionBarButtonStateChanged;
+
+		public void PublishEvent(object sender, ActionBarButtonStateChangedEventArgs eventArgs)
+		{
+			OnActionBarButtonStateChanged?.Invoke(sender, eventArgs);
+		}
+
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
