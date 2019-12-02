1b2d3d3cff009e467815bd6e3bcfbd45a8bb9ecc
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 06:12:44 -0600

    Added Start/End index of IUIActionBarRow

--- src/Client/GladMMO.Client.Game/UI/Interfaces/IUIActionBarRow.cs
+++ src/Client/GladMMO.Client.Game/UI/Interfaces/IUIActionBarRow.cs
@@ -20,6 +20,14 @@ namespace GladMMO
 
 	public interface IUIActionBarRow : IReadOnlyDictionary<ActionBarIndex, ActionBarButtonComposite>
 	{
-		
+		/// <summary>
+		/// The starting action bar index for the row.
+		/// </summary>
+		ActionBarIndex StartIndex { get; }
+
+		/// <summary>
+		/// The ending action bar index for the row.
+		/// </summary>
+		ActionBarIndex EndIndex { get; }
 	}
 }
