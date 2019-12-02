d543ea03437cb9f21766cad8d1dab16c072ad6fb
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 10:10:56 -0600

    Added ContentIconInstanceModel

---
+++ src/GladMMO.Models.ContentServer/Models/ContentIconInstanceModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class ContentIconInstanceModel : IContentIconEntry
+	{
+		[JsonProperty]
+		public int IconId { get; private set; }
+
+		[JsonProperty]
+		public string IconPathName { get; private set; }
+
+		public ContentIconInstanceModel(int iconId, [NotNull] string iconPathName)
+		{
+			if (iconId <= 0) throw new ArgumentOutOfRangeException(nameof(iconId));
+			if (string.IsNullOrWhiteSpace(iconPathName)) throw new ArgumentException("Value cannot be null or whitespace.", nameof(iconPathName));
+
+			IconId = iconId;
+			IconPathName = iconPathName;
+		}
+
+		[JsonConstructor]
+		internal ContentIconInstanceModel()
+		{
+
+		}
+	}
+}
