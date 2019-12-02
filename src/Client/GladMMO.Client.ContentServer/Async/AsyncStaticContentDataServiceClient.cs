0cce5cd8beb7fd8597cb9607ddc3008418906420
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 10:27:57 -0600

    Added IStaticContentDataServiceClient

---
+++ src/Client/GladMMO.Client.ContentServer/Async/AsyncStaticContentDataServiceClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncStaticContentDataServiceClient : BaseAsyncEndpointService<IStaticContentDataServiceClient>, IStaticContentDataServiceClient
+	{
+		/// <inheritdoc />
+		public AsyncStaticContentDataServiceClient(Task<string> futureEndpoint) 
+			: base(futureEndpoint)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public AsyncStaticContentDataServiceClient(Task<string> futureEndpoint, RefitSettings settings) 
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		public async Task<ContentIconInstanceModel[]> ContentIconsAsync()
+		{
+			return await (await GetService().ConfigureAwait(false)).ContentIconsAsync().ConfigureAwait(false);
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.ContentServer/StaticContent/IStaticContentDataServiceClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	//TODO: Automate user-agent SDK version headers
+	[Headers("User-Agent: SDK 0.0.1")]
+	public interface IStaticContentDataServiceClient
+	{
+		//TODO: Enable caching for production.
+		//[Headers("Cache-Control: max-age=5000")]
+		[Get("/api/ContentIcon")]
+		Task<ContentIconInstanceModel[]> ContentIconsAsync();
+	}
+}
