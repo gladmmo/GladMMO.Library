74afddff89852f0f7d39c27b3b5f90dbd7d872b8
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 10:13:23 -0600

    Added ContentIconDatabaseToTransportTypeConverter

---
+++ src/Server/GladMMO.Service.ContentServer/TypeConverters/StaticContent/ContentIconDatabaseToTransportTypeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class ContentIconDatabaseToTransportTypeConverter : ITypeConverterProvider<ContentIconEntryModel, ContentIconInstanceModel>
+	{
+		public ContentIconInstanceModel Convert([NotNull] ContentIconEntryModel fromObject)
+		{
+			if (fromObject == null) throw new ArgumentNullException(nameof(fromObject));
+
+			return new ContentIconInstanceModel(fromObject.IconId, fromObject.IconPathName);
+		}
+	}
+}
