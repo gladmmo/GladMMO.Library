4333cdf089a1d1b77b59cf5b2bea4fde08129656
HelloKitty <andrew.blakely@ymail.com>
2019-12-02 10:09:36 -0600

    Added IContentIconEntry

---
+++ src/GladMMO.Models.ContentServer/Models/IContentIconEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IContentIconEntry
+	{
+		/// <summary>
+		/// The unique identifier for the icon entry.
+		/// </summary>
+		int IconId { get; }
+
+		/// <summary>
+		/// The relative path name WITH extension.
+		/// </summary>
+		[NotNull]
+		string IconPathName { get; }
+	}
+}
