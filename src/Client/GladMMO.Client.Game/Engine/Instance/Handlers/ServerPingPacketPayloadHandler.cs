88824de51097c68f0a4ab4abac3d52f75519bd87
HelloKitty <andrew.blakely@ymail.com>
2019-12-08 13:16:59 -0600

    Added ServerPingPacketPayloadHandler

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/ServerPingPacketPayloadHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+using GladMMO;
+using GladNet;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ServerPingPacketPayloadHandler : BaseGameClientGameMessageHandler<ServerPingPacketPayload>
+	{
+		public ServerPingPacketPayloadHandler(ILog logger)
+			: base(logger)
+		{
+
+		}
+
+		//TODO: This is a work in progress, we need a time service.
+		/// <inheritdoc />
+		public override Task HandleMessage(IPeerMessageContext<GameClientPacketPayload> context, ServerPingPacketPayload payload)
+		{
+			//We send time sync first since we need to have a good grasp of the current network time before
+			//we even spawn into the world and start recieveing the world states.
+			context.PayloadSendService.SendMessageImmediately(new ServerTimeSyncronizationRequestPayload(DateTime.UtcNow.Ticks))
+				.ConfigureAwait(false);
+
+			return Task.CompletedTask;
+		}
+	}
+}
