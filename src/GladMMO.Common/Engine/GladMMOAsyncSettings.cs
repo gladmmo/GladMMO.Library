14c85bfb9b42776ebd1caf330ea3db5af82a4aa3
HelloKitty <andrew.blakely@ymail.com>
2020-01-26 20:49:58 -0600

    Added GladMMOAsyncSettings

---
+++ src/GladMMO.Common/Engine/GladMMOAsyncSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public static class GladMMOAsyncSettings
+	{
+		//This mostly exists to combat issues with WebGL not supporting ConfigureAwait(False)
+		//as pointed about by the ridiculous issue listed as "By Design"
+		//here: https://issuetracker.unity3d.com/issues/webgl-task-awaited-with-task-dot-configureawait-false-does-not-continue-in-a-webgl-build
+		public static bool ConfigureAwaitFalseSupported { get; set; } = true;
+
+		public static ConfiguredTaskAwaitable<TResult> ConfigureAwaitFalse<TResult>(this Task<TResult> awaitable)
+		{
+			if (awaitable == null) throw new ArgumentNullException(nameof(awaitable));
+
+			//Readaibility wise it is simpler to exprese it this way
+			if (ConfigureAwaitFalseSupported)
+				return awaitable.ConfigureAwait(false);
+			else
+				return awaitable.ConfigureAwait(true);
+		}
+	}
+}
