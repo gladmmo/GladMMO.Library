aa64e9dc79ee171cce1545e0def65f8e84354567
HelloKitty <andrew.blakely@ymail.com>
2020-03-26 22:23:12 -0500

    Added TrinityCoreCharacterRepository

---
+++ src/Server/GladMMO.Database.TrinityCore.Character/Repositories/ITrinityCharacterRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// TrinityCore version of <see cref="ICharacterRepository"/>.
+	/// </summary>
+	public interface ITrinityCharacterRepository : 
+		IGenericRepositoryCrudable<int, Characters>, INameQueryableRepository<int>
+	{
+		//TODO: Doc
+		Task<Characters> RetrieveAsync(string characterName);
+
+		/// <summary>
+		/// Checks if the repository contains a model with the specified charactername.
+		/// </summary>
+		/// <param name="characterName">The character name to check.</param>
+		/// <returns>True if the name is taken.</returns>
+		Task<bool> ContainsAsync(string characterName);
+
+		/// <summary>
+		/// Tries to load all the characters with the provided <see cref="accountId"/>.
+		/// If none exist it will produce an empty collection.
+		/// </summary>
+		/// <param name="accountId">The account id to check.</param>
+		/// <returns></returns>
+		Task<int[]> CharacterIdsForAccountId(int accountId);
+	}
+}
---
+++ src/Server/GladMMO.Database.TrinityCore.Character/Repositories/TrinityCoreCharacterRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	public sealed class TrinityCoreCharacterRepository : BaseGenericBackedDatabaseRepository<wotlk_charactersContext, int, Characters>, ITrinityCharacterRepository
+	{
+		public TrinityCoreCharacterRepository(wotlk_charactersContext context) 
+			: base(context)
+		{
+
+		}
+
+		public async Task<string> RetrieveNameAsync(int key)
+		{
+			Characters character = await Context
+				.Characters
+				.FindAsync(key);
+
+			return character.Name;
+		}
+
+		public async Task<Characters> RetrieveAsync(string characterName)
+		{
+			Characters character = await Context
+				.Characters
+				.FirstAsync(c => c.Name == characterName.ToUpperInvariant());
+
+			return character;
+		}
+
+		public async Task<bool> ContainsAsync(string characterName)
+		{
+			return await Context
+				.Characters
+				.AnyAsync(c => c.Name == characterName.ToUpperInvariant());
+		}
+
+		public async Task<int[]> CharacterIdsForAccountId(int accountId)
+		{
+			//TODO: uint
+			return await Context
+				.Characters
+				.Where(c => c.Account == accountId)
+				.Select(c => c.Guid)
+				.Cast<int>()
+				.ToArrayAsync();
+		}
+	}
+}
