05ab6de9129bb5921009b39f1488b9e05b3ebc9b
HelloKitty <andrew.blakely@ymail.com>
2020-04-09 12:09:58 -0500

    Added IKeyedClientDataCollection

---
+++ src/GladMMO.Client.Common/Data/IKeyedClientDataCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IKeyedClientDataCollection<out TClientDataType> : IReadOnlyList<TClientDataType>, IReadOnlyCollection<TClientDataType>
+	{
+		bool Contains(int key);
+	}
+}
