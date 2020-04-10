31b4a26936f467e40495bcab4d63d79b67108ac9
HelloKitty <andrew.blakely@ymail.com>
2020-04-10 07:23:36 -0500

    Added ClientStaticDataDependencyAutofacModule

---
+++ src/GladMMO.Client.Common/IoC/Modules/ClientStaticDataDependencyAutofacModule.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using FreecraftCore;
+using FreecraftCore.Serializer;
+
+namespace GladMMO
+{
+	public sealed class ClientStaticDataDependencyAutofacModule : Module
+	{
+		private static IClientDataCollectionContainer CachedClientDataCollection { get; set; }
+
+		protected override void Load([NotNull] ContainerBuilder builder)
+		{
+			if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+			builder.Register<IClientDataCollectionContainer>(context =>
+			{
+				if (CachedClientDataCollection != null)
+					return CachedClientDataCollection;
+				else
+				{
+					CachedClientDataCollection = BuildClientDataCollection(context.Resolve<ISerializerService>());
+				}
+
+				return CachedClientDataCollection;
+			});
+		}
+
+		private IClientDataCollectionContainer BuildClientDataCollection([NotNull] ISerializerService serializer)
+		{
+			if (serializer == null) throw new ArgumentNullException(nameof(serializer));
+
+			//TODO: Multithreaded loading
+			var collectionContainer = new DefaultClientDataCollectionContainer(serializer);
+			collectionContainer.LoadDataAsync().Wait();
+
+			return collectionContainer;
+		}
+	}
+}
