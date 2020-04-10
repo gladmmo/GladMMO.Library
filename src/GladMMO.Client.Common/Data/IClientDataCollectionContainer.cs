b4a236c720b1790dd67e52aa320d523e164e736b
HelloKitty <andrew.blakely@ymail.com>
2020-04-10 09:50:58 -0500

    Added missing DataLoadingTask

--- src/GladMMO.Client.Common/Data/IClientDataCollectionContainer.cs
+++ src/GladMMO.Client.Common/Data/IClientDataCollectionContainer.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using FreecraftCore;
 
 namespace GladMMO
 {
 	public interface IClientDataCollectionContainer
 	{
+		/// <summary>
+		/// Await this if you're unsure data has finished loading.
+		/// </summary>
+		Task DataLoadingTask { get; }
+
 		IKeyedClientDataCollection<MapEntry<StringDBCReference<MapEntry<string>>>> MapEntry { get; }
 	}
 }
