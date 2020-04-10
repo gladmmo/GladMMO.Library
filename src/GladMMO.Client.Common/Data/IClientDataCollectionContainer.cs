2b23309e743c3e1d2423f8fa57e821ef4dcfebdb
HelloKitty <andrew.blakely@ymail.com>
2020-04-10 06:40:26 -0500

    Added IClientDataCollectionContainer

---
+++ src/GladMMO.Client.Common/Data/IClientDataCollectionContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+
+namespace GladMMO
+{
+	public interface IClientDataCollectionContainer
+	{
+		IKeyedClientDataCollection<MapEntry<StringDBCReference<MapEntry<string>>>> MapEntry { get; }
+	}
+}
