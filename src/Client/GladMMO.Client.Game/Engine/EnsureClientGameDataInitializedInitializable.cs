55cc61106d3947df13d27edc5786df8fe6107240
HelloKitty <andrew.blakely@ymail.com>
2020-04-10 07:50:49 -0500

    Added EnsureClientGameDataInitializedInitializable

---
+++ src/Client/GladMMO.Client.Game/Engine/EnsureClientGameDataInitializedInitializable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO.Engine
+{
+	[SceneTypeCreateGladMMO(GameSceneType.TitleScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterCreationScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	[SceneTypeCreateGladMMO(GameSceneType.PreZoneBurstingScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public class EnsureClientGameDataInitializedInitializable : IGameInitializable
+	{
+		private IClientDataCollectionContainer DataContainer { get; }
+
+		public EnsureClientGameDataInitializedInitializable([NotNull] IClientDataCollectionContainer dataContainer)
+		{
+			DataContainer = dataContainer ?? throw new ArgumentNullException(nameof(dataContainer));
+		}
+
+		public async Task OnGameInitialized()
+		{
+			return;
+		}
+	}
+}
