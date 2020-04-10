9031faaeafe4f8ace952da6c26c5696b4a04ef47
HelloKitty <andrew.blakely@ymail.com>
2020-04-10 07:49:55 -0500

    Added ClientDataCollectionExtensions

---
+++ src/GladMMO.Client.Common/Data/ClientDataCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+
+namespace GladMMO
+{
+	public static class ClientDataCollectionExtensions
+	{
+		internal static Dictionary<Type, IReadOnlyDictionary<uint, string>> InternalStringReferenceMap { get; } = new Dictionary<Type, IReadOnlyDictionary<uint, string>>();
+
+		public static string GetString<T>(this StringDBCReference<T> stringReference)
+		{
+			IReadOnlyDictionary<uint, string> references = InternalStringReferenceMap[typeof(T)];
+
+			if (references.ContainsKey(stringReference.StringReferenceOffset))
+				return references[stringReference.StringReferenceOffset];
+
+			return $"UNKNOWN_DBC_STRING_{typeof(T).Name}";
+		}
+	}
+}
