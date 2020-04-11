62bc8152725dd7bf11bcdc2fd36ee57cff3412fe
HelloKitty <andrew.blakely@ymail.com>
2020-04-11 13:20:02 -0500

    Added CrossSceneLivingObject component

---
+++ src/Client/GladMMO.Client.Game/Component/CrossSceneLivingObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class CrossSceneLivingObject : MonoBehaviour
+	{
+		[SerializeField]
+		private CrossSceneObjectType Type = CrossSceneObjectType.Unknown;
+
+		private void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/Management/Scene/CrossSceneObjectType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Enumeration of objects that should exist cross scene.
+	/// </summary>
+	public enum CrossSceneObjectType
+	{
+		Unknown = 0,
+		LoadingScreen = 1,
+	}
+}
