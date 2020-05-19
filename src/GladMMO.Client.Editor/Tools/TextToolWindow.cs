23f20f831e0605869e3899633928b77dfa777aca
HelloKitty <andrew.blakely@ymail.com>
2020-05-19 01:38:23 -0500

    Added non-dynamic font fix tool

---
+++ src/GladMMO.Client.Editor/Tools/TextToolWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	public sealed class TextToolWindow : EditorWindow
+	{
+		[MenuItem("Tools/Fix Non-Dynamic Texts")]
+		public static void ShowWindow()
+		{
+			foreach (var text in EditorSceneManager.GetAllScenes()
+				.Where(s => s.isLoaded)
+				.SelectMany(s => s.GetRootGameObjects())
+				.SelectMany(go => go.GetComponentsInChildren<UnityEngine.UI.Text>(true)))
+			{
+				if (text.font != null && !text.font.dynamic)
+				{
+					text.fontStyle = FontStyle.Normal;
+
+					EditorUtility.SetDirty(text);
+					EditorSceneManager.MarkSceneDirty(text.gameObject.scene);
+				}
+			}
+
+			if (Selection.activeGameObject != null)
+			{
+				foreach(var text in Selection.activeGameObject
+					.GetComponentsInChildren<UnityEngine.UI.Text>(true))
+				{
+					if(text.font != null && !text.font.dynamic)
+					{
+						text.fontStyle = FontStyle.Normal;
+
+						EditorUtility.SetDirty(text);
+						EditorSceneManager.MarkSceneDirty(text.gameObject.scene);
+					}
+				}
+			}
+		}
+	}
+}
