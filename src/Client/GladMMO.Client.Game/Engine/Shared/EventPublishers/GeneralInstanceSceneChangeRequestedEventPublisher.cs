93ed132b68592aac46497aabb0df38727e26de2b
HelloKitty <andrew.blakely@ymail.com>
2020-06-01 00:08:58 -0500

    AddedGeneralInstanceSceneChangeRequestedEventPublisher for instances

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/EventPublishers/GeneralInstanceSceneChangeRequestedEventPublisher.cs
@@ -0,0 +1,33 @@
+﻿using System; using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IInstanceSceneChangeRequestedEventPublisher : IEventPublisher<IRequestedSceneChangeEventSubscribable, RequestedSceneChangeEventArgs>
+	{
+
+	}
+
+	[AdditionalRegisterationAs(typeof(IEventPublisher<IRequestedSceneChangeEventSubscribable, RequestedSceneChangeEventArgs>))]
+	[AdditionalRegisterationAs(typeof(IInstanceSceneChangeRequestedEventPublisher))]
+	[AdditionalRegisterationAs(typeof(IRequestedSceneChangeEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class GeneralInstanceSceneChangeRequestedEventPublisher : IRequestedSceneChangeEventSubscribable, IGameInitializable, IInstanceSceneChangeRequestedEventPublisher
+	{
+		public event EventHandler<RequestedSceneChangeEventArgs> OnRequestedSceneChange;
+
+		public void PublishEvent(object sender, RequestedSceneChangeEventArgs eventArgs)
+		{
+			OnRequestedSceneChange?.Invoke(sender, eventArgs);
+		}
+
+		public Task OnGameInitialized()
+		{
+			//Hack to get it into scene.
+			return Task.CompletedTask;
+		}
+	}
+}
