97cf831fd577834bfa89b517f9a1b8a658c99635
HelloKitty <andrew.blakely@ymail.com>
2020-06-02 03:31:14 -0500

    Added LoggableUnknownPacketHandler

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/LoggableUnknownPacketHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using FreecraftCore;
+using GladNet;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class LoggableUnknownPacketHandler : BaseGameClientGameMessageHandler<LoggableUnknownOpcodePayload>
+	{
+		public LoggableUnknownPacketHandler(ILog logger) 
+			: base(logger)
+		{
+
+		}
+
+		public override Task HandleMessage(IPeerMessageContext<GamePacketPayload> context, LoggableUnknownOpcodePayload payload)
+		{
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"Unhandled Packet: {payload.OpCode}");
+
+			return Task.CompletedTask;
+		}
+	}
+}
