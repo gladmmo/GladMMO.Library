bce5fe8a5d91e22ec51a6f859356bb8d8977dba1
HelloKitty <andrew.blakely@ymail.com>
2020-06-02 02:00:47 -0500

    Added logging to start and stop ProxiedHttpRequest manager

--- src/Server/GladMMO.Service.ZoneManager/Jobs/StartAzureHttpProxyServiceQueueJob.cs
+++ src/Server/GladMMO.Service.ZoneManager/Jobs/StartAzureHttpProxyServiceQueueJob.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace GladMMO
 {
@@ -11,19 +12,40 @@ namespace GladMMO
 	{
 		private ProxiedHttpRequestAzureServiceQueueManager ProxiedHttpRequestManager { get; }
 
-		public StartAzureHttpProxyServiceQueueJob([NotNull] ProxiedHttpRequestAzureServiceQueueManager proxiedHttpRequestManager)
+		private ILogger<StartAzureHttpProxyServiceQueueJob> Logger { get; }
+
+		public StartAzureHttpProxyServiceQueueJob([NotNull] ProxiedHttpRequestAzureServiceQueueManager proxiedHttpRequestManager, [NotNull] ILogger<StartAzureHttpProxyServiceQueueJob> logger)
 		{
 			ProxiedHttpRequestManager = proxiedHttpRequestManager ?? throw new ArgumentNullException(nameof(proxiedHttpRequestManager));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
 		public async Task StartAsync(CancellationToken cancellationToken)
 		{
-			await ProxiedHttpRequestManager.StartAsync();
+			try
+			{
+				await ProxiedHttpRequestManager.StartAsync();
+			}
+			catch (Exception e)
+			{
+				if(Logger.IsEnabled(LogLevel.Error))
+					Logger.LogError($"Failed to start {GetType().Name}. Reason: {e.Message}");
+				throw;
+			}
 		}
 
 		public async Task StopAsync(CancellationToken cancellationToken)
 		{
-			await ProxiedHttpRequestManager.DisposeAsync();
+			try
+			{
+				await ProxiedHttpRequestManager.DisposeAsync();
+			}
+			catch (Exception e)
+			{
+				if(Logger.IsEnabled(LogLevel.Error))
+					Logger.LogError($"Failed to stop {GetType().Name}. Reason: {e.Message}");
+				throw;
+			}
 		}
 	}
 }
