adb26704dbafde1b7a17bde707c29a9069803f98
HelloKitty <andrew.blakely@ymail.com>
2020-06-02 02:35:39 -0500

    Fixed CharacterListResponseHandler login request

--- src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/Character/CharacterListResponseHandler.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/Character/CharacterListResponseHandler.cs
@@ -13,22 +13,21 @@ namespace FreecraftCore.Swarm
 	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
 	public sealed class CharacterListResponseHandler : BaseGameClientGameMessageHandler<CharacterListResponse>
 	{
+		private ILocalCharacterDataRepository CharacterDataRepository { get; }
+
 		/// <inheritdoc />
-		public CharacterListResponseHandler(ILog logger)
+		public CharacterListResponseHandler(ILog logger, [NotNull] ILocalCharacterDataRepository characterDataRepository)
 			: base(logger)
 		{
-
+			CharacterDataRepository = characterDataRepository ?? throw new ArgumentNullException(nameof(characterDataRepository));
 		}
 
 		/// <inheritdoc />
 		public override async Task HandleMessage(IPeerMessageContext<GamePacketPayload> context, CharacterListResponse payload)
 		{
-			if(Logger.IsErrorEnabled)
-				Logger.Error("TODO: Implement character screen selection for character properly.");
-
 			//TODO: Support character login request to character selection selected character.
 			//Idea here is to just login to the first character.
-			await context.PayloadSendService.SendMessage(new CharacterLoginRequest(payload.Characters.First().Data.CharacterGuid));
+			await context.PayloadSendService.SendMessage(new CharacterLoginRequest(CharacterDataRepository.LocalCharacterGuid));
 		}
 	}
 }
