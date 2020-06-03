a6298d2a333b956acf5e520c2e5a291327c41b2b
HelloKitty <andrew.blakely@ymail.com>
2020-06-03 02:32:20 -0500

    Added generic enum HasAnyFlags

---
+++ src/GladMMO.Common/Enum/GenericEnumExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Generic.Math;
+
+namespace GladMMO
+{
+	public static class GenericEnumExtensions
+	{
+		public static bool HasAnyFlags<T>(this T enumValue, T flags)
+			where T : Enum
+		{
+			return (GenericMath.Convert<T, int>(enumValue) & GenericMath.Convert<T, int>(flags)) != 0;
+		}
+	}
+}
