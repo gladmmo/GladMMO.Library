3adfaa9e1d22540ce62d5f05820deabedd6bb5a3
HelloKitty <andrew.blakely@ymail.com>
2020-06-03 21:32:59 -0500

    Added Time sync packet handlers

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/Time/SMSG_QUERY_TIME_RESPONSE_PayloadHandler.cs
@@ -0,0 +1,34 @@
+﻿using System; using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+using GladMMO;
+using GladNet;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class SMSG_QUERY_TIME_RESPONSE_PayloadHandler : BaseGameClientGameMessageHandler<SMSG_QUERY_TIME_RESPONSE_Payload>
+	{
+		private INetworkTimeService TimeService { get; }
+
+		/// <inheritdoc />
+		public SMSG_QUERY_TIME_RESPONSE_PayloadHandler(ILog logger, INetworkTimeService timeService)
+			: base(logger)
+		{
+			TimeService = timeService ?? throw new ArgumentNullException(nameof(timeService));
+		}
+
+		//TODO: This is a work in progress, we need a time service.
+		/// <inheritdoc />
+		public override Task HandleMessage(IPeerMessageContext<GamePacketPayload> context, SMSG_QUERY_TIME_RESPONSE_Payload payload)
+		{
+			//SMSG_QUERY_TIME_RESPONSE_Payload actually returns a uint32 UNIX timestamp in the form of SECONDS.
+			//This is an important distinction to ticks. So we must convert it to ticks.
+			TimeService.SetTimeSyncronization(TimeService.LastQueryTime, payload.CurrentTime * TimeSpan.TicksPerSecond); //It's in UNIX timestamp SECONDS.
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/Time/SMSG_TIME_SYNC_REQ_PayloadHandler.cs
@@ -0,0 +1,32 @@
+﻿using System; using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+using GladMMO;
+using GladNet;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class SMSG_TIME_SYNC_REQ_PayloadHandler : BaseGameClientGameMessageHandler<SMSG_TIME_SYNC_REQ_Payload>
+	{
+		private INetworkTimeService TimeService { get; }
+
+		/// <inheritdoc />
+		public SMSG_TIME_SYNC_REQ_PayloadHandler(ILog logger, INetworkTimeService timeService)
+			: base(logger)
+		{
+			TimeService = timeService ?? throw new ArgumentNullException(nameof(timeService));
+		}
+
+		//TODO: This is a work in progress, we need a time service.
+		/// <inheritdoc />
+		public override Task HandleMessage(IPeerMessageContext<GamePacketPayload> context, SMSG_TIME_SYNC_REQ_Payload payload)
+		{
+			context.PayloadSendService.SendMessage(new CMSG_TIME_SYNC_RESP_Payload(payload.SynchronizationCounter, (uint)Environment.TickCount));
+			return Task.CompletedTask;
+		}
+	}
+}
