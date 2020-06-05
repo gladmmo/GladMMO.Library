45f468eb49a49ac52b78b3be17308d0d21d05a1d
HelloKitty <andrew.blakely@ymail.com>
2020-06-05 02:55:53 -0500

    Revert "Added automated packet registeration"
    This reverts commit ed3578da0c1d88d3b09d0695934644d43e592169.

--- src/Client/GladMMO.Client.Game/IoC/Modules/GladMMONetworkSerializerAutofacModule.cs
+++ src/Client/GladMMO.Client.Game/IoC/Modules/GladMMONetworkSerializerAutofacModule.cs
@@ -1,14 +1,9 @@
 ﻿using System; using FreecraftCore;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
 using System.Text;
 using Autofac;
-using Common.Logging;
 using FreecraftCore.Serializer;
 using GladNet;
-using Module = Autofac.Module;
 
 namespace GladMMO
 {
@@ -31,31 +26,36 @@ namespace GladMMO
 					args.Instance.RegisterType<GamePacketPayload>();
 					args.Instance.RegisterType<ServerPacketHeader>();
 					args.Instance.RegisterType<OutgoingClientPacketHeader>();
+					args.Instance.RegisterType<SessionAuthChallengeEvent>();
+					args.Instance.RegisterType<SessionAuthProofRequest>();
+					args.Instance.RegisterType<AuthenticateSessionResponse>();
+					args.Instance.RegisterType<CharacterLoginRequest>();
+					args.Instance.RegisterType<CharacterListRequest>();
 
-					ILog logger = args.Context.Resolve<ILog>();
+					//We don't really need this packet but we need it for normal TC login flow.
+					args.Instance.RegisterType<FreecraftCore.CharacterListResponse>();
+					args.Instance.RegisterType<SMSG_LOGIN_VERIFY_WORLD_PAYLOAD>();
+					args.Instance.RegisterType<SMSG_COMPRESSED_UPDATE_OBJECT_Payload>();
+					args.Instance.RegisterType<ChatMessageRequest>();
+					args.Instance.RegisterType<ClientGroupInviteRequest>();
+					args.Instance.RegisterType<ServerPartyCommandResultResponse>();
+					args.Instance.RegisterType<ServerGroupListEvent>();
+					args.Instance.RegisterType<MSG_MOVE_HEARTBEAT_Payload>();
 
-					try
-					{
-						Type[] types = typeof(MSG_MOVE_START_STRAFE_LEFT_Payload).Assembly
-							.GetExportedTypes()
-							.Where(t => t.IsAssignableTo<GamePacketPayload>())
-							.ToArray();
+					args.Instance.RegisterType<MSG_MOVE_START_FORWARD_Payload>();
+					args.Instance.RegisterType<MSG_MOVE_START_BACKWARD_Payload>();
+					args.Instance.RegisterType<MSG_MOVE_STOP_Payload>();
+					args.Instance.RegisterType<MSG_MOVE_START_STRAFE_LEFT_Payload>();
+					args.Instance.RegisterType<MSG_MOVE_START_STRAFE_RIGHT_Payload>();
+					args.Instance.RegisterType<MSG_MOVE_HEARTBEAT_Payload>();
+					args.Instance.RegisterType<MSG_MOVE_SET_FACING_Payload>();
 
-						foreach(Type t in types)
-						{
-							if(logger.IsInfoEnabled)
-								logger.Info($"Registered type: {t}");
+					args.Instance.RegisterType<SMSG_QUERY_TIME_RESPONSE_Payload>();
+					args.Instance.RegisterType<SMSG_TIME_SYNC_REQ_Payload>();
+					args.Instance.RegisterType<CMSG_TIME_SYNC_RESP_Payload>();
+					args.Instance.RegisterType<CMSG_QUERY_TIME_Payload>();
 
-							args.Instance.RegisterType(t);
-						}
-					}
-					catch (Exception e)
-					{
-						if(logger.IsErrorEnabled)
-							logger.Error($"Failed to Register Packet Types. Reason: {e.Message}");
-
-						throw;
-					}
+					args.Instance.RegisterType<PingRequest>();
 
 					args.Instance.Compile();
 				})
