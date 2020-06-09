7c333eee9c2f0e1a6f3622411ccde4936f54f58f
HelloKitty <andrew.blakely@ymail.com>
2020-06-08 20:37:15 -0500

    Added ITrinityCreatureTemplateRepository

---
+++ src/Server/GladMMO.Database.TrinityCore.World/Repositories/TrinityCoreCreatureTemplateRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	public sealed class TrinityCoreCreatureTemplateRepository : BaseGenericBackedDatabaseRepository<wotlk_worldContext, uint, CreatureTemplate>, ITrinityCreatureTemplateRepository
+	{
+		public TrinityCoreCreatureTemplateRepository([JetBrains.Annotations.NotNull] wotlk_worldContext context) 
+			: base(context)
+		{
+
+		}
+	}
+}
