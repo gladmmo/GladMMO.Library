bc59d8a570dbc6c755fced8f287ea780904724a9
HelloKitty <andrew.blakely@ymail.com>
2020-06-08 21:23:55 -0500

    Added logging for unhandled update block types

--- src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/Update/ObjectUpdateDispatcher.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/Update/ObjectUpdateDispatcher.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Logging;
 using FreecraftCore;
 using Glader.Essentials;
 
@@ -16,8 +17,11 @@ namespace GladMMO
 		//this WILL be a hot path in the network handling code
 		private IObjectUpdateBlockHandler[] Handlers { get; }
 
-		public ObjectUpdateDispatcher(IEnumerable<IObjectUpdateBlockHandler> handlers)
+		private ILog Logger { get; }
+
+		public ObjectUpdateDispatcher(IEnumerable<IObjectUpdateBlockHandler> handlers, [NotNull] ILog logger)
 		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 			Handlers = new IObjectUpdateBlockHandler[(int)Enum.GetValues(typeof(ObjectUpdateType)).Cast<ObjectUpdateType>().Max()];
 
 			foreach (IObjectUpdateBlockHandler handler in handlers)
@@ -28,7 +32,13 @@ namespace GladMMO
 		{
 			if (updateBlock == null) throw new ArgumentNullException(nameof(updateBlock));
 
-			Handlers[(int)updateBlock.UpdateType]?.HandleUpdateBlock(updateBlock);
+			IObjectUpdateBlockHandler handler = Handlers[(int) updateBlock.UpdateType];
+
+			if(handler != null)
+				handler.HandleUpdateBlock(updateBlock);
+			else
+				if(Logger.IsWarnEnabled)
+					Logger.Warn($"Unhandled UpdateBlockType: {updateBlock.UpdateType}");
 		}
 
 		public Task OnGameInitialized()
