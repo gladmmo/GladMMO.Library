0a47900493938a7706d372b08eb6667dab16a56d
HelloKitty <andrew.blakely@ymail.com>
2020-06-09 05:38:14 -0500

    Added loading screen disable on player spawn

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/OnLocalPlayerSpawned/DisableLoadingScreenOnLocalPlayerSpawnedEventListener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class DisableLoadingScreenOnLocalPlayerSpawnedEventListener : OnLocalPlayerSpawnedEventListener
+	{
+		public IUIElement LoadingScreenRoot { get; }
+
+		public DisableLoadingScreenOnLocalPlayerSpawnedEventListener(ILocalPlayerSpawnedEventSubscribable subscriptionService,
+			[KeyFilter(UnityUIRegisterationKey.LoadingScreen)] [NotNull] IUIElement loadingScreenRoot) 
+			: base(subscriptionService)
+		{
+			LoadingScreenRoot = loadingScreenRoot ?? throw new ArgumentNullException(nameof(loadingScreenRoot));
+		}
+
+		protected override void OnLocalPlayerSpawned(LocalPlayerSpawnedEventArgs args)
+		{
+			LoadingScreenRoot.SetElementActive(false);
+		}
+	}
+}
