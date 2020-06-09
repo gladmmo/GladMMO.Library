e7847a88ab704c1001ad6b57690924a31095f097
HelloKitty <andrew.blakely@ymail.com>
2020-06-09 05:43:28 -0500

    Disabled world spawning for non player/creatures for now

--- src/GladMMO.ZoneServer.Common/Engine/Instance/OnEntityCreationStarting/OnEntityCreatingCreateWorldObjectRepresentationEventListener.cs
+++ src/GladMMO.ZoneServer.Common/Engine/Instance/OnEntityCreationStarting/OnEntityCreatingCreateWorldObjectRepresentationEventListener.cs
@@ -1,6 +1,7 @@
 ﻿using System; using FreecraftCore;
 using System.Collections.Generic;
 using System.Text;
+using Common.Logging;
 using Fasterflect;
 using Glader.Essentials;
 using UnityEngine;
@@ -17,17 +18,42 @@ namespace GladMMO
 
 		private IReadonlyEntityGuidMappable<MovementBlockData> MovementDataMappable { get; }
 
+		private ILog Logger { get; }
+
 		protected SharedOnEntityCreatingCreateWorldObjectRepresentationEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
 			[NotNull] IFactoryCreatable<GameObject, EntityPrefab> prefabFactory,
-			[NotNull] IReadonlyEntityGuidMappable<MovementBlockData> movementDataMappable)
+			[NotNull] IReadonlyEntityGuidMappable<MovementBlockData> movementDataMappable,
+			[NotNull] ILog logger)
 			: base(subscriptionService)
 		{
 			PrefabFactory = prefabFactory ?? throw new ArgumentNullException(nameof(prefabFactory));
 			MovementDataMappable = movementDataMappable ?? throw new ArgumentNullException(nameof(movementDataMappable));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
 		protected override void OnEventFired(object source, EntityCreationStartingEventArgs args)
 		{
+			switch (args.EntityGuid.TypeId)
+			{
+				case EntityTypeId.TYPEID_OBJECT:
+				case EntityTypeId.TYPEID_ITEM:
+				case EntityTypeId.TYPEID_CONTAINER:
+					return;
+				case EntityTypeId.TYPEID_GAMEOBJECT:
+				case EntityTypeId.TYPEID_DYNAMICOBJECT:
+				case EntityTypeId.TYPEID_CORPSE:
+					if(Logger.IsWarnEnabled)
+						Logger.Warn($"Tried to create World Representation for: {args.EntityGuid.TypeId} Id: {args.EntityGuid}. Not implemented.");
+					return;
+
+				//We implemented handling for these so far.
+				case EntityTypeId.TYPEID_UNIT:
+				case EntityTypeId.TYPEID_PLAYER:
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+
 			EntityPrefab prefabType = ComputePrefabType(args.EntityGuid);
 			MovementBlockData movementData = MovementDataMappable.RetrieveEntity(args.EntityGuid);
 
