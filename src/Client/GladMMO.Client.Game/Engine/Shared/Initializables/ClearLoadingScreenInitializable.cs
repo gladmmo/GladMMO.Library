05d0b30714573e7b664271abb3bb381a8b1d269c
HelloKitty <andrew.blakely@ymail.com>
2020-06-09 22:13:46 -0500

    Clear loading screens on Title and Char Select scene load

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/Initializables/ClearLoadingScreenInitializable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.TitleScreen)]
+	[SceneTypeCreateGladMMO(GameSceneType.CharacterSelection)]
+	public sealed class ClearLoadingScreenInitializable : IGameInitializable
+	{
+		private IUIElement LoadingScreenRoot { get; }
+
+		public ClearLoadingScreenInitializable([KeyFilter(UnityUIRegisterationKey.LoadingScreen)] [NotNull] IUIElement loadingScreenRoot)
+		{
+			LoadingScreenRoot = loadingScreenRoot ?? throw new ArgumentNullException(nameof(loadingScreenRoot));
+		}
+
+		public Task OnGameInitialized()
+		{
+			if(LoadingScreenRoot.isActive)
+				LoadingScreenRoot.SetElementActive(false);
+
+			return Task.CompletedTask;
+		}
+	}
+}
