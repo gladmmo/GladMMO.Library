4d11ce49c6380039934b8c8384b53005a23fbedb
HelloKitty <andrew.blakely@ymail.com>
2020-06-10 06:09:04 -0500

    Added UnparentOnAwake

---
+++ src/Client/GladMMO.Client.Game/Component/UnparentOnAwake.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class UnparentOnAwake : MonoBehaviour
+	{
+		private void Awake()
+		{
+			gameObject.transform.parent = null;
+		}
+	}
+}
