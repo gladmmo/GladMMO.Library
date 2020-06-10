0e4549f364b837fe41f8b983e6c7248c31d2d121
HelloKitty <andrew.blakely@ymail.com>
2020-06-10 08:07:44 -0500

    Fixed SMSG_GROUP_INVITE handler

--- src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/Group/SMSG_GROUP_INVITE_PayloadHandler.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/Group/SMSG_GROUP_INVITE_PayloadHandler.cs
@@ -10,15 +10,15 @@ namespace GladMMO
 {
 	//TODO: Properly implement this.
 	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
-	public sealed class CMSG_GROUP_INVITE_PayloadHandler : BaseGameClientGameMessageHandler<CMSG_GROUP_INVITE_Payload>
+	public sealed class SMSG_GROUP_INVITE_PayloadHandler : BaseGameClientGameMessageHandler<SMSG_GROUP_INVITE_Payload>
 	{
-		public CMSG_GROUP_INVITE_PayloadHandler(ILog logger)
+		public SMSG_GROUP_INVITE_PayloadHandler(ILog logger)
 			: base(logger)
 		{
 
 		}
 
-		public override Task HandleMessage(IPeerMessageContext<GamePacketPayload> context, CMSG_GROUP_INVITE_Payload payload)
+		public override Task HandleMessage(IPeerMessageContext<GamePacketPayload> context, SMSG_GROUP_INVITE_Payload payload)
 		{
 			//This is for demo purposes, just auto-accepting.
 			context.PayloadSendService.SendMessage(new CMSG_GROUP_ACCEPT_Payload());
