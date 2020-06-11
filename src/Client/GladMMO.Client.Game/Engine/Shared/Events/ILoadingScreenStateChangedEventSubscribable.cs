a8ab66416f35ca1751a4ada1a884b0a0b4da89f3
HelloKitty <andrew.blakely@ymail.com>
2020-06-11 12:20:07 -0500

    Added ILoadingScreenStateChangedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Shared/Events/ILoadingScreenStateChangedEventSubscribable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface ILoadingScreenStateChangedEventSubscribable
+	{
+		event EventHandler<LoadingScreenStateChangedEventArgs> OnLoadingScreenStateChanged;
+	}
+
+	public sealed class LoadingScreenStateChangedEventArgs : EventArgs
+	{
+		public bool Active { get; private set; }
+
+		public LoadingScreenStateChangedEventArgs(bool active)
+		{
+			Active = active;
+		}
+	}
+}
