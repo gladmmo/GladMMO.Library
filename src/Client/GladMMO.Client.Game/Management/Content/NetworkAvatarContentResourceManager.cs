ad2114641d19032040683e1852ec276fc105a5df
HelloKitty <andrew.blakely@ymail.com>
2020-06-15 15:55:57 -0500

    Enabled player creature model loading

--- src/Client/GladMMO.Client.Game/Management/Content/NetworkAvatarContentResourceManager.cs
+++ src/Client/GladMMO.Client.Game/Management/Content/NetworkAvatarContentResourceManager.cs
@@ -8,18 +8,33 @@ namespace GladMMO
 {
 	public sealed class NetworkAvatarContentResourceManager : DefaultLoadableContentResourceManager
 	{
+		private IClientDataCollectionContainer ClientData { get; }
+
 		private IDownloadableContentServerServiceClient ContentClient { get; }
 
-		public NetworkAvatarContentResourceManager([NotNull] IDownloadableContentServerServiceClient contentClient, ILog logger) 
+		public NetworkAvatarContentResourceManager([NotNull] IDownloadableContentServerServiceClient contentClient, 
+			ILog logger,
+			[NotNull] IClientDataCollectionContainer clientData) 
 			: base(logger, UserContentType.Avatar)
 		{
 			ContentClient = contentClient ?? throw new ArgumentNullException(nameof(contentClient));
+			ClientData = clientData ?? throw new ArgumentNullException(nameof(clientData));
 		}
 
 		protected override async Task<ContentDownloadURLResponse> RequestDownloadURL(long contentId)
 		{
-			return await ContentClient.RequestAvatarDownloadUrl(contentId)
-				.ConfigureAwaitFalse();
+			if(!ClientData.HasEntry<CreatureDisplayInfoEntry<string>>((int)contentId))
+				return new ContentDownloadURLResponse(ContentDownloadURLResponseCode.NoContentId);
+
+			CreatureDisplayInfoEntry<string> displayInfoEntry = ClientData.AssertEntry<CreatureDisplayInfoEntry<string>>((int)contentId);
+
+			if(!ClientData.HasEntry<CreatureModelDataEntry<string>>((int)displayInfoEntry.ModelId))
+				return new ContentDownloadURLResponse(ContentDownloadURLResponseCode.NoContentId);
+
+			CreatureModelDataEntry<string> modelDataEntry = ClientData.AssertEntry<CreatureModelDataEntry<string>>(displayInfoEntry.ModelId);
+
+			//WoW content has MDX format in its path, so we should remove it if it exists.
+			return new ContentDownloadURLResponse(modelDataEntry.FilePath.Replace(".mdx", ""), 1);
 		}
 	}
 }
