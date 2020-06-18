688073fb6a146def852033730ce674385086ee8e
HelloKitty <andrew.blakely@ymail.com>
2020-06-18 15:21:26 -0500

    Added GameObject Template Repository for TC DB

---
+++ src/Server/GladMMO.Database.TrinityCore.World/Repositories/TrinityCoreGameObjectTemplateRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	public sealed class TrinityCoreGameObjectTemplateRepository : BaseGenericBackedDatabaseRepository<wotlk_worldContext, uint, GameobjectTemplate>, ITrinityGameObjectTemplateRepository
+	{
+		public TrinityCoreGameObjectTemplateRepository([JetBrains.Annotations.NotNull] wotlk_worldContext context) 
+			: base(context)
+		{
+
+		}
+	}
+}
