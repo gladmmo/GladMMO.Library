c174d62bc8c6e3f7e41991da7b1d22290ccf5825
HelloKitty <andrew.blakely@ymail.com>
2020-06-19 17:56:29 -0500

    Added neat Teleport SceneCamera DebugWindow

---
+++ src/GladMMO.Client.Editor/Tools/DebugWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class DebugWindow : EditorWindow
+	{
+		[MenuItem("Debug/Teleport To Scene Camera")]
+		public static void ShowWindow()
+		{
+			Vector3 position = SceneView
+				.GetAllSceneCameras()
+				.First().transform.position;
+
+			GameObject playerObject = FindObjectsOfType<GameObject>()
+				.FirstOrDefault(go => go.name.ToLower().Contains("localplayer"));
+
+			playerObject.transform.position = position;
+		}
+	}
+}
