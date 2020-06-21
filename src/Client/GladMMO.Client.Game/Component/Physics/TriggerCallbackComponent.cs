dc5d3db556f81db774ab38ca7088819ade186c82
HelloKitty <andrew.blakely@ymail.com>
2020-06-21 14:14:57 -0500

    Added TriggerCallbackComponent

---
+++ src/Client/GladMMO.Client.Game/Component/Physics/TriggerCallbackComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class TriggerCallbackComponent : MonoBehaviour, IPhysicsTrigger
+	{
+		/// <summary>
+		/// The editor-exposed serialized <see cref="BoxCollider"/> field.
+		/// </summary>
+		[SerializeField]
+		private BoxCollider _Collider;
+
+		public BoxCollider Collider => _Collider;
+
+		/// <summary>
+		/// Event published when a collider on the same layer enter the trigger.
+		/// </summary>
+		public event EventHandler OnTriggerEntered;
+
+		/// <summary>
+		/// Event published when a collider on the same layer exits the trigger.
+		/// </summary>
+		public event EventHandler OnTriggerExited;
+
+		public void OnTriggerEnter(Collider enteredCollider)
+		{
+			OnTriggerEntered?.Invoke(this, EventArgs.Empty);
+		}
+
+		public void OnTriggerExit(Collider exitedCollider)
+		{
+			OnTriggerExited?.Invoke(this, EventArgs.Empty);
+		}
+	}
+}
