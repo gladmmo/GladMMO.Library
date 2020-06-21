f9b68ef385475aa30ca2e48d8472644211e79798
HelloKitty <andrew.blakely@ymail.com>
2020-06-21 14:45:27 -0500

    Added missing IPhysicsTrigger

---
+++ src/Client/GladMMO.Client.Game/Component/Physics/IPhysicsTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public interface IPhysicsTrigger
+	{
+		void OnTriggerEnter(Collider enteredCollider);
+
+		void OnTriggerExit(Collider exitedCollider);
+	}
+}
