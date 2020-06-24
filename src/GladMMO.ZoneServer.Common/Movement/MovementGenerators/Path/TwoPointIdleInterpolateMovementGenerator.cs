13c1b9f16b703ceacff03576083e51867e4663aa
HelloKitty <andrew.blakely@ymail.com>
2020-06-23 20:13:38 -0500

    Added TwoPointIdleInterpolateMovementGenerator

---
+++ src/GladMMO.ZoneServer.Common/Movement/MovementGenerators/Path/TwoPointIdleInterpolateMovementGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class TwoPointIdleInterpolateMovementGenerator : BaseMovementGenerator
+	{
+		public Vector3 TargetPosition { get; }
+
+		public Vector3 StartPosition { get; set; }
+
+		public EntityMovementSpeed MovementSpeeds { get; }
+
+		public long StartTimeStamp { get; set; }
+
+		public const int MAX_TIME = 2 * 1000;
+
+		public TwoPointIdleInterpolateMovementGenerator(Vector3 targetPosition, EntityMovementSpeed movementSpeeds)
+		{
+			TargetPosition = targetPosition;
+			MovementSpeeds = movementSpeeds;
+		}
+
+		protected override Vector3 Start(GameObject entity, long currentTime)
+		{
+			StartTimeStamp = currentTime;
+			return StartPosition = entity.transform.position;
+		}
+
+		protected override Vector3 InternalUpdate(GameObject entity, long currentTime)
+		{
+			if (StartTimeStamp + MAX_TIME < currentTime)
+			{
+				StopGenerator();
+				return entity.transform.position = TargetPosition;
+			}
+
+			return entity.transform.position = Vector3.Lerp(StartPosition, TargetPosition, (currentTime - StartTimeStamp) / (float)MAX_TIME);
+		}
+	}
+}
