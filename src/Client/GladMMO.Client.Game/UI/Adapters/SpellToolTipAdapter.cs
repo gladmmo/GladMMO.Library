9cb08e62b2f51570bbcda29cf6357db92a35b00d
HelloKitty <andrew.blakely@ymail.com>
2020-06-25 14:14:08 -0500

    Fixed SpellTooltipAdapter data loading

--- src/Client/GladMMO.Client.Game/UI/Adapters/SpellToolTipAdapter.cs
+++ src/Client/GladMMO.Client.Game/UI/Adapters/SpellToolTipAdapter.cs
@@ -43,8 +43,13 @@ namespace GladMMO
 
 		private Dictionary<SpellTooltipInfoSlot, UnityEngine.UI.Text> TooltipTextMap { get; } = new Dictionary<SpellTooltipInfoSlot, Text>();
 
-		void Awake()
+		private bool isInitialized { get; set; } = false;
+
+		private void Awake()
 		{
+			if (isInitialized)
+				return;
+
 			TooltipTextMap.Add(SpellTooltipInfoSlot.Name, TextCollection.SpellName);
 			TooltipTextMap.Add(SpellTooltipInfoSlot.Rank, TextCollection.SpellRank);
 			TooltipTextMap.Add(SpellTooltipInfoSlot.Cost, TextCollection.SpellCost);
@@ -53,27 +58,47 @@ namespace GladMMO
 			TooltipTextMap.Add(SpellTooltipInfoSlot.Cooldown, TextCollection.SpellCooldown);
 			TooltipTextMap.Add(SpellTooltipInfoSlot.Requirements, TextCollection.SpellRequirements);
 			TooltipTextMap.Add(SpellTooltipInfoSlot.Info, TextCollection.SpellInfo);
+
+			isInitialized = true;
 		}
 
 		public void SetText(SpellTooltipInfoSlot slot, string text)
 		{
+			//Since the component may not be activate, we must ensure awake was called.
+			AssertInitialized();
+
 			TooltipTextMap[slot].text = text;
 		}
 
 		public void SetSlotState(SpellTooltipInfoSlot slot, bool state)
 		{
+			//Since the component may not be activate, we must ensure awake was called.
+			AssertInitialized();
+
 			TooltipTextMap[slot].gameObject.SetActive(state);
 		}
 
 		public bool IsSlotActive(SpellTooltipInfoSlot slot)
 		{
+			//Since the component may not be activate, we must ensure awake was called.
+			AssertInitialized();
+
 			return TooltipTextMap[slot].gameObject.activeSelf;
 		}
 
 		public bool SetActive(bool state)
 		{
+			//Since the component may not be activate, we must ensure awake was called.
+			AssertInitialized();
+
 			gameObject.SetActive(state);
 			return gameObject.activeSelf;
+		}
+
+		private void AssertInitialized()
+		{
+			if (!isInitialized)
+				Awake();
 		}
 	}
 }
