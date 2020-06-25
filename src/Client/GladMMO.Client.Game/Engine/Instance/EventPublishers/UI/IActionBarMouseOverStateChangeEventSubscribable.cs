6c7e8bf28e3983325d7333400270e3de2a1a6414
HelloKitty <andrew.blakely@ymail.com>
2020-06-24 19:30:34 -0500

    Added IActionBarMouseOverStateChangeEventPublisher

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventPublishers/UI/IActionBarMouseOverStateChangeEventSubscribable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IActionBarMouseOverStateChangeEventPublisher : IEventPublisher<IActionBarMouseOverStateChangeEventSubscribable, ActionBarMouseOverStateChangeEventArgs>
+	{
+
+	}
+
+	[AdditionalRegisterationAs(typeof(IActionBarMouseOverStateChangeEventSubscribable))]
+	[AdditionalRegisterationAs(typeof(IActionBarMouseOverStateChangeEventPublisher))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ActionBarMouseOverStateChangeEventPublisher : IActionBarMouseOverStateChangeEventSubscribable, IActionBarMouseOverStateChangeEventPublisher, IGameInitializable
+	{
+		public event EventHandler<ActionBarMouseOverStateChangeEventArgs> OnActionBarMouseOverStateChanged;
+
+		public void PublishEvent(object sender, ActionBarMouseOverStateChangeEventArgs eventArgs)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
