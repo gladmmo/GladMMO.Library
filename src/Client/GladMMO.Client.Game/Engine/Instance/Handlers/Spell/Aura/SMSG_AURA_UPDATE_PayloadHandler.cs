147c8570d5d3952c4c21a7d0ece0d24b5e834847
HelloKitty <andrew.blakely@ymail.com>
2020-06-26 19:40:59 -0500

    Implement SMSG_AURA_UPDATE handler

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Handlers/Spell/Aura/SMSG_AURA_UPDATE_PayloadHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Glader.Essentials;
+using GladMMO;
+using GladNet;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class SMSG_AURA_UPDATE_PayloadHandler : BaseGameClientGameMessageHandler<SMSG_AURA_UPDATE_Payload>
+	{
+		private IAuraStateChangedEventPublisher AuraPublisher { get; }
+
+		/// <inheritdoc />
+		public SMSG_AURA_UPDATE_PayloadHandler(ILog logger,
+			[NotNull] IAuraStateChangedEventPublisher auraPublisher)
+			: base(logger)
+		{
+			AuraPublisher = auraPublisher ?? throw new ArgumentNullException(nameof(auraPublisher));
+		}
+
+		/// <inheritdoc />
+		public override Task HandleMessage(IPeerMessageContext<GamePacketPayload> context, SMSG_AURA_UPDATE_Payload payload)
+		{
+			AuraPublisher.PublishEvent(this, new AuraStateChangedEventArgs(payload.TargetGuid, payload.Data));
+			return Task.CompletedTask;
+		}
+	}
+}
