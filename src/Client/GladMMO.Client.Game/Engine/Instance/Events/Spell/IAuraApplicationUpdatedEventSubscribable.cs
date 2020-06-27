d047ceb48f663f3d636fa32c0ef0f004a5221289
HelloKitty <andrew.blakely@ymail.com>
2020-06-27 17:37:53 -0500

    Added IAuraApplicationUpdatedEventSubscribable event

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Spell/IAuraApplicationUpdatedEventSubscribable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for a type that implements a subscription service for events that publish <see cref="AuraApplicationUpdatedEventArgs"/>
+	/// through the <see cref="EventHandler{TEventArgs}"/> <see cref="OnAuraApplicationUpdated"/>
+	/// </summary>
+	public interface IAuraApplicationUpdatedEventSubscribable
+	{
+		event EventHandler<AuraApplicationUpdatedEventArgs> OnAuraApplicationUpdated;
+	}
+
+	/// <summary>
+	/// Event arguments for the <see cref="IAuraApplicationUpdatedEventSubscribable"/> interface.
+	/// </summary>
+	public sealed class AuraApplicationUpdatedEventArgs : EventArgs, IAuraApplicationDataEventContainer
+	{
+		/// <summary>
+		/// The aura slot to occupy.
+		/// </summary>
+		public byte Slot { get; }
+
+		/// <summary>
+		/// The ID of the spell associated with the aura application.
+		/// </summary>
+		public int SpellId { get; }
+
+		/// <summary>
+		/// The application data.
+		/// </summary>
+		public AuraApplicationStateUpdate ApplicationData { get; }
+
+		public AuraApplicationUpdatedEventArgs(byte slot, int spellId, [NotNull] AuraApplicationStateUpdate applicationData)
+		{
+			if(spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			SpellId = spellId;
+			ApplicationData = applicationData ?? throw new ArgumentNullException(nameof(applicationData));
+			Slot = slot;
+		}
+	}
+}
