c883b41e7372243d15b29425ad4c17e3af502c26
HelloKitty <andrew.blakely@ymail.com>
2020-06-27 14:51:17 -0500

    Added IAuraApplicationCollection

---
+++ src/Client/GladMMO.Client.Game/Collections/Spell/DefaultAuraApplicationCollection.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+
+namespace GladMMO
+{
+	public sealed class DefaultAuraApplicationCollection : IAuraApplicationCollection
+	{
+		private static AuraUpdateData[] Empty { get; } = new AuraUpdateData[byte.MaxValue];
+
+		private AuraUpdateData[] InternalAuraApplicationMap { get; } = new AuraUpdateData[byte.MaxValue];
+
+		static DefaultAuraApplicationCollection()
+		{
+			for(byte i = 0; i < byte.MaxValue; i++)
+				Empty[i] = new AuraUpdateData(i);
+		}
+
+		public bool IsSlotActive(byte auraSlot)
+		{
+			return InternalAuraApplicationMap[auraSlot] != null && !InternalAuraApplicationMap[auraSlot].IsAuraRemoved;
+		}
+
+		public AuraUpdateData this[byte slot]
+		{
+			get
+			{
+				if (InternalAuraApplicationMap[slot] == null)
+					return Empty[slot];
+				else
+					return InternalAuraApplicationMap[slot];
+			}
+		}
+
+		public void Apply(AuraUpdateData data)
+		{
+			InternalAuraApplicationMap[data.SlotIndex] = data;
+		}
+
+		public void Remove(AuraUpdateData data)
+		{
+			InternalAuraApplicationMap[data.SlotIndex] = null;
+		}
+
+		public void Remove(byte slot)
+		{
+			InternalAuraApplicationMap[slot] = null;
+		}
+
+		public IEnumerator<AuraUpdateData> GetEnumerator()
+		{
+			//Only return non-null valid applications.
+			foreach(var entry in InternalAuraApplicationMap)
+				if (entry != null && !entry.IsAuraRemoved)
+					yield return entry;
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/Collections/Spell/IAuraApplicationCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+
+namespace GladMMO
+{
+	public interface IAuraApplicationCollection : IReadonlyAuraApplicationCollection, IEnumerable<AuraUpdateData>
+	{
+		/// <summary>
+		/// Replaces the data at the specified index with <see cref="data"/>
+		/// </summary>
+		/// <param name="data">The data.</param>
+		void Apply(AuraUpdateData data);
+
+		/// <summary>
+		/// Removes the data at the specified index within <see cref="data"/>.
+		/// </summary>
+		/// <param name="data">The data.</param>
+		void Remove(AuraUpdateData data);
+
+		/// <summary>
+		/// Removes the data at the specified index <see cref="slot"/>.
+		/// </summary>
+		/// <param name="slot">The aura slot to clear.</param>
+		void Remove(byte slot);
+	}
+
+	public interface IReadonlyAuraApplicationCollection : IEnumerable<AuraUpdateData>
+	{
+		/// <summary>
+		/// Indicates if the aura slot is active or empty.
+		/// </summary>
+		/// <param name="auraSlot">The 8bit aura slot id.</param>
+		/// <returns>True if the aura slot is active.</returns>
+		bool IsSlotActive(byte auraSlot);
+
+		/// <summary>
+		/// Accesses the 
+		/// </summary>
+		/// <param name="slot">The aura slot.</param>
+		/// <returns>The aura application data.</returns>
+		AuraUpdateData this[byte slot] { get; }
+	}
+}
