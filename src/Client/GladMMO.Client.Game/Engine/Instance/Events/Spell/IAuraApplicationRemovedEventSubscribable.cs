3c23643ab9e563691f05cb03788018a2e5cfd068
HelloKitty <andrew.blakely@ymail.com>
2020-06-27 15:13:19 -0500

    Added IAuraApplicationRemovedEventSubscribable event

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Spell/IAuraApplicationRemovedEventSubscribable.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for a type that implements a subscription service for events that publish <see cref="AuraApplicationRemovedEventArgs"/>
+	/// through the <see cref="EventHandler{TEventArgs}"/> <see cref="OnAuraApplicationRemoved"/>
+	/// </summary>
+	public interface IAuraApplicationRemovedEventSubscribable
+	{
+		event EventHandler<AuraApplicationRemovedEventArgs> OnAuraApplicationRemoved;
+	}
+
+	/// <summary>
+	/// Event arguments for the <see cref="IAuraApplicationRemovedEventSubscribable"/> interface.
+	/// </summary>
+	public sealed class AuraApplicationRemovedEventArgs : EventArgs
+	{
+		/// <summary>
+		/// The aura slot to occupy.
+		/// </summary>
+		public byte Slot { get; }
+
+		/// <summary>
+		/// The ID of the spell associated with the aura application.
+		/// </summary>
+		public int SpellId { get; }
+
+		public AuraApplicationRemovedEventArgs(byte slot, int spellId)
+		{
+			if (spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			Slot = slot;
+			SpellId = spellId;
+		}
+	}
+}
