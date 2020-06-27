bc8963a949b936f97b3fd9f8a467bdd9fca9c349
HelloKitty <andrew.blakely@ymail.com>
2020-06-27 15:10:03 -0500

    Added IAuraApplicationAppliedEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Spell/IAuraApplicationAppliedEventSubscribable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for a type that implements a subscription service for events that publish <see cref="AuraApplicationAppliedEventArgs"/>
+	/// through the <see cref="EventHandler{TEventArgs}"/> <see cref="OnAuraApplicationApplied"/>
+	/// </summary>
+	public interface IAuraApplicationAppliedEventSubscribable
+	{
+		event EventHandler<AuraApplicationAppliedEventArgs> OnAuraApplicationApplied;
+	}
+
+	/// <summary>
+	/// Event arguments for the <see cref="IAuraApplicationAppliedEventSubscribable"/> interface.
+	/// </summary>
+	public sealed class AuraApplicationAppliedEventArgs : EventArgs
+	{
+		public int SpellId { get; }
+
+		public AuraApplicationStateUpdate ApplicationData { get; }
+
+		public AuraApplicationAppliedEventArgs(int spellId, [NotNull] AuraApplicationStateUpdate applicationData)
+		{
+			if (spellId <= 0) throw new ArgumentOutOfRangeException(nameof(spellId));
+
+			SpellId = spellId;
+			ApplicationData = applicationData ?? throw new ArgumentNullException(nameof(applicationData));
+		}
+	}
+}
