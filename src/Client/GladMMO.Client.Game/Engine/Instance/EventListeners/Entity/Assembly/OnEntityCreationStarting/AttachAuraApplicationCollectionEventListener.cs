8fa6ab14166f59bc17363ce01c86ee9f16145d67
HelloKitty <andrew.blakely@ymail.com>
2020-06-27 14:51:24 -0500

    Added AttachAuraApplicationCollectionEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/Assembly/OnEntityCreationStarting/AttachAuraApplicationCollectionEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class AttachAuraApplicationCollectionEventListener : BaseSingleEventListenerInitializable<IEntityCreationStartingEventSubscribable, EntityCreationStartingEventArgs>
+	{
+		private IEntityGuidMappable<IAuraApplicationCollection> AuraApplicationMappable { get; }
+
+		public AttachAuraApplicationCollectionEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<IAuraApplicationCollection> auraApplicationMappable) 
+			: base(subscriptionService)
+		{
+			AuraApplicationMappable = auraApplicationMappable ?? throw new ArgumentNullException(nameof(auraApplicationMappable));
+		}
+
+		protected override void OnEventFired(object source, EntityCreationStartingEventArgs args)
+		{
+			AuraApplicationMappable.Add(args.EntityGuid, new DefaultAuraApplicationCollection());
+		}
+	}
+}
