4bdcd0d1dea2c34059df4a6b176d7c867fc0bf80
HelloKitty <andrew.blakely@ymail.com>
2020-06-28 13:57:15 -0500

    Added RemoveLocalPlayerAppliedAuraEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Spell/Aura/RemoveLocalPlayerAppliedAuraEventListener.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Common.Logging;
+using FreecraftCore;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class RemoveLocalPlayerAppliedAuraEventListener : BaseSingleEventListenerInitializable<IAuraApplicationRemovedEventSubscribable, AuraApplicationRemovedEventArgs>
+	{
+		private IUIAuraBuffCollection AuraBuffUICollection { get; }
+
+		private IReadonlyLocalPlayerDetails PlayerDetails { get; }
+
+		private ILog Logger { get; }
+
+		public RemoveLocalPlayerAppliedAuraEventListener([NotNull] IAuraApplicationRemovedEventSubscribable subscriptionService,
+			[KeyFilter(UnityUIRegisterationKey.AuraBuffCollection)] [NotNull] IUIAuraBuffCollection auraBuffUiCollection,
+			[NotNull] IReadonlyLocalPlayerDetails playerDetails,
+			[NotNull] ILog logger)
+			: base(subscriptionService)
+		{
+			AuraBuffUICollection = auraBuffUiCollection ?? throw new ArgumentNullException(nameof(auraBuffUiCollection));
+			PlayerDetails = playerDetails ?? throw new ArgumentNullException(nameof(playerDetails));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		protected override void OnEventFired(object source, AuraApplicationRemovedEventArgs args)
+		{
+			if (PlayerDetails.LocalPlayerGuid != args.Target)
+				return;
+
+			IUIAuraBuffSlot slot = AuraBuffUICollection[AuraBuffType.Positive, args.Slot];
+			slot.RootElement.SetElementActive(false);
+		}
+	}
+}
