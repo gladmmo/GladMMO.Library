cb762bc1459d544d3bca64533921ea7003e1e41f
HelloKitty <andrew.blakely@ymail.com>
2020-06-27 19:47:09 -0500

    Added IUIAuraBuffSlot

---
+++ src/Client/GladMMO.Client.Game/UI/Adapters/AuraBuffSlotUIAdapter.cs
@@ -0,0 +1,48 @@
+﻿using System; using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GladMMO
+{
+	public sealed class AuraBuffSlotUIAdapter : BaseUnityUI<IUIAuraBuffSlot>, IUIAuraBuffSlot, IUIMouseBoundsEventListener, IUIElement
+	{
+		[SerializeField]
+		private TextMeshProUGUIUITextAdapter _DurationText;
+
+		[SerializeField]
+		private TextMeshProUGUIUITextAdapter _CounterText;
+
+		[SerializeField]
+		private UnityImageUIImageAdapter _AuraIconImage;
+
+		public IUIText DurationText => _DurationText;
+
+		public IUIText CounterText => _CounterText;
+
+		public IUIImage AuraIconImage =>_AuraIconImage;
+
+		public IUIElement RootElement => this;
+
+		public bool isActive => gameObject.activeSelf;
+
+		public event EventHandler<bool> OnAuraBuffMouseHoverChanged;
+
+		public void OnMouseOver()
+		{
+			OnAuraBuffMouseHoverChanged?.Invoke(this, true);
+		}
+
+		public void OnMouseExit()
+		{
+			OnAuraBuffMouseHoverChanged?.Invoke(this, false);
+		}
+
+		public void SetElementActive(bool state)
+		{
+			gameObject.SetActive(state);
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/UI/Interfaces/IUIAuraBuffSlot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IUIAuraBuffSlot
+	{
+		/// <summary>
+		/// The text element responsible for the duration of the buff.
+		/// </summary>
+		IUIText DurationText { get; }
+
+		/// <summary>
+		/// Stack or charge aura buff counter.
+		/// </summary>
+		IUIText CounterText { get; }
+
+		/// <summary>
+		/// The icon image for the aura.
+		/// </summary>
+		IUIImage AuraIconImage { get; }
+
+		/// <summary>
+		/// The root element for the aura buff slot.
+		/// </summary>
+		IUIElement RootElement { get; }
+
+		event EventHandler<bool> OnAuraBuffMouseHoverChanged;
+	}
+}
