0ad01e32c2148213c7f633404655a0f561137cce
HelloKitty <andrew.blakely@ymail.com>
2020-06-27 23:08:52 -0500

    Added IUIAuraBuffCollection

---
+++ src/Client/GladMMO.Client.Game/UI/Adapters/AuraBuffCollectionUIAdapter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class AuraBuffCollectionUIAdapter : BaseUnityUI<IUIAuraBuffCollection>, IUIAuraBuffCollection
+	{
+		[SerializeField]
+		private GameObject AuraBuffPrefab;
+		
+		[SerializeField]
+		private Transform PositiveAuraBarTransform;
+
+		private Dictionary<byte, IUIAuraBuffSlot> InternalAuraMap { get; } = new Dictionary<byte, IUIAuraBuffSlot>(byte.MaxValue);
+
+		public IUIAuraBuffSlot this[AuraBuffType type, byte index] => RetrieveUIAuraSlot(type, index);
+
+		private IUIAuraBuffSlot RetrieveUIAuraSlot(AuraBuffType type, byte index)
+		{
+			if (!Enum.IsDefined(typeof(AuraBuffType), type)) throw new InvalidEnumArgumentException(nameof(type), (int) type, typeof(AuraBuffType));
+
+			if (InternalAuraMap.ContainsKey(index))
+				return InternalAuraMap[index];
+			else
+			{
+				GameObject auraSlot = Instantiate(AuraBuffPrefab, Vector3.zero, Quaternion.identity, PositiveAuraBarTransform);
+
+				return InternalAuraMap[index] = auraSlot.GetComponent<AuraBuffSlotUIAdapter>();
+			}
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/UI/Interfaces/IUIAuraBuffCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+
+namespace GladMMO
+{
+	//TODO: Shouldwe use a different enum?? Should we put this somewhere else?
+	public enum AuraBuffType
+	{
+		Positive,
+		Negative
+	}
+
+	public interface IUIAuraBuffCollection
+	{
+		IUIAuraBuffSlot this[AuraBuffType type, byte index] { get; }
+	}
+}
