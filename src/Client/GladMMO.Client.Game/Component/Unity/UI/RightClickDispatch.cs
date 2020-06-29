e2879d30588fa2a12658d8911f6c935c146a5040
HelloKitty <andrew.blakely@ymail.com>
2020-06-29 04:40:53 -0500

    Added RightClickDispatch component

---
+++ src/Client/GladMMO.Client.Game/Component/Unity/UI/RightClickDispatch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Component that can point click events and dispatches them via a <see cref="UnityEvent"/>
+	/// only if it's a right-click.
+	/// </summary>
+	public sealed class RightClickDispatch : MonoBehaviour, IPointerClickHandler
+	{
+		[SerializeField]
+		private UnityEvent OnRightClicked;
+
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			if (eventData.button == PointerEventData.InputButton.Right)
+				OnRightClicked?.Invoke();
+		}
+	}
+}
