98ec085042e37b53bd6b2b9efe6b675ae8a3f66d
HelloKitty <andrew.blakely@ymail.com>
2020-06-30 02:43:20 -0500

    Added IAuraDataUpdateApplyable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Spell/Aura/Base/IAuraDataUpdateApplyable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public interface IAuraDataUpdateApplyable
+	{
+		void ApplyAuraData(IAuraApplicationDataEventContainer data);
+	}
+}
