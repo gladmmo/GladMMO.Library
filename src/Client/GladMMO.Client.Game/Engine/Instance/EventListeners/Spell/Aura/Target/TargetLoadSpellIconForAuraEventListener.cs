759a727bd368aeec710f48cbbbb94c17c802fc82
HelloKitty <andrew.blakely@ymail.com>
2020-06-30 01:34:29 -0500

    Added TargetLoadSpellIconForAuraEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Spell/Aura/Target/TargetLoadSpellIconForAuraEventListener.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using FreecraftCore;
+using Glader.Essentials;
+using Nito.AsyncEx;
+using UnityEngine;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class TargetLoadSpellIconForAuraEventListener : LoadSpellIconForAuraEventListener
+	{
+		private IReadonlyLocalPlayerDetails PlayerDetails { get; }
+
+		public TargetLoadSpellIconForAuraEventListener(IAuraApplicationAppliedEventSubscribable subscriptionService, 
+			IAddressableContentLoader contentLoadService,
+			[KeyFilter(UnityUIRegisterationKey.TargetAuraBuffCollection)] IUIAuraBuffCollection auraBuffUiCollection, 
+			IClientDataCollectionContainer clientData, 
+			IReadonlyEntityGuidMappable<IAuraApplicationCollection> auraApplicationMappable,
+			[NotNull] IReadonlyLocalPlayerDetails playerDetails) 
+			: base(subscriptionService, contentLoadService, auraBuffUiCollection, clientData, auraApplicationMappable)
+		{
+			PlayerDetails = playerDetails ?? throw new ArgumentNullException(nameof(playerDetails));
+		}
+
+		protected override bool IsHandlingTarget(ObjectGuid target)
+		{
+			return PlayerDetails.EntityData.GetEntityGuidValue(EUnitFields.UNIT_FIELD_TARGET) == target;
+		}
+	}
+}
