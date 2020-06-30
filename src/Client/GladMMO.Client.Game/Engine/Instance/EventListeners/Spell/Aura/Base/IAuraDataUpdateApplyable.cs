1b7095a7bf46445f908b68022ebfa17eab3d6dfc
HelloKitty <andrew.blakely@ymail.com>
2020-06-30 03:00:05 -0500

    Added doc to IAuraDataUpdateApplyable

--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Spell/Aura/Base/IAuraDataUpdateApplyable.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Spell/Aura/Base/IAuraDataUpdateApplyable.cs
@@ -4,8 +4,15 @@ using System.Text;
 
 namespace GladMMO
 {
+	/// <summary>
+	/// Contract for aura data updatable.
+	/// </summary>
 	public interface IAuraDataUpdateApplyable
 	{
+		/// <summary>
+		/// Calls will apply or re-apply aura data.
+		/// </summary>
+		/// <param name="data">The data.</param>
 		void ApplyAuraData(IAuraApplicationDataEventContainer data);
 	}
 }
