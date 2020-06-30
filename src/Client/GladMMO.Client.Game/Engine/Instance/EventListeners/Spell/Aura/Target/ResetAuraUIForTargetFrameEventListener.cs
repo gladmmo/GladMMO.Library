abeefcc2c15dd29e422e9851e0f8cfc939d069a1
HelloKitty <andrew.blakely@ymail.com>
2020-06-30 02:44:22 -0500

    Reimplemented Rest Aura listener to spoof aura updates for target

--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Spell/Aura/Target/ResetAuraUIForTargetFrameEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Spell/Aura/Target/ResetAuraUIForTargetFrameEventListener.cs
@@ -16,15 +16,19 @@ namespace GladMMO
 
 		private IReadonlyEntityGuidMappable<IAuraApplicationCollection> AuraApplicationMappable { get; }
 
+		private IEnumerable<IAuraDataUpdateApplyable> AuraDataUpdateApplyable { get; }
+
 		public ResetAuraUIForTargetFrameEventListener(ILocalPlayerTargetChangedEventListener subscriptionService, 
 			ILog logger,
 			[KeyFilter(UnityUIRegisterationKey.TargetUnitFrame)] IUIUnitFrame targetUnitFrame,
 			[KeyFilter(UnityUIRegisterationKey.TargetAuraBuffCollection)] [NotNull] IUIAuraBuffCollection targetUiAuraBuffCollection,
-			[NotNull] IReadonlyEntityGuidMappable<IAuraApplicationCollection> auraApplicationMappable) 
+			[NotNull] IReadonlyEntityGuidMappable<IAuraApplicationCollection> auraApplicationMappable,
+			[NotNull] IEnumerable<IAuraDataUpdateApplyable> auraDataUpdateApplyable)
 			: base(subscriptionService, logger, targetUnitFrame)
 		{
 			TargetUIAuraBuffCollection = targetUiAuraBuffCollection ?? throw new ArgumentNullException(nameof(targetUiAuraBuffCollection));
 			AuraApplicationMappable = auraApplicationMappable ?? throw new ArgumentNullException(nameof(auraApplicationMappable));
+			AuraDataUpdateApplyable = auraDataUpdateApplyable ?? throw new ArgumentNullException(nameof(auraDataUpdateApplyable));
 		}
 
 		protected override void OnLocalPlayerTargetChanged(LocalPlayerTargetChangedEventArgs args)
@@ -42,8 +46,10 @@ namespace GladMMO
 				if (entry.Data.IsAuraRemoved)
 					continue;
 
-				IUIAuraBuffSlot slot = TargetUIAuraBuffCollection[entry.Data.State.Flags.ToBuffType(), entry.Data.SlotIndex];
-				slot.RootElement.SetElementActive(true);
+				AuraApplicationUpdatedEventArgs spoofEventArgs = new AuraApplicationUpdatedEventArgs(entry.Data.SlotIndex, args.TargetedEntity, entry.Data.AuraSpellId, entry.Data.State);
+
+				foreach(var applyable in AuraDataUpdateApplyable)
+					applyable.ApplyAuraData(spoofEventArgs);
 			}
 		}
 	}
