53daca57265c47a4e69863c7c0f3fef0638c4d71
HelloKitty <andrew.blakely@ymail.com>
2020-07-01 23:13:06 -0500

    Refactored avatar/model spawning into SpawnAvatarOnDownloadEventListener

--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnAvatarPrefabCompletedDownload/SpawnAvatarOnDownloadEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/OnAvatarPrefabCompletedDownload/SpawnAvatarOnDownloadEventListener.cs
@@ -1,15 +1,16 @@
-﻿using System; using FreecraftCore;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using Common.Logging;
+using FreecraftCore;
 using Glader.Essentials;
-using GladMMO.FinalIK;
 using UnityEngine;
 
 namespace GladMMO
 {
+	[AdditionalRegisterationAs(typeof(IEntityAvatarChangedEventSubscribable))]
 	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
-	public sealed class OnCreatureDownloadedSpawnNewCreatureModelEventListener : BaseSingleEventListenerInitializable<IContentPrefabCompletedDownloadEventSubscribable, ContentPrefabCompletedDownloadEventArgs>
+	public sealed class SpawnAvatarOnDownloadEventListener : ContentPrefabCompletedDownloadEventListener, IEntityAvatarChangedEventSubscribable
 	{
 		private IReadonlyEntityGuidMappable<GameObject> GameObjectMappable { get; }
 
@@ -19,9 +20,12 @@ namespace GladMMO
 
 		private IReadonlyKnownEntitySet KnownEntitySet { get; }
 
-		public OnCreatureDownloadedSpawnNewCreatureModelEventListener(IContentPrefabCompletedDownloadEventSubscribable subscriptionService,
+		public event EventHandler<EntityAvatarChangedEventArgs> OnEntityAvatarChanged;
+
+		public SpawnAvatarOnDownloadEventListener(IContentPrefabCompletedDownloadEventSubscribable subscriptionService,
 			[NotNull] ILog logger,
-			[NotNull] IReadonlyEntityGuidMappable<GameObject> gameObjectMappable, [NotNull] IModelScaleStrategy modelScaler,
+			[NotNull] IReadonlyEntityGuidMappable<GameObject> gameObjectMappable,
+			[NotNull] IModelScaleStrategy modelScaler,
 			[NotNull] IReadonlyKnownEntitySet knownEntitySet)
 			: base(subscriptionService)
 		{
@@ -33,15 +37,11 @@ namespace GladMMO
 
 		protected override void OnEventFired(object source, ContentPrefabCompletedDownloadEventArgs args)
 		{
-			//Only interested creatures
-			if (args.EntityGuid.TypeId != EntityTypeId.TYPEID_UNIT)
-				return;
-
 			if(Logger.IsInfoEnabled)
-				Logger.Info($"About to create new Creature model for Entity: {args.EntityGuid}");
+				Logger.Info($"About to create new GameObject model for Entity: {args.EntityGuid}");
 
 			//Possible it was despawned before we finished loading it.
-			if (!KnownEntitySet.isEntityKnown(args.EntityGuid))
+			if(!KnownEntitySet.isEntityKnown(args.EntityGuid))
 			{
 				if(Logger.IsInfoEnabled)
 					Logger.Info($"No longer known Entity: {args.EntityGuid} finished loading model Id: {args.DownloadedPrefabObject.name}");
@@ -51,14 +51,13 @@ namespace GladMMO
 
 			try
 			{
-				//TODO: Handle the case of a creature CHANGING it's model ID.
-				//Now we've assigned the handle, we need to actually handle the spawning/loading of the creature
+				//Now we've assigned the handle, we need to actually handle the spawning/loading of the GameObject
 				//model as a child of the parent.
 				GameObject root = GameObjectMappable.RetrieveEntity(args.EntityGuid);
 
 				//TODO: This is kinda hacky, we should maintain a management component that provides access to these things.
 				//Destroy existing one
-				foreach (Transform transform in root.transform)
+				foreach(Transform transform in root.transform)
 				{
 					if (transform.gameObject.name == "VISUAL_MODEL")
 					{
@@ -67,31 +66,34 @@ namespace GladMMO
 					}
 				}
 
-				GameObject newlySpawnedAvatar = InstantiateNewFromPrefab(args, root);
+				GameObject newlySpawnedAvatar = InstantiateNewFromPrefab(args.EntityGuid, args.DownloadedPrefabObject, root);
 				newlySpawnedAvatar.name = "VISUAL_MODEL";
+
+				//Broacast and Avatar/Model change to the framework.
+				OnEntityAvatarChanged?.Invoke(this, new EntityAvatarChangedEventArgs(args.EntityGuid, newlySpawnedAvatar));
 			}
-			catch (Exception e)
+			catch(Exception e)
 			{
 				if(Logger.IsErrorEnabled)
-					Logger.Error($"Failed to create Creature model for Entity: {args.EntityGuid}. Error: {e.Message}\n\nStack: {e.StackTrace}");
+					Logger.Error($"Failed to create GameObject model for Entity: {args.EntityGuid}. Error: {e.Message}\n\nStack: {e.StackTrace}");
 
 				throw;
 			}
 		}
 
-		private GameObject InstantiateNewFromPrefab([NotNull] ContentPrefabCompletedDownloadEventArgs contentEvent, [NotNull] GameObject root)
+		private GameObject InstantiateNewFromPrefab(ObjectGuid guid, [NotNull] GameObject prefab, [NotNull] GameObject root)
 		{
-			if (contentEvent == null) throw new ArgumentNullException(nameof(contentEvent));
-			if (root == null) throw new ArgumentNullException(nameof(root));
+			if(prefab == null) throw new ArgumentNullException(nameof(prefab));
+			if(root == null) throw new ArgumentNullException(nameof(root));
 
 			//This should be the avatar prefab
 			//and we should be on the main thread here so we can now do the spawning and such
 			//Replace the current avatar root gameobject with the new one.
-			GameObject newAvatarRoot = GameObject.Instantiate(contentEvent.DownloadedPrefabObject, root.transform);
+			GameObject newAvatarRoot = GameObject.Instantiate(prefab, root.transform);
 			newAvatarRoot.transform.localScale = root.transform.localScale;
 
 			//Apply creature scale, loading from DBC and dynamic unit field stuff.
-			newAvatarRoot.transform.localScale *= ModelScaler.ComputeScale(contentEvent.EntityGuid);
+			newAvatarRoot.transform.localScale *= ModelScaler.ComputeScale(guid);
 
 			newAvatarRoot.transform.localPosition = Vector3.zero;
 			newAvatarRoot.transform.localRotation = Quaternion.identity;
---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Content/IEntityAvatarChangedEventSubscribable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for a type that implements a subscription service for events that publish <see cref="EntityAvatarChangedEventArgs"/>
+	/// through the <see cref="EventHandler{TEventArgs}"/> <see cref="OnEntityAvatarChanged"/>
+	/// </summary>
+	public interface IEntityAvatarChangedEventSubscribable
+	{
+		event EventHandler<EntityAvatarChangedEventArgs> OnEntityAvatarChanged;
+	}
+
+	/// <summary>
+	/// Event arguments for the <see cref="IEntityAvatarChangedEventSubscribable"/> interface.
+	/// </summary>
+	public sealed class EntityAvatarChangedEventArgs : EventArgs, IEntityGuidContainer
+	{
+		/// <summary>
+		/// The guid of the entity the avatar is changed for.
+		/// </summary>
+		public ObjectGuid EntityGuid { get; }
+
+		/// <summary>
+		/// The world model of the new avatar.
+		/// </summary>
+		public GameObject AvatarWorldRepresentation { get; }
+
+		public EntityAvatarChangedEventArgs([NotNull] ObjectGuid entityGuid, [NotNull] GameObject avatarWorldRepresentation)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+			AvatarWorldRepresentation = avatarWorldRepresentation ?? throw new ArgumentNullException(nameof(avatarWorldRepresentation));
+		}
+	}
+}
