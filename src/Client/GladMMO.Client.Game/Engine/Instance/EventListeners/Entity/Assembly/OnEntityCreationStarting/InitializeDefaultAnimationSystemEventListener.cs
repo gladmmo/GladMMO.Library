a7f250419fcd9363d3abe5e1ddd9ee4cadef0cc7
HelloKitty <andrew.blakely@ymail.com>
2020-07-01 19:31:43 -0500

    Added InitializeDefaultAnimationSystemEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Entity/Assembly/OnEntityCreationStarting/InitializeDefaultAnimationSystemEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class InitializeDefaultAnimationSystemEventListener : EntityCreationStartingEventListener
+	{
+		private IEntityGuidMappable<EntityAnimationController> AnimationControllerMappable { get; }
+
+		public InitializeDefaultAnimationSystemEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
+			[NotNull] IEntityGuidMappable<EntityAnimationController> animationControllerMappable) 
+			: base(subscriptionService)
+		{
+			AnimationControllerMappable = animationControllerMappable ?? throw new ArgumentNullException(nameof(animationControllerMappable));
+		}
+
+		protected override void OnEventFired(object source, EntityCreationStartingEventArgs args)
+		{
+			//Since we're just starting to create, we are null for animation
+			AnimationControllerMappable.AddObject(args.EntityGuid, new EntityAnimationController(null));
+		}
+	}
+}
