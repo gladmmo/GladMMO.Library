1a600cc84d93b956c1c3a18725a5d8bf6e3a6cc8
HelloKitty <andrew.blakely@ymail.com>
2020-07-02 18:26:56 -0500

    Fix stutter in Gaia integration movement animation

--- src/Integrations/GladMMO.Integrations.GaiaOnline.Client/Components/GaiaOnlineAvatarStateController.cs
+++ src/Integrations/GladMMO.Integrations.GaiaOnline.Client/Components/GaiaOnlineAvatarStateController.cs
@@ -103,12 +103,12 @@ namespace GaiaOnline
 				LegAnimator.gameObject.SetActive(true);
 
 				//positionDelta = Quaternion.AngleAxis(delta, Vector3.up) * positionDelta;
-				var direction = transform.InverseTransformDirection(positionDelta);
+				var direction = transform.InverseTransformDirection(positionDelta).normalized;
 
 				//assume normalization, won't matter
-				if(direction.x > Vector3.kEpsilon) //TODO: Is this the best way to determine facing?
+				if (direction.x > Vector3.kEpsilon * 10.0f) //TODO: Is this the best way to determine facing?
 					gameObject.transform.localScale = new Vector3(Mathf.Abs(gameObject.transform.localScale.x) * -1.0f, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
-				else if(Math.Abs(direction.x) > Vector3.kEpsilon) //ignore 0 to make sure it doesn't change from other inputs
+				else if(Math.Abs(direction.x) > Vector3.kEpsilon * 10.0f) //ignore 0 to make sure it doesn't change from other inputs
 					gameObject.transform.localScale = new Vector3(Mathf.Abs(gameObject.transform.localScale.x), gameObject.transform.localScale.y, gameObject.transform.localScale.z);
 			}
 			else
