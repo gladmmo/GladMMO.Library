7075f2141ec4cd1cbbf3b3dbf6ed26f63b5243dc
HelloKitty <andrew.blakely@ymail.com>
2020-07-02 22:45:18 -0500

    Moved DefaultModelScaleStrategy to seperate file

--- src/Client/GladMMO.Client.Game/Services/Avatar/IModelScaleStrategy.cs
+++ src/Client/GladMMO.Client.Game/Services/Avatar/IModelScaleStrategy.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 using FreecraftCore;
 
@@ -11,82 +10,5 @@ namespace GladMMO
 	public interface IModelScaleStrategy
 	{
 		float ComputeScale(ObjectGuid guid);
-	}
-
-	public sealed class DefaultModelScaleStrategy : IModelScaleStrategy
-	{
-		private IClientDataCollectionContainer ClientData { get; }
-
-		private IReadonlyEntityGuidMappable<IEntityDataFieldContainer> EntityFieldDataMappable { get; }
-
-		public DefaultModelScaleStrategy([NotNull] IClientDataCollectionContainer clientData,
-			[NotNull] IReadonlyEntityGuidMappable<IEntityDataFieldContainer> entityFieldDataMappable)
-		{
-			ClientData = clientData ?? throw new ArgumentNullException(nameof(clientData));
-			EntityFieldDataMappable = entityFieldDataMappable ?? throw new ArgumentNullException(nameof(entityFieldDataMappable));
-		}
-
-		public float ComputeScale([NotNull] ObjectGuid guid)
-		{
-			if (guid == null) throw new ArgumentNullException(nameof(guid));
-
-			float staticModelScale = ModelScaleFromGDBC(guid);
-
-			IEntityDataFieldContainer fieldContainer = EntityFieldDataMappable.RetrieveEntity(guid);
-
-			if (!fieldContainer.DataSetIndicationArray.Get((int) EObjectFields.OBJECT_FIELD_SCALE_X))
-				return staticModelScale;
-
-			return staticModelScale + fieldContainer.GetFieldValue<float>(EObjectFields.OBJECT_FIELD_SCALE_X);
-		}
-
-		//TrinityCore:
-		/*    int32 scaleAuras = GetTotalAuraModifier(SPELL_AURA_MOD_SCALE) + GetTotalAuraModifier(SPELL_AURA_MOD_SCALE_2);
-		float scale = GetNativeObjectScale() + CalculatePct(1.0f, scaleAuras);
-		float scaleMin = GetTypeId() == TYPEID_PLAYER ? 0.1 : 0.01;
-		SetObjectScale(std::max(scale, scaleMin));*/
-
-		private float ModelScaleFromGDBC([NotNull] ObjectGuid guid)
-		{
-			if (guid == null) throw new ArgumentNullException(nameof(guid));
-
-			//TODO: Implement players and gameobjects for scaling.
-			if (guid.TypeId != EntityTypeId.TYPEID_GAMEOBJECT)
-				return CalculateUnitScale(guid);
-			else
-				return CalculateGameObjectScale(guid);
-
-		}
-
-		private float CalculateGameObjectScale([NotNull] ObjectGuid guid)
-		{
-			if (guid == null) throw new ArgumentNullException(nameof(guid));
-
-			IEntityDataFieldContainer fieldContainer = EntityFieldDataMappable.RetrieveEntity(guid);
-
-			if(!fieldContainer.DataSetIndicationArray.Get((int)EGameObjectFields.GAMEOBJECT_DISPLAYID))
-				return 1.0f;
-
-			int displayId = fieldContainer.GetFieldValue<int>(EGameObjectFields.GAMEOBJECT_DISPLAYID);
-
-			//GameObjects don't have model scale clientside
-			//var displayInfo = ClientData.AssertEntry<GameObjectDisplayInfoEntry<string>>(displayId);
-			return 1.0f;
-		}
-
-		private float CalculateUnitScale(ObjectGuid guid)
-		{
-			IEntityDataFieldContainer fieldContainer = EntityFieldDataMappable.RetrieveEntity(guid);
-
-			if (!fieldContainer.DataSetIndicationArray.Get((int) EUnitFields.UNIT_FIELD_DISPLAYID))
-				return 1.0f;
-
-			int displayId = fieldContainer.GetFieldValue<int>(EUnitFields.UNIT_FIELD_DISPLAYID);
-
-			var displayInfo = ClientData.AssertEntry<CreatureDisplayInfoEntry<string>>(displayId);
-			var modelData = ClientData.AssertEntry<CreatureModelDataEntry<string>>(displayInfo.ModelId);
-
-			return displayInfo.CreatureModelScale * modelData.ModelScale;
-		}
 	}
 }
