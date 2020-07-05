2f0c6a6c2ad6e6792ea4bf1c22a6b90980cb1e07
HelloKitty <andrew.blakely@ymail.com>
2020-07-05 18:57:24 -0500

    Added Database.Global library

---
+++ src/Server/GladMMO.Database.Global/GlobalDatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladMMO
+{
+	public sealed class GlobalDatabaseContext : DbContext
+	{
+		//We do the below for local database creation stuff
+#if DATABASE_MIGRATION
+		/// <inheritdoc />
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			//TODO: Should I have local or also AWS setup here?
+			optionsBuilder.UseMySql("Server=127.0.0.1;Database=guardians.global;Uid=root;Pwd=test;");
+
+			base.OnConfiguring(optionsBuilder);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+#endif
+	}
+}
---
+++ src/Server/GladMMO.Database.Global/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GladMMO
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
