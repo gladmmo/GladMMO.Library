61b11ec7af207a8754169f9a6316148171bc0d2b
HelloKitty <andrew.blakely@ymail.com>
2020-07-05 02:48:52 -0500

    Added ForceTeleportMovementGenerator

---
+++ src/GladMMO.ZoneServer.Common/Movement/MovementGenerators/Teleport/ForceTeleportMovementGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class ForceTeleportMovementGenerator : BaseMovementGenerator<MovementInfo>
+	{
+		//Force teleports MUST resolve, we cannot be interrupt for any reason EVER.
+		public override bool IsClientInterruptable => false;
+
+		public ForceTeleportMovementGenerator(MovementInfo movementData) 
+			: base(movementData)
+		{
+		}
+
+		protected override Vector3 Start(GameObject entity, long currentTime)
+		{
+			//Just force teleport them to location.
+			Vector3 teleportPosition = MovementData.Position.ToUnityVector();
+
+			//TODO: Handle orientation/rotation
+			//TODO: This could mess with Physics. How do we resolve this??
+			entity.transform.position = teleportPosition;
+
+			StopGenerator();
+
+			return teleportPosition;
+		}
+
+		protected override Vector3 InternalUpdate(GameObject entity, long currentTime)
+		{
+			return entity.transform.position;
+		}
+	}
+}
