426a9fa364bfadb7e283d5a98ed677c1dcfca283
HelloKitty <andrew.blakely@ymail.com>
2020-07-05 20:45:18 -0500

    Added missing endpoint to ServiceEndpoint DB model

---
+++ src/Server/GladMMO.Database.Global/Migrations/20200706014414_AddedServiceEndpointMissingEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GladMMO.Database.Global.Migrations
+{
+    public partial class AddedServiceEndpointMissingEndpoint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Endpoint_Address",
+                table: "service_endpoints",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Endpoint_Port",
+                table: "service_endpoints",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Endpoint_Address",
+                table: "service_endpoints");
+
+            migrationBuilder.DropColumn(
+                name: "Endpoint_Port",
+                table: "service_endpoints");
+        }
+    }
+}
