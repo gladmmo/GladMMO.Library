edff5f7cf28a4e13b2fcde7287bba1e3df7b29c1
HelloKitty <andrew.blakely@ymail.com>
2020-07-05 19:32:39 -0500

    Added DeploymentMode enum

---
+++ src/GladMMO.Models.Common/Data/DeploymentMode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public enum DeploymentMode
+	{
+		/// <summary>
+		/// Used for internal or local testing.
+		/// </summary>
+		Internal = 0,
+
+		/// <summary>
+		/// Used for non-local development or testing.
+		/// </summary>
+		PrivateDevelopment = 1,
+
+		/// <summary>
+		/// Used for public testing dev
+		/// </summary>
+		PublicDevelopment = 2,
+
+		/// <summary>
+		/// Used for the live deployment of the build.
+		/// </summary>
+		Production = 3,
+	}
+}
