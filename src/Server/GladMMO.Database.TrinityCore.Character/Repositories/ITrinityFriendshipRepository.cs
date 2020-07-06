1e7675b2cc92d89ea9b28d8ef734c00a22c45927
HelloKitty <andrew.blakely@ymail.com>
2020-07-06 01:45:09 -0500

    Added ITrinityFriendshipRepository

---
+++ src/Server/GladMMO.Database.TrinityCore.Character/Repositories/ITrinityFriendshipRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FreecraftCore;
+
+namespace GladMMO
+{
+	public interface ITrinityFriendshipRepository : IGenericRepositoryCrudable<uint, CharacterSocial>
+	{
+		Task<int[]> GetCharactersFriendsList(int characterId);
+
+		Task<bool> IsFriendshipPresentAsync(int characterId, int otherCharacterId);
+	}
+}
