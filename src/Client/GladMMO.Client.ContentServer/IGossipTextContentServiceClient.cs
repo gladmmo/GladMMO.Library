3f54436a1dd6db2bd24fe27a6074e099a19ea45f
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 08:32:25 -0500

    Added IGossipTextContentServiceClient

---
+++ src/Client/GladMMO.Client.ContentServer/IGossipTextContentServiceClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	[Headers("User-Agent: SDK 0.0.1")]
+	public interface IGossipTextContentServiceClient
+	{
+		[Get("/api/gossiptext/creature/{id}")]
+		Task<string> GetCreatureGossipTextAsync([AliasAs("id")] int textId);
+	}
+}
