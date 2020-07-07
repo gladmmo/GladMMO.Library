9c8e8f2e945f6f165096996310fb52db09677f74
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 18:29:43 -0500

    Added IGossipMenuCreateEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Interaction/IGossipMenuCreateEventSubscribable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for a type that implements a subscription service for events that publish <see cref="GossipMenuCreateEventArgs"/>
+	/// through the <see cref="EventHandler{TEventArgs}"/> <see cref="OnGossipMenuCreate"/>
+	/// </summary>
+	public interface IGossipMenuCreateEventSubscribable
+	{
+		event EventHandler<GossipMenuCreateEventArgs> OnGossipMenuCreate;
+	}
+
+	/// <summary>
+	/// Event arguments for the <see cref="IGossipMenuCreateEventSubscribable"/> interface.
+	/// </summary>
+	public sealed class GossipMenuCreateEventArgs : EventArgs
+	{
+		public ObjectGuid GossipSource { get; }
+
+		public IReadOnlyList<GossipMenuItem> GossipMenuEntries { get; }
+
+		public IReadOnlyList<QuestGossipEntry> QuestMenuEntries { get; }
+
+		public GossipMenuCreateEventArgs([NotNull] ObjectGuid gossipSource,
+			[NotNull] IReadOnlyList<GossipMenuItem> gossipMenuEntries,
+			[NotNull] IReadOnlyList<QuestGossipEntry> questMenuEntries)
+		{
+			GossipSource = gossipSource ?? throw new ArgumentNullException(nameof(gossipSource));
+			GossipMenuEntries = gossipMenuEntries ?? throw new ArgumentNullException(nameof(gossipMenuEntries));
+			QuestMenuEntries = questMenuEntries ?? throw new ArgumentNullException(nameof(questMenuEntries));
+		}
+	}
+}
