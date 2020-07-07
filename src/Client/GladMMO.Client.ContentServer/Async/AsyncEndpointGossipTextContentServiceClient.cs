d96b96d087910530628fec5e67c5edcb17bd3747
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 08:33:37 -0500

    Added AsyncEndpointGossipTextContentServiceClient

---
+++ src/Client/GladMMO.Client.ContentServer/Async/AsyncEndpointGossipTextContentServiceClient.cs
@@ -0,0 +1,30 @@
+﻿using System; using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class AsyncEndpointGossipTextContentServiceClient : BaseAsyncEndpointService<IGossipTextContentServiceClient>, IGossipTextContentServiceClient
+	{
+		/// <inheritdoc />
+		public AsyncEndpointGossipTextContentServiceClient(Task<string> futureEndpoint) 
+			: base(futureEndpoint)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public AsyncEndpointGossipTextContentServiceClient(Task<string> futureEndpoint, RefitSettings settings) 
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		public async Task<string> GetCreatureGossipTextAsync(int textId)
+		{
+			return await (await GetService().ConfigureAwaitFalse()).GetCreatureGossipTextAsync(textId).ConfigureAwaitFalse();
+		}
+	}
+}
