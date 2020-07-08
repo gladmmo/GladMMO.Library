63aefd1c842269372643260f1a727d653a0e7cee
HelloKitty <andrew.blakely@ymail.com>
2020-07-07 22:55:47 -0500

    Added BaseGossipMenuCreateEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Gossip/Base/BaseGossipMenuCreateEventListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class BaseGossipMenuCreateEventListener : BaseSingleEventListenerInitializable<IGossipMenuCreateEventSubscribable, GossipMenuCreateEventArgs>
+	{
+		protected BaseGossipMenuCreateEventListener(IGossipMenuCreateEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
