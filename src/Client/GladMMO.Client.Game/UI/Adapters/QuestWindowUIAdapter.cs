0af897ca5be6b6389a3c3cd627de4e94a28f13b8
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 08:17:44 -0500

    Added IUIQuestWindow

---
+++ src/Client/GladMMO.Client.Game/UI/Adapters/QuestWindowUIAdapter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class QuestWindowUIAdapter : BaseUnityUI<IUIQuestWindow>, IUIQuestWindow
+	{
+		[SerializeField]
+		private TextMeshProUGUIUITextAdapter _Title;
+
+		[SerializeField]
+		private TextMeshProUGUIUITextAdapter _Description;
+
+		[SerializeField]
+		private TextMeshProUGUIUITextAdapter _Objective;
+
+		public IUIText Title => _Title;
+
+		public IUIText Description => _Description;
+
+		public IUIText Objective => _Objective;
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/UI/Interfaces/IUIQuestWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IUIQuestWindow
+	{
+		IUIText Title { get; }
+
+		IUIText Description { get; }
+
+		IUIText Objective { get; }
+	}
+}
