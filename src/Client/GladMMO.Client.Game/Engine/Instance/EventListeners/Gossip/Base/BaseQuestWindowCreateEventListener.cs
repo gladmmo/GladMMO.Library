afde89c5b0c06c527599d0b39e09edf3c638e165
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 08:22:03 -0500

    Added BaseQuestWindowCreateEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Gossip/Base/BaseQuestWindowCreateEventListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class BaseQuestWindowCreateEventListener : BaseSingleEventListenerInitializable<IQuestWindowCreateEventSubscribable, QuestWindowCreateEventArgs>
+	{
+		protected BaseQuestWindowCreateEventListener(IQuestWindowCreateEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
