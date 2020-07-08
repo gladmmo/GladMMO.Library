be00414e5549fb83e37e4ebd48aa563ece02a7b7
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 03:56:48 -0500

    Added GameContentQueryResponseCode

---
+++ src/GladMMO.Models.ContentServer/ResultCodes/GameContentQueryResponseCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public enum GameContentQueryResponseCode
+	{
+		/// <summary>
+		/// Indicates the request was successful.
+		/// </summary>
+		Success = 1,
+
+		/// <summary>
+		/// Indicates the request's identifier is unknown
+		/// and not linked to a known content.
+		/// </summary>
+		UnknownContentIdentifier = 2,
+	}
+}
