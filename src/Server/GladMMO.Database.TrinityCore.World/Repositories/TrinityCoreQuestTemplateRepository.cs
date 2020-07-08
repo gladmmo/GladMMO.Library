f0cd1de97fe3b51f23211f97c4d22ee4ce3449b2
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 04:47:28 -0500

    Added ITrinityQuestTemplateRepository

---
+++ src/Server/GladMMO.Database.TrinityCore.World/Repositories/TrinityCoreQuestTemplateRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class TrinityCoreQuestTemplateRepository : BaseGenericBackedDatabaseRepository<wotlk_worldContext, uint, QuestTemplate>, ITrinityQuestTemplateRepository
+	{
+		public TrinityCoreQuestTemplateRepository([JetBrains.Annotations.NotNull] wotlk_worldContext context) 
+			: base(context)
+		{
+
+		}
+	}
+}
