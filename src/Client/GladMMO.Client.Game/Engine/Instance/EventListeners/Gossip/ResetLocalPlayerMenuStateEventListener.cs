5c5a991f1795a7c522cbb8b074904658603cc83e
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 01:15:11 -0500

    Added ResetLocalPlayerMenuStateEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Gossip/ResetLocalPlayerMenuStateEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class ResetLocalPlayerMenuStateEventListener : BaseGossipMenuCreateEventListener
+	{
+		private LocalPlayerMenuState MenuState { get; }
+
+		public ResetLocalPlayerMenuStateEventListener(IGossipMenuCreateEventSubscribable subscriptionService,
+			[NotNull] LocalPlayerMenuState menuState) 
+			: base(subscriptionService)
+		{
+			MenuState = menuState ?? throw new ArgumentNullException(nameof(menuState));
+		}
+
+		protected override void OnEventFired(object source, GossipMenuCreateEventArgs args)
+		{
+			MenuState.Clear();
+			MenuState.Update(args.GossipMenuEntries, args.QuestMenuEntries);
+		}
+	}
+}
