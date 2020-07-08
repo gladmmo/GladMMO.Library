7681848ac879597e17ea3dd4f60fd25d96e7be97
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 03:56:39 -0500

    Added QuestTextContentModel

---
+++ src/GladMMO.Models.ContentServer/Models/Quest/QuestTextContentModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class QuestTextContentModel
+	{
+		[JsonProperty]
+		public string Title { get; internal set; }
+
+		[JsonProperty]
+		public string Details { get; internal set; }
+
+		[JsonProperty]
+		public string Objectives { get; internal set; }
+
+		public QuestTextContentModel([NotNull] string title, [NotNull] string details, [NotNull] string objectives)
+		{
+			Title = title ?? throw new ArgumentNullException(nameof(title));
+			Details = details ?? throw new ArgumentNullException(nameof(details));
+			Objectives = objectives ?? throw new ArgumentNullException(nameof(objectives));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		internal QuestTextContentModel()
+		{
+			
+		}
+	}
+}
