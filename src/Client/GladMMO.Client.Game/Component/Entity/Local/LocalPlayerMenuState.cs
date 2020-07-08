0be2fbbf4f0aed2c8bba2d7d8eb74920038f7a33
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 02:59:31 -0500

    Added Guid to LocalPlayerMenuState

--- src/Client/GladMMO.Client.Game/Component/Entity/Local/LocalPlayerMenuState.cs
+++ src/Client/GladMMO.Client.Game/Component/Entity/Local/LocalPlayerMenuState.cs
@@ -7,6 +7,8 @@ namespace GladMMO
 {
 	public sealed class LocalPlayerMenuState
 	{
+		public ObjectGuid CurrentGossipEntity { get; private set; } = ObjectGuid.Empty;
+
 		public IReadOnlyList<GossipMenuItem> GossipOptions { get; private set; } = Array.Empty<GossipMenuItem>();
 
 		public IReadOnlyList<QuestGossipEntry> QuestOptions { get; private set; } = Array.Empty<QuestGossipEntry>();
@@ -15,12 +17,14 @@ namespace GladMMO
 		{
 			GossipOptions = Array.Empty<GossipMenuItem>();
 			QuestOptions = Array.Empty<QuestGossipEntry>();
+			CurrentGossipEntity = ObjectGuid.Empty;
 		}
 
-		public void Update([NotNull] IReadOnlyList<GossipMenuItem> options, [NotNull] IReadOnlyList<QuestGossipEntry> quests)
+		public void Update([NotNull] ObjectGuid gossipEntity, [NotNull] IReadOnlyList<GossipMenuItem> options, [NotNull] IReadOnlyList<QuestGossipEntry> quests)
 		{
 			GossipOptions = options ?? throw new ArgumentNullException(nameof(options));
 			QuestOptions = quests ?? throw new ArgumentNullException(nameof(quests));
+			CurrentGossipEntity = gossipEntity ?? throw new ArgumentNullException(nameof(gossipEntity));
 		}
 	}
 }
