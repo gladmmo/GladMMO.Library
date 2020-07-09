dc5c20921c7e8a7fe50427db7460ec24fce65c25
HelloKitty <andrew.blakely@ymail.com>
2020-07-09 02:26:08 -0500

    Added IGossipCompleteEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Gossip/IGossipCompleteEventSubscribable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for a type that implements a subscription service for events that publish <see cref="GossipCompleteEventArgs"/>
+	/// through the <see cref="EventHandler{TEventArgs}"/> <see cref="OnGossipComplete"/>
+	/// </summary>
+	public interface IGossipCompleteEventSubscribable
+	{
+		event EventHandler<GossipCompleteEventArgs> OnGossipComplete;
+	}
+
+	/// <summary>
+	/// Event arguments for the <see cref="IGossipCompleteEventSubscribable"/> interface.
+	/// </summary>
+	public sealed class GossipCompleteEventArgs : EventArgs
+	{
+
+	}
+}
