cded03ff0823d4bb20e93c87e5cff6701407bab1
HelloKitty <andrew.blakely@ymail.com>
2020-07-09 06:05:02 -0500

    Added initial LogQuestAcceptedToConsoleEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Quest/Base/LogQuestAcceptedToConsoleEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class LogQuestAcceptedToConsoleEventListener : BaseSingleEventListenerInitializable<ILocalPlayerQuestAddedEventSubscribable, LocalPlayerQuestAddedEventArgs>
+	{
+		private IChatTextMessageRecievedEventPublisher ChatPublisher { get; }
+
+		public LogQuestAcceptedToConsoleEventListener(ILocalPlayerQuestAddedEventSubscribable subscriptionService,
+			[NotNull] IChatTextMessageRecievedEventPublisher chatPublisher) 
+			: base(subscriptionService)
+		{
+			ChatPublisher = chatPublisher ?? throw new ArgumentNullException(nameof(chatPublisher));
+		}
+
+		protected override void OnEventFired(object source, LocalPlayerQuestAddedEventArgs args)
+		{
+			ChatPublisher.PublishEvent(this, new TextChatEventArgs($"Quest Accepted: {args.QuestId} TODO add name.", ChatChannelType.System));
+		}
+	}
+}
