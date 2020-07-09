e6ee29d4f447855e90cb529fedda6074112ee982
HelloKitty <andrew.blakely@ymail.com>
2020-07-09 05:28:59 -0500

    Added initial implementation of LocalPlayerQuestDataContainer

---
+++ src/Client/GladMMO.Client.Game/Services/Quest/LocalPlayerQuestDataContainer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using FreecraftCore;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[AdditionalRegisterationAs(typeof(ILocalPlayerQuestAddedEventSubscribable))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class LocalPlayerQuestDataContainer : IGameStartable, ILocalPlayerQuestAddedEventSubscribable
+	{
+		private ILocalPlayerDetails PlayerDetails { get; }
+
+		private IEntityDataChangeCallbackRegisterable DataChangeCallbackRegister { get; }
+
+		private ILog Logger { get; }
+
+		public event EventHandler<LocalPlayerQuestAddedEventArgs> OnLocalPlayerQuestAdded;
+
+		public LocalPlayerQuestDataContainer([NotNull] ILocalPlayerDetails playerDetails, 
+			[NotNull] IEntityDataChangeCallbackRegisterable dataChangeCallbackRegister,
+			[NotNull] ILog logger)
+		{
+			PlayerDetails = playerDetails ?? throw new ArgumentNullException(nameof(playerDetails));
+			DataChangeCallbackRegister = dataChangeCallbackRegister ?? throw new ArgumentNullException(nameof(dataChangeCallbackRegister));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		public Task OnGameStart()
+		{
+			for (int i = (int) EPlayerFields.PLAYER_QUEST_LOG_1_1; i < (int) EPlayerFields.PLAYER_QUEST_LOG_25_1; i += 5)
+			{
+				//Required for lambda capture.
+				var i1 = i;
+				DataChangeCallbackRegister.RegisterCallback<int>(PlayerDetails.LocalPlayerGuid, i, (e, args) => OnQuestLogQuestIdChanged(e, (EPlayerFields)i1, args));
+			}
+
+			return Task.CompletedTask;
+		}
+
+		private void OnQuestLogQuestIdChanged(ObjectGuid entity, EPlayerFields playerFieldChanged, EntityDataChangedArgs<int> questIdSlotChanged)
+		{
+			//Quest slot starts at 0.
+			int questSlot = ((int)playerFieldChanged - (int) EPlayerFields.PLAYER_QUEST_LOG_1_1) / 5; //5 field counts for quests.
+
+			if (Logger.IsInfoEnabled)
+				Logger.Info($"Quest Slot Changed: {questIdSlotChanged.OriginalValue} now {questIdSlotChanged.NewValue}");
+
+			//0 would probably be remove/abandon
+			//Therefore only broadcast Quest Add if it has a valid id.
+			if (questIdSlotChanged.NewValue != 0)
+			{
+				OnLocalPlayerQuestAdded?.Invoke(this, new LocalPlayerQuestAddedEventArgs(questSlot, questIdSlotChanged.NewValue));
+			}
+		}
+	}
+}
