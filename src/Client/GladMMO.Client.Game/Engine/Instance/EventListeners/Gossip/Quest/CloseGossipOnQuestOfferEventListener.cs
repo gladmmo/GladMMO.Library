94eb23a3fbd1a17307e91b785d5dda723ccb8b23
HelloKitty <andrew.blakely@ymail.com>
2020-07-08 22:20:02 -0500

    Added CloseGossipOnQuestOfferEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Gossip/Quest/CloseGossipOnQuestOfferEventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class CloseGossipOnQuestOfferEventListener : BaseQuestWindowCreateEventListener
+	{
+		public IUIGossipWindow GossipWindow { get; }
+
+		public CloseGossipOnQuestOfferEventListener(IQuestWindowCreateEventSubscribable subscriptionService,
+		[KeyFilter(UnityUIRegisterationKey.GossipWindow)] [NotNull] IUIGossipWindow gossipWindow) 
+			: base(subscriptionService)
+		{
+			GossipWindow = gossipWindow;
+		}
+
+		protected override void OnEventFired(object source, QuestWindowCreateEventArgs args)
+		{
+			GossipWindow.RootElement.SetElementActive(false);
+		}
+	}
+}
