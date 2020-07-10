76100e96ecfc5ce0d825fe86c04939972ceed2b8
HelloKitty <andrew.blakely@ymail.com>
2020-07-10 11:12:38 -0500

    Added IUIQuestCompleteWindow

---
+++ src/Client/GladMMO.Client.Game/UI/Interfaces/IUIQuestCompleteWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IUIQuestCompleteWindow
+	{
+		IUIElement RootElement { get; }
+
+		IUIText Title { get; }
+
+		IUIText Description { get; }
+
+		IUIButton AcceptButton { get; }
+	}
+}
