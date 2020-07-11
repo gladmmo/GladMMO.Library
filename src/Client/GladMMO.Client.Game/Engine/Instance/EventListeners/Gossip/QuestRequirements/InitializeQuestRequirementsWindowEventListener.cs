c5ee62a6d5208031b6f831b64eb9fa62b6b53694
HelloKitty <andrew.blakely@ymail.com>
2020-07-11 11:01:06 -0500

    Added InitializeQuestRequirementsWindowEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Gossip/QuestRequirements/InitializeQuestRequirementsWindowEventListener.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac.Features.AttributeFilters;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class InitializeQuestRequirementsWindowEventListener : BaseQuestRequirementsWindowCreateEventListener
+	{
+		private IUIQuestRequirementWindow QuestRequirementsWindow { get; }
+
+		private IGossipWindowCollection GossipWindows { get; }
+
+		public InitializeQuestRequirementsWindowEventListener(IQuestRequirementsWindowCreateEventSubscribable subscriptionService,
+			[KeyFilter(UnityUIRegisterationKey.QuestRequirementsWindow)] [NotNull] IUIQuestRequirementWindow questRequirementsWindow,
+			[NotNull] IGossipWindowCollection gossipWindows)
+			: base(subscriptionService)
+		{
+			QuestRequirementsWindow = questRequirementsWindow ?? throw new ArgumentNullException(nameof(questRequirementsWindow));
+			GossipWindows = gossipWindows ?? throw new ArgumentNullException(nameof(gossipWindows));
+		}
+
+		protected override void OnEventFired(object source, QuestRequirementsWindowCreateEventArgs args)
+		{
+			GossipWindows.CloseAll();
+
+			QuestRequirementsWindow.Title.Text = $"{args.QuestId} - TODO: Name!";
+			QuestRequirementsWindow.Description.Text = args.Requirements;
+
+			//TODO: Continue button handling.
+			//QuestWindow.AcceptButton.IsInteractable = true;
+
+			QuestRequirementsWindow.RootElement.SetElementActive(true);
+		}
+	}
+}
