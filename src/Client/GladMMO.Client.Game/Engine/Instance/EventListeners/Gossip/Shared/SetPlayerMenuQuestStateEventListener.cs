f8490cfaac73ed7307903fd72deefe17d5cac664
HelloKitty <andrew.blakely@ymail.com>
2020-07-11 07:07:36 -0500

    Generalized setting quest menu id state event listener

--- src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Gossip/Shared/SetPlayerMenuQuestStateEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Gossip/Shared/SetPlayerMenuQuestStateEventListener.cs
@@ -1,45 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Common.Logging;
 using FreecraftCore;
+using Glader.Essentials;
 
 namespace GladMMO
 {
 	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
-	public class SetPlayerMenuQuestStateEventListener : BaseQuestWindowCreateEventListener
+	public class SetPlayerMenuQuestStateEventListener : IGameInitializable
 	{
 		private LocalPlayerMenuState MenuState { get; }
 
 		private ILog Logger { get; }
 
-		public SetPlayerMenuQuestStateEventListener(IQuestWindowCreateEventSubscribable subscriptionService,
+		public SetPlayerMenuQuestStateEventListener(IQuestWindowCreateEventSubscribable questWindowSubscriptionService,
+			IQuestRequirementsWindowCreateEventSubscribable requirementsWindowSubscriptionService,
 			[NotNull] LocalPlayerMenuState menuState,
-			[NotNull] ILog logger) 
-			: base(subscriptionService)
+			[NotNull] ILog logger)
 		{
 			MenuState = menuState ?? throw new ArgumentNullException(nameof(menuState));
 			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+			requirementsWindowSubscriptionService.OnQuestRequirementWindowCreate += (sender, args) => SetQuestId(args.QuestGiver, args.QuestId);
+			questWindowSubscriptionService.OnQuestWindowCreate += (sender, args) => SetQuestId(args.QuestGiver, args.QuestId);
 		}
 
-		protected override void OnEventFired(object source, QuestWindowCreateEventArgs args)
+		private void SetQuestId(ObjectGuid questGiver, int questId)
 		{
 			//TODO: Properly handle quest entry for shared quests.
 			//Sooo, complicated abit but we need to cover the case of sharing quests.
 			//If the current menu UI state doesn't match the guid of who is offerring the quest then the menu state is technically wrong.
-			if (MenuState.CurrentGossipEntity != args.QuestGiver)
+			if (MenuState.CurrentGossipEntity != questGiver)
 			{
 				//TODO: Construct quest entry here.
 				QuestGossipEntry questEntry = null; //new QuestGossipEntry();
 				MenuState.Clear();
-				MenuState.Update(args.QuestGiver, Array.Empty<GossipMenuItem>(), new []{ questEntry });
+				MenuState.Update(questGiver, Array.Empty<GossipMenuItem>(), new[] {questEntry});
 
 				if (Logger.IsErrorEnabled)
 					Logger.Error($"Encountered Quest Offer from non-current Gossip (probably shared). TODO: Implement case.");
 			}
 
 			//Set the current offered quest state.
-			MenuState.SelectQuestByQuestId(args.QuestId);
+			MenuState.SelectQuestByQuestId(questId);
+		}
+
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
 		}
 	}
 }
