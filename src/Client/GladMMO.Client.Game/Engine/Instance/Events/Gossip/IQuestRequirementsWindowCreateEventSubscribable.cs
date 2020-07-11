4201866d5f89cbad0f779a4b053ccb35d82ba314
HelloKitty <andrew.blakely@ymail.com>
2020-07-11 07:11:01 -0500

    Added IQuestRequirementsWindowCreateEventSubscribable

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/Events/Gossip/IQuestRequirementsWindowCreateEventSubscribable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using FreecraftCore;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for a type that implements a subscription service for events that publish <see cref="QuestRequirementWindowCreateEventArgs"/>
+	/// through the <see cref="EventHandler{TEventArgs}"/> <see cref="OnQuestRequirementWindowCreate"/>
+	/// </summary>
+	public interface IQuestRequirementsWindowCreateEventSubscribable
+	{
+		event EventHandler<QuestRequirementWindowCreateEventArgs> OnQuestRequirementWindowCreate;
+	}
+
+	/// <summary>
+	/// Event arguments for the <see cref="IQuestRequirementsWindowCreateEventSubscribable"/> interface.
+	/// </summary>
+	public sealed class QuestRequirementWindowCreateEventArgs : EventArgs
+	{
+		public ObjectGuid QuestGiver { get; }
+
+		public int QuestId { get; }
+
+		public string Requirements { get; }
+
+		public QuestRequirementWindowCreateEventArgs([NotNull] ObjectGuid questGiver, int questId, [NotNull] string requirements)
+		{
+			if (questId <= 0) throw new ArgumentOutOfRangeException(nameof(questId));
+
+			QuestGiver = questGiver ?? throw new ArgumentNullException(nameof(questGiver));
+			QuestId = questId;
+			Requirements = requirements ?? throw new ArgumentNullException(nameof(requirements));
+		}
+	}
+}
