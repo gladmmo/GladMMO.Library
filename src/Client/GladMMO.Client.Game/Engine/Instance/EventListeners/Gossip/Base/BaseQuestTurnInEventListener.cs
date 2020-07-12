f06dabc29c27532ac1501206a768b1f25779c5ea
HelloKitty <andrew.blakely@ymail.com>
2020-07-11 22:44:57 -0500

    Added BaseQuestTurnInEventListener

---
+++ src/Client/GladMMO.Client.Game/Engine/Instance/EventListeners/Gossip/Base/BaseQuestTurnInEventListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public abstract class BaseQuestTurnInEventListener : BaseSingleEventListenerInitializable<IQuestTurnInEventSubscribable, QuestTurnInEventArgs>
+	{
+		protected BaseQuestTurnInEventListener(IQuestTurnInEventSubscribable subscriptionService) 
+			: base(subscriptionService)
+		{
+
+		}
+	}
+}
