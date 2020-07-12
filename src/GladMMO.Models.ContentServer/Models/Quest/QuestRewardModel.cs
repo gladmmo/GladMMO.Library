e4659efb427affdf6beef59708ccca645e0fffd7
HelloKitty <andrew.blakely@ymail.com>
2020-07-11 22:14:15 -0500

    Added missing QuestRewardModel

---
+++ src/GladMMO.Models.ContentServer/Models/Quest/QuestRewardModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	[JsonObject]
+	public sealed class QuestRewardModel
+	{
+		/// <summary>
+		/// The experience rewarded.
+		/// </summary>
+		public int Experience { get; internal set; }
+
+		/// <summary>
+		/// The money rewarded.
+		/// </summary>
+		public int Money { get; internal set; }
+
+		public QuestRewardModel(int experience, int money)
+		{
+			Experience = experience;
+			Money = money;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		internal QuestRewardModel()
+		{
+			
+		}
+	}
+}
