e56285fbf8560d35e2b1f31452265949ba800777
HelloKitty <andrew.blakely@ymail.com>
2020-07-12 07:01:55 -0500

    Added gaia avatar tool material setup window

---
+++ src/GladMMO.Client.Editor/Tools/GaiaOnlineWindow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GladMMO
+{
+	public sealed class GaiaOnlineWindow : EditorWindow
+	{
+		Material mat;
+
+		[MenuItem("Tools/Make GaiaOnline Avatar Material")]
+		public static void ShowWindow()
+		{
+			GetWindow<GaiaOnlineWindow>("Material Editor Test");
+		}
+
+		void OnGUI()
+		{
+			mat = EditorGUILayout.ObjectField(mat, typeof(Material), false) as Material;
+
+			if (GUILayout.Button("Change") && mat != null)
+			{
+				mat.SetFloat("_Mode", 2);
+				mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+				mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+				mat.SetInt("_ZWrite", 0);
+				mat.DisableKeyword("_ALPHATEST_ON");
+				mat.EnableKeyword("_ALPHABLEND_ON");
+				mat.DisableKeyword("_ALPHAPREMULTIPLY_ON");
+				mat.renderQueue = 3000;
+			}
+		}
+	}
+}
