a44ae6779db02df03d7752a63525a094426e862e
HelloKitty <andrew.blakely@ymail.com>
2020-08-04 09:56:08 -0500

    Added GladMMOServiceDiscordConstants

---
+++ src/Server/GladMMO.Service.Discord/GladMMOServiceDiscordConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class GladMMOServiceDiscordConstants
+	{
+		public const string EVENT_GRID_VALIDATE_ACTION = "Validate";
+	}
+}
