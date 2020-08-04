e59bf44144cd1cbd1a57cc6b11f35fe69ffc1a6a
HelloKitty <andrew.blakely@ymail.com>
2020-08-04 09:56:00 -0500

    Added missing certs to Discord service

--- src/Server/GladMMO.Service.Discord/Startup.cs
+++ src/Server/GladMMO.Service.Discord/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -34,6 +35,26 @@ namespace GladMMO
 
 			services.AddResponseCaching();
 			services.AddLogging();
+
+			AddJWTAuthorizationServices(services);
+		}
+
+		private static void AddJWTAuthorizationServices(IServiceCollection services)
+		{
+			X509Certificate2 cert = null;
+			string certPath = "Certs/TestCert.pfx";
+
+			try
+			{
+				cert = X509Certificate2Loader.Create(certPath).Load();
+			}
+			catch(Exception e)
+			{
+				throw new System.InvalidOperationException($"Failed to load {nameof(X509Certificate2)} from Path: {certPath} \n\n StackTrace: {e.StackTrace}", e);
+			}
+
+			//This provides JwtBearer support for Authorize attribute/header
+			services.AddJwtAuthorization(cert);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
