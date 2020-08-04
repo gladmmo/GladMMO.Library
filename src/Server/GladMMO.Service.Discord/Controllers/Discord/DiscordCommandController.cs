97b86df80681c146a73a52189c5e10a0b0b44c3a
HelloKitty <andrew.blakely@ymail.com>
2020-08-04 09:56:43 -0500

    Added DiscordCommandController

---
+++ src/Server/GladMMO.Service.Discord/Controllers/Discord/DiscordCommandController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Extensions.Logging;
+
+namespace GladMMO
+{
+	[Route("api/discord/command")]
+	public sealed class DiscordCommandController : AuthorizationReadyController
+	{
+		public DiscordCommandController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger)
+		{
+
+		}
+
+		[ProducesJson]
+		[HttpPost(GladMMOServiceDiscordConstants.EVENT_GRID_VALIDATE_ACTION)]
+		public async Task<IActionResult> ValidateEventAsync([FromBody] EventGridSubscriptionEventModel[] eventModels)
+		{
+			//TODO: Validate URL
+			return Json(new SubscriptionValidationResponse(eventModels.First().Data.ValidationCode));
+		}
+	}
+}
