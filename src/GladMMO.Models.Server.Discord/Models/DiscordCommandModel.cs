8817672c3ff2628efa08a74a2215c77bd575bc5e
HelloKitty <andrew.blakely@ymail.com>
2020-08-06 04:50:47 -0500

    Added Base DiscordCommandModel

---
+++ src/GladMMO.Models.Server.Discord/Models/DiscordCommandModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Abstract model type for Discord command types.
+	/// </summary>
+	/// <typeparam name="TChildModelType">The derived command type.</typeparam>
+	[JsonObject]
+	public abstract class DiscordCommandModel<TChildModelType>
+		where TChildModelType : DiscordCommandModel<TChildModelType>
+	{
+		/// <summary>
+		/// Represents the name of the command type.
+		/// </summary>
+		[JsonProperty]
+		public static string CommandName { get; private set; } = typeof(TChildModelType).Name.Replace("Command", "");
+
+		protected DiscordCommandModel()
+		{
+
+		}
+	}
+}
