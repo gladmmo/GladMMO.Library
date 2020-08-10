8d4e2fdc0e2e06afc75865e0c125e4739ecbc73a
HelloKitty <andrew.blakely@ymail.com>
2020-08-10 02:49:50 -0500

    Added Discoed Test command model

--- src/GladMMO.Models.Server.Discord/Models/DiscordCommandModel.cs
+++ src/GladMMO.Models.Server.Discord/Models/DiscordCommandModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json;
 
-namespace GladMMO
+namespace GladMMO.Discord
 {
 	[JsonObject]
 	public abstract class DiscordCommandModel
---
+++ src/GladMMO.Models.Server.Discord/Models/TestCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace GladMMO.Discord
+{
+	[JsonObject]
+	public sealed class TestCommand : DiscordCommandModel<TestCommand>
+	{
+		[JsonProperty]
+		public string Message { get; private set; }
+
+		public TestCommand(string message)
+		{
+			Message = message;
+		}
+
+		[JsonConstructor]
+		internal TestCommand()
+		{
+			
+		}
+	}
+}
