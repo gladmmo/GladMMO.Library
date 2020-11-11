fc6204f018690d8ff6dc8255c44dc49bf7fe5ec4
HelloKitty <andrew.blakely@ymail.com>
2020-11-10 18:23:00 -0600

    Switched to SAOGG build deploy

--- build-norestore-client-withcopy-TrinityCore.bat
+++ build-norestore-client-withcopy-TrinityCore.bat
@@ -1,4 +1,5 @@
 call build-norestore-client.bat
 
 xcopy build\client  C:\Users\Glader\Documents\Github\GladMMOTrinityCore.Client\Assets\DLLs /Y /q /EXCLUDE:BuildExclude.txt
-xcopy build\client  C:\Users\Glader\Documents\Github\GladMMOTrinityCore.Client\Build\Booma.Client_Data\Managed /Y /q /EXCLUDE:BuildExclude.txt
+xcopy build\client  C:\Users\Glader\Documents\Github\GladMMO.GaiaOnline.Client\Assets\DLLs /Y /q /EXCLUDE:BuildExclude.txt
+xcopy build\client  "C:\Users\Glader\Documents\Unity Projects\GladMMO.SAOGG.Client\Assets\DLLs" /Y /q /EXCLUDE:BuildExclude.txt
