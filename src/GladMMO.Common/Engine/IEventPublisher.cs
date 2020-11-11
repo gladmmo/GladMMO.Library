0924126fee7be882c8715d07bb4483f953236f9b
HelloKitty <andrew.blakely@ymail.com>
2020-11-10 18:19:38 -0600

    Updated Event Publisher doc

--- src/GladMMO.Common/Engine/IEventPublisher.cs
+++ src/GladMMO.Common/Engine/IEventPublisher.cs
@@ -7,7 +7,8 @@ namespace GladMMO
 	/// <summary>
 	/// Contract for a type that publishes a particular event interface type.
 	/// </summary>
-	/// <typeparam name="TEventArgsType"></typeparam>
+	/// <typeparam name="TEventArgsType">The event args type name.</typeparam>
+	/// <typeparam name="TEventPublisherInterface">The even publisher interface.</typeparam>
 	public interface IEventPublisher<TEventPublisherInterface, in TEventArgsType> //One day I dream of a generic type arg that can be constrained to a type in the implementing types hierarchy.
 		where TEventArgsType : EventArgs
 	{
@@ -15,8 +16,8 @@ namespace GladMMO
 		/// Publishes the event with the args <see cref="eventArgs"/>
 		/// with sender <see cref="sender"/>.
 		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="eventArgs"></param>
+		/// <param name="sender">The sender of the event.</param>
+		/// <param name="eventArgs">The arguments for the event.</param>
 		void PublishEvent(object sender, TEventArgsType eventArgs);
 	}
 }
